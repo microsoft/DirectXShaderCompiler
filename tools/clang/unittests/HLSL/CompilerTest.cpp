56fc5b774ab8dda69a088f493e8bca7f0181d25a
Xiang Li <python3kgae@outlook.com>
2017-02-03 11:35:45 -0800

    Write thread group uniform value will not cause race condition.

--- tools/clang/unittests/HLSL/CompilerTest.cpp
+++ tools/clang/unittests/HLSL/CompilerTest.cpp
@@ -435,6 +435,7 @@ public:
   TEST_METHOD(CodeGenPrecise4)
   TEST_METHOD(CodeGenPreciseOnCall)
   TEST_METHOD(CodeGenPreciseOnCallNot)
+  TEST_METHOD(CodeGenRaceCond2)
   TEST_METHOD(CodeGenRaw_Buf1)
   TEST_METHOD(CodeGenRcp1)
   TEST_METHOD(CodeGenReadFromOutput)
@@ -2376,6 +2377,10 @@ TEST_F(CompilerTest, CodeGenPreciseOnCall) {
 
 TEST_F(CompilerTest, CodeGenPreciseOnCallNot) {
   CodeGenTestCheck(L"..\\CodeGenHLSL\\precise_call_not.hlsl");
+}
+
+TEST_F(CompilerTest, CodeGenRaceCond2) {
+  CodeGenTest(L"..\\CodeGenHLSL\\RaceCond2.hlsl");
 }
 
 TEST_F(CompilerTest, CodeGenRaw_Buf1) {
