d67eedebb1000955af630ce53a188e1cf67c0dbb
Lei Zhang <antiagainst@google.com>
2017-03-03 16:28:25 -0500

    [spirv] Add libclangSPIRV and skeleton FrontendAction for SPIR-V

---
+++ tools/clang/include/clang/SPIRV/EmitSPIRVAction.h
@@ -0,0 +1,24 @@
+//===-- EmitSPIRVAction.h - FrontendAction for Emitting SPIR-V --*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef LLVM_CLANG_SPIRV_EMITSPIRVACTION_H
+#define LLVM_CLANG_SPIRV_EMITSPIRVACTION_H
+
+#include "clang/Frontend/FrontendAction.h"
+
+namespace clang {
+
+class EmitSPIRVAction : public ASTFrontendAction {
+protected:
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 StringRef InFile) override;
+};
+
+} // end namespace clang
+
+#endif
