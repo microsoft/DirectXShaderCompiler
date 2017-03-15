6f76a24704129530de2e983cdfb31cda9ef1f467
Xiang Li <python3kgae@outlook.com>
2017-03-15 14:02:28 -0700

    Add check for test.

--- tools/clang/test/CodeGenHLSL/matSubscript7.hlsl
+++ tools/clang/test/CodeGenHLSL/matSubscript7.hlsl
@@ -1,8 +1,10 @@
 // RUN: %dxc -E main -T vs_6_0 %s | FileCheck %s
 
+// CHECK: @main
 
 float4x4 m;
 uint i;
 float4 main() : SV_POSITION {
-  return m[2][i];
+  float4x4 m2 = m;
+  return m[2][i] + m2[i][i];
 }
