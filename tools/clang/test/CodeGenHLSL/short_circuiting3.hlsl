1d0da57987f8ac63dce28ca6299a83f887096233
Xiang Li <python3kgae@outlook.com>
2017-05-30 16:06:36 -0700

    Make sure no short circuiting. (#336)

---
+++ tools/clang/test/CodeGenHLSL/short_circuiting3.hlsl
@@ -0,0 +1,14 @@
+// RUN: %dxc -E main -T ps_6_0 %s | FileCheck %s
+
+// inc(color) will add 1 to color.
+// Make sure result is add 1.
+//CHECK: 1.0
+
+bool inc(inout float4 v) { v++; return true; }
+
+float4 main(int val: A, float4 color: COLOR) : SV_TARGET {
+  if (true || inc(color)) return color;
+
+  return color + 4.2;
+}
+
