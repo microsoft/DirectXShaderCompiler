178460aed7e4ed16b3280058647a8718d55e9857
Xiang Li <xiagli@microsoft.com>
2017-06-10 12:48:41 -0700

    Update test.

--- tools/clang/test/CodeGenHLSL/atomic2.hlsl
+++ tools/clang/test/CodeGenHLSL/atomic2.hlsl
@@ -1,12 +1,10 @@
 // RUN: %dxc -E main -T cs_6_0 %s | FileCheck %s
 
-// CHECK: dx.op.atomicBinOp.i32(i32 78
 // Make sure offset is 0
-// CHECK: i32 0, i32 undef, i32 1)
+// CHECK: dx.op.atomicBinOp.i32(i32 78, {{.*}}, i32 0, i32 undef, i32 1)
 
-// CHECK: dx.op.atomicBinOp.i32(i32 78
 // Make sure offset is 0
-// CHECK: i32 0, i32 undef, i32 %
+// CHECK: dx.op.atomicBinOp.i32(i32 78, {{.*}}, i32 0, i32 undef, i32 %
 
 RWStructuredBuffer<uint> structBuf1 : register( u1 );
 
