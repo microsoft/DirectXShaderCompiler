323c0e41a84384bbaf7f79b33797cff429a12316
Xiang Li <python3kgae@outlook.com>
2017-07-14 12:28:18 -0700

    Refine test.

--- tools/clang/test/CodeGenHLSL/lib_arg_flatten.hlsl
+++ tools/clang/test/CodeGenHLSL/lib_arg_flatten.hlsl
@@ -2,16 +2,16 @@
 
 // Make sure function call on external function is flattened.
 
-// CHECK: call void @"\01?test_extern@@YAMUT@@@Z"(float %0, float %1
+// CHECK: call void @"\01?test_extern@@YAMUT@@Y01U1@0AAV?$matrix@M$01$01@@@Z"(float %{{.*}}, float %{{.*}}, [2 x float]* nonnull %{{.*}}, [2 x float]* nonnull %{{.*}}, float* nonnull %{{.*}}, float* nonnull %{{.*}}, [4 x float]* nonnull %{{.*}}, float* nonnull %{{.*}})
 
 struct T {
   float a;
   float b;
 };
 
-float test_extern(T t);
+float test_extern(T t, T t2[2], out T t3, inout float2x2 m);
 
-float test(T t)
+float test(T t, T t2[2], out T t3, inout float2x2 m)
 {
-  return test_extern(t);
+  return test_extern(t, t2, t3, m);
 }
