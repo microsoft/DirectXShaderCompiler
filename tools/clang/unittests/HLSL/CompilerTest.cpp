6346920a0cf2a2820d5535b04aeaab905ebb688b
Xiang Li <python3kgae@outlook.com>
2017-07-18 15:18:56 -0700

    Mark noalias for out parameters.

--- tools/clang/unittests/HLSL/CompilerTest.cpp
+++ tools/clang/unittests/HLSL/CompilerTest.cpp
@@ -527,6 +527,7 @@ public:
   TEST_METHOD(CodeGenLibCsEntry3)
   TEST_METHOD(CodeGenLibEntries)
   TEST_METHOD(CodeGenLibEntries2)
+  TEST_METHOD(CodeGenLibNoAlias)
   TEST_METHOD(CodeGenLibResource)
   TEST_METHOD(CodeGenLibUnusedFunc)
   TEST_METHOD(CodeGenLitInParen)
@@ -3094,6 +3095,10 @@ TEST_F(CompilerTest, CodeGenLibEntries) {
 
 TEST_F(CompilerTest, CodeGenLibEntries2) {
   CodeGenTestCheck(L"..\\CodeGenHLSL\\lib_entries2.hlsl");
+}
+
+TEST_F(CompilerTest, CodeGenLibNoAlias) {
+  CodeGenTestCheck(L"..\\CodeGenHLSL\\lib_no_alias.hlsl");
 }
 
 TEST_F(CompilerTest, CodeGenLibResource) {
