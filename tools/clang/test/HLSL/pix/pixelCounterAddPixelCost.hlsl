7b0f5aa9485505ea1b883c196a3616f9dff3119b
Jeff Noyle <jeffno@ntdev.microsoft.com>
2017-07-24 13:44:25 -0700

    Fix the test accordingly (now that the compiler can run without crashing)

--- tools/clang/test/HLSL/pix/pixelCounterAddPixelCost.hlsl
+++ tools/clang/test/HLSL/pix/pixelCounterAddPixelCost.hlsl
@@ -4,10 +4,11 @@
 // CHECK: %UAVIncResult = call i32 @dx.op.atomicBinOp.i32(i32 78, %dx.types.Handle %PIX_CountUAV_Handle, i32 0, i32 %Clamped, i32 0, i32 0, i32 1)
 
 // Check for pixel cost instructions:
-// CHECK: %Weight = call %dx.types.ResRet.i32 @dx.op.bufferLoad.i32(i32 68, %dx.types.Handle %PIX_CountUAV_Handle, i32 128, i32 0)
+// CHECK: %WeightStruct = call %dx.types.ResRet.i32 @dx.op.bufferLoad.i32(i32 68, %dx.types.Handle %PIX_CountUAV_Handle, i32 128, i32 0)
+// CHECK: %Weight = extractvalue %dx.types.ResRet.i32 %WeightStruct, 0
 // CHECK: %7 = add i32 %Clamped, 64
-// Note this is intentionally failing right now: the last bit should have %weight, but when it runs in PIX, there's a problem...
-// CHECK: %UAVIncResult2 = call i32 @dx.op.atomicBinOp.i32(i32 78, %dx.types.Handle %PIX_CountUAV_Handle, i32 0, i32 %7, i32 0, i32 0, i32 1)
+// CHECK: %UAVIncResult2 = call i32 @dx.op.atomicBinOp.i32(i32 78, %dx.types.Handle %PIX_CountUAV_Handle, i32 0, i32 %7, i32 0, i32 0, i32 %Weight)
+
 
 
 float4 main(float4 pos : SV_Position) : SV_Target {
