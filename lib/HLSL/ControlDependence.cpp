a46267f5870008c5811350afa710533a413ac3ce
Tex Riddell <texr@microsoft.com>
2017-07-28 11:51:15 -0700

    Fix ViewID state for flow control and consistent ViewID detection (#484)
    * Fix ViewID state for flow control and consistent ViewID detection
    
    1. Different ways of determining ViewID usage led to assert or crash.
    Use DxilModule flag to guarantee consistency.  But this required
    splitting DxilEmitMetadata pass into two passes, one that finalizes
    the module and computes flags run before ComputeViewIdState, and one
    that simply writes the metadata.
    
    2. storeOutput in flow control with values not dependent on that flow,
    such as literals, would not pick up control flow dependence.
    Fix by picking up control flow dependence on storeOutput instructions,
    not just the value being written.
    
    * Fix control depenence computation
    
    There was a bug in the implementation of the control dependence algorithm.
    We were incorrectly iterating over all descendents in the postdom tree where
    the algorithm should only iterate over immediate children.

--- lib/HLSL/ControlDependence.cpp
+++ lib/HLSL/ControlDependence.cpp
@@ -1,10 +1,14 @@
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-// ViewIdAnalysis.cpp                                                        //
+// ControlDependence.cpp                                                     //
 // Copyright (C) Microsoft Corporation. All rights reserved.                 //
 // This file is distributed under the University of Illinois Open Source     //
 // License. See LICENSE.TXT for details.                                     //
 //                                                                           //
+// Control dependence is computed using algorithm in Figure 7.9 from [AK].   //
+//                                                                           //
+// References                                                                //
+// [AK] Optimizing Compilers for Modern Architectures by Allen and Kennedy.  //
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "dxc/HLSL/ControlDependence.h"
@@ -67,11 +71,10 @@ void ControlDependence::Compute(Function *F, PostDomRelationType &PostDomRel) {
       }
     }
 
-    SmallVector<BasicBlock *, 8> Descendants;
-    PostDomRel.getDescendants(x, Descendants);
     // For all z such that ipostdom(z) = x
-    for (BasicBlock *z : Descendants) {
-      if (z == x) continue;
+    for (DomTreeNode *child : PostDomRel.getNode(x)->getChildren()) {
+      BasicBlock *z = child->getBlock();
+
       auto it = m_ControlDependence.find(z);
       if (it == m_ControlDependence.end())
         continue;
