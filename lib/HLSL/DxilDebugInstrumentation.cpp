24e2792903b654aefc0b8d06c63b4d3e13352008
Jeff Noyle <jeffno@ntdev.microsoft.com>
2017-07-28 14:27:57 -0700

    checkpoint

--- lib/HLSL/DxilDebugInstrumentation.cpp
+++ lib/HLSL/DxilDebugInstrumentation.cpp
@@ -26,13 +26,51 @@ public:
 };
 
 
-bool DxilDebugInstrumentation::runOnModule(Module &)
+bool DxilDebugInstrumentation::runOnModule(Module &M)
 {
-  //DxilModule &DM = M.GetOrCreateDxilModule();
-  //
-  //DM.m_ShaderFlags.SetForceEarlyDepthStencil(true);
-  //
-  //DM.ReEmitDxilResources();
+  DxilModule &DM = M.GetOrCreateDxilModule();
+
+  auto & Functions = M.getFunctionList();
+  for (auto & f : Functions)
+  {
+    auto & Blocks = f.getBasicBlockList();
+    for (auto & b : Blocks)
+    {
+      auto & Instructions = b.getInstList();
+      for (auto & i : Instructions)
+      {
+        auto OpIterator = i.op_begin();
+        while(OpIterator != i.op_end())
+        {
+          Value const * operand = *OpIterator;
+
+          Type::TypeID ID = operand->getType()->getTypeID();
+
+          switch (ID)
+          {
+              case VoidTyID     : OutputDebugStringW("VoidTyID     "); break;
+              case HalfTyID     : OutputDebugStringW("HalfTyID     "); break;
+              case FloatTyID    : OutputDebugStringW("FloatTyID    "); break;
+              case DoubleTyID   : OutputDebugStringW("DoubleTyID   "); break;
+              case X86_FP80TyID : OutputDebugStringW("X86_FP80TyID "); break;
+              case FP128TyID    : OutputDebugStringW("FP128TyID    "); break;
+              case PPC_FP128TyID: OutputDebugStringW("PPC_FP128TyID"); break;
+              case LabelTyID    : OutputDebugStringW("LabelTyID    "); break;
+              case MetadataTyID : OutputDebugStringW("MetadataTyID "); break;
+              case X86_MMXTyID  : OutputDebugStringW("X86_MMXTyID  "); break;
+              case IntegerTyID  : OutputDebugStringW("IntegerTyID  "); break;
+              case FunctionTyID : OutputDebugStringW("FunctionTyID "); break;
+              case StructTyID   : OutputDebugStringW("StructTyID   "); break;
+              case ArrayTyID    : OutputDebugStringW("ArrayTyID    "); break;
+              case PointerTyID  : OutputDebugStringW("PointerTyID  "); break;
+              case VectorTyID   : OutputDebugStringW("VectorTyID   "); break;
+          }
+        }
+      }
+    }
+  }
+  
+  DM.ReEmitDxilResources();
 
   return true;
 }
