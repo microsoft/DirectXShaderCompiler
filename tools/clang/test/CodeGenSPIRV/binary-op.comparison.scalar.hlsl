660a367778b7911e2f56a8fe0cb90bf19cba5721
Lei Zhang <antiagainst@google.com>
2017-07-31 12:13:45 -0400

    [spirv] Vector types, variable initializers, and comparison (#488)
    * [spirv] Translate vector types and handle variable initializers
    
    * Translated vector types and operations on vector types
      * +, -, *, /, % on vector <op> vector and vector <op> scalar
      * Prefix increment on vectors
      * < on vector <op> vector and vector <op> scalar
    * Covered both constant initializer and variable initializer
    * Applied constant modifier to variables
    
    * [spirv] Translate all comparision operators
    
    * Covers <, <=, >, >=, ==, !=
    * For both scalars and vectors
    * Also update docs about arithmetic and comparison operators

--- tools/clang/test/CodeGenSPIRV/binary-op.comparison.scalar.hlsl
+++ tools/clang/test/CodeGenSPIRV/binary-op.comparison.scalar.hlsl
@@ -6,18 +6,78 @@ void main() {
     uint i, j;
     float o, p;
 
-// CHECK:      [[a:%\d+]] = OpLoad %int %a
-// CHECK-NEXT: [[b:%\d+]] = OpLoad %int %b
-// CHECK-NEXT: %{{\d+}} = OpSLessThan %bool [[a]] [[b]]
+// CHECK:      [[a0:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b0:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpSLessThan %bool [[a0]] [[b0]]
     r = a < b;
+// CHECK:      [[a1:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b1:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpSLessThanEqual %bool [[a1]] [[b1]]
+    r = a <= b;
+// CHECK:      [[a2:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b2:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpSGreaterThan %bool [[a2]] [[b2]]
+    r = a > b;
+// CHECK:      [[a3:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b3:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpSGreaterThanEqual %bool [[a3]] [[b3]]
+    r = a >= b;
+// CHECK:      [[a4:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b4:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpIEqual %bool [[a4]] [[b4]]
+    r = a == b;
+// CHECK:      [[a5:%\d+]] = OpLoad %int %a
+// CHECK-NEXT: [[b5:%\d+]] = OpLoad %int %b
+// CHECK-NEXT: {{%\d+}} = OpINotEqual %bool [[a5]] [[b5]]
+    r = a != b;
 
-// CHECK:      [[i:%\d+]] = OpLoad %uint %i
-// CHECK-NEXT: [[j:%\d+]] = OpLoad %uint %j
-// CHECK-NEXT: %{{\d+}} = OpULessThan %bool [[i]] [[j]]
+// CHECK:      [[i0:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j0:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpULessThan %bool [[i0]] [[j0]]
     r = i < j;
+// CHECK:      [[i1:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j1:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpULessThanEqual %bool [[i1]] [[j1]]
+    r = i <= j;
+// CHECK:      [[i2:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j2:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpUGreaterThan %bool [[i2]] [[j2]]
+    r = i > j;
+// CHECK:      [[i3:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j3:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpUGreaterThanEqual %bool [[i3]] [[j3]]
+    r = i >= j;
+// CHECK:      [[i4:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j4:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpIEqual %bool [[i4]] [[j4]]
+    r = i == j;
+// CHECK:      [[i5:%\d+]] = OpLoad %uint %i
+// CHECK-NEXT: [[j5:%\d+]] = OpLoad %uint %j
+// CHECK-NEXT: {{%\d+}} = OpINotEqual %bool [[i5]] [[j5]]
+    r = i != j;
 
-// CHECK:      [[o:%\d+]] = OpLoad %float %o
-// CHECK-NEXT: [[p:%\d+]] = OpLoad %float %p
-// CHECK-NEXT: %{{\d+}} = OpFOrdLessThan %bool [[o]] [[p]]
+// CHECK:      [[o0:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p0:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdLessThan %bool [[o0]] [[p0]]
     r = o < p;
+// CHECK:      [[o1:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p1:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdLessThanEqual %bool [[o1]] [[p1]]
+    r = o <= p;
+// CHECK:      [[o2:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p2:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdGreaterThan %bool [[o2]] [[p2]]
+    r = o > p;
+// CHECK:      [[o3:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p3:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdGreaterThanEqual %bool [[o3]] [[p3]]
+    r = o >= p;
+// CHECK:      [[o4:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p4:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdEqual %bool [[o4]] [[p4]]
+    r = o == p;
+// CHECK:      [[o5:%\d+]] = OpLoad %float %o
+// CHECK-NEXT: [[p5:%\d+]] = OpLoad %float %p
+// CHECK-NEXT: {{%\d+}} = OpFOrdNotEqual %bool [[o5]] [[p5]]
+    r = o != p;
 }
