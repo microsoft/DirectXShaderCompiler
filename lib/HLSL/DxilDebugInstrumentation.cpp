1e196ed8b1453a16cf05ab24e75f77f099306344
Jeff Noyle <jeffno@ntdev.microsoft.com>
2017-08-08 15:18:05 -0700

    Sorta works. (atomic writes/sv_pos no work on AMD) (PSO creation fails on nv) (WARP too difficult to use to tell)

--- lib/HLSL/DxilDebugInstrumentation.cpp
+++ lib/HLSL/DxilDebugInstrumentation.cpp
@@ -15,11 +15,122 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #include "llvm/support/FormattedStream.h"
 
 using namespace llvm;
 using namespace hlsl;
+
+
+/*
+Debug Format
+
+[31:0] Header
+[31:28] Record size in DWORDs (EXCLUDING Header & UID)
+[27:24] Flags [Currently Reserved = 0]
+[23:16] Record Type
+0 = Invocation Start Marker
+1 = Step
+2 = Event
+3 = Input
+4 = Read Register
+5 = Written Register
+6 = Register Relative Index 0
+7 = Register Relative Index 1
+[15:0] Header Payload.  Payload depends on Record Type
+[31:0] UID
+[31:0] ... Payload DWORDs 0-N defined by the Payload Size.  Payload depends on Record Type
+*/
+
+enum DebugShaderModifierRecordType {
+  DebugShaderModifierRecordTypeInvocationStartMarker,
+  DebugShaderModifierRecordTypeStep,
+  DebugShaderModifierRecordTypeEvent,
+  DebugShaderModifierRecordTypeInputRegister,
+  DebugShaderModifierRecordTypeReadRegister,
+  DebugShaderModifierRecordTypeWrittenRegister,
+  DebugShaderModifierRecordTypeRegisterRelativeIndex0,
+  DebugShaderModifierRecordTypeRegisterRelativeIndex1,
+  DebugShaderModifierRecordTypeRegisterRelativeIndex2,
+};
+
+enum DebugShaderModifierComponentMask {
+  DebugShaderModifierComponent_NONE = 0x0,
+  DebugShaderModifierComponent_X = 0x1, // (PIX_TRACE_COMPONENT_X),
+  DebugShaderModifierComponent_Y = 0x2, // (PIX_TRACE_COMPONENT_Y),
+  DebugShaderModifierComponent_XY = 0x3, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Y),
+  DebugShaderModifierComponent_Z = 0x4, // (PIX_TRACE_COMPONENT_Z),
+  DebugShaderModifierComponent_XZ = 0x5, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Z),
+  DebugShaderModifierComponent_YZ = 0x6, // (PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_Z),
+  DebugShaderModifierComponent_XYZ = 0x7, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_Z),
+  DebugShaderModifierComponent_W = 0x8, // (PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_XW = 0x9, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_YW = 0xA, // (PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_XYW = 0xB, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_ZW = 0xC, // (PIX_TRACE_COMPONENT_Z | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_XZW = 0xD, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Z | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_YZW = 0xE, // (PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_Z | PIX_TRACE_COMPONENT_W),
+  DebugShaderModifierComponent_XYZW = 0xF, // (PIX_TRACE_COMPONENT_X | PIX_TRACE_COMPONENT_Y | PIX_TRACE_COMPONENT_Z | PIX_TRACE_COMPONENT_W),
+};
+
+struct DebugShaderModifierBufferHeader {
+  uint32_t DwordCount;
+};
+
+struct DebugShaderModifierRecordHeader {
+  union  {
+    struct {
+      uint32_t SizeDwords : 4;
+      uint32_t Flags : 4;
+      uint32_t Type : 8;
+      uint32_t HeaderPayload : 16;
+    } Details;
+    uint32_t u32Header;
+  } Header;
+  uint32_t UID;
+};
+
+struct DebugShaderModifierRecordStep {
+  union {
+    struct {
+      uint32_t SizeDwords : 4;
+      uint32_t Flags : 4;
+      uint32_t Type : 8;
+      uint32_t HeaderPayload : 16;
+    } Details;
+    uint32_t u32Header;
+  } Header;
+  uint32_t UID;
+  uint32_t InstructionOffset;
+  uint32_t Opcode;
+};
+
+struct DebugShaderModifierRecordRegister {
+  union {
+    struct  {
+      uint32_t PayloadSizeDwords : 4;
+      uint32_t Flags : 4;
+      uint32_t Type : 8;
+      uint32_t Register : 8;
+      uint32_t Operand : 4;
+      uint32_t Mask : 4;
+    } Details;
+    uint32_t u32Header;
+  } Header;
+  uint32_t UID;
+  uint32_t Index0;
+  uint32_t Index1;
+  uint32_t Index2;
+  union {
+    uint32_t u32Value[4];
+    float f32Value[4];
+  } Value;
+};
+
+uint32_t DebugShaderModifierRecordPayloadSizeDwords(size_t recordTotalSizeBytes) {
+  return ((recordTotalSizeBytes - sizeof(DebugShaderModifierRecordHeader)) / sizeof(uint32_t));
+}
 
 class DxilDebugInstrumentation : public ModulePass {
 
@@ -64,6 +175,12 @@ private:
     } ComputeShader;
   };
 
+  CallInst * IndexVariable;
+  Value * SelectionCriterion;
+  CallInst * HandleForUAV;
+  Value * XAsInt;
+  Value * YAsInt;
+
   struct BuilderContext
   {
     Module &M;
@@ -87,6 +204,10 @@ private:
   Value * addInvocationSelectionProlog(BuilderContext & BC, SystemValueIndices SVIndices);
   Value * addPixelShaderProlog(BuilderContext & BC, SystemValueIndices SVIndices);
   void recordInputValue(BuilderContext & BC, CallInst * HandleForUAV, Value const * operand);
+  void addDebugEntryValue(BuilderContext & BC, Value * Index, Value * TheValue);
+  void addInvocationStartMarker(BuilderContext & BC);
+  Value * reserveDebugEntrySpace(BuilderContext & BC, uint32_t SpaceInDwords);
+  Value * incrementUAVIndex(BuilderContext & BC, Value * CurrentValue);
 };
 
 void DxilDebugInstrumentation::applyOptions(PassOptions O)
@@ -154,7 +275,7 @@ DxilDebugInstrumentation::SystemValueIndices DxilDebugInstrumentation::addRequir
 
     if (Existing_SV_PrimId == InputElements.end()) {
       auto Added_SV_PrimId = std::make_unique<DxilSignatureElement>(DXIL::SigPointKind::PSIn);
-      Added_SV_PrimId->Initialize("PrimitiveId", hlsl::CompType::getF32(), hlsl::DXIL::InterpolationMode::Linear, 1, 4, 0, 0);
+      Added_SV_PrimId->Initialize("PrimitiveId", hlsl::CompType::getF32(), hlsl::DXIL::InterpolationMode::Linear, 1, 1, 0, 0);
       Added_SV_PrimId->AppendSemanticIndex(0);
       Added_SV_PrimId->SetSigPointKind(DXIL::SigPointKind::PSIn);
       Added_SV_PrimId->SetKind(hlsl::DXIL::SemanticKind::PrimitiveID);
@@ -182,8 +303,6 @@ Value * DxilDebugInstrumentation::addPixelShaderProlog(BuilderContext & BC, Syst
   UndefValue* UndefArg = UndefValue::get(Type::getInt32Ty(BC.Ctx));
 
   // Convert SV_POSITION to UINT          
-  Value * XAsInt;
-  Value * YAsInt;
   {
     auto LoadInputOpFunc = BC.HlslOP->GetOpFunc(DXIL::OpCode::LoadInput, Type::getFloatTy(BC.Ctx));
     Constant* LoadInputOpcode = BC.HlslOP->GetU32Const((unsigned)DXIL::OpCode::LoadInput);
@@ -197,11 +316,12 @@ Value * DxilDebugInstrumentation::addPixelShaderProlog(BuilderContext & BC, Syst
     YAsInt = BC.Builder.CreateCast(Instruction::CastOps::FPToUI, YPos, Type::getInt32Ty(BC.Ctx), "YIndex");
   }
 
+
   // Compare to expected pixel position and primitive ID
   auto CompareToX = BC.Builder.CreateICmpEQ(XAsInt, BC.HlslOP->GetU32Const(Parameters.PixelShader.X), "CompareToX");
   auto CompareToY = BC.Builder.CreateICmpEQ(YAsInt, BC.HlslOP->GetU32Const(Parameters.PixelShader.Y), "CompareToY");
   auto ComparePos = BC.Builder.CreateAnd(CompareToX, CompareToY, "ComparePos");
-
+  
   // Compare primitve ID to expected primitive ID
   CallInst * PrimitiveId;
   {
@@ -211,13 +331,11 @@ Value * DxilDebugInstrumentation::addPixelShaderProlog(BuilderContext & BC, Syst
     PrimitiveId = BC.Builder.CreateCall(LoadInputOpFunc,
       { LoadInputOpcode, SV_Prim_ID, Zero32Arg /*row*/, Zero8Arg /*column*/, UndefArg }, "PrimitiveId");
   }
-
+  
   auto CompareToExpectedPrimId = BC.Builder.CreateICmpEQ(PrimitiveId, BC.HlslOP->GetU32Const(Parameters.PixelShader.PrimitiveId), "CompareToPrimId");
-
+  
   // Merge comparisons into one:
-  auto CompareAll = BC.Builder.CreateAnd(ComparePos, CompareToExpectedPrimId, "ComparePosAndPrimId");
-
-  return CompareAll;
+  return BC.Builder.CreateAnd(ComparePos, CompareToExpectedPrimId, "ComparePosAndPrimId");
 }
 
 CallInst * DxilDebugInstrumentation::addUAV(BuilderContext & BC)
@@ -225,9 +343,9 @@ CallInst * DxilDebugInstrumentation::addUAV(BuilderContext & BC)
 
   // Set up a UAV with structure of a single int
   SmallVector<llvm::Type*, 1> Elements{ Type::getInt32Ty(BC.Ctx) };
-  llvm::StructType *UAVStructTy = llvm::StructType::create(Elements, "PIX_CountUAV_Type");
+  llvm::StructType *UAVStructTy = llvm::StructType::create(Elements, "PIX_DebugUAV_Type");
   std::unique_ptr<DxilResource> pUAV = llvm::make_unique<DxilResource>();
-  pUAV->SetGlobalName("PIX_CountUAVName");
+  pUAV->SetGlobalName("PIX_DebugUAVName");
   pUAV->SetGlobalSymbol(UndefValue::get(UAVStructTy->getPointerTo()));
   pUAV->SetID(0);
   pUAV->SetSpaceID((unsigned int)-2); // This is the reserved-for-tools register space
@@ -237,8 +355,7 @@ CallInst * DxilDebugInstrumentation::addUAV(BuilderContext & BC)
   pUAV->SetCompType(CompType::getI32());
   pUAV->SetLowerBound(0);
   pUAV->SetRangeSize(1);
-  pUAV->SetKind(DXIL::ResourceKind::StructuredBuffer);
-  pUAV->SetElementStride(4);
+  pUAV->SetKind(DXIL::ResourceKind::RawBuffer);
 
   auto ID = BC.DM.AddUAV(std::move(pUAV));
 
@@ -250,7 +367,7 @@ CallInst * DxilDebugInstrumentation::addUAV(BuilderContext & BC)
   Constant* IndexArg = BC.HlslOP->GetU32Const(0); // 
   Constant* FalseArg = BC.HlslOP->GetI1Const(0); // non-uniform resource index: false
   auto HandleForUAV = BC.Builder.CreateCall(CreateHandleOpFunc,
-  { CreateHandleOpcodeArg, UAVVArg, MetaDataArg, IndexArg, FalseArg }, "PIX_CountUAV_Handle");
+  { CreateHandleOpcodeArg, UAVVArg, MetaDataArg, IndexArg, FalseArg }, "PIX_DebugUAV_Handle");
 
   return HandleForUAV;
 }
@@ -305,6 +422,73 @@ Value * DxilDebugInstrumentation::addInvocationSelectionProlog(BuilderContext & 
   return ParameterTestResult;
 }
 
+Value * DxilDebugInstrumentation::reserveDebugEntrySpace(BuilderContext & BC, uint32_t SpaceInDwords)
+{
+  // Insert the UAV increment instruction:
+  Function* AtomicOpFunc = BC.HlslOP->GetOpFunc(OP::OpCode::AtomicBinOp, Type::getInt32Ty(BC.Ctx));
+  Constant* AtomicBinOpcode = BC.HlslOP->GetU32Const((unsigned)OP::OpCode::AtomicBinOp);
+  Constant* AtomicAdd = BC.HlslOP->GetU32Const((unsigned)DXIL::AtomicBinOpCode::Add);
+  Constant* Zero32Arg = BC.HlslOP->GetU32Const(0);
+  auto PreviousValue = BC.Builder.CreateCall(AtomicOpFunc, {
+    AtomicBinOpcode,// i32, ; opcode
+    HandleForUAV,   // %dx.types.Handle, ; resource handle
+    AtomicAdd,      // i32, ; binary operation code : EXCHANGE, IADD, AND, OR, XOR, IMIN, IMAX, UMIN, UMAX
+    Zero32Arg,      // i32, ; coordinate c0: index in bytes
+    Zero32Arg,      // i32, ; coordinate c1 (unused)
+    Zero32Arg,      // i32, ; coordinate c2 (unused)
+    BC.HlslOP->GetU32Const(SpaceInDwords),        // i32); increment value
+  }, "UAVIncResult");
+
+  // *sizeof(DWORD), and leave 1 DWORD of space for the counter in the first dword of the UAV:
+  auto MulBy4 = BC.Builder.CreateMul(PreviousValue, BC.HlslOP->GetU32Const(4));
+  return incrementUAVIndex(BC, MulBy4);
+}
+
+Value * DxilDebugInstrumentation::incrementUAVIndex(BuilderContext & BC, Value * CurrentValue)
+{
+  return BC.Builder.CreateAdd(CurrentValue, BC.HlslOP->GetU32Const(4));
+}
+
+void DxilDebugInstrumentation::addDebugEntryValue(BuilderContext & BC, Value * Index, Value * TheValue)
+{
+  UndefValue* Undefi32Arg = UndefValue::get(Type::getInt32Ty(BC.Ctx));
+
+  Function* StoreValue = BC.HlslOP->GetOpFunc(OP::OpCode::BufferStore, Type::getInt32Ty(BC.Ctx));
+  Constant* StoreValueOpcode = BC.HlslOP->GetU32Const((unsigned)DXIL::OpCode::BufferStore);
+  Constant* Zero32Arg = BC.HlslOP->GetU32Const(0);
+  Constant* WriteMask_X = BC.HlslOP->GetI8Const(1);
+  (void)BC.Builder.CreateCall(StoreValue, {
+    StoreValueOpcode, // i32 opcode
+    HandleForUAV,     // %dx.types.Handle, ; resource handle
+    Index,            // i32 c0: index in bytes into UAV
+    Undefi32Arg,      // i32 c1: unused
+    TheValue,
+    Zero32Arg,        // unused values
+    Zero32Arg,        // unused values
+    Zero32Arg,        // unused values
+    WriteMask_X
+  });
+}
+
+void DxilDebugInstrumentation::addInvocationStartMarker(BuilderContext & BC)
+{
+  auto ThenBlockTail = SplitBlockAndInsertIfThen(SelectionCriterion, BC.Builder.GetInsertPoint(), false);
+
+  IRBuilder<> ThenBuilder(ThenBlockTail);
+  BuilderContext ThenBuilderContext = { BC.M, BC.DM, BC.Ctx, BC.HlslOP, ThenBuilder };
+  
+  auto StartIndex = reserveDebugEntrySpace(ThenBuilderContext, 2);
+  
+  DebugShaderModifierRecordHeader marker{ 0 };
+  marker.Header.Details.SizeDwords = 0;
+  marker.Header.Details.Flags = 0;
+  marker.Header.Details.Type = DebugShaderModifierRecordTypeInvocationStartMarker;
+  addDebugEntryValue(ThenBuilderContext, StartIndex, ThenBuilderContext.HlslOP->GetU32Const(marker.Header.u32Header));
+  auto NextIndex = incrementUAVIndex(ThenBuilderContext, StartIndex);
+  addDebugEntryValue(ThenBuilderContext, NextIndex, StartIndex );
+}
+
+
 bool DxilDebugInstrumentation::runOnModule(Module &M)
 {
   DxilModule &DM = M.GetOrCreateDxilModule();
@@ -316,8 +500,12 @@ bool DxilDebugInstrumentation::runOnModule(Module &M)
   BuilderContext BC{ M, DM, Ctx, HlslOP, Builder };
 
   auto SystemValues = addRequiredSystemValues(BC);
-  auto HandleForUAV = addUAV(BC);
-  /*auto SelectionCriterion = */addInvocationSelectionProlog(BC, SystemValues);
+  HandleForUAV = addUAV(BC);
+  SelectionCriterion = addInvocationSelectionProlog(BC, SystemValues);
+
+  addInvocationStartMarker(BC);
+#if 0
+  auto FirstInstructionToInstrument = BC.Builder.GetInsertPoint();
 
   auto & Functions = M.getFunctionList();
   for (auto & f : Functions)
@@ -329,21 +517,17 @@ bool DxilDebugInstrumentation::runOnModule(Module &M)
 
       Instruction * InstructionIterator = Instructions.begin();
 
-      if (&f == DM.GetEntryFunction())
+      if (&f == DM.GetEntryFunction() && &b == &DM.GetEntryFunction()->getEntryBlock())
       {
         // Skip over the instructions we added at the beginning: Don't instrument them.
-        while (InstructionIterator != firstInsertionPt)
-        {
-          InstructionIterator = InstructionIterator->getNextNode();
-          assert(InstructionIterator != nullptr);
-        }
+        InstructionIterator = FirstInstructionToInstrument;
       }
 
       for (; InstructionIterator != Instructions.end(); InstructionIterator = InstructionIterator->getNextNode())
       {
         OutputDebugStringW(M, "Beginning of instruction\n");
         auto OpIterator = InstructionIterator->op_begin();
-        while(OpIterator != InstructionIterator->op_end())
+        while (OpIterator != InstructionIterator->op_end())
         {
           Value const * operand = *OpIterator;
 
@@ -354,7 +538,8 @@ bool DxilDebugInstrumentation::runOnModule(Module &M)
       }
     }
   }
-  
+#endif
+
   DM.ReEmitDxilResources();
 
   return true;
