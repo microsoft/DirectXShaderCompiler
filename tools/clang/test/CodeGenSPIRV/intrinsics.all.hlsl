b5043877bcda4bf863eda9a43f1cdc2f2d79b8a9
Ehsan <ehsann@google.com>
2017-08-09 01:19:14 -0400

    [spirv] Support matrices in any, all, asfloat, asint, asuint (#533)

--- tools/clang/test/CodeGenSPIRV/intrinsics.all.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.all.hlsl
@@ -7,6 +7,8 @@
 // CHECK:      [[v4int_0:%\d+]] = OpConstantComposite %v4int %int_0 %int_0 %int_0 %int_0
 // CHECK-NEXT: [[v4uint_0:%\d+]] = OpConstantComposite %v4uint %uint_0 %uint_0 %uint_0 %uint_0
 // CHECK-NEXT: [[v4float_0:%\d+]] = OpConstantComposite %v4float %float_0 %float_0 %float_0 %float_0
+// CHECK-NEXT: [[v3float_0:%\d+]] = OpConstantComposite %v3float %float_0 %float_0 %float_0
+// CHECK-NEXT: [[v2float_0:%\d+]] = OpConstantComposite %v2float %float_0 %float_0
 
 void main() {
     bool result;
@@ -58,14 +60,14 @@ void main() {
     result = all(h);
 
     // CHECK-NEXT: [[i:%\d+]] = OpLoad %v4int %i
-    // CHECK-NEXT: [[v4int_to_bool:%\d+]] = OpINotEqual %bool [[i]] [[v4int_0]]
+    // CHECK-NEXT: [[v4int_to_bool:%\d+]] = OpINotEqual %v4bool [[i]] [[v4int_0]]
     // CHECK-NEXT: [[all_int4:%\d+]] = OpAll %bool [[v4int_to_bool]]
     // CHECK-NEXT: OpStore %result [[all_int4]]
     int4 i;
     result = all(i);
 
     // CHECK-NEXT: [[j:%\d+]] = OpLoad %v4uint %j
-    // CHECK-NEXT: [[v4uint_to_bool:%\d+]] = OpINotEqual %bool [[j]] [[v4uint_0]]
+    // CHECK-NEXT: [[v4uint_to_bool:%\d+]] = OpINotEqual %v4bool [[j]] [[v4uint_0]]
     // CHECK-NEXT: [[all_uint4:%\d+]] = OpAll %bool [[v4uint_to_bool]]
     // CHECK-NEXT: OpStore %result [[all_uint4]]
     uint4 j;
@@ -78,10 +80,45 @@ void main() {
     result = all(k);
 
     // CHECK-NEXT: [[l:%\d+]] = OpLoad %v4float %l
-    // CHECK-NEXT: [[v4float_to_bool:%\d+]] = OpFOrdNotEqual %bool [[l]] [[v4float_0]]
+    // CHECK-NEXT: [[v4float_to_bool:%\d+]] = OpFOrdNotEqual %v4bool [[l]] [[v4float_0]]
     // CHECK-NEXT: [[all_float4:%\d+]] = OpAll %bool [[v4float_to_bool]]
     // CHECK-NEXT: OpStore %result [[all_float4]]
     float4 l;
     result = all(l);
-}
 
+    // CHECK-NEXT: [[m:%\d+]] = OpLoad %float %m
+    // CHECK-NEXT: [[mat1x1_to_bool:%\d+]] = OpFOrdNotEqual %bool [[m]] %float_0
+    // CHECK-NEXT: OpStore %result [[mat1x1_to_bool]]
+    float1x1 m;
+    result = all(m);
+
+    // CHECK-NEXT: [[n:%\d+]] = OpLoad %v3float %n
+    // CHECK-NEXT: [[mat1x3_to_bool:%\d+]] = OpFOrdNotEqual %v3bool [[n]] [[v3float_0]]
+    // CHECK-NEXT: [[all_mat1x3:%\d+]] = OpAll %bool [[mat1x3_to_bool]]
+    // CHECK-NEXT: OpStore %result [[all_mat1x3]]
+    float1x3 n;
+    result = all(n);
+
+    // CHECK-NEXT: [[o:%\d+]] = OpLoad %v2float %o
+    // CHECK-NEXT: [[mat2x1_to_bool:%\d+]] = OpFOrdNotEqual %v2bool [[o]] [[v2float_0]]
+    // CHECK-NEXT: [[all_mat2x1:%\d+]] = OpAll %bool [[mat2x1_to_bool]]
+    // CHECK-NEXT: OpStore %result [[all_mat2x1]]
+    float2x1 o;
+    result = all(o);
+
+    // CHECK-NEXT: [[p:%\d+]] = OpLoad %mat3v4float %p
+    // CHECK-NEXT: [[row0:%\d+]] = OpCompositeExtract %v4float [[p]] 0
+    // CHECK-NEXT: [[row0_to_bool_vec:%\d+]] = OpFOrdNotEqual %v4bool [[row0]] [[v4float_0]]
+    // CHECK-NEXT: [[all_row0:%\d+]] = OpAll %bool [[row0_to_bool_vec]]
+    // CHECK-NEXT: [[row1:%\d+]] = OpCompositeExtract %v4float [[p]] 1
+    // CHECK-NEXT: [[row1_to_bool_vec:%\d+]] = OpFOrdNotEqual %v4bool [[row1]] [[v4float_0]]
+    // CHECK-NEXT: [[all_row1:%\d+]] = OpAll %bool [[row1_to_bool_vec]]
+    // CHECK-NEXT: [[row2:%\d+]] = OpCompositeExtract %v4float [[p]] 2
+    // CHECK-NEXT: [[row2_to_bool_vec:%\d+]] = OpFOrdNotEqual %v4bool [[row2]] [[v4float_0]]
+    // CHECK-NEXT: [[all_row2:%\d+]] = OpAll %bool [[row2_to_bool_vec]]
+    // CHECK-NEXT: [[all_rows:%\d+]] = OpCompositeConstruct %v3bool [[all_row0]] [[all_row1]] [[all_row2]]
+    // CHECK-NEXT: [[all_mat3x4:%\d+]] = OpAll %bool [[all_rows]]
+    // CHECK-NEXT: OpStore %result [[all_mat3x4]]
+    float3x4 p;
+    result = all(p);
+}
--- tools/clang/test/CodeGenSPIRV/intrinsics.asfloat.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.asfloat.hlsl
@@ -7,6 +7,11 @@
 void main() {
     float result;
     float4 result4;
+    float1x1 result1x1;
+    float1x3 result1x3;
+    float2x1 result2x1;
+    float2x3 result2x3; 
+
 
     // CHECK:      [[a:%\d+]] = OpLoad %int %a
     // CHECK-NEXT: [[a_as_float:%\d+]] = OpBitcast %float [[a]]
@@ -58,4 +63,24 @@ void main() {
     // CHECK-NEXT: OpStore %result4 [[i]]
     float4 i;
     result4 = asfloat(i);
+    
+    // CHECK-NEXT: [[j:%\d+]] = OpLoad %float %j
+    // CHECK-NEXT: OpStore %result1x1 [[j]]
+    float1x1 j;
+    result1x1 = asfloat(j);
+    
+    // CHECK-NEXT: [[k:%\d+]] = OpLoad %v3float %k
+    // CHECK-NEXT: OpStore %result1x3 [[k]]    
+    float1x3 k;
+    result1x3 = asfloat(k);
+    
+    // CHECK-NEXT: [[l:%\d+]] = OpLoad %v2float %l
+    // CHECK-NEXT: OpStore %result2x1 [[l]]
+    float2x1 l;
+    result2x1 = asfloat(l);
+    
+    // CHECK-NEXT: [[m:%\d+]] = OpLoad %mat2v3float %m
+    // CHECK-NEXT: OpStore %result2x3 [[m]]
+    float2x3 m;
+    result2x3 = asfloat(m);
 }
