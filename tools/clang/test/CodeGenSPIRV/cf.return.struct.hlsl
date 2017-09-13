4e2735af11bfcd4629dee35a960af424c3f98555
Ehsan <ehsannas@gmail.com>
2017-09-13 11:51:13 -0400

    [spirv] Test for returning a struct from function.

---
+++ tools/clang/test/CodeGenSPIRV/cf.return.struct.hlsl
@@ -0,0 +1,25 @@
+// Run: %dxc -T ps_6_0 -E main
+
+struct S {
+  int x;
+  float y;
+};
+
+S foo();
+
+void main() {
+
+// CHECK: [[foo:%\d+]] = OpFunctionCall %S %foo
+// CHECK: OpStore %result [[foo]]
+  S result = foo();
+}
+
+S foo() {
+// CHECK: %s = OpVariable %_ptr_Function_S Function
+// CHECK: OpStore %s {{%\d+}}
+  S s = {1, 2.0};
+
+// CHECK: [[s:%\d+]] = OpLoad %S %s
+// CHECK: OpReturnValue [[s]]
+  return s;
+}
