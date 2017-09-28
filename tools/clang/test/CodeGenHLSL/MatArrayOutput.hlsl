8c429b62f019fec02bf2a6e72ea22365246e76e3
Xiang Li <python3kgae@outlook.com>
2017-09-28 11:31:26 -0700

    Refine test.

--- tools/clang/test/CodeGenHLSL/MatArrayOutput.hlsl
+++ tools/clang/test/CodeGenHLSL/MatArrayOutput.hlsl
@@ -19,20 +19,20 @@
 struct Vertex
 {
     float2x2 pos     : POSITION0;
+    float3x2 t     : T;
 };
 
 struct Interpolants
 {
     float4 pos     : SV_POSITION0;
-    float2x2 p[3] : O;
+    row_major float3x2 p[2] : O;
 };
 
 Interpolants main( Vertex In )
 {
     Interpolants o;
     o.pos = (float4)In.pos;
-    o.p[0] = In.pos*2;
-    o.p[1] = In.pos*3;
-    o.p[2] = In.pos*4;
+    o.p[0] = In.t*2;
+    o.p[1] = In.t*3;
     return o;
 }
