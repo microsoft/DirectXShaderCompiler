cd5299e9464fcde9018771eb0afae70c4cbdd260
Lei Zhang <antiagainst@google.com>
2017-10-03 14:25:51 -0400

    [spirv] Add support explicit binding for CTBuffer (#672)
    This commit adds support for explicit binding assignment in
    cbuffer/tbuffer/ConstantBuffer/TextureBuffer.

--- tools/clang/test/HLSL/cxx11-attributes.hlsl
+++ tools/clang/test/HLSL/cxx11-attributes.hlsl
@@ -1,9 +1,17 @@
 // RUN: %clang_cc1 -fsyntax-only -ffreestanding -verify %s
 
+[[vk::binding(4, 3)]] // expected-warning {{'binding' attribute ignored}}
+cbuffer myCBuffer {
+    float cbfield;
+};
+
 struct S {
     [[vk::location(1)]] // expected-warning {{'location' attribute ignored}}
     float a: A;
 };
+
+[[vk::binding(5, 3)]] // expected-warning {{'binding' attribute ignored}}
+ConstantBuffer<S> myConstantBuffer;
 
 [[maybe_unused]] // expected-warning {{unknown attribute 'maybe_unused' ignored}}
 float main([[scope::attr(0, "str")]] // expected-warning {{unknown attribute 'attr' ignored}}
