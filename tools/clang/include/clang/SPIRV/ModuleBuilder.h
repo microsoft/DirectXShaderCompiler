4db50faf2227c2106e71a7c3fa1148eff8cdb57a
Lei Zhang <antiagainst@google.com>
2017-10-06 09:59:13 -0400

    [spirv] Support RWByteAddressBuffer atomic methods (#678)

--- tools/clang/include/clang/SPIRV/ModuleBuilder.h
+++ tools/clang/include/clang/SPIRV/ModuleBuilder.h
@@ -144,11 +144,17 @@ public:
   uint32_t createBinaryOp(spv::Op op, uint32_t resultType, uint32_t lhs,
                           uint32_t rhs);
 
-  /// \brief Creates an OpAtomicIAdd or OpAtomicISub instruction with the given
-  /// parameters. Returns the <result-id> for the result.
-  uint32_t createAtomicIAddSub(uint32_t resultType, uint32_t orignalValuePtr,
-                               uint32_t scopeId, uint32_t memorySemanticsId,
-                               uint32_t valueToOp, bool isAdd);
+  /// \brief Creates an atomic instruction with the given parameters.
+  /// Returns the <result-id> for the result.
+  uint32_t createAtomicOp(spv::Op opcode, uint32_t resultType,
+                          uint32_t orignalValuePtr, uint32_t scopeId,
+                          uint32_t memorySemanticsId, uint32_t valueToOp);
+  uint32_t createAtomicCompareExchange(uint32_t resultType,
+                                       uint32_t orignalValuePtr,
+                                       uint32_t scopeId,
+                                       uint32_t equalMemorySemanticsId,
+                                       uint32_t unequalMemorySemanticsId,
+                                       uint32_t valueToOp, uint32_t comparator);
 
   /// \brief Creates SPIR-V instructions for sampling the given image.
   ///
---
+++ tools/clang/test/CodeGenSPIRV/method.rw-byte-address-buffer.atomic.hlsl
@@ -0,0 +1,109 @@
+// Run: %dxc -T ps_6_0 -E main
+
+RWByteAddressBuffer myBuffer;
+
+float4 main() : SV_Target
+{
+    uint originalVal;
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicIAdd %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedAdd(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicIAdd %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedAdd(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicAnd %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedAnd(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicAnd %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedAnd(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicOr %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedOr(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicOr %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedOr(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicXor %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedXor(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicXor %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedXor(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicUMax %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedMax(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicUMax %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedMax(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicSMax %uint [[ptr]] %uint_1 %uint_0 %int_n42
+    myBuffer.InterlockedMax(16, -42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicSMax %uint [[ptr]] %uint_1 %uint_0 %int_n42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedMax(16, -42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicUMin %uint [[ptr]] %uint_1 %uint_0 %uint_42
+    myBuffer.InterlockedMin(16, 42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicUMin %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedMin(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:        {{%\d+}} = OpAtomicSMin %uint [[ptr]] %uint_1 %uint_0 %int_n42
+    myBuffer.InterlockedMin(16, -42);
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicSMin %uint [[ptr]] %uint_1 %uint_0 %int_n42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedMin(16, -42, originalVal);
+
+    // .InterlockedExchnage() has no two-parameter overload.
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicExchange %uint [[ptr]] %uint_1 %uint_0 %uint_42
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedExchange(16, 42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicCompareExchange %uint [[ptr]] %uint_1 %uint_0 %uint_0 %uint_42 %uint_30
+// CHECK-NEXT:                   OpStore %originalVal [[val]]
+    myBuffer.InterlockedCompareExchange(/*offset=*/16, /*compare_value=*/30, /*value=*/42, originalVal);
+
+// CHECK:      [[offset:%\d+]] = OpShiftRightLogical %uint %uint_16 %uint_2
+// CHECK-NEXT:    [[ptr:%\d+]] = OpAccessChain %_ptr_Uniform_uint %myBuffer %uint_0 [[offset]]
+// CHECK-NEXT:    [[val:%\d+]] = OpAtomicCompareExchange %uint [[ptr]] %uint_1 %uint_0 %uint_0 %uint_42 %uint_30
+// CHECK-NOT:                    [[val]]
+    myBuffer.InterlockedCompareStore(/*offset=*/16, /*compare_value=*/30, /*value=*/42);
+
+    return 1.0;
+}
