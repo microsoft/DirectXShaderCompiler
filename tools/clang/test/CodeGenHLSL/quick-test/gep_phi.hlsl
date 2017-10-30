3be53c234029466820b51d3c6e2c911baad6c767
Xiang Li <python3kgae@outlook.com>
2017-10-30 14:09:04 -0700

    Add test for select of pointer.

--- tools/clang/test/CodeGenHLSL/quick-test/gep_phi.hlsl
+++ tools/clang/test/CodeGenHLSL/quick-test/gep_phi.hlsl
@@ -6,12 +6,22 @@
 static float a[4] = {1,2,3,4};
 static float b[4] = {5,6,7,8};
 
-float main(float x : X, uint y : Y) : SV_Target {
-  float c[4] = {x, x, x, x};
-  if (x > 2.5)
-    return a[y];
-  else if (x > 1.0)
-    return b[y];
+float main(float xx : X, uint y : Y) : SV_Target {
+  float c[4] = {xx, xx, xx, xx};
+  float x = 0;
+  if (xx > 2.5)
+    x = a[y];
+  else if (xx > 1.0)
+    x = b[y];
   else
-    return c[y];
+    x = c[y];
+
+  float x2 = 0;
+  if (xx < 1)
+    x2 = b[y+1];
+  else
+    x2 = a[y-1];
+
+  return x + y;
+
 }
