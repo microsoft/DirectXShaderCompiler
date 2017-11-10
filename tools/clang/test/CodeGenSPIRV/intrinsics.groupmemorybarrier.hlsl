1472fcb3304b7dc97af4d20bf77ba3b9044ee97f
Ehsan Nasiri <ehsannas@gmail.com>
2017-11-10 14:25:40 -0500

    Fix shader kind in tests.

--- tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrier.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrier.hlsl
@@ -1,4 +1,4 @@
-// Run: %dxc -T vs_6_0 -E main
+// Run: %dxc -T cs_6_0 -E main
 
 void main() {
 // CHECK: OpMemoryBarrier %uint_2 %uint_256
--- tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrierwithgroupsync.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrierwithgroupsync.hlsl
@@ -1,4 +1,4 @@
-// Run: %dxc -T vs_6_0 -E main
+// Run: %dxc -T cs_6_0 -E main
 
 void main() {
 // CHECK: OpControlBarrier %uint_2 %uint_2 %uint_256
