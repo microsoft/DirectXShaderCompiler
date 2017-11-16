283537beb8efe3d3b32b8d5c3757f3f6f41944d5
Young Kim <kiyoung@microsoft.com>
2017-11-16 13:05:55 -0800

    Update dll version and dxc version

--- cmake/modules/AddLLVM.cmake
+++ cmake/modules/AddLLVM.cmake
@@ -960,14 +960,14 @@ function(hlsl_update_product_ver RC_INTERNAL_NAME)
                  "RC_COMPANY_NAME=\"Microsoft(r) Corporation\""
                  "RC_VERSION_FIELD_1=0"
                  "RC_VERSION_FIELD_2=2017"
-                 "RC_VERSION_FIELD_3=10"
+                 "RC_VERSION_FIELD_3=11"
                  "RC_VERSION_FIELD_4=0"
-                 "RC_FILE_VERSION=\"0.2017.10.0\""
+                 "RC_FILE_VERSION=\"0.2017.11.0\""
                  "RC_FILE_DESCRIPTION=\"DirectX Compiler - Out Of Band\""
                  "RC_INTERNAL_NAME=\"${RC_INTERNAL_NAME}\""
                  "RC_COPYRIGHT=\"(c) Microsoft Corporation. All rights reserved.\""
                  "RC_PRODUCT_NAME=\"Microsoft(r) DirectX for Windows(r) - Out Of Band\""
-                 "RC_PRODUCT_VERSION=\"0.2017.10.0\"")
+                 "RC_PRODUCT_VERSION=\"0.2017.11.0\"")
   endif (HLSL_ENABLE_FIXED_VER)
 endfunction(hlsl_update_product_ver)
 # HLSL Change Ends
