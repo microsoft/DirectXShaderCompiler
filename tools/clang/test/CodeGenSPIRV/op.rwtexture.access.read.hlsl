76312d432966a75959eaa5f4929262797b7b8a9e
Lei Zhang <antiagainst@google.com>
2017-12-01 13:09:09 -0500

    [spirv] Fix OpImageRead return type (#868)
    OpImageRead should return a 4-component vector.

--- tools/clang/test/CodeGenSPIRV/op.rwtexture.access.read.hlsl
+++ tools/clang/test/CodeGenSPIRV/op.rwtexture.access.read.hlsl
@@ -12,18 +12,21 @@ RWTexture2DArray <int3>   t5;
 void main() {
 
 // CHECK:      [[t1:%\d+]] = OpLoad %type_1d_image %t1
-// CHECK-NEXT: [[img1:%\d+]] = OpImageRead %float [[t1]] %uint_0 None
-// CHECK-NEXT: OpStore %a [[img1]]
+// CHECK-NEXT: [[img1:%\d+]] = OpImageRead %v4float [[t1]] %uint_0 None
+// CHECK-NEXT: [[r1:%\d+]] = OpCompositeExtract %float [[img1]] 0
+// CHECK-NEXT: OpStore %a [[r1]]
   float  a = t1[0];
 
 // CHECK-NEXT: [[t2:%\d+]] = OpLoad %type_2d_image %t2
-// CHECK-NEXT: [[img2:%\d+]] = OpImageRead %v2int [[t2]] [[c01]] None
-// CHECK-NEXT: OpStore %b [[img2]]
+// CHECK-NEXT: [[img2:%\d+]] = OpImageRead %v4int [[t2]] [[c01]] None
+// CHECK-NEXT: [[r2:%\d+]] = OpVectorShuffle %v2int [[img2]] [[img2]] 0 1
+// CHECK-NEXT: OpStore %b [[r2]]
   int2   b = t2[uint2(0,1)];
 
 // CHECK-NEXT: [[t3:%\d+]] = OpLoad %type_3d_image %t3
-// CHECK-NEXT: [[img3:%\d+]] = OpImageRead %v3uint [[t3]] [[c012]] None
-// CHECK-NEXT: OpStore %c [[img3]]
+// CHECK-NEXT: [[img3:%\d+]] = OpImageRead %v4uint [[t3]] [[c012]] None
+// CHECK-NEXT: [[r3:%\d+]] = OpVectorShuffle %v3uint [[img3]] [[img3]] 0 1 2
+// CHECK-NEXT: OpStore %c [[r3]]
   uint3  c = t3[uint3(0,1,2)];
 
 // CHECK-NEXT: [[t4:%\d+]] = OpLoad %type_1d_image_array %t4
@@ -32,7 +35,8 @@ void main() {
   float4 d = t4[uint2(0,1)];
 
 // CHECK-NEXT: [[t5:%\d+]] = OpLoad %type_2d_image_array %t5
-// CHECK-NEXT: [[img5:%\d+]] = OpImageRead %v3int [[t5]] [[c012]] None
-// CHECK-NEXT: OpStore %e [[img5]]
+// CHECK-NEXT: [[img5:%\d+]] = OpImageRead %v4int [[t5]] [[c012]] None
+// CHECK-NEXT: [[r5:%\d+]] = OpVectorShuffle %v3int [[img5]] [[img5]] 0 1 2
+// CHECK-NEXT: OpStore %e [[r5]]
   int3   e = t5[uint3(0,1,2)];
 }
