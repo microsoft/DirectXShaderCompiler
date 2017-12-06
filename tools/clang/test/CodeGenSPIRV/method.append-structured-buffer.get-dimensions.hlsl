fb224f19b6a1fa1286bb0f345ecda2123f5b94f7
Lei Zhang <antiagainst@google.com>
2017-12-06 09:12:31 -0500

    [spirv] Fix structured buffer .GetDimensions() (#885)
    OpArrayLength takes a pointer to an OpTypeStruct whose last member
    is a run-time array, not a loaded struct value.

--- tools/clang/test/CodeGenSPIRV/method.append-structured-buffer.get-dimensions.hlsl
+++ tools/clang/test/CodeGenSPIRV/method.append-structured-buffer.get-dimensions.hlsl
@@ -10,9 +10,8 @@ AppendStructuredBuffer<S> buffer;
 
 void main() {
   uint numStructs, stride;
-  
-// CHECK:      [[buf:%\d+]] = OpLoad %type_AppendStructuredBuffer_S %buffer
-// CHECK-NEXT: [[len:%\d+]] = OpArrayLength %uint [[buf]] 0
+
+// CHECK:      [[len:%\d+]] = OpArrayLength %uint %buffer 0
 // CHECK-NEXT: OpStore %numStructs [[len]]
 // CHECK-NEXT: OpStore %stride %uint_64
   buffer.GetDimensions(numStructs, stride);
