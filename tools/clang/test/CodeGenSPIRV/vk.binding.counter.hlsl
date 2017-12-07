d7a96e9e80ce1cbb442ffb87bf0fd3d4ac67a96f
Lei Zhang <antiagainst@google.com>
2017-12-07 17:02:16 -0500

    [spirv] Always emit a counter variable for RWStructuredBuffer (#896)
    This will make handling RWStructuredBuffer aliasing legalization
    easier later.

--- tools/clang/test/CodeGenSPIRV/vk.binding.counter.hlsl
+++ tools/clang/test/CodeGenSPIRV/vk.binding.counter.hlsl
@@ -10,6 +10,9 @@ RWStructuredBuffer<S> mySBuffer1;
 // CHECK:      OpDecorate %mySBuffer1 DescriptorSet 3
 // CHECK-NEXT: OpDecorate %mySBuffer1 Binding 5
 
+// CHECK:      OpDecorate %counter_var_mySBuffer1 DescriptorSet 3
+// CHECK-NEXT: OpDecorate %counter_var_mySBuffer1 Binding 10
+
 // :register + vk::counter_binding
 [[vk::counter_binding(20)]]
 AppendStructuredBuffer<S> myASBuffer1 : register(u1, space1);
@@ -28,9 +31,6 @@ RWStructuredBuffer<S> mySBuffer2;
 // CHECK:      OpDecorate %mySBuffer2 DescriptorSet 0
 // CHECK-NEXT: OpDecorate %mySBuffer2 Binding 1
 
-// CHECK:      OpDecorate %counter_var_mySBuffer1 DescriptorSet 3
-// CHECK-NEXT: OpDecorate %counter_var_mySBuffer1 Binding 10
-
 // CHECK-NEXT: OpDecorate %myASBuffer1 DescriptorSet 1
 // CHECK-NEXT: OpDecorate %myASBuffer1 Binding 1
 
@@ -42,19 +42,19 @@ AppendStructuredBuffer<S> myASBuffer2 : register(u3, space2);
 // CHECK-NEXT: OpDecorate %myCSBuffer1 DescriptorSet 0
 // CHECK-NEXT: OpDecorate %myCSBuffer1 Binding 0
 
+// CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 DescriptorSet 0
+// CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 Binding 3
+
 // CHECK-NEXT: OpDecorate %counter_var_myASBuffer2 DescriptorSet 2
 // CHECK-NEXT: OpDecorate %counter_var_myASBuffer2 Binding 0
 
 // none + none
 ConsumeStructuredBuffer<S> myCSBuffer2;
 // CHECK-NEXT: OpDecorate %myCSBuffer2 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %myCSBuffer2 Binding 3
+// CHECK-NEXT: OpDecorate %myCSBuffer2 Binding 4
 
 // CHECK-NEXT: OpDecorate %counter_var_myCSBuffer2 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %counter_var_myCSBuffer2 Binding 4
-
-// CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 Binding 5
+// CHECK-NEXT: OpDecorate %counter_var_myCSBuffer2 Binding 5
 
 float4 main() : SV_Target {
     uint a = mySBuffer1.IncrementCounter();
