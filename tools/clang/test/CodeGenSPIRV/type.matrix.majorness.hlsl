cfb12da44861691ba39deedd88aa31c00ab9b5cc
Ehsan Nasiri <ehsannas@gmail.com>
2017-12-18 14:08:05 -0500

    Add line number to CHECK in tests.

--- tools/clang/test/CodeGenSPIRV/type.matrix.majorness.hlsl
+++ tools/clang/test/CodeGenSPIRV/type.matrix.majorness.hlsl
@@ -1,18 +1,18 @@
 // Run: %dxc -T ps_6_0 -E main
 
-// CHECK: warning: row_major attribute for stand-alone matrix is not supported
+// CHECK: 4:20: warning: row_major attribute for stand-alone matrix is not supported
 row_major float2x3 grMajorMat;
-// CHECK: warning: column_major attribute for stand-alone matrix is not supported
+// CHECK: 6:23: warning: column_major attribute for stand-alone matrix is not supported
 column_major float2x3 gcMajorMat;
 
-// CHECK: warning: row_major attribute for stand-alone matrix is not supported
+// CHECK: 9:27: warning: row_major attribute for stand-alone matrix is not supported
 static row_major float2x3 gsrMajorMat;
-// CHECK: warning: column_major attribute for stand-alone matrix is not supported
+// CHECK: 11:30: warning: column_major attribute for stand-alone matrix is not supported
 static column_major float2x3 gscMajorMat;
 
 void main() {
-  // CHECK: warning: row_major attribute for stand-alone matrix is not supported
+  // CHECK: 15:22: warning: row_major attribute for stand-alone matrix is not supported
   row_major float2x3 rMajorMat;
-  // CHECK: warning: column_major attribute for stand-alone matrix is not supported
+  // CHECK: 17:25: warning: column_major attribute for stand-alone matrix is not supported
   column_major float2x3 cMajorMat;
 }
