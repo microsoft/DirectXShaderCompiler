8c5a38ba22b9f126049f75152f554d9a45478600
Ehsan <ehsann@google.com>
2017-12-18 14:07:45 -0500

    [spirv] Fix Memory Barrier mappings to SPIR-V. (#917)

--- tools/clang/test/CodeGenSPIRV/intrinsics.devicememorybarrierwithgroupsync.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.devicememorybarrierwithgroupsync.hlsl
@@ -1,6 +1,10 @@
 // Run: %dxc -T cs_6_0 -E main
 
+// Execution scope : Workgroup = 0x2 = 2
+// Memory scope : Device = 0x1 = 1
+// Semantics: ImageMemory | UniformMemory | AcquireRelease = 0x800 | 0x40 | 0x8 = 2120
+
 void main() {
-// CHECK: OpControlBarrier %uint_1 %uint_1 %uint_512
+// CHECK: OpControlBarrier %uint_2 %uint_1 %uint_2120
   DeviceMemoryBarrierWithGroupSync();
 }
--- tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrierwithgroupsync.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.groupmemorybarrierwithgroupsync.hlsl
@@ -1,6 +1,10 @@
 // Run: %dxc -T cs_6_0 -E main
 
+// Execution scope : Workgroup = 0x2 = 2
+// Memory scope : Workgroup = 0x2 = 2
+// Semantics: WorkgroupMemory | AcquireRelease = 0x100 | 0x8 = 264
+
 void main() {
-// CHECK: OpControlBarrier %uint_2 %uint_2 %uint_256
+// CHECK: OpControlBarrier %uint_2 %uint_2 %uint_264
   GroupMemoryBarrierWithGroupSync();
 }
