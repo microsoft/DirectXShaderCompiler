8ffe3f5fd995b6116d67d35888f8357a930b381b
Lei Zhang <antiagainst@google.com>
2018-02-09 14:31:12 -0500

    Add an option to allow pre-expand token-pasting (##) operands (#1065)
    The fxc compiler evaluates both operands before performing the
    token-pasting operation. But the default Clang way is to follow
    C standard, which token-pastes operands as-is, without any
    pre-expanding.
    
    This commit adds support for the fxc behavior via the command
    line option: -flegacy-macro-expansion.
    
    Fixes https://github.com/Microsoft/DirectXShaderCompiler/issues/1005

--- lib/DxcSupport/HLSLOptions.cpp
+++ lib/DxcSupport/HLSLOptions.cpp
@@ -406,6 +406,7 @@ int ReadDxcOpts(const OptTable *optionTable, unsigned flagsToInclude,
   opts.DisassembleInstNumbers = Args.hasFlag(OPT_Ni, OPT_INVALID, false);
   opts.DisassembleByteOffset = Args.hasFlag(OPT_No, OPT_INVALID, false);
   opts.DisaseembleHex = Args.hasFlag(OPT_Lx, OPT_INVALID, false);
+  opts.LegacyMacroExpansion = Args.hasFlag(OPT_flegacy_macro_expansion, OPT_INVALID, false);
 
   if (opts.DefaultColMajor && opts.DefaultRowMajor) {
     errors << "Cannot specify /Zpr and /Zpc together, use /? to get usage information";
