ac82f0355cf065393f5c2a14e7548fc985c75151
Tex Riddell <texr@microsoft.com>
2018-02-13 19:07:25 -0800

    Treat builtin structs like normal UDT except when necessary for intrinsic.

--- tools/clang/test/CodeGenHLSL/quick-test/raytracing_attr_struct.hlsl
+++ tools/clang/test/CodeGenHLSL/quick-test/raytracing_attr_struct.hlsl
@@ -1,6 +1,6 @@
 // RUN: %dxc -E main -T lib_6_3 %s | FileCheck %s
 
-//CHECK: User define type intrinsic arg must be struct
+//CHECK: User defined type intrinsic arg must be struct
 
 float main(float THit : t, uint HitKind : h, float2 f2 : F) {
   return ReportHit(THit, HitKind, f2);
