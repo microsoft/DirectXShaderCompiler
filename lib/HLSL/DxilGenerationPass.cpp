f174990c6e49ee3ca19ba50f13c1a8f815aa9e63
Young Kim <youngseongkim93@gmail.com>
2018-03-17 08:45:46 -0700

    Fix checking LoadInput for GetAttributeAtVertex for no Opt (#1149)

--- lib/HLSL/DxilGenerationPass.cpp
+++ lib/HLSL/DxilGenerationPass.cpp
@@ -1776,7 +1776,8 @@ public:
                 static_cast<IntrinsicOp>(hlsl::GetHLOpcode(CI));
             if (evalOp == IntrinsicOp::IOP_EvaluateAttributeAtSample ||
                 evalOp == IntrinsicOp::IOP_EvaluateAttributeCentroid ||
-                evalOp == IntrinsicOp::IOP_EvaluateAttributeSnapped) {
+                evalOp == IntrinsicOp::IOP_EvaluateAttributeSnapped ||
+                evalOp == IntrinsicOp::IOP_GetAttributeAtVertex) {
               EvalFunctionCalls.push_back(CI);
             }
           }
