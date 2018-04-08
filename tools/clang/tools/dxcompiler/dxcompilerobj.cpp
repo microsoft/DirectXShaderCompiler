1ce96ddfb59ee34e566a98cf8983d96edf47cef0
Lei Zhang <antiagainst@google.com>
2018-04-08 12:50:57 -0400

    [spirv] Add support for DX layout rules (#1198)
    These layout rules can be turned on with -fvk-use-dx-layout.
    For both cbuffer/tbuffer and structured buffers.

--- tools/clang/tools/dxcompiler/dxcompilerobj.cpp
+++ tools/clang/tools/dxcompiler/dxcompilerobj.cpp
@@ -470,6 +470,7 @@ public:
           spirvOpts.disableValidation = opts.DisableValidation;
           spirvOpts.invertY = opts.VkInvertY;
           spirvOpts.useGlslLayout = opts.VkUseGlslLayout;
+          spirvOpts.useDxLayout = opts.VkUseDxLayout;
           spirvOpts.enableReflect = opts.SpvEnableReflect;
           spirvOpts.ignoreUnusedResources = opts.VkIgnoreUnusedResources;
           spirvOpts.defaultRowMajor = opts.DefaultRowMajor;
