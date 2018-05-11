7e3fef5ccaa52fa0c3c13e51e06ff34ed6cc1b28
Lei Zhang <antiagainst@google.com>
2018-05-11 15:45:34 -0400

    [spirv] Accept parentheses around LHS in assignments (#1280)

--- tools/clang/test/CodeGenSPIRV/op.vector.swizzle.const-scalar.hlsl
+++ tools/clang/test/CodeGenSPIRV/op.vector.swizzle.const-scalar.hlsl
@@ -3,6 +3,7 @@
 // CHECK:  [[v4f1:%\d+]] = OpConstantComposite %v4float %float_1 %float_1 %float_1 %float_1
 // CHECK: [[v4f25:%\d+]] = OpConstantComposite %v4float %float_2_5 %float_2_5 %float_2_5 %float_2_5
 // CHECK:  [[v4f0:%\d+]] = OpConstantComposite %v4float %float_0 %float_0 %float_0 %float_0
+// CHECK:  [[v3f1:%\d+]] = OpConstantComposite %v3float %float_1 %float_1 %float_1
 
 float4 main(float input: INPUT) : SV_Target {
 
@@ -21,6 +22,12 @@ float4 main(float input: INPUT) : SV_Target {
 // CHECK-NEXT: [[rhs:%\d+]] = OpCompositeConstruct %v4float {{%\d+}} {{%\d+}} [[cc0]] [[cc1]]
 // CHECK-NEXT:                OpStore %d [[rhs]]
   float4 d = float4(input, input, 0.0.xx);
+
+// CHECK:        [[e:%\d+]] = OpLoad %v4float %e
+// CHECK-NEXT: [[val:%\d+]] = OpVectorShuffle %v4float [[e]] [[v3f1]] 4 5 6 3
+// CHECK-NEXT:                OpStore %e [[val]]
+  float4 e;
+  (e.xyz)  = 1.0.xxx; // Parentheses
 
 // CHECK: OpReturnValue [[v4f0]]
   return 0.0.xxxx;
