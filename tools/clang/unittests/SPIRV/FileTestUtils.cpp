c049aa042aeb0109f7cd912e8e779b5286c9da6a
Lei Zhang <antiagainst@google.com>
2018-06-27 16:23:23 -0400

    [spirv] Update SPIRV-Tools for not merging resource types
    Keeping the original struct type is important for some reflection
    workflows.
    
    The validation check for layout is alo included. But we disable
    it for now and will re-enable it after fixing all the failures.
    
    Fixes https://github.com/Microsoft/DirectXShaderCompiler/issues/1364

--- tools/clang/unittests/SPIRV/FileTestUtils.cpp
+++ tools/clang/unittests/SPIRV/FileTestUtils.cpp
@@ -38,6 +38,8 @@ bool validateSpirvBinary(spv_target_env env, std::vector<uint32_t> &binary,
                          bool relaxLogicalPointer) {
   spvtools::ValidatorOptions options;
   options.SetRelaxLogicalPointer(relaxLogicalPointer);
+  // TODO: Fix the following to enable validating layout.
+  options.SetRelaxBlockLayout(true);
   spvtools::SpirvTools spirvTools(env);
   spirvTools.SetMessageConsumer(
       [](spv_message_level_t, const char *, const spv_position_t &,
