e3966770a19464bda5af42d99f0bd102082ed337
Vishal C Sharma <visshar@microsoft.com>
2018-07-11 15:19:37 -0700

    Accomodate CR comments

--- lib/HLSL/DxilLegalizeSampleOffsetPass.cpp
+++ lib/HLSL/DxilLegalizeSampleOffsetPass.cpp
@@ -153,6 +153,7 @@ void DxilLegalizeSampleOffsetPass::FinalCheck(
 void DxilLegalizeSampleOffsetPass::TryUnrollLoop(
     std::vector<Instruction *> &illegalOffsets, Function &F) {
   legacy::FunctionPassManager PM(F.getParent());
+  // Scalarize aggregates as mem2reg only applies on scalars.
   PM.add(createSROAPass());
   // Always need mem2reg for simplify illegal offsets.
   PM.add(createPromoteMemoryToRegisterPass());
