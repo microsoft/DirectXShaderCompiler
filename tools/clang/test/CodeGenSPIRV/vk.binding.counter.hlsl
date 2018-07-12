b28947319a1da4aabde5abea8fb60a78d9a5700f
Lei Zhang <antiagainst@google.com>
2018-07-12 15:35:41 -0400

    [spirv] Implement SPIR-V side for space-only :register()

--- tools/clang/test/CodeGenSPIRV/vk.binding.counter.hlsl
+++ tools/clang/test/CodeGenSPIRV/vk.binding.counter.hlsl
@@ -7,17 +7,20 @@ struct S {
 // vk::binding + vk::counter_binding
 [[vk::binding(5, 3), vk::counter_binding(10)]]
 RWStructuredBuffer<S> mySBuffer1;
-// CHECK:      OpDecorate %mySBuffer1 DescriptorSet 3
-// CHECK-NEXT: OpDecorate %mySBuffer1 Binding 5
-
 // CHECK:      OpDecorate %counter_var_mySBuffer1 DescriptorSet 3
 // CHECK-NEXT: OpDecorate %counter_var_mySBuffer1 Binding 10
 
-// :register + vk::counter_binding
+// :register(xX, spaceY) + vk::counter_binding
 [[vk::counter_binding(20)]]
 AppendStructuredBuffer<S> myASBuffer1 : register(u1, space1);
 // CHECK:      OpDecorate %counter_var_myASBuffer1 DescriptorSet 1
 // CHECK-NEXT: OpDecorate %counter_var_myASBuffer1 Binding 20
+
+// :register(spaceY) + vk::counter_binding
+[[vk::counter_binding(15)]]
+RWStructuredBuffer<S> mySBuffer3 : register(space3);
+// CHECK:      OpDecorate %counter_var_mySBuffer3 DescriptorSet 3
+// CHECK-NEXT: OpDecorate %counter_var_mySBuffer3 Binding 15
 
 // none + vk::counter_binding
 [[vk::counter_binding(2)]]
@@ -28,32 +31,22 @@ ConsumeStructuredBuffer<S> myCSBuffer1;
 // vk::binding + none
 [[vk::binding(1)]]
 RWStructuredBuffer<S> mySBuffer2;
-// CHECK:      OpDecorate %mySBuffer2 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %mySBuffer2 Binding 1
-
-// CHECK-NEXT: OpDecorate %myASBuffer1 DescriptorSet 1
-// CHECK-NEXT: OpDecorate %myASBuffer1 Binding 1
-
-// :register + none
-AppendStructuredBuffer<S> myASBuffer2 : register(u3, space2);
-// CHECK-NEXT: OpDecorate %myASBuffer2 DescriptorSet 2
-// CHECK-NEXT: OpDecorate %myASBuffer2 Binding 3
-
-// CHECK-NEXT: OpDecorate %myCSBuffer1 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %myCSBuffer1 Binding 0
-
-// CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 DescriptorSet 0
+// CHECK:      OpDecorate %counter_var_mySBuffer2 DescriptorSet 0
 // CHECK-NEXT: OpDecorate %counter_var_mySBuffer2 Binding 3
 
-// CHECK-NEXT: OpDecorate %counter_var_myASBuffer2 DescriptorSet 2
+// :register(xX, spaceY) + none
+AppendStructuredBuffer<S> myASBuffer2 : register(u3, space2);
+// CHECK:      OpDecorate %counter_var_myASBuffer2 DescriptorSet 2
 // CHECK-NEXT: OpDecorate %counter_var_myASBuffer2 Binding 0
+
+// :register(spaceY) + None
+ConsumeStructuredBuffer<S> myCSBuffer3 : register(space2);
+// CHECK:      OpDecorate %counter_var_myCSBuffer3 DescriptorSet 2
+// CHECK-NEXT: OpDecorate %counter_var_myCSBuffer3 Binding 2
 
 // none + none
 ConsumeStructuredBuffer<S> myCSBuffer2;
-// CHECK-NEXT: OpDecorate %myCSBuffer2 DescriptorSet 0
-// CHECK-NEXT: OpDecorate %myCSBuffer2 Binding 4
-
-// CHECK-NEXT: OpDecorate %counter_var_myCSBuffer2 DescriptorSet 0
+// CHECK:      OpDecorate %counter_var_myCSBuffer2 DescriptorSet 0
 // CHECK-NEXT: OpDecorate %counter_var_myCSBuffer2 Binding 5
 
 float4 main() : SV_Target {
