55ba4fce38754b2da1cac0a554fe454e5b81c1bd
Ehsan Nasiri <ehsann@google.com>
2018-08-21 09:33:04 -0400

    Use list-initialization rather than copy initialization.
    Fixes the build failure on Linux.
    
    There's a full discussion about this subject here:
    https://stackoverflow.com/questions/21708606/why-does-an-in-place-member-initialization-use-a-copy-constructor-in-c11

--- include/dxc/Support/microcom.h
+++ include/dxc/Support/microcom.h
@@ -77,7 +77,7 @@ public:
 };
 
 #define DXC_MICROCOM_REF_FIELD(m_dwRef)                                        \
-  volatile std::atomic<llvm::sys::cas_flag> m_dwRef = 0;
+  volatile std::atomic<llvm::sys::cas_flag> m_dwRef = {0};
 #define DXC_MICROCOM_ADDREF_IMPL(m_dwRef)                                      \
   ULONG STDMETHODCALLTYPE AddRef() override {                                  \
     return (ULONG)++m_dwRef;                                                   \
@@ -107,7 +107,7 @@ void DxcCallDestructor(T *obj) {
 // The "TM" version keep an IMalloc field that, if not null, indicate
 // ownership of 'this' and of any allocations used during release.
 #define DXC_MICROCOM_TM_REF_FIELDS()                                           \
-  volatile std::atomic<llvm::sys::cas_flag> m_dwRef = 0;                       \
+  volatile std::atomic<llvm::sys::cas_flag> m_dwRef = {0};                     \
   CComPtr<IMalloc> m_pMalloc;
 
 #define DXC_MICROCOM_TM_ADDREF_RELEASE_IMPL()                                  \
