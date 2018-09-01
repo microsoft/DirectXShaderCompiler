a489105825aa29311d00feeb26cd35e9a5f8bc5a
Helena Kotas <hekotas@microsoft.com>
2018-08-31 18:15:17 -0700

    Implement Length property on constant arrays (FXC regression) (#1517)
    Implement Length property on constant arrays (FXC regression)
    
    The FXC compiler supports Length property on arrays. This change adds it
    to the DXC compiler for backwards compatibility. However, it is marked
    obsolete and in HLSL Version 2016 will report a warning. On higher versions
    it will result in an error to prompt the developer to either change the code 
    or to use the -HV flag for backwards compatibility.
    
    Fix HLSLVersion not initialized by default
    
    Adds RewriterTest::RunArrayLength tests which is currently disabled until we have 
    a way to set HLSLVersion on RewriterTests.

---
+++ tools/clang/test/HLSL/array-length.hlsl
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -fsyntax-only -ffreestanding -verify -HV 2016  %s
+
+float4 planes1[8];
+float4 planes2[3+2]; 
+
+struct S {
+    float4 planes[2]; 
+};
+
+[RootSignature("CBV(b0, space=0, visibility=SHADER_VISIBILITY_ALL)")]
+float main(S s:POSITION) : SV_Target {
+    float4 planes3[] = {{ 1.0, 2.0, 3.0, 4.0 }};
+    
+    int total = planes1.Length;	// expected-warning {{Length is deprecated}}
+    total += planes2.Length;  	// expected-warning {{Length is deprecated}}
+    total += planes3.Length;    // expected-warning {{Length is deprecated}}
+    total += s.planes.Length;   // expected-warning {{Length is deprecated}}
+
+    return total;
+}
