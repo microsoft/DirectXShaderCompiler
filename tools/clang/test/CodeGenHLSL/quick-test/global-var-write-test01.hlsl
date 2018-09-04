f743518811758c038df63c60ecbdc4f34001f212
Vishal C Sharma <visshar@microsoft.com>
2018-09-04 13:07:22 -0700

    Enable write to global vars with HLSLVersion <= 2016

---
+++ tools/clang/test/CodeGenHLSL/quick-test/global-var-write-test01.hlsl
@@ -0,0 +1,36 @@
+// RUN: %dxc -E main -T ps_6_0 -HV 2016 %s | FileCheck %s
+
+// Writing to globals only supported with HV <= 2016
+// CHECK: define void @main
+// CHECK: ret void
+
+float g_s;
+float4 g_v = float4(1.0, -1.0, 0.0, 1.0);
+int2x2 g_m;
+bool g_b = false;
+int g_a[5];
+float4 main(uint a
+            : A) : SV_Target {
+  // update global scalar
+  g_s = a;
+
+  // update global vector
+  g_v = float4(a + 1, a + 2, a + 3, a + 4);
+
+  // update global matrix
+  for (uint i = 0; i < 2; i++)
+    for (uint j = 0; j < 2; j++)
+      g_m[i][j] = a + i + j;
+
+  // update global array
+  for (uint i = 0; i < 5; i++)
+    g_a[i] = a + i;
+
+  // update global boolean
+  g_b = true;
+
+  return float4(g_s, g_s, g_s, g_s) +
+         g_v +
+         float4(g_m[0][0], g_m[0][1], g_m[1][0], g_m[1][1]) +
+         float4(g_a[0], g_a[1], g_a[2], g_a[3]);
+}
