48e28878abbccedcf552eca59363ef9b0477ff81
Lei Zhang <antiagainst@google.com>
2018-10-05 10:05:04 -0400

    [spirv] Update SPIRV-Tools for bug fixes (#1585)
    * Removed dangling OpDecorateId
    * Stopped scalarizing spec constant sized arrays
    * Added a bunch validation checks
    
    Fixes https://github.com/Microsoft/DirectXShaderCompiler/issues/1528

--- tools/clang/test/CodeGenSPIRV/primitive.lineadj.gs.hlsl
+++ tools/clang/test/CodeGenSPIRV/primitive.lineadj.gs.hlsl
@@ -2,5 +2,8 @@
 
 // CHECK: OpExecutionMode %main InputLinesAdjacency
 
+struct S { float4 val : VAL; };
+
 [maxvertexcount(3)]
-void main(lineadj in uint id[4] : VertexID) {}
+void main(lineadj in uint id[4] : VertexID, inout LineStream<S> outData) {
+}
