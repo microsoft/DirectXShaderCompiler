386ea9c9478c5fb3989dcf18b4ee9b4b49d9995b
Ehsan <ehsann@google.com>
2018-10-17 10:21:49 -0400

    [spirv] SpirvBuilder function and basic block methods.

--- tools/clang/lib/SPIRV/SpirvBuilder.cpp
+++ tools/clang/lib/SPIRV/SpirvBuilder.cpp
@@ -18,5 +18,73 @@ SpirvBuilder::SpirvBuilder(SpirvContext &ctx)
   module = new (context) SpirvModule;
 }
 
+SpirvFunction *SpirvBuilder::beginFunction(QualType returnType,
+                                           SourceLocation loc,
+                                           llvm::StringRef funcName) {
+  assert(!function && "found nested function");
+  function = new (context) SpirvFunction(
+      returnType, /*id*/ 0, spv::FunctionControlMask::MaskNone, loc, funcName);
+  return function;
+}
+
+SpirvFunctionParameter *SpirvBuilder::addFnParam(QualType ptrType,
+                                                 SourceLocation loc,
+                                                 llvm::StringRef name) {
+  assert(function && "found detached parameter");
+  auto *param = new (context) SpirvFunctionParameter(ptrType, /*id*/ 0, loc);
+  param->setDebugName(name);
+  function->addParameter(param);
+  return param;
+}
+
+SpirvVariable *SpirvBuilder::addFnVar(QualType valueType, SourceLocation loc,
+                                      llvm::StringRef name,
+                                      SpirvInstruction *init) {
+  assert(function && "found detached local variable");
+  auto *var = new (context) SpirvVariable(valueType, /*id*/ 0, loc,
+                                          spv::StorageClass::Function, init);
+  var->setDebugName(name);
+  function->addVariable(var);
+  return var;
+}
+
+void SpirvBuilder::endFunction() {
+  assert(function && "no active function");
+
+  // Move all basic blocks into the current function.
+  // TODO: we should adjust the order the basic blocks according to
+  // SPIR-V validation rules.
+  for (auto *bb : basicBlocks) {
+    function->addBasicBlock(bb);
+  }
+  basicBlocks.clear();
+
+  module->addFunction(function);
+  function = nullptr;
+  insertPoint = nullptr;
+}
+
+SpirvBasicBlock *SpirvBuilder::createBasicBlock(llvm::StringRef name) {
+  assert(function && "found detached basic block");
+  auto *bb = new (context) SpirvBasicBlock(/*id*/ 0, name);
+  basicBlocks.push_back(bb);
+  return bb;
+}
+
+void SpirvBuilder::addSuccessor(SpirvBasicBlock *successorBB) {
+  assert(insertPoint && "null insert point");
+  insertPoint->addSuccessor(successorBB);
+}
+
+void SpirvBuilder::setMergeTarget(SpirvBasicBlock *mergeLabel) {
+  assert(insertPoint && "null insert point");
+  insertPoint->setMergeTarget(mergeLabel);
+}
+
+void SpirvBuilder::setContinueTarget(SpirvBasicBlock *continueLabel) {
+  assert(insertPoint && "null insert point");
+  insertPoint->setContinueTarget(continueLabel);
+}
+
 } // end namespace spirv
 } // end namespace clang
