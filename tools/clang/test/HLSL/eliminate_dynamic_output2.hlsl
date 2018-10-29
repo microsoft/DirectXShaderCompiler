b551f372628fd58331eb679aff265b9e042df0e3
Adam Yang <31109344+adam-yang@users.noreply.github.com>
2018-10-29 16:30:20 -0700

    Make return value the first output element. (#1622)

--- tools/clang/test/HLSL/eliminate_dynamic_output2.hlsl
+++ tools/clang/test/HLSL/eliminate_dynamic_output2.hlsl
@@ -1,14 +1,14 @@
 // RUN: %dxc -Emain -Tvs_6_0 %s | %opt -S -hlsl-dxil-eliminate-output-dynamic | %FileCheck %s
 
 // CHECK-NOT: storeOutput.f32(i32 5, i32 0, i32 %i
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 0, i8 0
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 1, i8 0
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 2, i8 0
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 3, i8 0
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 0, i8 1
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 1, i8 1
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 2, i8 1
-// CHECK: storeOutput.f32(i32 5, i32 0, i32 3, i8 1
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 0, i8 0
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 1, i8 0
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 2, i8 0
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 3, i8 0
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 0, i8 1
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 1, i8 1
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 2, i8 1
+// CHECK: storeOutput.f32(i32 5, i32 1, i32 3, i8 1
 int  count;
 float4 c[16];
 
