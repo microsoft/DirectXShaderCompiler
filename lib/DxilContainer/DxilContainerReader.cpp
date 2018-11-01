60fe4a64d282d3818c00652544f0734552d9b074
Helena Kotas <hekotas@microsoft.com>
2018-11-01 10:49:15 -0700

    Fix build break (llvm namespace has been removed from DxilSubobject.h)

--- lib/DxilContainer/DxilContainerReader.cpp
+++ lib/DxilContainer/DxilContainerReader.cpp
@@ -41,7 +41,7 @@ void LoadSubobjectsFromRDAT(DxilSubobjects &subobjects, RDAT::SubobjectTableRead
     }
     case DXIL::SubobjectKind::SubobjectToExportsAssociation: {
       uint32_t NumExports = reader.GetSubobjectToExportsAssociation_NumExports();
-      std::vector<StringRef> Exports;
+      std::vector<llvm::StringRef> Exports;
       Exports.resize(NumExports);
       for (unsigned i = 0; i < NumExports; ++i) {
         Exports[i] = reader.GetSubobjectToExportsAssociation_Export(i);
