94e6a353ade3e6f4bb3a6f741bad990817fbb216
Ehsan Nasiri <ehsannas@gmail.com>
2018-11-23 00:16:57 -0500

    [spirv] Update sm6 tests (use uint for scope).

--- tools/clang/test/CodeGenSPIRV/sm6.wave-active-min.hlsl
+++ tools/clang/test/CodeGenSPIRV/sm6.wave-active-min.hlsl
@@ -20,12 +20,12 @@ void main(uint3 id: SV_DispatchThreadID) {
        int val3 = values[x].val3;
 
 // CHECK:      [[val1:%\d+]] = OpLoad %v4uint %val1
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformUMin %v4uint %int_3 Reduce [[val1]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformUMin %v4uint %uint_3 Reduce [[val1]]
     values[x].val1 = WaveActiveMin(val1);
 // CHECK:      [[val2:%\d+]] = OpLoad %v2float %val2
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformFMin %v2float %int_3 Reduce [[val2]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformFMin %v2float %uint_3 Reduce [[val2]]
     values[x].val2 = WaveActiveMin(val2);
 // CHECK:      [[val3:%\d+]] = OpLoad %int %val3
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformSMin %int %int_3 Reduce [[val3]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformSMin %int %uint_3 Reduce [[val3]]
     values[x].val3 = WaveActiveMin(val3);
 }
