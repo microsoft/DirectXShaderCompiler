00cf7446ecf3a08e39147e751bf2527327a7854e
Ehsan Nasiri <ehsannas@gmail.com>
2018-11-23 00:16:57 -0500

    [spirv] Update sm6 tests (use uint for scope).

--- tools/clang/test/CodeGenSPIRV/sm6.wave-active-ballot.hlsl
+++ tools/clang/test/CodeGenSPIRV/sm6.wave-active-ballot.hlsl
@@ -15,6 +15,6 @@ RWStructuredBuffer<S> values;
 void main(uint3 id: SV_DispatchThreadID) {
     uint x = id.x;
 // CHECK:      [[cmp:%\d+]] = OpIEqual %bool {{%\d+}} %uint_2
-// CHECK-NEXT:     {{%\d+}} = OpGroupNonUniformBallot %v4uint %int_3 [[cmp]]
+// CHECK-NEXT:     {{%\d+}} = OpGroupNonUniformBallot %v4uint %uint_3 [[cmp]]
     values[x].res = WaveActiveBallot(values[x].val == 2);
 }
--- tools/clang/test/CodeGenSPIRV/sm6.wave-read-lane-first.hlsl
+++ tools/clang/test/CodeGenSPIRV/sm6.wave-read-lane-first.hlsl
@@ -21,12 +21,12 @@ void main(uint3 id: SV_DispatchThreadID) {
     float val3 = values[x].val3;
 
 // CHECK:      [[val1:%\d+]] = OpLoad %v4uint %val1
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %v4uint %int_3 [[val1]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %v4uint %uint_3 [[val1]]
     values[x].val1 = WaveReadLaneFirst(val1);
 // CHECK:      [[val2:%\d+]] = OpLoad %v2int %val2
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %v2int %int_3 [[val2]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %v2int %uint_3 [[val2]]
     values[x].val2 = WaveReadLaneFirst(val2);
 // CHECK:      [[val3:%\d+]] = OpLoad %float %val3
-// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %float %int_3 [[val3]]
+// CHECK-NEXT:      {{%\d+}} = OpGroupNonUniformBroadcastFirst %float %uint_3 [[val3]]
     values[x].val3 = WaveReadLaneFirst(val3);
 }
