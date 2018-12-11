5890654c55b27fdaa2fbbed87b5fb83ecb85386f
Steven Perron <stevenperron@google.com>
2018-12-11 15:50:53 -0500

    Update tests.

--- tools/clang/test/CodeGenSPIRV/intrinsics.allmemorybarrier.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.allmemorybarrier.hlsl
@@ -1,9 +1,9 @@
 // Run: %dxc -T cs_6_0 -E main
 
 // Memory scope : Device = 0x1 = 1
-// Semantics: ImageMemory | AtomicCounterMemory | UniformMemory | WorkgroupMemory | AcquireRelease = 0x800 | 0x400 | 0x40 | 0x100 | 0x8 = 3400
+// Semantics: ImageMemory | UniformMemory | WorkgroupMemory | AcquireRelease = 0x800 | 0x40 | 0x100 | 0x8 = 2376
 
 void main() {
-// CHECK: OpMemoryBarrier %uint_1 %uint_3400
+// CHECK: OpMemoryBarrier %uint_1 %uint_2376
   AllMemoryBarrier();
 }
