a49715de951627662ddf704a448c5230addb8eb8
Ehsan Nasiri <ehsannas@gmail.com>
2018-12-17 12:14:03 -0500

    [spirv] The creator of a BB shouldn't know its result-id.

--- tools/clang/unittests/SPIRV/SpirvBasicBlockTest.cpp
+++ tools/clang/unittests/SPIRV/SpirvBasicBlockTest.cpp
@@ -17,34 +17,34 @@ using namespace clang::spirv;
 namespace {
 
 TEST(SpirvBasicBlockTest, CheckName) {
-  SpirvBasicBlock bb(0, "myBasicBlock");
+  SpirvBasicBlock bb("myBasicBlock");
   EXPECT_EQ(bb.getName(), "myBasicBlock");
 }
 
 TEST(SpirvBasicBlockTest, CheckResultId) {
-  SpirvBasicBlock bb(0, "myBasicBlock");
+  SpirvBasicBlock bb("myBasicBlock");
   bb.setResultId(5);
   EXPECT_EQ(bb.getResultId(), 5);
 }
 
 TEST(SpirvBasicBlockTest, CheckMergeTarget) {
-  SpirvBasicBlock bb1(0, "bb1");
-  SpirvBasicBlock bb2(0, "bb2");
+  SpirvBasicBlock bb1("bb1");
+  SpirvBasicBlock bb2("bb2");
   bb1.setMergeTarget(&bb2);
   EXPECT_EQ(bb1.getMergeTarget(), &bb2);
 }
 
 TEST(SpirvBasicBlockTest, CheckContinueTarget) {
-  SpirvBasicBlock bb1(0, "bb1");
-  SpirvBasicBlock bb2(0, "bb2");
+  SpirvBasicBlock bb1("bb1");
+  SpirvBasicBlock bb2("bb2");
   bb1.setContinueTarget(&bb2);
   EXPECT_EQ(bb1.getContinueTarget(), &bb2);
 }
 
 TEST(SpirvBasicBlockTest, CheckSuccessors) {
-  SpirvBasicBlock bb1(0, "bb1");
-  SpirvBasicBlock bb2(0, "bb2");
-  SpirvBasicBlock bb3(0, "bb3");
+  SpirvBasicBlock bb1("bb1");
+  SpirvBasicBlock bb2("bb2");
+  SpirvBasicBlock bb3("bb3");
   bb1.addSuccessor(&bb2);
   bb1.addSuccessor(&bb3);
   auto successors = bb1.getSuccessors();
@@ -53,42 +53,42 @@ TEST(SpirvBasicBlockTest, CheckSuccessors) {
 }
 
 TEST(SpirvBasicBlockTest, CheckTerminatedByKill) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvKill kill({});
   bb.addInstruction(&kill);
   EXPECT_TRUE(bb.hasTerminator());
 }
 
 TEST(SpirvBasicBlockTest, CheckTerminatedByBranch) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvBranch branch({}, nullptr);
   bb.addInstruction(&branch);
   EXPECT_TRUE(bb.hasTerminator());
 }
 
 TEST(SpirvBasicBlockTest, CheckTerminatedByBranchConditional) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvBranchConditional branch({}, nullptr, nullptr, nullptr);
   bb.addInstruction(&branch);
   EXPECT_TRUE(bb.hasTerminator());
 }
 
 TEST(SpirvBasicBlockTest, CheckTerminatedByReturn) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvReturn returnInstr({});
   bb.addInstruction(&returnInstr);
   EXPECT_TRUE(bb.hasTerminator());
 }
 
 TEST(SpirvBasicBlockTest, CheckTerminatedByUnreachable) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvUnreachable unreachable({});
   bb.addInstruction(&unreachable);
   EXPECT_TRUE(bb.hasTerminator());
 }
 
 TEST(SpirvBasicBlockTest, CheckNotTerminated) {
-  SpirvBasicBlock bb(0, "bb");
+  SpirvBasicBlock bb("bb");
   SpirvLoad load({}, 0, {}, nullptr);
   bb.addInstruction(&load);
   EXPECT_FALSE(bb.hasTerminator());
