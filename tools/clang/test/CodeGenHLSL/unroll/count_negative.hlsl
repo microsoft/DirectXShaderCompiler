b3d766ae56cb0c6a1d86e58d1e7f60006b2fcd86
Adam Yang <hanbyang@microsoft.com>
2018-12-19 10:36:01 -0800

    Simplified more tests

--- tools/clang/test/CodeGenHLSL/unroll/count_negative.hlsl
+++ tools/clang/test/CodeGenHLSL/unroll/count_negative.hlsl
@@ -3,24 +3,13 @@
 // CHECK: attribute 'unroll' must have a uint literal argument
 // CHECK-NOT: @main
 
-AppendStructuredBuffer<float> buf0;
-AppendStructuredBuffer<float> buf1;
-AppendStructuredBuffer<float> buf2;
-
 uint g_cond;
-uint g_cond2;
+
 float main() : SV_Target {
-
-  AppendStructuredBuffer<float> buf[3] = {
-    buf0, buf1, buf2
-  };
-
+  float result = 0;
   [unroll(-1)]
   for (int i = 0; i < g_cond; i++) {
-    if (i == g_cond2) {
-      buf[i].Append(i);
-      return 1;
-    }
+    result += i;
   }
 
   return 0;
--- tools/clang/test/CodeGenHLSL/unroll/count_zero.hlsl
+++ tools/clang/test/CodeGenHLSL/unroll/count_zero.hlsl
@@ -2,27 +2,14 @@
 
 // CHECK-DAG: Could not unroll loop.
 // CHECK-NOT: @main
-
-AppendStructuredBuffer<float> buf0;
-AppendStructuredBuffer<float> buf1;
-AppendStructuredBuffer<float> buf2;
-
 uint g_cond;
-uint g_cond2;
 float main() : SV_Target {
 
-  AppendStructuredBuffer<float> buf[3] = {
-    buf0, buf1, buf2
-  };
-
+  float result = 0;
   [unroll(0)]
   for (int i = 0; i < g_cond; i++) {
-    if (i == g_cond2) {
-      buf[i].Append(i);
-      return 1;
-    }
+    result += i;
   }
-
-  return 0;
+  return result;
 }
 
