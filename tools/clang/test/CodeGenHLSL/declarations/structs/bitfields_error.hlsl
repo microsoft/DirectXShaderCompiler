305c2f3fa6aa88a1717e4621de38bb4726b2c2a5
Tristan Labelle <trlabell@microsoft.com>
2019-01-15 13:09:15 -0800

    Start organizing codegen tests.

--- tools/clang/test/CodeGenHLSL/declarations/structs/bitfields_error.hlsl
+++ tools/clang/test/CodeGenHLSL/declarations/structs/bitfields_error.hlsl
@@ -1,6 +1,6 @@
-// RUN: %dxc /T ps_6_0 /E main %s | FileCheck %s
+// RUN: %dxc /T vs_6_0 /E main %s | FileCheck %s
 
 // CHECK: error: bitfields are not supported in HLSL
 
 struct Struct { uint field : 1; };
-float main() : SV_Target { return 0; }
+void main() {}
