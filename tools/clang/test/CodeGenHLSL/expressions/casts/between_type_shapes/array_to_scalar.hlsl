77f75a613cc270880a4e401c9c1f04321b725b0e
Tristan Labelle <trlabell@microsoft.com>
2019-01-17 08:31:25 -0800

    Added exhaustive tests for implicit conversions and explicit casts between type shapes.

--- tools/clang/test/CodeGenHLSL/expressions/casts/between_type_shapes/array_to_scalar.hlsl
+++
@@ -1,8 +0,0 @@
-// RUN: %dxc -T vs_6_0 -E main %s | FileCheck %s
-
-int main() : OUT
-{
-    // CHECK: @dx.op.storeOutput.i32(i32 5, i32 0, i32 0, i8 0, i32 1)
-    int array[] = { 1 };
-    return (int)array;
-}
