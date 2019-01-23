44ebeaaaf37c571693e64b0b00d14112a4884762
Tristan Labelle <trlabell@microsoft.com>
2019-01-23 05:49:58 -0800

    Test updates

--- tools/clang/test/CodeGenHLSL/expressions/conversions_and_casts/numerical_to_compound_roundtrip.hlsl
+++ tools/clang/test/CodeGenHLSL/expressions/conversions_and_casts/numerical_to_compound_roundtrip.hlsl
@@ -1,4 +1,4 @@
-// RUN: %dxc -T vs_6_2 -E main %s | FileCheck %s
+// RUN: %dxc -T vs_6_0 -E main %s | FileCheck %s
 
 // Test round-trip conversions from scalar/vector/matrices to structs/arrays and back
 // If the round-trip conversion succeeds, we assume both single-way conversions did too.
@@ -45,13 +45,13 @@ void main() {
     int4x4 m4x4 = int4x4(11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44); // For truncation tests
 
     // Scalar cases
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 1, i32 0, i32 0, i32 0,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 0, i32 0, i32 0, i8 15)
+    // CHECK: 8888
     output_i((int)(s_int)1);
     output_v1((int1)(s_int)int1(1));
     output_m1x1((int1x1)(s_int)int1x1(1));
@@ -61,11 +61,11 @@ void main() {
     output_separator();
 
     // 1-to-1 vector/matrix cases
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_ints)v4);
     output_m2x2((int2x2)(s_ints)m2x2);
     output_v4((int4)(a_ints)v4);
@@ -75,11 +75,11 @@ void main() {
     // With numerical conversions
     
     // With vectors in compound type
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_vecs)v4);
     output_m2x2((int2x2)(s_vecs)m2x2);
     output_v4((int4)(a_vecs)v4);
@@ -87,11 +87,11 @@ void main() {
     output_separator();
     
     // With matrices in compound type
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_mat)v4);
     output_m2x2((int2x2)(s_mat)m2x2);
     output_v4((int4)(a_mat)v4);
@@ -99,11 +99,11 @@ void main() {
     output_separator();
     
     // With homogeneous nesting (struct of structs, array of arrays)
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_structs)v4);
     output_m2x2((int2x2)(s_structs)m2x2);
     output_v4((int4)(a_ints_2d)v4);
@@ -111,11 +111,11 @@ void main() {
     output_separator();
     
     // With heterogeneous nesting (struct of arrays, array of structs)
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_arrays)v4);
     output_m2x2((int2x2)(s_arrays)m2x2);
     output_v4((int4)(a_structs)v4);
@@ -123,22 +123,19 @@ void main() {
     output_separator();
 
     // With nested empty struct
-    // CHECK: i32 1, i32 2, i32 3, i32 4,
-    // CHECK: i32 11, i32 12, i32 21, i32 22,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // CHECK: i32 1, i32 2, i32 3, i32 4, i8 15)
+    // CHECK: i32 11, i32 12, i32 21, i32 22, i8 15)
+    // CHECK: 8888
     output_v4((int4)(s_empty_structs)v4);
     output_m2x2((int2x2)(s_empty_structs)m2x2);
     output_separator();
 
     // Truncation case
-    // CHECK: i32 1, i32 2, i32 0, i32 0,
-    // CHECK: i32 11, i32 12, i32 13, i32 14,
-    // CHECK: i32 1, i32 2, i32 0, i32 0,
-    // CHECK: i32 11, i32 12, i32 13, i32 14,
-    // CHECK: i32 8888, i32 8888, i32 8888, i32 8888,
+    // Casting a 2D matrix to a smaller struct or struct to smaller 2D matrix is illegal
+    // CHECK: i32 1, i32 2, i32 0, i32 0, i8 15)
+    // CHECK: i32 1, i32 2, i32 0, i32 0, i8 15)
+    // CHECK: 8888
     output_v2((int2)(s_three_ints)v4);
-    output_m2x2((int2x2)(s_mat_3x3)m4x4);
     output_v2((int2)(a_three_ints)v4);
-    output_m2x2((int2x2)(a_mat_3x3)m4x4);
     output_separator();
 }
