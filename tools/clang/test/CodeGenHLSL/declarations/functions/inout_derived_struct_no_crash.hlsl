92a2c898e39069e8ce874a71317a0feac96095d4
Tristan Labelle <trlabell@microsoft.com>
2019-02-13 12:01:11 -0800

    Removed random chars in test comment.

--- tools/clang/test/CodeGenHLSL/declarations/functions/inout_derived_struct_no_crash.hlsl
+++ tools/clang/test/CodeGenHLSL/declarations/functions/inout_derived_struct_no_crash.hlsl
@@ -1,7 +1,7 @@
 // RUN: %dxc -E main -T vs_6_2 %s | FileCheck %s
 
 // Regression test for GitHub #1929, where we used the C++ definition
-// of an aggregate type and failed to match derived structs.ctt
+// of an aggregate type and failed to match derived structs.
 
 // CHECK: ret void
 
