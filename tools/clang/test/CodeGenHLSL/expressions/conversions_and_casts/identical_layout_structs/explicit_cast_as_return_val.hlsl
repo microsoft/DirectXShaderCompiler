f3ec83736f839dc826643b8958a2bc80158180c7
Vishal C Sharma <visshar@microsoft.com>
2019-02-23 07:41:43 +0530

    Add test coverage for cast between struct of identical layout

--- tools/clang/test/CodeGenHLSL/expressions/conversions_and_casts/identical_layout_structs/explicit_cast_as_return_val.hlsl
+++ tools/clang/test/CodeGenHLSL/expressions/conversions_and_casts/identical_layout_structs/explicit_cast_as_return_val.hlsl
@@ -1,5 +1,7 @@
 // RUN: %dxc /Tps_6_0 /Emain %s | FileCheck %s
 // github issue #1684
+// Test explicit cast between structs of identical layouts.
+
 // CHECK: call void @dx.op.storeOutput.f32(i32 5, i32 0, i32 0, i8 0, float 0.000000e+00)  ; StoreOutput(outputSigId,rowIndex,colIndex,value)
 // CHECK: call void @dx.op.storeOutput.f32(i32 5, i32 0, i32 0, i8 1, float 0.000000e+00)  ; StoreOutput(outputSigId,rowIndex,colIndex,value)
 // CHECK: call void @dx.op.storeOutput.f32(i32 5, i32 0, i32 0, i8 2, float 0.000000e+00)  ; StoreOutput(outputSigId,rowIndex,colIndex,value)
