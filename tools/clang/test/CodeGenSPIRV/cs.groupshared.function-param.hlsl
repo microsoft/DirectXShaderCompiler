834823701e76af2b8579d19089a620f71afe20b5
Jaebaek Seo <jaebaek@google.com>
2019-03-05 12:16:06 -0500

    clang-format

--- tools/clang/test/CodeGenSPIRV/cs.groupshared.function-param.hlsl
+++ tools/clang/test/CodeGenSPIRV/cs.groupshared.function-param.hlsl
@@ -3,13 +3,11 @@
 // CHECK: %foo = OpVariable %_ptr_Workgroup__arr__arr_int_uint_10_uint_10 Workgroup
 groupshared int foo[10][10];
 
-int bar(int arg[10][10])
-{
-    return arg[0][0];
+int bar(int arg[10][10]) {
+  return arg[0][0];
 }
 
-void main()
-{
-// CHECK: [[callBar:%\d+]] = OpFunctionCall %int %bar %foo
-    int a = bar(foo);
+void main() {
+  // CHECK: [[callBar:%\d+]] = OpFunctionCall %int %bar %foo
+  int a = bar(foo);
 }
