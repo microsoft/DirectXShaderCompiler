f8c02c243d96e981a162f462057980b452f36900
Tristan Labelle <github@tristanlabelle.com>
2019-03-05 16:37:38 -0800

    Migrate CodeGenTest-based tests to quick-test (#1975)

--- tools/clang/test/CodeGenHLSL/cbuffer2.50.hlsl
+++
@@ -1,8 +0,0 @@
-// RUN: %dxc -E main -T ps_6_0 %s
-
-float4 g1;
-
-float4 main() : SV_TARGET
-{
-  return g1.wyyy;
-}
--- tools/clang/test/CodeGenHLSL/quick-test/cast2.hlsl
+++ tools/clang/test/CodeGenHLSL/quick-test/cast2.hlsl
@@ -1,7 +1,7 @@
 // RUN: %dxc -E main -T ps_6_0 %s | FileCheck %s
 
 // CHECK: @main
-float4 main(float4 a : A) : SV_TARGET
+float main(uint a : A) : SV_Target
 {
-  return -a.yxxx;
+  return a;
 }
