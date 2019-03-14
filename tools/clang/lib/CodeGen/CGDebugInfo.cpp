52c6c0f668617b3f51529d3bc4cd6ca834e0f040
Tristan Labelle <github@tristanlabelle.com>
2019-03-14 14:23:56 -0700

    Fix bogus DIFile debug metadata (#2030)
    When compiling with debug info and passing in a full path, we ended up with a bogus !DIFile metadata entry where the path had the drive and directory components twice before the filename. Other code would compute the correct path and create a second !DIFile metadata entry.
    
    Apparently we're misusing CodeGenOptions::MainFileName, because it should not contain a full path but, due to other code of ours, it seems like a riskier change to fix that because we depend on it in CodeGeneratorImpl::HandleTranslationUnit.

--- tools/clang/lib/CodeGen/CGDebugInfo.cpp
+++ tools/clang/lib/CodeGen/CGDebugInfo.cpp
@@ -321,15 +321,17 @@ void CGDebugInfo::CreateCompileUnit() {
   // the file name itself with no path information. This file name may have had
   // a relative path, so we look into the actual file entry for the main
   // file to determine the real absolute path for the file.
-  std::string MainFileDir;
-  if (const FileEntry *MainFile = SM.getFileEntryForID(SM.getMainFileID())) {
-    MainFileDir = MainFile->getDir()->getName();
-    if (MainFileDir != ".") {
-      llvm::SmallString<1024> MainFileDirSS(MainFileDir);
-      llvm::sys::path::append(MainFileDirSS, MainFileName);
-      MainFileName = MainFileDirSS.str();
+  if (!llvm::sys::path::is_absolute(MainFileName)) { // HLSL Change: we put the full path in -main-file-name
+    std::string MainFileDir;
+    if (const FileEntry *MainFile = SM.getFileEntryForID(SM.getMainFileID())) {
+      MainFileDir = MainFile->getDir()->getName();
+      if (MainFileDir != ".") {
+        llvm::SmallString<1024> MainFileDirSS(MainFileDir);
+        llvm::sys::path::append(MainFileDirSS, MainFileName);
+        MainFileName = MainFileDirSS.str();
+      }
     }
-  }
+  } // HLSL Change
 
   // Save filename string.
   StringRef Filename = internString(MainFileName);
