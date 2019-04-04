239c605fe9ecde9d008fa96baaf4666c37ba8685
Tristan Labelle <trlabell@microsoft.com>
2019-04-04 12:42:03 -0700

    Better test comment.

--- tools/clang/test/CodeGenHLSL/batch/declarations/resources/norm_floats/srv_uav.hlsl
+++ tools/clang/test/CodeGenHLSL/batch/declarations/resources/norm_floats/srv_uav.hlsl
@@ -2,7 +2,7 @@
 
 // Test that snorm/unorm is preserved in the DXIL metadata.
 
-// With an SRV and textures
+// With an SRV and textures (9 = float, 13 = snorm float, 14 = unorm float)
 // CHECK-DAG: !{i32 0, %{{.*}}* undef, !{{.*}}, i32 0, i32 0, i32 1, i32 1, i32 0, ![[bf:.*]]}
 // CHECK-DAG: ![[bf]] = !{i32 0, i32 9}
 // CHECK-DAG: !{i32 1, %{{.*}}* undef, !{{.*}}, i32 0, i32 1, i32 1, i32 1, i32 0, ![[bs:.*]]}
@@ -13,7 +13,7 @@ Texture1D<float> tex_float;
 Texture1D<snorm float> tex_snorm;
 Texture1D<unorm float> tex_unorm;
 
-// With a UAV and buffers
+// With a UAV and buffers (9 = float, 13 = snorm float, 14 = unorm float)
 // CHECK-DAG: !{i32 0, %{{.*}}* undef, !{{.*}}, i32 0, i32 0, i32 1, i32 10, i1 false, i1 false, i1 false, ![[rwbf:.*]]}
 // CHECK-DAG: ![[rwbf]] = !{i32 0, i32 9}
 // CHECK-DAG: !{i32 1, %{{.*}}* undef, !{{.*}}, i32 0, i32 1, i32 1, i32 10, i1 false, i1 false, i1 false, ![[rwbs:.*]]}
