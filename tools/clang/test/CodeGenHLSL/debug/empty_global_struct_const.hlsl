75df1b1436fb2382d658cc27056bfd93e4dd4cf5
Adam Yang <31109344+adam-yang@users.noreply.github.com>
2019-04-04 11:04:22 -0700

    Correct size for empty struct element in debug info. (#2103)

---
+++ tools/clang/test/CodeGenHLSL/debug/empty_global_struct_const.hlsl
@@ -0,0 +1,17 @@
+// RUN: %dxc -E main -T ps_6_0 %s -Zi -Od | FileCheck %s
+
+// CHECK-DAG: !DIGlobalVariable(name: "foo.0", linkageName: "foo.0", scope: !{{[0-9]+}}, file: !{{[0-9]+}}, line: {{[0-9]+}}, type: ![[ty:[0-9]+]]
+// CHECK-DAG: ![[ty]] = !DIDerivedType(tag: DW_TAG_member, name: ".0", baseType: !{{[0-9]+}}, size: 8
+
+struct S {
+  float f() {
+    return 420;
+  }
+};
+
+const static S foo;
+
+float main() : SV_Target {
+  return foo.f();
+}
+
