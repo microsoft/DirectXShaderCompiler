c886ae68f36bc0806c88aa1cd6594677f2d5deb5
Ehsan <ehsann@google.com>
2019-04-11 11:34:34 -0400

    [spirv] Support for #pragma pack_matrix(). (#2112)
    The front-end has taken care of annotating the AST nodes with
    the right attribute on QualTypes. We need to read that information
    properly and use it in the SPIR-V backend.

--- tools/clang/test/CodeGenSPIRV/type.matrix.majorness.zpc.hlsl
+++ tools/clang/test/CodeGenSPIRV/type.matrix.majorness.zpc.hlsl
@@ -17,21 +17,18 @@
 // CHECK: OpMemberDecorate %type_ConsumeStructuredBuffer_mat3v4float 0 RowMajor
 ConsumeStructuredBuffer<float3x4> CSB1;
 
-// NOTE: The parsed AST does not convey the majorness information for
-// the following cases right now.
-/*
-       StructuredBuffer<row_major float2x3> ROSB2;
-     RWStructuredBuffer<row_major float3x2> RWSB2;
- AppendStructuredBuffer<row_major float4x3> ASB2;
-ConsumeStructuredBuffer<row_major float3x4> CSB2;
+// NOTE: -Zpc does not override explicit matrix orientation specified for the cases below.
 
-       StructuredBuffer<column_major float2x3> ROSB3;
-     RWStructuredBuffer<column_major float3x2> RWSB3;
- AppendStructuredBuffer<column_major float4x3> ASB3;
-ConsumeStructuredBuffer<column_major float3x4> CSB3;
-*/
+// CHECK: OpDecorate %_runtimearr_mat4v4float ArrayStride 64
+// CHECK: OpMemberDecorate %type_StructuredBuffer_mat4v4float 0 MatrixStride 16
+// CHECK: OpMemberDecorate %type_StructuredBuffer_mat4v4float 0 ColMajor
+     StructuredBuffer<row_major float4x4> ROSB2;
+
+// CHECK: OpDecorate %_runtimearr_mat3v3float ArrayStride 48
+// CHECK: OpMemberDecorate %type_RWStructuredBuffer_mat3v3float 0 MatrixStride 16
+// CHECK: OpMemberDecorate %type_RWStructuredBuffer_mat3v3float 0 RowMajor
+RWStructuredBuffer<column_major float3x3> RWSB2;
 
 float4 main() : SV_Target {
-    return ROSB1[0][0][0] // + ROSB2[0][0][0] + ROSB3[0][0][0]
-        ;
+  return ROSB1[0][0][0] + ROSB2[0][0][0] + RWSB2[0][0][0];
 }
