84f446d595808572805742fa015faccb3a46d68b
Adam Yang <hanbyang@microsoft.com>
2019-04-24 15:17:59 -0700

    More tabs

--- tools/clang/test/CodeGenHLSL/batch/misc/eval_attr_at_sample/in_loop.hlsl
+++ tools/clang/test/CodeGenHLSL/batch/misc/eval_attr_at_sample/in_loop.hlsl
@@ -6,9 +6,9 @@ Texture2D tex0 : register(t0);
 SamplerState samp0 : register(s0);
 
 float4 main(float2 uv : TEXCOORD0) : SV_TARGET {
-	float4 result = float4(0,0,0,0);
+  float4 result = float4(0,0,0,0);
   [loop]
-	for (int i = 0; i < 4; ++i)	{
+  for (int i = 0; i < 4; ++i)	{
     float2 coord = EvaluateAttributeAtSample(uv, i);
     result += tex0.Sample(samp0, coord);
   }
