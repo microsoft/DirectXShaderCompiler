2e6a343e6468b534c9075136999418c1113ec21d
Tristan Labelle <trlabell@microsoft.com>
2019-05-06 09:33:16 -0700

    Another pass at organizing tests (#2168)
    Moved stuff outside of quick-test, organized some misc tests, occasionally editing them a bit. In a few cases, made the editorial decision that the test was worthless and should just be deleted.

--- tools/clang/test/CodeGenHLSL/batch/declarations/bool_representation/bool_vector_no_nrvo.hlsl
+++ tools/clang/test/CodeGenHLSL/batch/declarations/bool_representation/bool_vector_no_nrvo.hlsl
@@ -1,5 +1,9 @@
 // RUN: %dxc -E main -T vs_6_0 %s | FileCheck %s
 
+// Regression test for a crash where bool vectors where eligible
+// for named return value optimizations, which caused register/memory
+// representation mismatches.
+
 // CHECK: call void @dx.op.storeOutput.i32(i32 5, i32 0, i32 0, i8 0, i32 0)
 
 bool3 main() : OUT {
