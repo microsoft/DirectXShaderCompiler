98af273f5387ed4714f0b7eccef4a1b0d0c3edc6
Tristan Labelle <trlabell@microsoft.com>
2019-05-06 09:33:16 -0700

    Another pass at organizing tests (#2168)
    Moved stuff outside of quick-test, organized some misc tests, occasionally editing them a bit. In a few cases, made the editorial decision that the test was worthless and should just be deleted.

--- tools/clang/test/CodeGenHLSL/batch/misc/cast1.hlsl
+++
@@ -1,7 +0,0 @@
-// RUN: %dxc -E main -T ps_6_0 %s | FileCheck %s
-
-// CHECK: @main
-float main(int a : A) : SV_Target
-{
-  return a;
-}
--- tools/clang/test/CodeGenHLSL/batch/misc/cast2.hlsl
+++
@@ -1,7 +0,0 @@
-// RUN: %dxc -E main -T ps_6_0 %s | FileCheck %s
-
-// CHECK: @main
-float main(uint a : A) : SV_Target
-{
-  return a;
-}
