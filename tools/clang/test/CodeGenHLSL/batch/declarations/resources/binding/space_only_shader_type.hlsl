ade945e167de1ab897060f2f3d8eb1304fc408b0
Tristan Labelle <trlabell@microsoft.com>
2019-05-09 09:54:01 -0700

    Typo in test.

--- tools/clang/test/CodeGenHLSL/batch/declarations/resources/binding/space_only_shader_type.hlsl
+++ tools/clang/test/CodeGenHLSL/batch/declarations/resources/binding/space_only_shader_type.hlsl
@@ -2,7 +2,7 @@
 
 // Test space-only register annotations with shader type specifier.
 
-// CHECK: buf texture f32 buf T0 t0,space2 1
+// CHECK: buf texture u32 buf T0 t0,space2 1
 Buffer<uint> buf : register(ps, space1) : register(vs, space2);
 
 uint main() : OUT { return buf[0]; }
