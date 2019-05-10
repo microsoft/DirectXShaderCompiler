5fecea7d697c307c14aa34b792965e13f9764aa7
John Porto <jpporto@gmail.com>
2019-05-10 11:00:24 -0700

    DxilDia uses factories (not lambdas) for symbol creation (#2177)
    * Modifies DxilDia's to use factories instead of lambdas for creating symbols
    
    DxilDia used to hold a vector of lambdas -- constructors -- for the symbols
    it knew about. When the user requested a symbol, said constructor was to be
    invoked, creating a brand new symbol.
    
    C++ lambdas can be hard to debug (i.e., how does one inspect the lambda's
    state?), so the library is modified to hold a vector of factories -- i.e.,
    objects with fields and a CreateSymbol method.
    
    * fixes typo when creating Typedef symbols
    
    * caches the VarInfo when creating the LocalVariable factories

--- lib/DxilDia/DxilDiaSymbolManager.h
+++ lib/DxilDia/DxilDiaSymbolManager.h
@@ -42,7 +42,18 @@ public:
     unsigned Length;
   };
 
-  using CreateSymbolFn = std::function<HRESULT(Session *pSession, Symbol **)>;
+  class SymbolFactory {
+  protected:
+      SymbolFactory(DWORD ID, DWORD ParentID);
+
+      DWORD m_ID;
+      DWORD m_ParentID;
+
+  public:
+      virtual ~SymbolFactory();
+      virtual HRESULT Create(Session *pSession, Symbol **) = 0;
+  };
+
   using ScopeToIDMap = llvm::DenseMap<llvm::DIScope *, DWORD>;
   using IDToLiveRangeMap = std::unordered_map<DWORD, LiveRange>;
   using ParentToChildrenMap = std::unordered_multimap<DWORD, DWORD>;
@@ -66,8 +77,8 @@ private:
   // Not a CComPtr, and not AddRef'd - m_pSession is the owner of this.
   Session *m_pSession = nullptr;
 
-  // Vector of all symbols in the DXIL module.
-  std::vector<CreateSymbolFn> m_symbolCtors;
+  // Vector of factories for all symbols in the DXIL module.
+  std::vector<std::unique_ptr<SymbolFactory>> m_symbolCtors;
 
   // Mapping from scope to its ID.
   ScopeToIDMap m_scopeToID;
