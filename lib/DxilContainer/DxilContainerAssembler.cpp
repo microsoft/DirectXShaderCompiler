41aff0a43a4fa51440558a6f152744a8a6ce0761
Adam Yang <hanbyang@microsoft.com>
2019-05-10 18:49:07 -0700

    Reverted moving a variable for no reason

--- lib/DxilContainer/DxilContainerAssembler.cpp
+++ lib/DxilContainer/DxilContainerAssembler.cpp
@@ -1570,9 +1570,9 @@ void hlsl::SerializeDxilContainerForModule(DxilModule *pModule,
     WriteBitcodeToFile(pModule->GetModule(), outStream, true);
   }
 
-  bool bModuleStripped = false;
   // If we have debug information present, serialize it to a debug part, then use the stripped version as the canonical program version.
   CComPtr<AbstractMemoryStream> pProgramStream = pInputProgramStream;
+  bool bModuleStripped = false;
   bool bHasDebugInfo = HasDebugInfo(*pModule->GetModule());
   if (bHasDebugInfo) {
     uint32_t debugInUInt32, debugPaddingBytes;
