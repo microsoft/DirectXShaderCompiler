22f59f9793013eb87c0c6d2515c8fa98f7a1741b
Tristan Labelle <trlabell@microsoft.com>
2019-05-15 16:14:52 -0700

    Remove crash repro test.

--- tools/clang/test/CodeGenHLSL/crashes/inout_vector_numerical_conversion.hlsl
+++
@@ -1,6 +0,0 @@
-// RUN: %dxc -E main -T vs_6_2 %s | FileCheck %s
-
-// Repro of GitHub #1916
-
-void inc_i32x2(inout int2 val) { val++; }
-void main(inout float2 f32x2 : F32X2) { inc_i32x2(f32x2); }
