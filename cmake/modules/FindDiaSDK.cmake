2dc416fc6b56f3545aff9464593eea2624da19ed
Minmin Gong <mgong@microsoft.com>
2019-05-24 02:25:14 +0800

    Enable to detect DIA SDK in VS2019 (#2136)

--- cmake/modules/FindDiaSDK.cmake
+++ cmake/modules/FindDiaSDK.cmake
@@ -3,17 +3,25 @@ get_filename_component(VS_PATH32 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Visu
 get_filename_component(VS_PATH64 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\14.0;InstallDir]" ABSOLUTE CACHE)
 # VS_PATH32 will be something like C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE
 
-# Also look for in vs15 install.
+# Also look for in vs15 or vs16 install.
 set(PROGRAMFILES_X86 "ProgramFiles(x86)")
-get_filename_component(VS15_C_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Community/Common7/IDE" ABSOLUTE CACHE)
-get_filename_component(VS15_P_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Professional/Common7/IDE" ABSOLUTE CACHE)
-get_filename_component(VS15_E_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Enterprise/Common7/IDE" ABSOLUTE CACHE)
+set(VS_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio")
+get_filename_component(VS16_C_PATH32 "${VS_PATH32}/2019/Community/Common7/IDE" ABSOLUTE CACHE)
+get_filename_component(VS16_P_PATH32 "${VS_PATH32}/2019/Professional/Common7/IDE" ABSOLUTE CACHE)
+get_filename_component(VS16_E_PATH32 "${VS_PATH32}/2019/Enterprise/Common7/IDE" ABSOLUTE CACHE)
+get_filename_component(VS15_C_PATH32 "${VS_PATH32}/2017/Community/Common7/IDE" ABSOLUTE CACHE)
+get_filename_component(VS15_P_PATH32 "${VS_PATH32}/2017/Professional/Common7/IDE" ABSOLUTE CACHE)
+get_filename_component(VS15_E_PATH32 "${VS_PATH32}/2017/Enterprise/Common7/IDE" ABSOLUTE CACHE)
 
 # Find the DIA SDK path, it will typically look something like this.
+# C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\DIA SDK\include
 # C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\DIA SDK\include
-# C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include\dia2.h
+# C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include
 find_path(DIASDK_INCLUDE_DIR    # Set variable DIASDK_INCLUDE_DIR
           dia2.h                # Find a path with dia2.h
+          HINTS "${VS16_C_PATH32}/../../DIA SDK/include" 
+          HINTS "${VS16_P_PATH32}/../../DIA SDK/include"
+          HINTS "${VS16_E_PATH32}/../../DIA SDK/include"
           HINTS "${VS15_C_PATH32}/../../DIA SDK/include" 
           HINTS "${VS15_P_PATH32}/../../DIA SDK/include"
           HINTS "${VS15_E_PATH32}/../../DIA SDK/include"
