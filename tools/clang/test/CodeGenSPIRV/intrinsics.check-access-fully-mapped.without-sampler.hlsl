cc8ceac8ba163bfe185feddae9dcfc11290dd188
Jaebaek Seo <jaebaek@google.com>
2019-06-12 16:28:43 -0400

    [spirv] fix CheckAccessFullyMapped without sampler
    Fixes #2256

---
+++ tools/clang/test/CodeGenSPIRV/intrinsics.check-access-fully-mapped.without-sampler.hlsl
@@ -0,0 +1,15 @@
+// Run: %dxc -T ps_6_0 -E main
+
+// This test checks if capability visitor emits SparseResidency capability
+// correctly when OpImageSparseTexelsResident is used.
+
+// CHECK: OpCapability SparseResidency
+
+void main() {
+  uint status;
+
+// CHECK: [[residency_code:%\d+]] = OpLoad %uint %status
+// CHECK:         [[result:%\d+]] = OpImageSparseTexelsResident %bool [[residency_code]]
+// CHECK:                           OpStore %result [[result]]
+  bool result = CheckAccessFullyMapped(status);
+}
