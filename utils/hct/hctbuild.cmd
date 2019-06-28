dd41762f97dea051e0479880bb272c2347b6482d
Helena Kotas <hekotas@microsoft.com>
2019-06-28 15:16:34 -0700

    Generate new version for each DX Compiler build (#2200)
    Added generating of new version for each DX Compiler build. 
    
    There are 3 kinds of version:
    1. **Official build**
    Built by using `hctbuild -official`. The version is based on the current DXIL version, latest official release and a number of commits since then. The format is `dxil_major.dxil_minor.release_no.commit_count`. For example a current official version would be something like `1.5.1905.42`. The latest release information is read from `utils\version\latest-release.json`. The `1905` corresponds to `dxil-2019-05-16` release branch and `42` is the number of commits since that release branch was created. For master branch the `commit_count` will be incremented by 10000 to distinguish it from stabilized official release branch builds. So the current official version of master would be someting like `1.5.1905.10042`.
    
    2. **Dev build**
    Build by using `hctbuild` with no other version-related option. The format is `dxil_major.dxil_minor.0.commit_count` where commit_count is the number of total commits since the beginning of the project.
    
    3. **Fixed version build**
    Build by using `hctbuild -fv`. Enables overriding of the version information. The fixed version is read from `utils\version\version.inc`. Location of the version file can be overriden by `-fvloc` option on `hctbuild`.
    
    In addition to the numbered version the product version string on the binaries will also include branch name and last commit sha - `"1.5.1905.10042 (master, 47e31c8a)"`. This product version string is included in `dxc -?` output.

--- utils/hct/hctbuild.cmd
+++ utils/hct/hctbuild.cmd
@@ -65,9 +65,20 @@ if "%1"=="-analyze" (
   set CMAKE_OPTS=%CMAKE_OPTS% -DHLSL_ENABLE_ANALYZE:BOOL=ON
   shift /1
 )
+if "%1"=="-official" (
+  echo Will generate official version for build
+  set CMAKE_OPTS=%CMAKE_OPTS% -DHLSL_OFFICIAL_BUILD:BOOL=ON
+  shift /1
+)
 if "%1"=="-fv" (
   echo Fixed version flag set for build.
   set CMAKE_OPTS=%CMAKE_OPTS% -DHLSL_ENABLE_FIXED_VER:BOOL=ON
+  shift /1
+)
+if "%1"=="-fvloc" (
+  echo Fixed version flag set for build, version file location: %2
+  set CMAKE_OPTS=%CMAKE_OPTS% -DHLSL_ENABLE_FIXED_VER:BOOL=ON -DHLSL_FIXED_VERSION_LOCATION:STRING=%2
+  shift /1
   shift /1
 )
 if "%1"=="-cv" (
@@ -251,18 +262,19 @@ exit /b 0
 echo Builds HLSL solutions and the product and test binaries for the current
 echo flavor and architecture.
 echo.
-echo hctbuild [-s or -b] [-alldef] [-analyze] [-fv] [-rel] [-arm or -arm64 or -x86 or -x64] [-Release] [-Debug] [-vs2015] [-ninja] [-tblgen path] [-dont-speak] [-parallel]
+echo hctbuild [-s or -b] [-alldef] [-analyze] [-official] [-fv] [-fvloc <path>] [-rel] [-arm or -arm64 or -x86 or -x64] [-Release] [-Debug] [-vs2015] [-ninja] [-tblgen path] [-dont-speak] [-parallel]
 echo.
 echo   -s   creates the projects only, without building
 echo   -b   builds the existing project
 echo.
-echo   -alldef  adds optional projects to the default build
-echo   -analyze adds /analyze option
-echo   -fv      fixes the resource version for release
-echo.
-echo   -rel     builds release rather than debug
-echo   -dont-speak  disables audible build confirmation
-echo   -parallel    enables parallel build
+echo   -alldef        adds optional projects to the default build
+echo   -analyze       adds /analyze option
+echo   -official      will generate official version for build
+echo   -fv            fixes the resource version for release (utils\version\version.inc)
+echo   -fvloc <path>  directory with the version.inc file
+echo   -rel           builds release rather than debug
+echo   -dont-speak    disables audible build confirmation
+echo   -parallel      enables parallel build
 echo.
 echo current BUILD_ARCH=%BUILD_ARCH%.  Override with:
 echo   -x86 targets an x86 build (aka. Win32)
