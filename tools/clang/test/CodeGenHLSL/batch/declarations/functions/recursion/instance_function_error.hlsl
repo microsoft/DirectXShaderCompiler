265a7d5ef6b977b1a72d7ef74c1a3b7fca37a9fa
Tristan Labelle <trlabell@microsoft.com>
2019-07-26 11:29:54 -0700

    Improve recursion detection to handle member functions and add tests. (#2371)
    Recursion detection did not consider member functions.

---
+++ tools/clang/test/CodeGenHLSL/batch/declarations/functions/recursion/instance_function_error.hlsl
@@ -0,0 +1,18 @@
+// RUN: %dxc -T vs_6_2 -E main %s | FileCheck %s
+
+// Test for recursion detection. Note that this cannot be a syntax test
+// because we detect from the entry point and syntax tests have none.
+
+// CHECK: error: recursive functions not allowed
+
+struct MyClass
+{
+  void A() { B(); }
+  void B() { A(); }
+};
+
+void main()
+{
+  MyClass c;
+  c.A();
+} 
