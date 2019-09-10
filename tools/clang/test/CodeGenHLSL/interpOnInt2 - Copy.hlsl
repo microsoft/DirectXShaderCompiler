bc5490efa86f213cb3bf472e22c87051a0b6bff8
Vishal C Sharma <visshar@microsoft.com>
2019-09-09 23:14:54 -0700

    Update

--- tools/clang/test/CodeGenHLSL/interpOnInt2 - Copy.hlsl
+++ tools/clang/test/CodeGenHLSL/interpOnInt2 - Copy.hlsl
@@ -2,8 +2,8 @@
 
 // CHECK: main
 // After lowering, these would turn into multiple abs calls rather than a 4 x float
-// CHECK: call <4 x float> @"dx.hl.op..<4 x float> (i32, <4 x float>)"(i32 84,
+// CHECK: call <4 x float> @"dx.hl.op..<4 x float> (i32, <4 x float>)"(i32 62,
 
-float4 main(float4 a : A) : SV_TARGET {
-  return abs(a*a.yxxx);
+float4 main(float4 a : A, uint4 b : A1) : SV_TARGET {
+  return abs(a*b.yxxx);
 }
