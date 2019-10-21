a698c7128329a4b17af63e9a59a8c218a619e448
Helena Kotas <hekotas@microsoft.com>
2019-10-21 11:26:37 -0700

    Fix validation failure for 16-bit float resource types (#2524)
    Fix validation failure for 16-bit float resource types, add fp16 test coverage for Texture2D.Sample*

--- tools/clang/test/HLSLFileCheck/hlsl/objects/Texture/sampleBias.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/Texture/sampleBias.hlsl
@@ -1,18 +1,31 @@
-// RUN: %dxc -E main -T ps_6_0 %s  | FileCheck %s
-
-// CHECK: sampleBias
+// RUN: %dxc -E main -T ps_6_0 -DTYPE=float4 %s                            | FileCheck %s -check-prefix=CHK_FLOAT
+// RUN: %dxc -E main -T ps_6_2 -DTYPE=half4       -enable-16bit-types %s   | FileCheck %s -check-prefix=CHK_HALF
+// RUN: %dxc -E main -T ps_6_2 -DTYPE=min16float4 -enable-16bit-types %s   | FileCheck %s -check-prefix=CHK_HALF
 
 SamplerState samp1 : register(s5);
-Texture2D<float4> text1 : register(t3);
+Texture2D<TYPE> text1 : register(t3);
 float bias;
-float4 main(float2 a : A) : SV_Target
-{
+TYPE main(float2 a
+          : A) : SV_Target {
   uint status;
-  float4 r = 0;
+  TYPE r = 0;
+
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleBias.f32(i32 61,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleBias.f16(i32 61,
   r += text1.SampleBias(samp1, a, -999);
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleBias.f32(i32 61,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleBias.f16(i32 61,
   r += text1.SampleBias(samp1, a, bias, uint2(-5, 7));
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleBias.f32(i32 61,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleBias.f16(i32 61,
   r += text1.SampleBias(samp1, a, bias, uint2(-4, 1), 0.5f);
-  r += text1.SampleBias(samp1, a, bias, uint2(-3, 2), 0.f, status); r += status;
-  r += text1.SampleBias(samp1, a, bias, uint2(-3, 2), a.x, status); r += status;
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleBias.f32(i32 61,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleBias.f16(i32 61,
+  r += text1.SampleBias(samp1, a, bias, uint2(-3, 2), 0.f, status);
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleBias.f32(i32 61,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleBias.f16(i32 61,
+  r += status;
+  r += text1.SampleBias(samp1, a, bias, uint2(-3, 2), a.x, status);
+  r += status;
   return r;
 }
--- tools/clang/test/HLSLFileCheck/hlsl/objects/Texture/sampleL.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/Texture/sampleL.hlsl
@@ -1,25 +1,38 @@
-// RUN: %dxc -E main -T ps_6_0 %s   | FileCheck %s
+// RUN: %dxc -E main -T ps_6_0 -DTYPE=float4 %s                            | FileCheck %s -check-prefix=CHK_FLOAT
+// RUN: %dxc -E main -T ps_6_2 -DTYPE=half4       -enable-16bit-types %s   | FileCheck %s -check-prefix=CHK_HALF
+// RUN: %dxc -E main -T ps_6_2 -DTYPE=min16float4 -enable-16bit-types %s   | FileCheck %s -check-prefix=CHK_HALF
 
 SamplerState samp1 : register(s5);
-Texture2D<float4> text1 : register(t3);
-TextureCubeArray<float4> text2 : register(t5);
-int LOD;
-float4 main(float2 a : A) : SV_Target
-{
-  uint status;
-  float4 r = 0;
+Texture2D<TYPE> text1 : register(t3);
+TextureCubeArray<TYPE> text2 : register(t5);
 
-  // CHECK: call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+int LOD;
+TYPE main(float2 a
+            : A) : SV_Target {
+  uint status;
+  TYPE r = 0;
+
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleLevel.f16(i32 62,
   r += text1.SampleLevel(samp1, a, LOD);
-  // CHECK: call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleLevel.f16(i32 62,
   r += text1.SampleLevel(samp1, a, LOD, uint2(-5, 7));
-  // CHECK: call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
-  // CHECK: call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
-  r += text1.SampleLevel(samp1, a, LOD, uint2(-3, 2), status); r += CheckAccessFullyMapped(status);
-  // CHECK: call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleLevel.f16(i32 62,
+  // CHK_FLOAT:  call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
+  // CHK_HALF:   call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
+  r += text1.SampleLevel(samp1, a, LOD, uint2(-3, 2), status);
+  r += CheckAccessFullyMapped(status);
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleLevel.f16(i32 62,
   r += text2.SampleLevel(samp1, a.xyxy, LOD);
-  // CHECK: call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
-  // CHECK: call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
-  r += text2.SampleLevel(samp1, a.xyxy, LOD * 0.5, status); r += CheckAccessFullyMapped(status);
+  // CHK_FLOAT:  call %dx.types.ResRet.f32 @dx.op.sampleLevel.f32(i32 62,
+  // CHK_HALF:   call %dx.types.ResRet.f16 @dx.op.sampleLevel.f16(i32 62,
+  // CHK_FLOAT:  call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
+  // CHK_HALF:   call i1 @dx.op.checkAccessFullyMapped.i32(i32 71,
+  r += text2.SampleLevel(samp1, a.xyxy, LOD * 0.5, status);
+  r += CheckAccessFullyMapped(status);
+
   return r;
 }
