d9242790d41ae727719d242482b37d7d2394f7e0
Greg Roth <grroth@microsoft.com>
2020-01-30 16:54:59 -0700

    Add HLSL macros with version information (#2665)
    * Add HLSL macros with version information
    
    Adds three sets of version information as defined macros available
    to the shader.
    
    The DXC version is included as __DXC_VERSION_{MAJOR|MINOR|RELEASE|COMMITS}
    The values of these are set at compile time and derived from version.inc
    RELEASE and COMMITS information varies according to the criteria
    described in gen_version.py.
    
    The HLSL Version is included as __HLSL_VERSION. It may be taken from
    the -HV argument with which dxc was invoked, derived from other
    arguments, or the default.
    
    The shader target stage and version information is taken from the
    -T argument with which dxc was invoked. For the stage, defined
    constants representing each stage are defined at compile time that
    correspond to the enum values used internally. The shader stage
    specified by -T is determined at runtime and set to the corresponding
    enum value. The major and minor versions are similarly derived from
    the -T argument at runtime.
    
    The runtime defines are set in InitializeStandardPredefinedMacros()
    This allows them to be set just once and the same way for any way
    that dxc is invoked including testing.
    
    This makes some changes to how the version.inc file is generated and
    found in cmake files in order to generate it regardless of the need
    for a .rc resource file as it was before.
    
    A series of hlsl tests are added that test for the defines presence
    and values in order to add versus subtract with checks for the adds.
    Since many of the defines are determined by arguments at runtime, a
    few different tests were needed, but as much consolidation as possible
    was done to reduce the number of individual test runs.

--- tools/clang/lib/Frontend/InitPreprocessor.cpp
+++ tools/clang/lib/Frontend/InitPreprocessor.cpp
@@ -27,6 +27,9 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
+#include "version.inc" // HLSL Change
+#include "dxc/DXIL/DxilConstants.h" // HLSL Change
+#include "dxc/DXIL/DxilShaderModel.h" // HLSL Change
 using namespace clang;
 
 static bool MacroBodyEndsInBackslash(StringRef MacroBody) {
@@ -355,13 +358,41 @@ static void AddObjCXXARCLibstdcxxDefines(const LangOptions &LangOpts,
   Builder.append(Result);
 }
 
+#define STRINGIFY_INNER(str) #str
+#define STRINGIFY(str) STRINGIFY_INNER(str)
+
 static void InitializeStandardPredefinedMacros(const TargetInfo &TI,
                                                const LangOptions &LangOpts,
                                                const FrontendOptions &FEOpts,
                                                MacroBuilder &Builder) {
 #if 1 // HLSL Change Starts
-  if (LangOpts.HLSL)
+  if (LangOpts.HLSL) {
     Builder.defineMacro("__hlsl_dx_compiler");
+    // DXC Version information
+    Builder.defineMacro("__DXC_VERSION_MAJOR", STRINGIFY(RC_VERSION_FIELD_1));
+    Builder.defineMacro("__DXC_VERSION_MINOR", STRINGIFY(RC_VERSION_FIELD_2));
+    Builder.defineMacro("__DXC_VERSION_RELEASE", STRINGIFY(RC_VERSION_FIELD_3));
+    Builder.defineMacro("__DXC_VERSION_COMMITS", STRINGIFY(RC_VERSION_FIELD_4));
+    // HLSL Version
+    Builder.defineMacro("__HLSL_VERSION", Twine(LangOpts.HLSLVersion));
+    // Shader target information
+    // "enums" for shader stages
+    Builder.defineMacro("__SHADER_STAGE_VERTEX",  Twine((unsigned)hlsl::DXIL::ShaderKind::Vertex));
+    Builder.defineMacro("__SHADER_STAGE_PIXEL",   Twine((unsigned)hlsl::DXIL::ShaderKind::Pixel));
+    Builder.defineMacro("__SHADER_STAGE_GEOMETRY",Twine((unsigned)hlsl::DXIL::ShaderKind::Geometry));
+    Builder.defineMacro("__SHADER_STAGE_HULL",    Twine((unsigned)hlsl::DXIL::ShaderKind::Hull));
+    Builder.defineMacro("__SHADER_STAGE_DOMAIN",  Twine((unsigned)hlsl::DXIL::ShaderKind::Domain));
+    Builder.defineMacro("__SHADER_STAGE_COMPUTE", Twine((unsigned)hlsl::DXIL::ShaderKind::Compute));
+    Builder.defineMacro("__SHADER_STAGE_AMPLIFICATION",Twine((unsigned)hlsl::DXIL::ShaderKind::Amplification));
+    Builder.defineMacro("__SHADER_STAGE_MESH",    Twine((unsigned)hlsl::DXIL::ShaderKind::Mesh));
+    Builder.defineMacro("__SHADER_STAGE_LIBRARY", Twine((unsigned)hlsl::DXIL::ShaderKind::Library));
+    const hlsl::ShaderModel *SM = hlsl::ShaderModel::GetByName(LangOpts.HLSLProfile.c_str());
+    // The current shader stage itself
+    Builder.defineMacro("__SHADER_TARGET_STAGE", Twine((unsigned)SM->GetKind()));
+    // Add target versions
+    Builder.defineMacro("__SHADER_TARGET_MAJOR", Twine(SM->GetMajor()));
+    Builder.defineMacro("__SHADER_TARGET_MINOR", Twine(SM->GetMinor()));
+  }
   return;
 #else
   if (!LangOpts.MSVCCompat && !LangOpts.TraditionalCPP)
