cdeb1e3b6136abf8c3108c0703453856511a0e45
Adam Yang <31109344+adam-yang@users.noreply.github.com>
2020-01-30 17:27:22 -0800

    Fixed incorrect PHI value in DxilValueCache. Also using it in a few more places. (#2668)

--- tools/clang/test/HLSLFileCheck/dxil/debug/value_cache/resource_array2.hlsl
+++ tools/clang/test/HLSLFileCheck/dxil/debug/value_cache/resource_array2.hlsl
@@ -9,33 +9,44 @@ static bool gG;
 static bool gG2;
 static bool gG3;
 
-Texture2D tex0 : register(t0);
+Texture2D tex0[42] : register(t0);
 Texture2D tex1 : register(t42);
-Texture2D tex2 : register(t2);
-Texture2D tex3 : register(t3);
+Texture2D tex2[2][2] : register(t43);
+
+const uint idx1;
+const uint idx2;
+const uint idx3;
+
+static Texture2D local_tex1;
+static Texture2D local_tex2;
+static Texture2D local_tex3;
 
 Texture2D f(bool foo) {
   [branch]
   if (foo)
-    return tex0;
+    return local_tex1;
   else
     return tex1;
 }
 Texture2D g(bool foo) {
   [branch]
   if (foo)
-    return tex2;
+    return local_tex2;
   else
-    return tex3;
+    return local_tex3;
 }
 
 Texture2D h(bool foo3) {
   return foo3 ? f(gG2) : g(gG3);
 }
 
-[RootSignature("DescriptorTable(SRV(t0, numDescriptors=4), SRV(t42))")]
+[RootSignature("CBV(b0), DescriptorTable(SRV(t0, numDescriptors=42), SRV(t42), SRV(t43, numDescriptors=4))")]
 float4 main() : sv_target {
   // CHECK: %[[handle:.+]] = call %dx.types.Handle @dx.op.createHandle(i32 57, i8 0, i32 1, i32 42
+
+  local_tex1 = tex0[idx1];
+  local_tex2 = tex0[idx2];
+  local_tex3 = tex2[idx3][idx1];
 
   gG = true;
   gG2 = false;
