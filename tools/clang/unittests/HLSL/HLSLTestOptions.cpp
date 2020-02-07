fe3bbef0093ee21f03e78f712e15982dd6f1488b
Greg Roth <goyoroth@gmail.com>
2020-02-06 18:53:48 -0700

    Fix Linux build for HLSL test infrastructure
    An addendum to Helena's change reorganizing the test infrastructure and
    other things that resolves the various Linux and Unix issues.

--- tools/clang/unittests/HLSL/HLSLTestOptions.cpp
+++ tools/clang/unittests/HLSL/HLSLTestOptions.cpp
@@ -13,7 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "HLSLTestOptions.h"
-#include "WEXAdapter.h"
+#include "dxc/Test/WEXAdapter.h"
 #include "dxc/Support/WinAdapter.h"
 
 namespace clang {
