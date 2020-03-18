2e6a71b396aa103946f3097b2b1f8eba6c96fa84
Adam Yang <31109344+adam-yang@users.noreply.github.com>
2020-03-18 14:34:17 -0700

    Fixed tests

--- tools/clang/test/HLSLFileCheck/dxil/debug/noop_out_args.hlsl
+++ tools/clang/test/HLSLFileCheck/dxil/debug/noop_out_args.hlsl
@@ -25,28 +25,35 @@ void baz(inout float x, inout float y) {
 
 [RootSignature("")]
 float main() : SV_Target {
-  // CHECK: %[[p_load:[0-9]+]] = load i32, i32* @dx.preserve.value
+  // CHECK: %[[p_load:[0-9]+]] = load i32, i32*
+  // CHECK-SAME: @dx.preserve.value
   // CHECK: %[[p:[0-9]+]] = trunc i32 %[[p_load]] to i1
 
   S s;
 
-  // CHECK: load i32, i32* @dx.nothing
+  // CHECK: load i32, i32*
+  // CHECK: @dx.nothing
   foo(s);
     // CHECK: select i1 %[[p]]
     // CHECK: select i1 %[[p]]
-    // CHECK: load i32, i32* @dx.nothing
+    // CHECK: load i32, i32*
+    // CHECK: @dx.nothing
 
-  // CHECK: load i32, i32* @dx.nothing
+  // CHECK: load i32, i32*
+  // CHECK: @dx.nothing
   bar(s);
     // CHECK: fmul
     // CHECK: fmul
-    // CHECK: load i32, i32* @dx.nothing
+    // CHECK: load i32, i32*
+    // CHECK: @dx.nothing
 
-  // CHECK: load i32, i32* @dx.nothing
+  // CHECK: load i32, i32*
+  // CHECK: @dx.nothing
   baz(s.x, s.y);
     // CHECK: fmul
     // CHECK: fmul
-    // CHECK: load i32, i32* @dx.nothing
+    // CHECK: load i32, i32*
+    // CHECK: @dx.nothing
 
   // CHECK: fadd
   return s.x + s.y;
