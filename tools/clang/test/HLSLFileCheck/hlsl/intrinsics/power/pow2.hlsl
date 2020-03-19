3f8d6755e4cc3bc3231d4a069168cdbefc188584
Xiang Li <xiagli@microsoft.com>
2020-03-19 10:34:30 -0700

    Update test.

--- tools/clang/test/HLSLFileCheck/hlsl/intrinsics/power/pow2.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/intrinsics/power/pow2.hlsl
@@ -6,6 +6,10 @@
 // CHECK-NOT:fmul fast float %{{.*}}, 2.000000e+00
 // Exp
 // CHECK-NOT:call float @dx.op.unary.f32(i32 21,
+// CHECK: fmul fast float %[[a:.*]], %[[a]]
+// CHECK: fmul fast float %[[b:.*]], %[[b]]
+// CHECK: fmul fast float %[[c:.*]], %[[c]]
+// CHECK: fmul fast float %[[d:.*]], %[[d]]
 
 float4 main(float a :A, float3 b:B) : SV_Target {
   return float4(pow(a, 2), pow(b,2));
