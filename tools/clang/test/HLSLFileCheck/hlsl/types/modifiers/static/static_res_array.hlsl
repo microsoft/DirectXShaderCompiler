ab2381b9cef250269ec6401862ede73570c0d477
Tex Riddell <texr@microsoft.com>
2020-03-21 23:06:04 -0700

    Update test with static const, loop, and -Od test

--- tools/clang/test/HLSLFileCheck/hlsl/types/modifiers/static/static_res_array.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/types/modifiers/static/static_res_array.hlsl
@@ -1,6 +1,16 @@
 // RUN: %dxc -E main -T vs_6_0 %s | FileCheck %s
+// RUN: %dxc -E main -T vs_6_0 -Od %s | FileCheck %s -check-prefix=CHECKOD
 
 // CHECK: @dx.op.textureLoad.f32(i32 66,
+// CHECK: @dx.op.textureLoad.f32(i32 66,
+// CHECK-NOT: @dx.op.textureLoad.f32(i32 66,
+
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
+// CHECKOD: @dx.op.textureLoad.f32(i32 66,
 
 Texture2D<float4> A, B;
 
@@ -9,17 +19,14 @@ struct ResStruct {
 };
 
 static ResStruct RS = { { A, B } };
+static const ResStruct RS_const = { { A, B } };
 
 float4 main() : OUT {
-  return RS.arr[1].Load(uint3(0,0,0));
-}
-
-// TODO: The following still doesn't work because use of DxilValueCache
-// does not extend to LoadAndStorePromoter used in PromoteStaticGlobalResources
-float4 main2() : OUT {
-  float4 result = 0;
+  float4 result = RS.arr[1].Load(uint3(0,0,0))
+                + RS_const.arr[1].Load(uint3(0,0,0));
   [unroll]
   for (uint i = 0; i < 2; i++)
-    result += RS.arr[i].Load(uint3(0,0,0));
+    result += RS.arr[i].Load(uint3(0,0,0))
+            + RS_const.arr[i].Load(uint3(0,0,0));
   return result;
 }
