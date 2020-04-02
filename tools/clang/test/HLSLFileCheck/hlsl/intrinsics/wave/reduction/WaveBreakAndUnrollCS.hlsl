01391f5efde9fb2b3f7262767219d52720d60492
Greg Roth <grroth@microsoft.com>
2020-04-01 21:29:00 -0700

    Improve test shader for value cache

--- tools/clang/test/HLSLFileCheck/hlsl/intrinsics/wave/reduction/WaveBreakAndUnrollCS.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/intrinsics/wave/reduction/WaveBreakAndUnrollCS.hlsl
@@ -1,21 +1,41 @@
 // RUN: %dxc -T cs_6_0 %s | FileCheck %s
 // A test of explicit loop unrolling on a loop that uses a wave op in a break block
 
-StructuredBuffer<float> t0;
-RWStructuredBuffer<float> u0;
+// CHECK: void @main
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: @dx.op.cbufferLoadLegacy.i32
+// CHECK: br i1
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: @dx.op.cbufferLoadLegacy.i32
+// CHECK: br i1
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: @dx.op.cbufferLoadLegacy.i32
+// CHECK: br i1
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: @dx.op.cbufferLoadLegacy.i32
+// CHECK: br i1
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: @dx.op.cbufferLoadLegacy.i32
+// CHECK: br i1
+// CHECK: @dx.op.cbufferLoadLegacy.f32
+// CHECK: br
+// CHECK: ret void
 
-[RootSignature("DescriptorTable(SRV(t0), UAV(u0))")]
+RWStructuredBuffer<float> u0;
+uint C;
+float f;
 [numthreads(64,1,1)]
 void main(uint GI : SV_GroupIndex)
 {
-  float r = 0;
-  [unroll]
-  for (int i = 0; i < 8; ++i) {
-    r += t0[i];
-    if (i > 4) {
-      r += WaveActiveSum(t0[i+1]);
-      break;
+    float r = 0;
+    [unroll]
+    for (int i = 0; i < C && i < 64; ++i) {
+        r += WaveActiveSum(f);
+        if (i > 4) {
+          r *= 2;
+          break;
+        }
     }
-  }
-  u0[GI] = r;
+
+    u0[GI] = r;
 }
