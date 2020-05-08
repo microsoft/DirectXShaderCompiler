ed8c02e9e2c2bfbc5ade4c48625f28fcc0ee01fa
Helena Kotas <hekotas@microsoft.com>
2020-05-08 13:07:55 -0700

    hcttest.cmd: Enable running standalone dxilconv tests
    Adds an option to hcttest to specify a custom set of binaries to copy to a test folder
    plus a custom location of dxilconv.dll

--- projects/dxilconv/unittests/DxilConvTests.cpp
+++ projects/dxilconv/unittests/DxilConvTests.cpp
@@ -226,7 +226,7 @@ private:
 
 bool DxilConvTest::InitSupport() {
   if (!m_dllSupport.IsEnabled()) {
-    VERIFY_SUCCEEDED(m_dllSupport.Initialize());
+    VERIFY_SUCCEEDED(m_dllSupport.InitializeForDll(L"dxilconv.dll", "DxcCreateInstance"));
   }
 
   if (!FindFxc()) {
