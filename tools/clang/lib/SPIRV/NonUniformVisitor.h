352d8da2d42447460256b58588cf6e4a8ee2c7f3
Ehsan Nasiri <ehsannas@gmail.com>
2020-05-19 13:53:55 -0400

    [spirv] Add comments about NonUniform decoration handling.

--- tools/clang/lib/SPIRV/NonUniformVisitor.h
+++ tools/clang/lib/SPIRV/NonUniformVisitor.h
@@ -19,10 +19,18 @@ namespace spirv {
 
 class SpirvBuilder;
 
+/// Propagates the NonUniform decoration. According to the Vulkan Spec:
+///
+/// If an instruction loads from or stores to a resource (including atomics and
+/// image instructions) and the resource descriptor being accessed is not
+/// dynamically uniform, then the operand corresponding to that resource (e.g.
+/// the pointer or sampled image operand) must be decorated with NonUniformEXT.
+///
 class NonUniformVisitor : public Visitor {
 public:
   NonUniformVisitor(SpirvContext &spvCtx, const SpirvCodeGenOptions &opts)
       : Visitor(opts, spvCtx) {}
+
 
   bool visit(SpirvLoad *);
   bool visit(SpirvAccessChain *);
