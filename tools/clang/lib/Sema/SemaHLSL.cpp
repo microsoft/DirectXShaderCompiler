e21874df918092674a164310d6201d48311f4424
Tex Riddell <texr@microsoft.com>
2020-05-20 20:24:56 -0700

    Fix assert from LookupVectorMemberExprForHLSL calling Matrix function (#2899)

--- tools/clang/lib/Sema/SemaHLSL.cpp
+++ tools/clang/lib/Sema/SemaHLSL.cpp
@@ -10150,7 +10150,7 @@ ExprResult hlsl::LookupVectorMemberExprForHLSL(
   SourceLocation MemberLoc)
 {
   return HLSLExternalSource::FromSema(self)
-    ->LookupMatrixMemberExprForHLSL(BaseExpr, MemberName, IsArrow, OpLoc, MemberLoc);
+    ->LookupVectorMemberExprForHLSL(BaseExpr, MemberName, IsArrow, OpLoc, MemberLoc);
 }
 
 ExprResult hlsl::LookupArrayMemberExprForHLSL(
@@ -10162,7 +10162,7 @@ ExprResult hlsl::LookupArrayMemberExprForHLSL(
   SourceLocation MemberLoc)
 {
   return HLSLExternalSource::FromSema(self)
-    ->LookupMatrixMemberExprForHLSL(BaseExpr, MemberName, IsArrow, OpLoc, MemberLoc);
+    ->LookupArrayMemberExprForHLSL(BaseExpr, MemberName, IsArrow, OpLoc, MemberLoc);
 }
 
 bool hlsl::LookupRecordMemberExprForHLSL(
