57f66744acff6028e47fcf7d3b34f510b3ee2e3d
Adam Yang <laevus.y@gmail.com>
2020-05-24 14:55:37 -0700

    Added comments in test

--- tools/clang/test/HLSLFileCheck/dxil/debug/new_noops_memcpy2.hlsl
+++ tools/clang/test/HLSLFileCheck/dxil/debug/new_noops_memcpy2.hlsl
@@ -7,7 +7,7 @@ float4 main(float4 color : COLOR) : SV_Target {
 
   foo f1 = color;
   // CHECK: dx.nothing.a, i32 0, i32 0), !dbg !{{[0-9]+}} ; line:8
-  
+
   foo f2[2];
 
   f2[0].y = color.y;
@@ -15,15 +15,15 @@ float4 main(float4 color : COLOR) : SV_Target {
 
   f2[1].w = color.w;
   // CHECK: dx.nothing.a, i32 0, i32 0), !dbg !{{[0-9]+}} ; line:16
-  
+
   foo arrayOfFooArray[2][2];
 
-  arrayOfFooArray[0] = f2;
+  arrayOfFooArray[0] = f2; // This is a memcpy
   // CHECK: dx.nothing.a, i32 0, i32 0), !dbg !{{[0-9]+}} ; line:21
 
-  arrayOfFooArray[1] = f2;
+  arrayOfFooArray[1] = f2; // This is a memcpy
   // CHECK: dx.nothing.a, i32 0, i32 0), !dbg !{{[0-9]+}} ; line:24
-  
+
   return float4(f1.x + f1.z, + f2[0].y + f2[1].w, arrayOfFooArray[0][0].y, arrayOfFooArray[1][1].w) + color;
   // CHECK: dx.nothing.a, i32 0, i32 0), !dbg !{{[0-9]+}} ; line:27
 }
