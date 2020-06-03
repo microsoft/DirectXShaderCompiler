5a364f5f36c1e1fdd57bba215f30a0298eb7d23c
Vishal C Sharma <visshar@microsoft.com>
2020-06-03 11:30:08 -0700

    updates

--- tools/clang/test/HLSLFileCheck/hlsl/diagnostics/warnings/constexpr_cast_overflow_warning.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/diagnostics/warnings/constexpr_cast_overflow_warning.hlsl
@@ -1,36 +1,30 @@
-// RUN: %dxc -E real_lit_to_flt_warning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// RUN: %dxc -E real_lit_to_flt_nowarning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// RUN: %dxc -E real_lit_to_half_warning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// RUN: %dxc -E real_lit_to_half_nowarning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// RUN: %dxc -E int_lit_to_half_warning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// RUN: %dxc -E int_lit_to_half_nowarning -T vs_6_0 %s | FileCheck -check-prefix=CHK_WARNING %s
-// CHK_FLT_WARN: warning: overflow in the expression
-
+// RUN: %dxc -E real_lit_to_flt_warning -T vs_6_0 %s | FileCheck %s
+// CHECK: warning:
 
 // Verify that when a constant is cast to a different type leading to overflow
 // a warning is generated notifying the same. It also verifies that if no overflow
 // happens, then no warning is reported
 
-float real_lit_to_flt_warning() {  
+float real_lit_to_flt_warning() : OUT {  
   return 3.4e50;
 }
 
-float real_lit_to_flt_nowarning() {
+float real_lit_to_flt_nowarning() : OUT {
   return 3.4e10;
 }
 
-min16float real_lit_to_half_warning() {
+min16float real_lit_to_half_warning() : OUT {
   return 65520.0;
 }
 
-min16float real_lit_to_half_nowarning() {
+min16float real_lit_to_half_nowarning() : OUT {
   return 65500.0;
 }
 
-min16float int_lit_to_half_warning() {
+min16float int_lit_to_half_warning() : OUT {
   return 65520;
 }
 
-min16float int_lit_to_half_nowarning() {
+min16float int_lit_to_half_nowarning() : OUT {
   return 65500;
 }
