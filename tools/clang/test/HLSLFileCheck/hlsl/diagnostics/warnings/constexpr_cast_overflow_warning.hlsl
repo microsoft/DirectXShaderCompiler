6762007de1717a0e32c4669397c353fde060865c
Vishal C Sharma <visshar@microsoft.com>
2020-06-03 12:01:14 -0700

    update test

--- tools/clang/test/HLSLFileCheck/hlsl/diagnostics/warnings/constexpr_cast_overflow_warning.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/diagnostics/warnings/constexpr_cast_overflow_warning.hlsl
@@ -1,30 +1,19 @@
 // RUN: %dxc -E real_lit_to_flt_warning -T vs_6_0 %s | FileCheck %s
-// CHECK: warning:
+// RUN: %dxc -E real_lit_to_half_warning -T vs_6_0 %s | FileCheck %s
+// RUN: %dxc -E int_lit_to_half_warning -T vs_6_0 %s | FileCheck %s
+// CHECK: warning: overflow in the expression
 
 // Verify that when a constant is cast to a different type leading to overflow
-// a warning is generated notifying the same. It also verifies that if no overflow
-// happens, then no warning is reported
+// a warning is generated notifying the same.
 
-float real_lit_to_flt_warning() : OUT {  
+float real_lit_to_flt_warning() {  
   return 3.4e50;
 }
 
-float real_lit_to_flt_nowarning() : OUT {
-  return 3.4e10;
-}
-
-min16float real_lit_to_half_warning() : OUT {
+min16float real_lit_to_half_warning() {
   return 65520.0;
 }
 
-min16float real_lit_to_half_nowarning() : OUT {
-  return 65500.0;
-}
-
-min16float int_lit_to_half_warning() : OUT {
+min16float int_lit_to_half_warning() {
   return 65520;
-}
-
-min16float int_lit_to_half_nowarning() : OUT {
-  return 65500;
 }
