dc28ad5624015950b90b6e983f62ae87d2395eea
Xiang Li <xiagli@microsoft.com>
2020-07-13 13:50:40 -0700

    Add debug info for constant in cbuffer.

---
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/CbufferLegacy/write_const_debug_info.hlsl
@@ -0,0 +1,20 @@
+// RUN: %dxc -E main -T ps_6_0 -Gec -Zi %s | FileCheck %s
+
+// Make sure debug info for s.
+// CHECK:!DIGlobalVariable(name: "s.0"
+
+struct S {
+  float a;
+};
+
+cbuffer A {
+  S s;
+};
+
+
+
+float main() : SV_Target {
+
+  s.a++;
+  return s.a;
+}
