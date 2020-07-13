38c529cbd674eeb639e3628b93e510ef2a2b118e
Vishal C Sharma <visshar@microsoft.com>
2020-07-13 08:36:13 -0700

    Update

--- tools/clang/lib/CodeGen/CGHLSLMSFinishCodeGen.cpp
+++ tools/clang/lib/CodeGen/CGHLSLMSFinishCodeGen.cpp
@@ -1530,19 +1530,20 @@ Value *TryEvalIntrinsic(CallInst *CI, IntrinsicOp intriOp, unsigned hlslVersion)
   case IntrinsicOp::IOP_round: {
     // Both FXC and DXC would apply nearest even rounding mode on variables, but on compile-time constants
     // it would apply away from zero for midway values. This would cause differences in results in cases such
-    // as round(x) (where x is 0.5) and round(0.5) where the former would evaluate to 1 and latter would
+    // as round(x) (where x is 0.5) and round(0.5) where the former would evaluate to 1 and the latter would
     // evaluate to 0.
     //
-    // For compatibility with FXC,  DXC still preserve above behavior for language version 2016 or below.
-    // However for newer language version, DXC would always use nearest even for round() intrinsic in all
+    // For compatibility with FXC,  DXC still preserves the above behavior for language versions 2016 or below.
+    // However, for newer language versions, DXC now always use nearest even for round() intrinsic in all
     // cases.
     if (hlslVersion <= 2016) {
       return EvalUnaryIntrinsic(CI, roundf, round);
     } else {
       auto roundingMode = fegetround();
       fesetround(FE_TONEAREST);
-      return EvalUnaryIntrinsic(CI, nearbyint, nearbyint);
+      Value *result = EvalUnaryIntrinsic(CI, nearbyint, nearbyint);
       fesetround(roundingMode);
+      return result;
     }
   } break;
   case IntrinsicOp::IOP_trunc: {
