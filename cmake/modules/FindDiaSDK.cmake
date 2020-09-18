df3d0980adcbca79b11959c28a5d8ba666e1407c
Helena Kotas <hekotas@microsoft.com>
2020-09-17 17:42:08 -0700

    Enable Visual Studio 2019 build by default (#3138)
    This PR is switching the default build of DXC to Visual Studio 2019.
    The build script hctbuild.cmd will error when VS2019 is not installed. Use -vs2017 option to build with VS2017.
    VS2015 is no longer supported.
    
    Removes cmake version checking from hctstart.cmd
    Fixes cmake detection in Visual Studio installations
    Fixes DIA SDK lookup - use CMAKE_GENERATOR to detect VS version used and select the corresponding DIA SDK
    Use environment variable instead of c:\Program Files

--- cmake/modules/FindDiaSDK.cmake
+++ cmake/modules/FindDiaSDK.cmake
@@ -1,37 +1,27 @@
 # Find the DIA SDK path.
-get_filename_component(VS_PATH32 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0;InstallDir]" ABSOLUTE CACHE)
-get_filename_component(VS_PATH64 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\14.0;InstallDir]" ABSOLUTE CACHE)
-# VS_PATH32 will be something like C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE
+# It will typically look something like this:
+# C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\DIA SDK\include
 
-# Also look for in vs15 or vs16 install.
-set(PROGRAMFILES_X86 "ProgramFiles(x86)")
-set(VS_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio")
-get_filename_component(VS15_C_PATH32 "${VS_PATH32}/2017/Community/Common7/IDE" ABSOLUTE CACHE)
-get_filename_component(VS15_P_PATH32 "${VS_PATH32}/2017/Professional/Common7/IDE" ABSOLUTE CACHE)
-get_filename_component(VS15_E_PATH32 "${VS_PATH32}/2017/Enterprise/Common7/IDE" ABSOLUTE CACHE)
+# CMAKE_GENERATOR_INSTANCE has the location of Visual Studio used
+# i.e. C:/Program Files (x86)/Microsoft Visual Studio/2019/Community
+set(VS_PATH ${CMAKE_GENERATOR_INSTANCE})
+get_filename_component(VS_DIA_INC_PATH "${VS_PATH}/DIA SDK/include" ABSOLUTE CACHE)
 
 # Starting in VS 15.2, vswhere is included.
 # Unclear what the right component to search for is, might be Microsoft.VisualStudio.Component.VC.DiagnosticTools
 # (although the friendly name of that is C++ profiling tools).  The toolset is the most likely target.
+set(PROGRAMFILES_X86 "ProgramFiles(x86)")
 execute_process(
-  COMMAND "${VS_PATH32}/Installer/vswhere.exe" -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+  COMMAND "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
   OUTPUT_VARIABLE VSWHERE_LATEST
   ERROR_QUIET
   OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
-# Find the DIA SDK path, it will typically look something like this.
-# C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\DIA SDK\include
-# C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\DIA SDK\include
-# C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include
 find_path(DIASDK_INCLUDE_DIR    # Set variable DIASDK_INCLUDE_DIR
           dia2.h                # Find a path with dia2.h
+          HINTS "${VS_DIA_INC_PATH}"
           HINTS "${VSWHERE_LATEST}/DIA SDK/include"
-          HINTS "${VS15_C_PATH32}/../../DIA SDK/include" 
-          HINTS "${VS15_P_PATH32}/../../DIA SDK/include"
-          HINTS "${VS15_E_PATH32}/../../DIA SDK/include"
-          HINTS "${VS_PATH64}/../../DIA SDK/include"
-          HINTS "${VS_PATH32}/../../DIA SDK/include"
           DOC "path to DIA SDK header files"
           )
 
