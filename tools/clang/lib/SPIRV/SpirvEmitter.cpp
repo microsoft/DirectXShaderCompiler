da6a6740a8adc3b4e3069d1a9a9eb74c7f8688a6
Jaebaek Seo <jaebaek@google.com>
2020-10-21 12:45:54 -0400

    [spirv] handle sizeof using Clang's const folding (#3208)
    * [spirv] handle sizeof using clang's const folding
    
    * Code review

--- tools/clang/lib/SPIRV/SpirvEmitter.cpp
+++ tools/clang/lib/SPIRV/SpirvEmitter.cpp
@@ -11984,10 +11984,15 @@ SpirvEmitter::doUnaryExprOrTypeTraitExpr(const UnaryExprOrTypeTraitExpr *expr) {
     return nullptr;
   }
 
+  if (auto *constExpr = tryToEvaluateAsConst(expr)) {
+    constExpr->setRValue();
+    return constExpr;
+  }
+
   AlignmentSizeCalculator alignmentCalc(astContext, spirvOptions);
   uint32_t size = 0, stride = 0;
   std::tie(std::ignore, size) = alignmentCalc.getAlignmentAndSize(
-      expr->getArgumentType(), SpirvLayoutRule::Void,
+      expr->getArgumentType(), SpirvLayoutRule::Scalar,
       /*isRowMajor*/ llvm::None, &stride);
   auto *sizeConst = spvBuilder.getConstantInt(astContext.UnsignedIntTy,
                                               llvm::APInt(32, size));
