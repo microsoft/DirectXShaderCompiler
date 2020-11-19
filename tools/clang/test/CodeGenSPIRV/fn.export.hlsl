12f98c5f000eb1c8620556502a5f72dcfc1ca04b
Ehsan Nasiri <ehsannas@gmail.com>
2020-11-19 13:22:55 -0500

    Get target environment for tests from the test file.

--- tools/clang/test/CodeGenSPIRV/fn.export.hlsl
+++ tools/clang/test/CodeGenSPIRV/fn.export.hlsl
@@ -1,8 +1,9 @@
-// Run: %dxc -T lib_6_3 -Od
+// Run: %dxc -T lib_6_3 -fspv-target-env=universal1.5
 
+// CHECK: OpCapability Shader
+// CHECK: OpCapability Linkage
 RWBuffer< float4 > output : register(u1);
 
-// CHECK: OpEntryPoint GLCompute %dummyEntry "dummyEntry"
 // CHECK: %main = OpFunction %int None
 export int main(inout float4 color) {
   output[0] = color;
