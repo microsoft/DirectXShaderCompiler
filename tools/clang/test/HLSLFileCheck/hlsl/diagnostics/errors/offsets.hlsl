1cd43e309c167ade0dfcb81e7d63664d3ebd0625
Greg Roth <goyoroth@gmail.com>
2020-11-27 03:33:26 -0700

    Add missing GatherCmp test

--- tools/clang/test/HLSLFileCheck/hlsl/diagnostics/errors/offsets.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/diagnostics/errors/offsets.hlsl
@@ -1,10 +1,13 @@
 // RUN: %dxc -E Range -T ps_6_0 %s | FileCheck %s -check-prefix=CHK_RANGE
 // RUN: %dxc -E VarOffset -T ps_6_0 %s | FileCheck %s -check-prefix=CHK_VAROFF
-xo
+
+// CHK_RANGE:  error: offset texture instructions must take offset which can resolve to integer literal in the range -8 to 7.
 // CHK_RANGE:  error: offset texture instructions must take offset which can resolve to integer literal in the range -8 to 7.
 // CHK_RANGE:  error: offset texture instructions must take offset which can resolve to integer literal in the range -8 to 7.
 // CHK_RANGE:  error: offset texture instructions must take offset which can resolve to integer literal in the range -8 to 7.
 
+// CHK_VAROFF: Offsets to texture access operations must be immediate values
+// CHK_VAROFF: Offsets to texture access operations must be immediate values
 // CHK_VAROFF: Offsets to texture access operations must be immediate values
 // CHK_VAROFF: Offsets to texture access operations must be immediate values
 // CHK_VAROFF: Offsets to texture access operations must be immediate values
@@ -15,13 +18,15 @@ xo
 
 Texture2D t;
 SamplerState s;
+SamplerComparisonState sc;
 
 float4 Range(float2 uv : UV, uint2 offset : O) : SV_TARGET
 {
     float4 a = t.GatherRed(s, uv, int2(-9,-8));
-    float4 b = t.Sample(s, uv, int2(-9,8));
-    float4 c = t.Load(0, int2(8, 9));
-    return a + b + c;
+    float4 b = t.Sample(s, uv, int2(-18,19));
+    float4 c = t.Load(0, int2(80, 90));
+    float4 d = t.GatherCmp(sc, uv, 0.0, int2(999999, -999999));
+    return a + b + c + d;
 }
 
 float4 VarOffset(float2 uv : UV, uint2 offset : O) : SV_TARGET
@@ -29,5 +34,6 @@ float4 VarOffset(float2 uv : UV, uint2 offset : O) : SV_TARGET
     float4 a = t.GatherRed(s, uv, offset);
     float4 b = t.Sample(s, uv, offset);
     float4 c = t.Load(0, offset);
-    return a + b + c;
+    float4 d = t.GatherCmp(sc, uv, 0.0, offset);
+    return a + b + c + d;
 }
