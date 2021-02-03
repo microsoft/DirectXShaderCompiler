7edbebd8e447e5da5c1e2d19cdacc59ae9e16ee2
Vishal C Sharma <visshar@microsoft.com>
2021-02-02 19:52:03 -0800

    Have separate tests for row-major and col-major

--- tools/clang/test/HLSLFileCheck/hlsl/objects/ByteAddressBuffer/rwbab_store_struct_with_mat_zpr.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/ByteAddressBuffer/rwbab_store_struct_with_mat_zpr.hlsl
@@ -1,5 +1,5 @@
-// RUN: %dxc -E main -T vs_6_5 /DTEST1=1 %s | FileCheck %s -check-prefix=CHK_TEST1
-// RUN: %dxc -E main -T vs_6_5 %s | FileCheck %s -check-prefix=CHK_TEST2
+// RUN: %dxc -E main -Zpr -T vs_6_5 /DTEST1=1 %s | FileCheck %s -check-prefix=CHK_TEST1
+// RUN: %dxc -E main -Zpr -T vs_6_5 %s | FileCheck %s -check-prefix=CHK_TEST2
 
 // Regression test for github issue# #3226
 
@@ -15,8 +15,7 @@ struct S
   float2x2 mat;
   float f1; 
   int i1; 
-  struct S1
-  {
+  struct S1 {
       float1x1 mat1; // nested struct
   } s1;
 };
@@ -24,23 +23,15 @@ struct S
 
 RWByteAddressBuffer buf;
 
-void main()
+void main(S a : IN0)
 {
-
-#ifdef TEST1
-    S t = {{1}};
-#else
-    S t = {{1, 2}, {3, 4}, 5.f, 6, {7}};
-#endif
+    S t = a;
+    
     // CHK_TEST1: dx.op.rawBufferStore.f32
-    // CHK_TEST1: float 1.000000e+00
     
     // CHK_TEST2: dx.op.rawBufferStore.f32
-    // CHK_TEST2: float 1.000000e+00, float 2.000000e+00, float 3.000000e+00, float 4.000000e+00
     // CHK_TEST2: dx.op.rawBufferStore.f32
-    // CHK_TEST2: float 5.000000e+00
     // CHK_TEST2: dx.op.rawBufferStore.i32
     // CHK_TEST2: dx.op.rawBufferStore.f32
-    // CHK_TEST2: float 7.000000e+00
     buf.Store(0, t);
 }
