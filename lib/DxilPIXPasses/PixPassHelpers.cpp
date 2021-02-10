fa8e001f4fe8d60353689838ee783544bb3bb776
Jeff Noyle <jeffno@ntdev.microsoft.com>
2021-02-10 11:29:23 -0800

    Adam's suggestion

--- lib/DxilPIXPasses/PixPassHelpers.cpp
+++ lib/DxilPIXPasses/PixPassHelpers.cpp
@@ -7,16 +7,7 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "dxc/DXIL/DxilConstants.h"
-#include "dxc/DXIL/DxilModule.h"
-#include "dxc/DXIL/DxilResourceBase.h"
-#include "dxc/DxilPIXPasses/DxilPIXPasses.h"
-#include "llvm/IR/DebugInfo.h"
-#include "llvm/IR/DebugInfoMetadata.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/IntrinsicInst.h"
-#include "llvm/IR/Intrinsics.h"
+#include "dxc/DXIL/DxilOperations.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 
@@ -24,17 +15,7 @@ namespace PIXPassHelpers
 {
     bool IsAllocateRayQueryInstruction(llvm::Value* Val) {
         if (llvm::Instruction* Inst = llvm::dyn_cast<llvm::Instruction>(Val)) {
-            if (Inst->getOpcode() == llvm::Instruction::OtherOps::Call) {
-                if (Inst->getNumOperands() > 0) {
-                    if (auto* asInt =
-                        llvm::cast_or_null<llvm::ConstantInt>(Inst->getOperand(0))) {
-                        if (asInt->getZExtValue() ==
-                            (uint64_t)hlsl::DXIL::OpCode::AllocateRayQuery) {
-                            return true;
-                        }
-                    }
-                }
-            }
+            return hlsl::OP::IsDxilOpFuncCallInst(Inst, hlsl::OP::OpCode::AllocateRayQuery);
         }
         return false;
     }
