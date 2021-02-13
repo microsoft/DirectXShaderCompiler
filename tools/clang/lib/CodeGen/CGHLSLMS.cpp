3dbcea200df9d0d08e0117d8da340081cef59f3a
Xiang Li <xiagli@microsoft.com>
2021-02-13 10:34:32 -0800

    Fix assert when use glc resource as call arg.

--- tools/clang/lib/CodeGen/CGHLSLMS.cpp
+++ tools/clang/lib/CodeGen/CGHLSLMS.cpp
@@ -2383,6 +2383,9 @@ void CGMSHLSLRuntime::MarkRetTemp(CodeGenFunction &CGF, Value *V,
 void CGMSHLSLRuntime::MarkCallArgumentTemp(CodeGenFunction &CGF, llvm::Value *V,
                                            clang::QualType QualTy) {
   // Save object properties for call arg temp.
+  // Igonre V already in property map.
+  if (objectProperties.GetResource(V).isValid())
+    return;
   AddValToPropertyMap(V, QualTy);
 }
 
