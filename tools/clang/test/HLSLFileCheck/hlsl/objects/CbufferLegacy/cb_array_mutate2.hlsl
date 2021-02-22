e80ca5c755af9ccf18cc75d94428f24b8c0c32fa
Xiang Li <xiagli@microsoft.com>
2021-02-22 11:50:43 -0800

    Decide vector size based on EltTy when lower dynamic vector indexing for cbuffer.

--- tools/clang/test/HLSLFileCheck/hlsl/objects/CbufferLegacy/cb_array_mutate2.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/CbufferLegacy/cb_array_mutate2.hlsl
@@ -3,8 +3,7 @@
 // CHECK:%[[ID:[0-9]+]] = call i32 @dx.op.loadInput
 // CHECK:lshr i32 %[[ID]], 1
 // CHECK:and i32 %[[ID]], 1
-// Make sure only 2 cb load.
-// CHECK:call %dx.types.CBufRet.i64 @dx.op.cbufferLoadLegacy.i64
+// Make sure only 1 cb load.
 // CHECK:call %dx.types.CBufRet.i64 @dx.op.cbufferLoadLegacy.i64
 // CHECK-NOT:call %dx.types.CBufRet
 
