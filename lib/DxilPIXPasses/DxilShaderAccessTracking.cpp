e9081ecd43ed49f26c9a2574b1b94d6e78f9f250
Jeff Noyle <jeffno@ntdev.microsoft.com>
2021-03-12 13:46:20 -0800

    Redundant

--- lib/DxilPIXPasses/DxilShaderAccessTracking.cpp
+++ lib/DxilPIXPasses/DxilShaderAccessTracking.cpp
@@ -595,8 +595,7 @@ DxilShaderAccessTracking::GetResourceFromHandle(Value *resHandle,
               ? AccessStyle::SamplerFromDescriptorHeap : AccessStyle::ResourceFromDescriptorHeap;
           ret.dynamicallyBoundIndex = createHandleFromHeap.get_index();
 
-          auto props = resource_helper::loadPropsFromConstant(*dyn_cast<Constant>(annotateHandle.get_props()));
-          ret.registerType = RegisterTypeFromResourceClass(props.getResourceClass());
+          ret.registerType = RegisterTypeFromResourceClass(properties.getResourceClass());
 
           DynamicResourceBinding drb{};
           drb.HeapIsSampler = createHandleFromHeap.get_samplerHeap_val();
