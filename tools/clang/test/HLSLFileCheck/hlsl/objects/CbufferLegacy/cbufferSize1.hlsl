20396101903c899c98dc3f3fcb35381f0b281f9b
Tex Riddell <texr@microsoft.com>
2021-03-21 15:00:53 -0700

    Fix Dxil validator compat and test issues (#3610)

--- tools/clang/test/HLSLFileCheck/hlsl/objects/CbufferLegacy/cbufferSize1.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/CbufferLegacy/cbufferSize1.hlsl
@@ -1,4 +1,4 @@
-// RUN: %dxc -E main -T ps_6_0 %s | FileCheck %s
+// RUN: %dxilver 1.6 | %dxc -E main -T ps_6_0 %s | FileCheck %s
 
 // CHECK: error: CBuffer size is 65552 bytes, exceeding maximum of 65536 bytes.
 
