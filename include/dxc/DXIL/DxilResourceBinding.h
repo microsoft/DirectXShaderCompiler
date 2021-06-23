ceb1818898cbce86ae2105a0f2f9a94804fd2e03
Adam Yang <hanbyang@microsoft.com>
2021-06-22 22:33:33 -0700

    Fixed the file name in the content

--- include/dxc/DXIL/DxilResourceBinding.h
+++ include/dxc/DXIL/DxilResourceBinding.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-// DxcBindingTable.h                                                        //
+// DxilResourceBinding.h                                                     //
 // Copyright (C) Microsoft Corporation. All rights reserved.                 //
 // This file is distributed under the University of Illinois Open Source     //
 // License. See LICENSE.TXT for details.                                     //
