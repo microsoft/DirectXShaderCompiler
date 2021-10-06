086112d02e512ac22a0121e73a76a2521dfe3e22
Vishal C Sharma <visshar@microsoft.com>
2021-10-06 12:52:37 -0700

    Update test

--- tools/clang/test/HLSLFileCheck/shader_targets/library/lib_no_vanilla_fn_attributes.hlsl
+++ tools/clang/test/HLSLFileCheck/shader_targets/library/lib_no_vanilla_fn_attributes.hlsl
@@ -2,15 +2,19 @@
 
 // Make sure a bunch of vanilla function attributes are not added to the functions which are not inlined. 
 
-// CHECK-NOT: "disable-tail-calls"="false"
-// CHECK-NOT: "less-precise-fpmad"="false"
-// CHECK-NOT: "no-frame-pointer-elim"="false"
-// CHECK-NOT: "no-infs-fp-math"="false" 
-// CHECK-NOT: "no-nans-fp-math"="false" 
-// CHECK-NOT: "no-realign-stack" 
-// CHECK-NOT: "stack-protector-buffer-size"="0" 
-// CHECK-NOT: "unsafe-fp-math"="false" 
-// CHECK-NOT: "use-soft-float"="false"
+// CHECK: define float @"\01?foo@@YAMM@Z"(float %a)
+// CHECK: nounwind
+// CHECK: readnone
+// CHECK-NOT: disable-tail-calls
+// CHECK-NOT: less-precise-fpmad
+// CHECK-NOT: no-frame-pointer-elim
+// CHECK-NOT: no-infs-fp-math
+// CHECK-NOT: no-nans-fp-math
+// CHECK-NOT: no-realign-stack 
+// CHECK-NOT: stack-protector-buffer-size
+// CHECK-NOT: unsafe-fp-math
+// CHECK-NOT: use-soft-float
+
 
 export float foo(float a) {
     return a*a;
