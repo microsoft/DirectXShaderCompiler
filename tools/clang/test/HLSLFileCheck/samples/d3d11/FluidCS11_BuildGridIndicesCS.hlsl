b111f73125dc680538dbd94be91d8fd08b4a4679
Chris B <cbieneman@microsoft.com>
2021-10-25 09:10:41 -0500

    Convert Windows 1252 to UTF-8 (#4033)
    These testcases all had a Windows 1252 ü character instead of a utf-8
    character. Converting to utf-8 makes the world happy :)

--- tools/clang/test/HLSLFileCheck/samples/d3d11/FluidCS11_BuildGridIndicesCS.hlsl
+++ tools/clang/test/HLSLFileCheck/samples/d3d11/FluidCS11_BuildGridIndicesCS.hlsl
@@ -13,7 +13,7 @@
 //--------------------------------------------------------------------------------------
 // Smoothed Particle Hydrodynamics Algorithm Based Upon:
 // Particle-Based Fluid Simulation for Interactive Applications
-// Matthias Müller
+// Matthias MÃ¼ller
 //--------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------
