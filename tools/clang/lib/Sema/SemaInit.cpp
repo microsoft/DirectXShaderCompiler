a49ee0ca50425c2b3364c5f79c260883662ebf6e
Xiang Li <python3kgae@outlook.com>
2021-11-19 11:03:47 -0800

    Set SourceLocation for begin/end of initlist for hlsl. (#4091)
    * Set SourceLocation for begin/end of initlist for hlsl.
    
    * Add VectorInitWithCXXFunctionalCastExpr to mark vector init like float4(a, b, c, d).

--- tools/clang/lib/Sema/SemaInit.cpp
+++ tools/clang/lib/Sema/SemaInit.cpp
@@ -6183,7 +6183,11 @@ InitializationSequence::Perform(Sema &S,
             CK_LValueToRValue, Args[i], /*BasePath=*/0, VK_RValue);
         }
       }
-      CurInit = new (S.getASTContext())InitListExpr(S.getASTContext(), SourceLocation(), Args, SourceLocation());
+      InitListExpr *castInit = new (S.getASTContext())
+          InitListExpr(S.getASTContext(), Kind.getParenRange().getBegin(), Args,
+                       Kind.getParenRange().getEnd());
+      castInit->sawVectorInitWithCXXFunctionalCastExpr(true);
+      CurInit = castInit;
     }
     break;
   }
