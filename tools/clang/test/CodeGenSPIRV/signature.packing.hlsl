482981a5d21d0cde2cfbea4677703201b4cacbfb
Jaebaek Seo <jaebaek@google.com>
2021-11-30 06:19:45 -0500

    Fix unit test

--- tools/clang/test/CodeGenSPIRV/signature.packing.hlsl
+++ tools/clang/test/CodeGenSPIRV/signature.packing.hlsl
@@ -4,28 +4,31 @@ struct VS_OUTPUT {
   float4 pos : SV_POSITION;
 
 // CHECK: OpDecorate %out_var_A Location 0
-// CHECK: OpDecorate %out_var_A Component 0
   float a : A;
 
 // CHECK: OpDecorate %out_var_B Location 0
 // CHECK: OpDecorate %out_var_B Component 2
   double b : B;
 
-// CHECK: OpDecorate %out_var_C Location 1
+// CHECK: OpDecorate %out_var_C_0 Location 1
+// CHECK: OpDecorate %out_var_C_1 Location 1
+// CHECK: OpDecorate %out_var_C_1 Component 2
+// CHECK: OpDecorate %out_var_C_2 Location 2
   float2 c[3] : C;
 
-// CHECK: OpDecorate %out_var_D Location 4
+// CHECK: OpDecorate %out_var_D_0 Location 2
+// CHECK: OpDecorate %out_var_D_0 Component 2
+// CHECK: OpDecorate %out_var_D_1 Location 3
   float2x2 d : D;
 
-// CHECK: OpDecorate %out_var_E Location 1
+// CHECK: OpDecorate %out_var_E Location 3
 // CHECK: OpDecorate %out_var_E Component 2
   int e : E;
 
-// CHECK: OpDecorate %out_var_F Location 2
-// CHECK: OpDecorate %out_var_F Component 2
+// CHECK: OpDecorate %out_var_F Location 4
   float2 f : F;
 
-// CHECK: OpDecorate %out_var_G Location 1
+// CHECK: OpDecorate %out_var_G Location 3
 // CHECK: OpDecorate %out_var_G Component 3
   float g : G;
 };
