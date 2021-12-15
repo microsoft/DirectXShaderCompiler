71a4c821af812826cf6b29f28d362e843e2684ae
Chris Bieneman <cbieneman@microsoft.com>
2021-12-15 11:17:35 -0600

    Enable running multiple run lines in verifier tests
    Several of our verifier tests have multiple run lines, but the testing
    infrastructure was only running one run line.

--- tools/clang/unittests/HLSL/VerifierTest.cpp
+++ tools/clang/unittests/HLSL/VerifierTest.cpp
@@ -98,7 +98,9 @@ public:
     const char startMarker[] = "%clang_cc1";
     const char endMarker[] = "%s";
 
-    char firstLine[200];
+    // I bumped this up to 1024, if we have a run line that large in our tests
+    // bad will happen, and we probably deserve the pain...
+    char firstLine[1024];
     memset(firstLine, 0, sizeof(firstLine));
 
     char* commandLine;
@@ -114,30 +116,44 @@ public:
     // and chopping off everything after '%s'.
     //
 
-    {
-      ifstream infile(CW2A(path).m_psz);
-      ASSERT_EQ(false, infile.bad());
+    ifstream infile(CW2A(path).m_psz);
+    ASSERT_EQ(false, infile.bad());
 
+    bool FoundRun = false;
+
+    // This loop is super hacky, and not at all how we should do this. We should
+    // have _one_ implementation of reading and interpreting RUN but instead we
+    // currently have many, and this one only supports one RUN directive, which
+    // is confusing and has resulted in test cases that aren't being run at all,
+    // and are hiding bugs.
+
+    // The goal of this loop is to process RUN lines at the top of the file,
+    // until the first non-run line. This also isn't ideal, but is better.
+    while (!infile.eof()) {
       infile.getline(firstLine, _countof(firstLine));
-      char* found = strstr(firstLine, startMarker);
-      ASSERT_NE(nullptr, found);
+      char *found = strstr(firstLine, startMarker);
+      if (found)
+        FoundRun = true;
+      else
+        break;
 
       commandLine = found + strlen(startMarker);
 
-      char* fileArgument = strstr(commandLine, endMarker);
+      char *fileArgument = strstr(commandLine, endMarker);
       ASSERT_NE(nullptr, fileArgument);
       *fileArgument = '\0';
-    }
 
-    CW2A asciiPath(path);
-    CompilationResult result = CompilationResult::CreateForCommandLine(commandLine, asciiPath);
-    if (!result.ParseSucceeded()) {
-      std::stringstream ss;
-      ss << "for program " << asciiPath << " with errors:\n" << result.GetTextForErrors();
-      CA2W pszW(ss.str().c_str());
-      ::WEX::Logging::Log::Comment(pszW);
+      CW2A asciiPath(path);
+      CompilationResult result = CompilationResult::CreateForCommandLine(commandLine, asciiPath);
+      if (!result.ParseSucceeded()) {
+        std::stringstream ss;
+        ss << "for program " << asciiPath << " with errors:\n" << result.GetTextForErrors();
+        CA2W pszW(ss.str().c_str());
+        ::WEX::Logging::Log::Comment(pszW);
+      }
+      VERIFY_IS_TRUE(result.ParseSucceeded());
     }
-    VERIFY_IS_TRUE(result.ParseSucceeded());
+    ASSERT_TRUE(FoundRun);
   }
 
   void CheckVerifiesHLSL(LPCWSTR name) {
