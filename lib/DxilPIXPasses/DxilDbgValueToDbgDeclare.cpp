microsoft/DirectXShaderCompiler

commit d23ede6fc58c2019d233054778f36d6d3d0acbf1

PIX: Changes to run PIX passes on libraries. Also add raygen shader debugging prolog (#3985)
These changes are a result of prototype work in PIX to support shader debugging for DXR,, which was an exercise to figure out how to support libraries in general in PIX. These changes are the result. It's all about iterating over all the library functions, rather than "the" entry point function.
In addition, to have something concrete to submit to a driver, there's the addition of codegen for the shader debugging "prolog", which chooses at runtime whether or not to emit instrumentation data for a given shader invocation based on its (in this case) thread id. This last is not yet ready for production in PIX, in that it is necessary but not sufficient to enable DXR shader debugging.

lib/DxilPIXPasses/DxilDbgValueToDbgDeclare.cpp
@@ -240,7 +240,7 @@ class VariableRegisters
 {
 public:
   VariableRegisters(
-      llvm::DebugLoc const &,
+      llvm::DbgValueInst* DbgValue,
       llvm::DIVariable *Variable,
       llvm::DIType* Ty,
       llvm::Module *M
@@ -684,7 +684,7 @@ void DxilDbgValueToDbgDeclare::handleDbgValue(
   if (Register == nullptr)
   {
     Register.reset(
-        new VariableRegisters(DbgValue->getDebugLoc(), Variable, Ty, &M));
+        new VariableRegisters(DbgValue, Variable, Ty, &M));
   }
 
   // Convert the offset from DbgValue's expression to a packed
@@ -808,13 +808,13 @@ static llvm::DIType *DITypePeelTypeAlias(
 #endif // NDEBUG
 
 VariableRegisters::VariableRegisters(
-    llvm::DebugLoc const & dbgLoc,
+    llvm::DbgValueInst *DbgValue,
     llvm::DIVariable *Variable,
     llvm::DIType* Ty,
     llvm::Module *M)
-  : m_dbgLoc(dbgLoc)
-  ,m_Variable(Variable)
-  , m_B(PIXPassHelpers::GetEntryFunction(M->GetOrCreateDxilModule())->getEntryBlock().begin())
+  : m_dbgLoc(DbgValue->getDebugLoc())
+  , m_Variable(Variable)
+  , m_B(DbgValue)
   , m_DbgDeclareFn(llvm::Intrinsic::getDeclaration(
       M, llvm::Intrinsic::dbg_declare))
 {

include/dxc/DXIL/DxilFunctionProps.h
@@ -11,6 +11,9 @@
 
 #pragma once
 
+// for memset dependency:
+#include <cstring>
+
 #include "dxc/DXIL/DxilConstants.h"
 
 namespace llvm {

include/dxc/DXIL/DxilModule.h
@@ -82,6 +82,7 @@ public:
   const llvm::Function *GetPatchConstantFunction() const;
   void SetPatchConstantFunction(llvm::Function *pFunc);
   bool IsEntryOrPatchConstantFunction(const llvm::Function* pFunc) const;
+  llvm::SmallVector<llvm::Function *, 64> GetExportedFunctions() const;
 
   // Flags.
   unsigned GetGlobalFlags() const;

lib/DXIL/DxilModule.cpp
@@ -244,6 +244,14 @@ const Function *DxilModule::GetEntryFunction() const {
   return m_pEntryFunc;
 }
 
+llvm::SmallVector<llvm::Function *, 64> DxilModule::GetExportedFunctions() const {
+    llvm::SmallVector<llvm::Function *, 64> ret;
+    for (auto const& fn : m_DxilEntryPropsMap) {
+        ret.push_back(const_cast<llvm::Function*>(fn.first));
+    }
+    return ret;
+}
+
 void DxilModule::SetEntryFunction(Function *pEntryFunc) {
   if (m_pSM->IsLib()) {
     DXASSERT(pEntryFunc == nullptr,

lib/DxilPIXPasses/DxilAnnotateWithVirtualRegister.cpp
@@ -89,7 +89,10 @@ private:
     m_DM = &M.GetOrCreateDxilModule();
     m_uVReg = 0;
     m_MST.reset(new llvm::ModuleSlotTracker(&M));
-    m_MST->incorporateFunction(*PIXPassHelpers::GetEntryFunction(*m_DM));
+    auto functions = m_DM->GetExportedFunctions();
+    for (auto& fn : functions) {
+      m_MST->incorporateFunction(*fn);
+    }
   }
 };
 
@@ -108,9 +111,12 @@ bool DxilAnnotateWithVirtualRegister::runOnModule(llvm::Module &M) {
   }
 
   std::uint32_t InstNum = 0;
-  for (llvm::Instruction &I : llvm::inst_range(PIXPassHelpers::GetEntryFunction(*m_DM))) {
-    if (!llvm::isa<llvm::DbgDeclareInst>(&I)) {
-      pix_dxil::PixDxilInstNum::AddMD(M.getContext(), &I, InstNum++);
+  auto blocks = PIXPassHelpers::GetAllBlocks(*m_DM);
+  for(auto * block : blocks) {
+    for (llvm::Instruction& I : block->getInstList()) {
+      if (!llvm::isa<llvm::DbgDeclareInst>(&I)) {
+        pix_dxil::PixDxilInstNum::AddMD(M.getContext(), &I, InstNum++);
+      }
     }
   }
 
@@ -126,12 +132,16 @@ bool DxilAnnotateWithVirtualRegister::runOnModule(llvm::Module &M) {
     *OSOverride << "\nBegin - dxil values to virtual register mapping\n";
   }
 
-  for (llvm::Instruction &I : llvm::inst_range(PIXPassHelpers::GetEntryFunction(*m_DM))) {
-    AnnotateValues(&I);
+  for (auto* block : blocks) {
+    for (llvm::Instruction& I : block->getInstList()) {
+      AnnotateValues(&I);
+    }
   }
 
-  for (llvm::Instruction &I : llvm::inst_range(PIXPassHelpers::GetEntryFunction(*m_DM))) {
-    AnnotateStore(&I);
+  for (auto* block : blocks) {
+    for (llvm::Instruction& I : block->getInstList()) {
+      AnnotateStore(&I);
+    }
   }
 
   if (OSOverride != nullptr) {

lib/DxilPIXPasses/DxilDebugInstrumentation.cpp
@@ -9,6 +9,9 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <vector>
+
+#include "dxc/DXIL/DxilFunctionProps.h"
 #include "dxc/DXIL/DxilModule.h"
 #include "dxc/DXIL/DxilOperations.h"
 #include "dxc/DXIL/DxilUtil.h"
@@ -250,15 +253,20 @@ public:
   void applyOptions(PassOptions O) override;
   bool runOnModule(Module &M) override;
 
+  bool RunOnFunction(Module& M, DxilModule& DM, 
+    llvm::Function *function);
+
 private:
-  SystemValueIndices addRequiredSystemValues(BuilderContext &BC);
+  SystemValueIndices addRequiredSystemValues(BuilderContext &BC, DXIL::ShaderKind shaderKind);
   void addInvocationSelectionProlog(BuilderContext &BC,
-                                    SystemValueIndices SVIndices);
+                                    SystemValueIndices SVIndices,
+                                    DXIL::ShaderKind shaderKind);
   Value *addPixelShaderProlog(BuilderContext &BC, SystemValueIndices SVIndices);
   Value *addGeometryShaderProlog(BuilderContext &BC,
                                  SystemValueIndices SVIndices);
   Value *addDispatchedShaderProlog(BuilderContext &BC);
-  Value *addVertexShaderProlog(BuilderContext &BC,
+  Value* addRaygenShaderProlog(BuilderContext& BC);
+  Value* addVertexShaderProlog(BuilderContext& BC,
                                SystemValueIndices SVIndices);
   void addDebugEntryValue(BuilderContext &BC, Value *TheValue);
   void addInvocationStartMarker(BuilderContext &BC);
@@ -287,22 +295,23 @@ uint32_t DxilDebugInstrumentation::UAVDumpingGroundOffset() {
   return static_cast<uint32_t>(m_UAVSize - DebugBufferDumpingGroundSize);
 }
 
-DxilDebugInstrumentation::SystemValueIndices
-DxilDebugInstrumentation::addRequiredSystemValues(BuilderContext &BC) {
+DxilDebugInstrumentation::SystemValueIndices DxilDebugInstrumentation::addRequiredSystemValues(BuilderContext &BC, DXIL::ShaderKind shaderKind) {
   SystemValueIndices SVIndices{};
 
-  hlsl::DxilSignature &InputSignature = BC.DM.GetInputSignature();
-
-  auto &InputElements = InputSignature.GetElements();
-
-  auto ShaderModel = BC.DM.GetShaderModel();
-  switch (ShaderModel->GetKind()) {
+  switch (shaderKind) {
   case DXIL::ShaderKind::Amplification:
   case DXIL::ShaderKind::Mesh:
   case DXIL::ShaderKind::Compute:
+  case DXIL::ShaderKind::RayGeneration:
+  //case DXIL::ShaderKind::Intersection:
+  //case DXIL::ShaderKind::AnyHit:
+  //case DXIL::ShaderKind::ClosestHit:
+  //case DXIL::ShaderKind::Miss:
     // Dispatch* thread Id is not in the input signature
     break;
   case DXIL::ShaderKind::Vertex: {
+    hlsl::DxilSignature &InputSignature = BC.DM.GetInputSignature();
+    auto &InputElements = InputSignature.GetElements();
     {
       auto Existing_SV_VertexId = std::find_if(
           InputElements.begin(), InputElements.end(),
@@ -358,6 +367,9 @@ DxilDebugInstrumentation::addRequiredSystemValues(BuilderContext &BC) {
     // GS Instance Id and Primitive Id are not in the input signature
     break;
   case DXIL::ShaderKind::Pixel: {
+    hlsl::DxilSignature &InputSignature = BC.DM.GetInputSignature();
+    auto &InputElements = InputSignature.GetElements();
+
     auto Existing_SV_Position =
         std::find_if(InputElements.begin(), InputElements.end(),
                      [](const std::unique_ptr<DxilSignatureElement> &Element) {
@@ -423,6 +435,36 @@ Value *DxilDebugInstrumentation::addDispatchedShaderProlog(BuilderContext &BC) {
   auto CompareAll =
       BC.Builder.CreateAnd(CompareXAndY, CompareToZ, "CompareAll");
 
+  return CompareAll;
+}
+
+Value *DxilDebugInstrumentation::addRaygenShaderProlog(BuilderContext &BC) {
+  auto DispatchRaysIndexOpFunc =
+      BC.HlslOP->GetOpFunc(DXIL::OpCode::DispatchRaysIndex, Type::getInt32Ty(BC.Ctx));
+  Constant *DispatchRaysIndexOpcode =
+      BC.HlslOP->GetU32Const((unsigned)DXIL::OpCode::DispatchRaysIndex);
+  auto RayX =
+      BC.Builder.CreateCall(DispatchRaysIndexOpFunc, {DispatchRaysIndexOpcode, BC.HlslOP->GetI8Const(0) }, "RayX");
+  auto RayY =
+      BC.Builder.CreateCall(DispatchRaysIndexOpFunc, {DispatchRaysIndexOpcode, BC.HlslOP->GetI8Const(1) }, "RayY");
+  auto RayZ =
+      BC.Builder.CreateCall(DispatchRaysIndexOpFunc, {DispatchRaysIndexOpcode, BC.HlslOP->GetI8Const(2) }, "RayZ");
+
+  auto CompareToX = BC.Builder.CreateICmpEQ(
+      RayX, BC.HlslOP->GetU32Const(m_Parameters.ComputeShader.ThreadIdX),
+      "CompareToThreadIdX");
+  auto CompareToY = BC.Builder.CreateICmpEQ(
+      RayY, BC.HlslOP->GetU32Const(m_Parameters.ComputeShader.ThreadIdY),
+      "CompareToThreadIdY");
+  auto CompareToZ = BC.Builder.CreateICmpEQ(
+      RayZ, BC.HlslOP->GetU32Const(m_Parameters.ComputeShader.ThreadIdZ),
+      "CompareToThreadIdZ");
+
+  auto CompareXAndY =
+      BC.Builder.CreateAnd(CompareToX, CompareToY, "CompareXAndY");
+
+  auto CompareAll =
+      BC.Builder.CreateAnd(CompareXAndY, CompareToZ, "CompareAll");
   return CompareAll;
 }
 
@@ -548,14 +590,19 @@ DxilDebugInstrumentation::addPixelShaderProlog(BuilderContext &BC,
 }
 
 void DxilDebugInstrumentation::addInvocationSelectionProlog(
-    BuilderContext &BC, SystemValueIndices SVIndices) {
-  auto ShaderModel = BC.DM.GetShaderModel();
-
+    BuilderContext &BC, SystemValueIndices SVIndices, DXIL::ShaderKind shaderKind) {
   Value *ParameterTestResult = nullptr;
-  switch (ShaderModel->GetKind()) {
+  switch (shaderKind) {
+  case DXIL::ShaderKind::RayGeneration:
+    ParameterTestResult = addRaygenShaderProlog(BC);
+    break;
   case DXIL::ShaderKind::Compute:
   case DXIL::ShaderKind::Amplification:
   case DXIL::ShaderKind::Mesh:
+  //case DXIL::ShaderKind::Intersection:
+  //case DXIL::ShaderKind::AnyHit:
+  //case DXIL::ShaderKind::ClosestHit:
+  //case DXIL::ShaderKind::Miss:
     ParameterTestResult = addDispatchedShaderProlog(BC);
     break;
   case DXIL::ShaderKind::Geometry:
@@ -854,12 +901,14 @@ void DxilDebugInstrumentation::addStepDebugEntryValue(
     // subsequent instructions that dereference the pointer will be properly
     // instrumented and show the (meaningful) retrieved value.
     break;
+  case Type::TypeID::VectorTyID:
+    // Shows up in "insertelement" in raygen shader?
+    break;
   case Type::TypeID::FP128TyID:
   case Type::TypeID::LabelTyID:
   case Type::TypeID::MetadataTyID:
   case Type::TypeID::FunctionTyID:
   case Type::TypeID::ArrayTyID:
-  case Type::TypeID::VectorTyID:
   case Type::TypeID::X86_FP80TyID:
   case Type::TypeID::X86_MMXTyID:
   case Type::TypeID::PPC_FP128TyID:
@@ -869,26 +918,58 @@ void DxilDebugInstrumentation::addStepDebugEntryValue(
 
 bool DxilDebugInstrumentation::runOnModule(Module &M) {
   DxilModule &DM = M.GetOrCreateDxilModule();
-  LLVMContext &Ctx = M.getContext();
-  OP *HlslOP = DM.GetOP();
 
   auto ShaderModel = DM.GetShaderModel();
-  switch (ShaderModel->GetKind()) {
+  auto shaderKind = ShaderModel->GetKind();
+
+  bool modified = false;
+  if (shaderKind == DXIL::ShaderKind::Library) {
+    for (llvm::Function& F : M.functions()) {
+      modified = modified | RunOnFunction(M, DM, &F); 
+      return modified;
+    }
+  }
+  else {
+    llvm::Function *entryFunction = PIXPassHelpers::GetEntryFunction(DM);
+    modified = RunOnFunction(M, DM, entryFunction);
+  }
+  return modified;
+}
+
+bool DxilDebugInstrumentation::RunOnFunction(
+  Module &M, 
+  DxilModule &DM,
+  llvm::Function * entryFunction) 
+{
+  if (!DM.HasDxilFunctionProps(entryFunction)) {
+    return false;
+  }
+
+  hlsl::DxilFunctionProps const &props = DM.GetDxilFunctionProps(entryFunction);
+  DXIL::ShaderKind shaderKind = props.shaderKind;
+
+  switch (shaderKind) {
   case DXIL::ShaderKind::Amplification:
   case DXIL::ShaderKind::Mesh:
   case DXIL::ShaderKind::Vertex:
   case DXIL::ShaderKind::Geometry:
   case DXIL::ShaderKind::Pixel:
   case DXIL::ShaderKind::Compute:
+  case DXIL::ShaderKind::RayGeneration:
     break;
+    //todo:
+  case DXIL::ShaderKind::Intersection:
+  case DXIL::ShaderKind::AnyHit:
+  case DXIL::ShaderKind::ClosestHit:
+  case DXIL::ShaderKind::Miss:
   default:
     return false;
   }
 
   // First record pointers to all instructions in the function:
   std::vector<Instruction *> AllInstructions;
-  for (inst_iterator I = inst_begin(PIXPassHelpers::GetEntryFunction(DM)),
-                     E = inst_end(PIXPassHelpers::GetEntryFunction(DM));
+  for (inst_iterator I = inst_begin(entryFunction),
+                     E = inst_end(entryFunction);
        I != E; ++I) {
     AllInstructions.push_back(&*I);
   }
@@ -907,22 +988,26 @@ bool DxilDebugInstrumentation::runOnModule(Module &M) {
   //
 
   Instruction *firstInsertionPt =
-      dxilutil::FirstNonAllocaInsertionPt(PIXPassHelpers::GetEntryFunction(DM));
+      dxilutil::FirstNonAllocaInsertionPt(entryFunction);
   IRBuilder<> Builder(firstInsertionPt);
+
+  LLVMContext &Ctx = M.getContext();
+  OP *HlslOP = DM.GetOP();
 
   BuilderContext BC{M, DM, Ctx, HlslOP, Builder};
 
-  m_HandleForUAV = PIXPassHelpers::CreateUAV(BC.DM, BC.Builder, 0, "PIX_DebugUAV_Handle");
+  m_HandleForUAV =
+      PIXPassHelpers::CreateUAV(BC.DM, BC.Builder, 0, "PIX_DebugUAV_Handle");
 
-  auto SystemValues = addRequiredSystemValues(BC);
-  addInvocationSelectionProlog(BC, SystemValues);
+  auto SystemValues = addRequiredSystemValues(BC, shaderKind);
+  addInvocationSelectionProlog(BC, SystemValues, shaderKind);
   addInvocationStartMarker(BC);
 
-  // Explicitly name new blocks in order to provide stable names for testing purposes
+  // Explicitly name new blocks in order to provide stable names for testing
+  // purposes
   int NewBlockCounter = 0;
 
-  auto Fn = PIXPassHelpers::GetEntryFunction(DM);
-  auto &Blocks = Fn->getBasicBlockList();
+  auto &Blocks = entryFunction->getBasicBlockList();
   for (auto &CurrentBlock : Blocks) {
     struct ValueAndPhi {
       Value *Val;
@@ -945,8 +1030,9 @@ bool DxilDebugInstrumentation::runOnModule(Module &M) {
     }
 
     for (auto &InsertableEdge : InsertableEdges) {
-      auto *NewBlock = BasicBlock::Create(Ctx, "PIXDebug" + std::to_string(NewBlockCounter++),
-                                          InsertableEdge.first->getParent());
+      auto *NewBlock = BasicBlock::Create(
+          Ctx, "PIXDebug" + std::to_string(NewBlockCounter++),
+          InsertableEdge.first->getParent());
       IRBuilder<> Builder(NewBlock);
 
       auto *PreviousBlock = InsertableEdge.first;

lib/DxilPIXPasses/PixPassHelpers.cpp
@@ -72,8 +72,21 @@ llvm::CallInst *CreateHandleForResource(hlsl::DxilModule &DM,
   unsigned int resourceMetaDataId =
       GetNextRegisterIdForClass(DM, resourceClass);
 
-  // Create handle for the newly-added resource
-  if (IsDynamicResourceShaderModel(DM)) {
+  auto const * shaderModel = DM.GetShaderModel();
+  if (shaderModel->IsLib())
+  {
+    llvm::Constant *object = resource->GetGlobalSymbol();
+    auto * load = Builder.CreateLoad(object);
+    Function *CreateHandleFromBindingOpFunc = HlslOP->GetOpFunc(
+        DXIL::OpCode::CreateHandleForLib, resource->GetHLSLType()->getVectorElementType());
+    Constant *CreateHandleFromBindingOpcodeArg =
+        HlslOP->GetU32Const((unsigned)DXIL::OpCode::CreateHandleForLib);
+    auto *handle =
+        Builder.CreateCall(CreateHandleFromBindingOpFunc,
+        {CreateHandleFromBindingOpcodeArg, load });
+    return handle;
+  }
+  else if (IsDynamicResourceShaderModel(DM)) {
     Function *CreateHandleFromBindingOpFunc = HlslOP->GetOpFunc(
         DXIL::OpCode::CreateHandleFromBinding, Type::getVoidTy(Ctx));
     Constant *CreateHandleFromBindingOpcodeArg =
@@ -134,9 +147,22 @@ llvm::CallInst *CreateUAV(DxilModule &DM, IRBuilder<> &Builder,
   SmallVector<llvm::Type *, 1> Elements{Type::getInt32Ty(Ctx)};
   llvm::StructType *UAVStructTy =
       llvm::StructType::create(Elements, "class.RWStructuredBuffer");
+
   std::unique_ptr<DxilResource> pUAV = llvm::make_unique<DxilResource>();
+
+  auto const *shaderModel = DM.GetShaderModel();
+  if (shaderModel->IsLib()) {
+    GlobalVariable *NewGV = cast<GlobalVariable>(
+        DM.GetModule()->getOrInsertGlobal("PIXUAV", UAVStructTy));
+    NewGV->setConstant(false);
+    NewGV->setLinkage(GlobalValue::ExternalLinkage);
+    NewGV->setThreadLocal(false);
+    pUAV->SetGlobalSymbol(NewGV);
+  }
+  else {
+    pUAV->SetGlobalSymbol(UndefValue::get(UAVStructTy->getPointerTo()));
+  }
   pUAV->SetGlobalName(name);
-  pUAV->SetGlobalSymbol(UndefValue::get(UAVStructTy->getPointerTo()));
   pUAV->SetID(GetNextRegisterIdForClass(DM, DXIL::ResourceClass::UAV));
   pUAV->SetRW(true); // sets UAV class
   pUAV->SetSpaceID(
@@ -171,6 +197,18 @@ llvm::Function* GetEntryFunction(hlsl::DxilModule& DM) {
         return DM.GetEntryFunction();
     }
     return DM.GetPatchConstantFunction();
+}
+
+std::vector<llvm::BasicBlock*> GetAllBlocks(hlsl::DxilModule& DM) {
+    std::vector<llvm::BasicBlock*> ret;
+    auto entryPoints = DM.GetExportedFunctions();
+    for (auto& fn : entryPoints) {
+        auto & blocks = fn->getBasicBlockList();
+        for (auto & block : blocks) {
+            ret.push_back(&block);
+        }
+    }
+    return ret;
 }
 
 ExpandedStruct ExpandStructType(LLVMContext &Ctx,

lib/DxilPIXPasses/PixPassHelpers.h
@@ -24,6 +24,7 @@ namespace PIXPassHelpers
         hlsl::DxilResourceBase * resource,
         const char* name);
     llvm::Function* GetEntryFunction(hlsl::DxilModule& DM);
+    std::vector<llvm::BasicBlock*> GetAllBlocks(hlsl::DxilModule& DM);
 #ifdef PIX_DEBUG_DUMP_HELPER
     void Log(const char* format, ...);
     void LogPartialLine(const char* format, ...);

commit 6fab8fdc623f63e969892362682cd438f04f4c85

PIX: ensure allocas generated by value-to-declare dominate uses (#4145)
This fixes a regression introduced by #3985

One IHV's driver compiler was upset by the placement of the allocas generated by this builder (m_B, that is).
Restoring the insertion point to the start of the function makes the driver happy again.

lib/DxilPIXPasses/DxilDbgValueToDbgDeclare.cpp
@@ -814,7 +814,7 @@ VariableRegisters::VariableRegisters(
     llvm::Module *M)
   : m_dbgLoc(DbgValue->getDebugLoc())
   , m_Variable(Variable)
-  , m_B(DbgValue)
+  , m_B(DbgValue->getParent()->getParent()->getEntryBlock().begin())
   , m_DbgDeclareFn(llvm::Intrinsic::getDeclaration(
       M, llvm::Intrinsic::dbg_declare))
 {