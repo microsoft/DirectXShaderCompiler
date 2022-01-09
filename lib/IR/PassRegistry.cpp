ef16c9fe7be645d760d0d321b7b881248977f226
Stephen Gutekanst <stephen@hexops.com>
2022-01-08 17:17:20 -0700

    [IR] Improve compatability with MinGW / Zig
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- lib/IR/PassRegistry.cpp
+++ lib/IR/PassRegistry.cpp
@@ -35,7 +35,9 @@ using namespace llvm;
 // A simple global initialized at DllMain-time will do (still does more work
 // than we should likely perform though).
 static uint32_t g_PassRegistryTid;
+#ifndef __MINGW32__
 extern "C" uint32_t __stdcall GetCurrentThreadId(void);
+#endif // __MINGW32__
 static void CheckThreadId() {
   if (g_PassRegistryTid == 0)
     g_PassRegistryTid = GetCurrentThreadId();
