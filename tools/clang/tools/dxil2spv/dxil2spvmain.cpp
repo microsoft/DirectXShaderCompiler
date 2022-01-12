fdd71df99072512fc8c6c889d6a027baca094598
Natalie Chouinard <chouinard@google.com>
2022-01-12 18:58:27 +0000

    [spirv] Add new dxil2spv target

---
+++ tools/clang/tools/dxil2spv/dxil2spvmain.cpp
@@ -0,0 +1,17 @@
+//===------- dxil2spv.cpp - DXIL to SPIR-V Tool -----------------*- C++ -*-===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//===----------------------------------------------------------------------===//
+//
+//  This file provides the entry point for the dxil2spv executable program.
+//
+//===----------------------------------------------------------------------===//
+
+#ifdef _WIN32
+int __cdecl wmain(int argc, const wchar_t **argv_) {
+#else
+int main(int argc, const char **argv_) {
+#endif // _WIN32
+  return 0;
+}
