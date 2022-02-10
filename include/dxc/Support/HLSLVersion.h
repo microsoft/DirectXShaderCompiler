b4cce2e76d60a2e275804df2f521136d0de83292
Chris Bieneman <cbieneman@microsoft.com>
2022-02-10 15:13:58 -0600

    Updates based on PR feedback

--- include/dxc/Support/HLSLVersion.h
+++ include/dxc/Support/HLSLVersion.h
@@ -5,7 +5,7 @@
 // This file is distributed under the University of Illinois Open Source     //
 // License. See LICENSE.TXT for details.                                     //
 //                                                                           //
-// Support for command-line-style option parsing.                            //
+// HLSL version enumeration and parsing support                              //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
