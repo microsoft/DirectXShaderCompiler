f13e282bd09d680cc96167e4e9d34bd0d9710255
Joshua Batista <jbatista@microsoft.com>
2022-05-27 15:54:01 -0700

    update check values

--- tools/clang/test/HLSLFileCheck/disassembler/numthreads.hlsl
+++ tools/clang/test/HLSLFileCheck/disassembler/numthreads.hlsl
@@ -2,9 +2,9 @@
 // CHECK: Compute Shader
 // CHECK: Numthreads: (2,2,1)
 // CHECK: Amplification Shader
-// CHECK: Numthreads: (2,2,1)
+// CHECK: Numthreads: (2,4,1)
 // CHECK: Mesh Shader
-// CHECK: Numthreads: (2,2,1)
+// CHECK: Numthreads: (2,3,1)
 
 [NumThreads(2,2,1)]
 void  main() {
