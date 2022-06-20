a707711d477a285259ba859eca8f1e1acd83173c
Natalie Chouinard <1953083+sudonatalie@users.noreply.github.com>
2022-06-20 06:42:58 -0700

    [spirv] Fail when unsupported options are used (#4518)
    Some DXC options are not supported in combination with the SPIR-V
    backend. Rather than silently ignore them, we should emit a clear error
    message. This change adds appropriate error messages for a couple
    options that have been mentioned in issues #3111 and #4496, but more
    should be added in future.

--- tools/clang/unittests/SPIRV/CodeGenSpirvTest.cpp
+++ tools/clang/unittests/SPIRV/CodeGenSpirvTest.cpp
@@ -3131,4 +3131,14 @@ TEST_F(FileTest, RenameEntrypoint) { runFileTest("fspv-entrypoint-name.hlsl"); }
 
 TEST_F(FileTest, PrintAll) { runFileTest("fspv-print-all.hlsl"); }
 
+TEST_F(FileTest, SpirvOptFd) {
+  runFileTest("spirv.opt.fd.hlsl", Expect::Failure);
+}
+TEST_F(FileTest, SpirvOptFre) {
+  runFileTest("spirv.opt.fre.hlsl", Expect::Failure);
+}
+TEST_F(FileTest, SpirvOptQStripReflect) {
+  runFileTest("spirv.opt.qstripreflect.hlsl", Expect::Failure);
+}
+
 } // namespace
