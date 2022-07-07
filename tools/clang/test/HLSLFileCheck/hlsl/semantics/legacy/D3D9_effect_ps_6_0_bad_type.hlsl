97639ca92ad72e23989d60430794748621efb1fa
Joshua Batista <jbatista@microsoft.com>
2022-07-07 12:11:34 -0700

    remove extra work to get type outputted on error, consolidate tests to 1 file

---
+++ tools/clang/test/HLSLFileCheck/hlsl/semantics/legacy/D3D9_effect_ps_6_0_bad_type.hlsl
@@ -0,0 +1,43 @@
+// RUN: %dxc -D PARAMTYPE=Tex2D /E main_fragment -T ps_6_0 %s | FileCheck %s -check-prefix=CHECK1
+// CHECK1: warning: effect state block ignored - effect syntax is deprecated. To use braces as an initializer use them with equal signs. [-Weffects-syntax]
+// CHECK1: (type for TEXUNIT) cannot be used as shader inputs or outputs.
+
+// RUN: %dxc -D PARAMTYPE=RW /E main_fragment -T ps_6_0 %s | FileCheck %s -check-prefix=CHECK2
+// CHECK2: warning: effect state block ignored - effect syntax is deprecated. To use braces as an initializer use them with equal signs. [-Weffects-syntax]
+// CHECK2: (type for TEXUNIT) cannot be used as shader inputs or outputs.
+
+
+// RUN: %dxc -D PARAMTYPE=SamplerState /E main_fragment -T ps_6_0 %s | FileCheck %s -check-prefix=CHECK3
+// CHECK3: warning: effect state block ignored - effect syntax is deprecated. To use braces as an initializer use them with equal signs. [-Weffects-syntax]
+// CHECK3: (type for TEXUNIT) cannot be used as shader inputs or outputs.
+
+
+
+#define Tex2D Texture2D<float4>
+#define RW RWStructuredBuffer<float4>
+
+SamplerState PointSampler { Filter = MIN_MAG_MIP_POINT; AddressU = Clamp; AddressV = Clamp; };
+
+
+void main_vertex
+(
+	float4 position	: POSITION,
+	float4 color	: COLOR,
+	float2 texCoord : TEXCOORD0,
+
+	uniform float4x4 modelViewProj,
+
+	out float4 oPosition : POSITION,
+	out float4 oColor    : COLOR,
+	out float2 otexCoord : TEXCOORD
+)
+{
+	oPosition = mul(modelViewProj, position);
+	oColor = color;
+	otexCoord = texCoord;
+}
+
+int main_fragment(float2 texCoord : TEXCOORD0, uniform PARAMTYPE decal : TEXUNIT0) : SV_Target
+{
+	return 3;
+} 
