6b5ddfde80bd76fa2415c00b4f3300586a81e7cb
Joshua Batista <jbatista@microsoft.com>
2022-07-13 14:30:19 -0700

    revert test

--- tools/clang/test/HLSLFileCheck/hlsl/semantics/sv_vertexid/input3.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/semantics/sv_vertexid/input3.hlsl
@@ -1,10 +1,5 @@
 // RUN: %dxc -E main -T vs_6_0 %s | FileCheck %s
 
-struct INPUT {
-    float a;
-    Texture2D t2d;
-    float b;
-};
 
 // CHECK: @main
 float4 main(
@@ -12,15 +7,11 @@ float4 main(
             // SGV
            ,uint vertid : SV_VertexID
            ,uint instid : SV_InstanceID
-           ,uniform Texture2D t2d : t2d
-           ,INPUT inWithRes : INPUT
            ) : SV_Position
 {
     float4 r = 0;
     r += a;
     r += vertid;
     r += instid;
-    r += t2d[int2(0,0)];
-    r += inWithRes.t2d[int2(0,0)];
     return r;
 }
