11d3833bffa415e8a2c870c32cf588ba20cdfbec
Joshua Batista <jbatista@microsoft.com>
2022-07-13 18:06:17 -0700

    merge both resource type tests into 1 block

--- tools/clang/test/HLSLFileCheck/hlsl/semantics/legacy/resource_in_entry_point_ps.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/semantics/legacy/resource_in_entry_point_ps.hlsl
@@ -1,8 +1,7 @@
-// RUN: %dxc -D PARAMTYPE=Tex2D /E main -T ps_6_0 %s | FileCheck %s -check-prefix=CHECK1
-// CHECK1: Resource or sampler parameter is not allowed on entry function.
+// RUN: %dxc -D PARAMTYPE=Tex2D /E main -T ps_6_0 %s | FileCheck %s
+// RUN: %dxc -D PARAMTYPE=SamplerState /E main -T ps_6_0 %s | FileCheck %s
 
-// RUN: %dxc -D PARAMTYPE=SamplerState /E main -T ps_6_0 %s | FileCheck %s -check-prefix=CHECK2
-// CHECK2: Resource or sampler parameter is not allowed on entry function.
+// CHECK: Resource or sampler parameter is not allowed on entry function.
 
 #define Tex2D Texture2D<float4>
 
