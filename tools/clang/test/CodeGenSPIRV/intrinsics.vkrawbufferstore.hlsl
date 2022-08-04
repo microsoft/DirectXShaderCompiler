2f9fc146f994acd71989c6aaf8e2937dad5b585b
n8vm <natemorrical@gmail.com>
2022-08-04 12:52:04 -0600

    more changes to unit test

--- tools/clang/test/CodeGenSPIRV/intrinsics.vkrawbufferstore.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.vkrawbufferstore.hlsl
@@ -9,23 +9,22 @@ uint64_t Address;
 void main(uint3 tid : SV_DispatchThreadID) {
   // CHECK:      [[addr:%\d+]] = OpLoad %ulong
   // CHECK-NEXT: [[buf:%\d+]] = OpBitcast %_ptr_PhysicalStorageBuffer_float [[addr]]
-  // CHECK-NEXT: OpStore [[buf]] %float Aligned 4
+  // CHECK-NEXT: OpStore [[buf]] %float_42 Aligned 4
   float x = 42.f;
   vk::RawBufferStore<float>(Address, x);
 
-  // CHECK:      [[addr:%\d+]] = OpLoad %ulong
-  // CHECK-NEXT: [[buf:%\d+]] = OpBitcast %_ptr_PhysicalStorageBuffer_double [[addr]]
-  // CHECK-NEXT: OpStore [[buf]] %double Aligned 8
+  // CHECK:      [[buf:%\d+]] = OpBitcast %_ptr_PhysicalStorageBuffer_double
+  // CHECK-NEXT: OpStore [[buf]] %double_42 Aligned 8
   double y = 42.0;
   vk::RawBufferStore<double>(Address, y, 8);
 
   // CHECK:      [[buf:%\d+]] = OpBitcast %_ptr_PhysicalStorageBuffer_uint
-  // CHECK-NEXT: OpStore [[buf]] %bool Aligned 4
+  // CHECK-NEXT: OpStore [[buf]] %uint_1 Aligned 4
   bool z = true;
   vk::RawBufferStore<bool>(Address, z);
 
   // CHECK:      [[buf:%\d+]] = OpBitcast %_ptr_PhysicalStorageBuffer_v2float
-  // CHECK-NEXT: OpStore [[buf]] %v2float Aligned 8
+  // CHECK-NEXT: OpStore [[buf]] [[val]] Aligned 8
   float2 w = float2(42.f, 1337.f);
   vk::RawBufferStore<float2>(Address, w, 8);
 
