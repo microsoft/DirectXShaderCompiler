419e5854b31df0a34bed3bec20d2463baafab95f
Joshua Batista <jbatista@microsoft.com>
2022-08-12 12:27:33 -0700

    update variable names, update check string

--- tools/clang/test/HLSLFileCheck/validation/NonVLAArrays.hlsl
+++ tools/clang/test/HLSLFileCheck/validation/NonVLAArrays.hlsl
@@ -1,13 +1,13 @@
 // RUN: %dxc -E PSMain -T ps_6_0 %s | FileCheck %s
 
-// CHECK: define void @PSMain()
+// CHECK: %"$Globals" = type { [2 x <4 x float>], [3 x <4 x float>] }
 
 // fix me: float4 BrokenTestArray0[int(.3)*12]; 
-float4 BrokenTestArray1[min(2, 3)];
-float4 WorkingTestArray0[max(2, 3)];
+float4 WorkingTestArray0[min(2, 3)];
+float4 WorkingTestArray1[max(2, 3)];
 
 float4 PSMain(float4 color : COLOR) : SV_TARGET
 {
-    return BrokenTestArray1[int(color.y)] +
-           WorkingTestArray0[int(color.z)];
+    return WorkingTestArray0[int(color.y)] +
+           WorkingTestArray1[int(color.z)];
 }
