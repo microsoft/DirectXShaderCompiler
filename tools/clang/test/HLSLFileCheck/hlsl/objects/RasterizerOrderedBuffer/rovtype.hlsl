6ad8643a5564ce1bb52cc1b85641edfca1043ca5
Greg Roth <grroth@microsoft.com>
2022-08-19 15:32:33 -0700

    better contain the ByteAddressBuffer differences
    This moves the code slightly closer to its counterparts with the CHECK
    values and removes a bit of duplicated code to try to make it more
    readable

--- tools/clang/test/HLSLFileCheck/hlsl/objects/RasterizerOrderedBuffer/rovtype.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/RasterizerOrderedBuffer/rovtype.hlsl
@@ -7,7 +7,7 @@
 // RUN: %dxc -DResType=RasterizerOrderedStructuredBuffer<uint> -DPosType=uint1 -T ps_6_0 %s | FileCheck %s -check-prefixes=BUFCHK,CHECK
 
 // BABs can't be indexed so they require special casing
-// RUN: %dxc -DBAB -DResType=RasterizerOrderedByteAddressBuffer -DPosType=uint1 -T ps_6_0 %s | FileCheck %s -check-prefixes=BUFCHK,CHECK
+// RUN: %dxc -DBAB -DResType=RasterizerOrderedByteAddressBuffer -DPosType=uint -T ps_6_0 %s | FileCheck %s -check-prefixes=BUFCHK,CHECK
 
 // Verify that the ROV texture type is correctly identified
 // such that a struct member of that type can be assigned
@@ -26,7 +26,6 @@ const static struct {
 
 const static ROVThings thing2 = {GlobalResource[1]};
 
-#ifndef BAB
 void main(in PosType pos : P, out float4 Out : SV_Target0)
 {
         // CHECK: %[[t3:[0-9a-zA-Z_]*]] = call %dx.types.Handle @dx.op.createHandle(i32 57, i8 1, i32 0, i32 2
@@ -46,6 +45,7 @@ void main(in PosType pos : P, out float4 Out : SV_Target0)
 
 	Out = 0.0;
 
+#ifndef BAB
         // CHECK: %[[t1:[0-9a-zA-Z_]*]] = call %dx.types.Handle @dx.op.createHandle(i32 57, i8 1, i32 0, i32 0
         // TXTCHK: call %dx.types.ResRet.i32 @dx.op.textureLoad.i32(i32 66, %dx.types.Handle %[[t1]]
         // BUFCHK: call %dx.types.ResRet.i32 @dx.op.bufferLoad.i32(i32 68, %dx.types.Handle %[[t1]]
@@ -83,32 +83,15 @@ void main(in PosType pos : P, out float4 Out : SV_Target0)
         // CHECK: add i32
         // BUFCHK: call void @dx.op.bufferStore.i32(i32 69, %dx.types.Handle %[[t6]]
 	thing6.Resource[pos] += 1;
-}
 #else
-// ByteAddressBuffers are too weird to play with the others
-
-void main(in uint pos : P, out float4 Out : SV_Target0)
-{
+        // ByteAddressBuffers are too weird to play with the others
         // Note that all the CHECKS this needs are included
         // in the corresponding locations of the BUFCHK variant above
-        const struct {
-          ResType Resource;
-        } thing3 = {GlobalResource[2]};
-        struct {
-          ResType Resource;
-        } thing4 = {GlobalResource[3]};
-
-        ROVThings thing5 = {GlobalResource[4]};
-        const ROVThings thing6 = {GlobalResource[5]};
-
-
-	Out = 0.0;
-
 	thing1.Resource.Store(pos, thing1.Resource.Load(pos) + 1);
 	thing2.Resource.Store(pos, thing2.Resource.Load(pos) + 1);
 	thing3.Resource.Store(pos, thing3.Resource.Load(pos) + 1);
 	thing4.Resource.Store(pos, thing4.Resource.Load(pos) + 1);
 	thing5.Resource.Store(pos, thing5.Resource.Load(pos) + 1);
 	thing6.Resource.Store(pos, thing6.Resource.Load(pos) + 1);
-}
 #endif
+}
