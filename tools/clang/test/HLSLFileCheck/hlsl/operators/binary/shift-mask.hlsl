2a5c9a495b4ef25d5f86743ebe0fc9d595fb6b80
Chris Bieneman <cbieneman@microsoft.com>
2022-09-12 10:38:16 -0500

    [NFC] Move HLSL shfit behavior to frontend
    The HLSL bit shift behavior was implemented in constant folding which
    alters the semantics of the IR. Alternatively we can implement it in
    the front end, leveraging the existing OpenCL code paths.
    
    This results in the same final IR optimization and allows reverting the
    ConstantFolder and IR tests back to match LLVM 3.7.
    
    The HLSL shift behavior's optimized behavior is verified in existing
    tests like the shift-fold.hlsl test, which is unimpacted by this change.
    
    The big motiviation for this change is heading off subtle bugs. All our
    tests cover cases where the shift's size is known at compile time. In
    those cases the compiler can resolve the masking behavior
    automatically. We have _no_ tests, nor compiler changes that impact
    shifts that aren't compile-time known.
    
    This means that prior to this change the behavior of shifting by a
    compile-time resolvable value and shifting by a non-resolvable value
    likely differs. Since backends often load DXIL and treat it as LLVM IR,
    this could cause concerning issues.

---
+++ tools/clang/test/HLSLFileCheck/hlsl/operators/binary/shift-mask.hlsl
@@ -0,0 +1,33 @@
+// RUN: %dxc -T lib_6_3 %s -fcgl | FileCheck %s
+
+int shl32(int V, int S) {
+  return V << S;
+}
+
+// CHECK: define internal i32 @"\01?shl32
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %5, 31
+// CHECK-DAG:  %{{[0-9]+}} = shl i32 %{{[0-9]+}}, %[[Masked]]
+
+int shr32(int V, int S) {
+  return V >> S;
+}
+
+// CHECK: define internal i32 @"\01?shr32
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %5, 31
+// CHECK-DAG:  %{{[0-9]+}} = ashr i32 %{{[0-9]+}}, %[[Masked]]
+
+int64_t shl64(int64_t V, int64_t S) {
+  return V << S;
+}
+
+// CHECK define internal i64 @"\01?shl64
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %5, 63
+// CHECK-DAG:  %{{[0-9]+}} = shl i64 %{{[0-9]+}}, %[[Masked]]
+
+int64_t shr64(int64_t V, int64_t S) {
+  return V >> S;
+}
+
+// CHECK: define internal i64 @"\01?shr64@@YA_J_J0@Z"(i64 %V, i64 %S) #0 {
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %5, 63
+// CHECK-DAG:  %{{[0-9]+}} = ashr i64 %{{[0-9]+}}, %[[Masked]]
