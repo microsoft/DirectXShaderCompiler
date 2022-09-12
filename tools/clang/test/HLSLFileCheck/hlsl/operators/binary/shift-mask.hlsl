efb86633fb1d064272457a2cb0af5293aca0adcd
Chris Bieneman <cbieneman@microsoft.com>
2022-09-12 11:16:06 -0500

    Clean up post-optimization code
    Looks like we did handle any non-constant folded shifts by adding the
    and post-optimization. This removes that code since it is not needed.

--- tools/clang/test/HLSLFileCheck/hlsl/operators/binary/shift-mask.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/operators/binary/shift-mask.hlsl
@@ -5,7 +5,7 @@ int shl32(int V, int S) {
 }
 
 // CHECK: define internal i32 @"\01?shl32
-// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %5, 31
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %{{[0-9]+}}, 31
 // CHECK-DAG:  %{{[0-9]+}} = shl i32 %{{[0-9]+}}, %[[Masked]]
 
 int shr32(int V, int S) {
@@ -13,7 +13,7 @@ int shr32(int V, int S) {
 }
 
 // CHECK: define internal i32 @"\01?shr32
-// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %5, 31
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i32 %{{[0-9]+}}, 31
 // CHECK-DAG:  %{{[0-9]+}} = ashr i32 %{{[0-9]+}}, %[[Masked]]
 
 int64_t shl64(int64_t V, int64_t S) {
@@ -21,7 +21,7 @@ int64_t shl64(int64_t V, int64_t S) {
 }
 
 // CHECK define internal i64 @"\01?shl64
-// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %5, 63
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %{{[0-9]+}}, 63
 // CHECK-DAG:  %{{[0-9]+}} = shl i64 %{{[0-9]+}}, %[[Masked]]
 
 int64_t shr64(int64_t V, int64_t S) {
@@ -29,5 +29,5 @@ int64_t shr64(int64_t V, int64_t S) {
 }
 
 // CHECK: define internal i64 @"\01?shr64@@YA_J_J0@Z"(i64 %V, i64 %S) #0 {
-// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %5, 63
+// CHECK-DAG:  %[[Masked:[0-9]+]] = and i64 %{{[0-9]+}}, 63
 // CHECK-DAG:  %{{[0-9]+}} = ashr i64 %{{[0-9]+}}, %[[Masked]]
