c4151b4cffa389780e9610976280c67615bc4cb8
Tex Riddell <texr@microsoft.com>
2022-09-17 19:47:49 -0700

    Check for resource property in type annotation metadata
    Should be present for 6.8 and 6.x, but not lower.

--- tools/clang/test/HLSLFileCheck/hlsl/resource_binding/resource-in-struct.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/resource_binding/resource-in-struct.hlsl
@@ -1,6 +1,19 @@
 // RUN: %dxc -E main -T ps_6_0 %s  | FileCheck %s
+// RUN: %dxc -T lib_6_7 %s  | FileCheck %s -check-prefixes=CHECKTYPE,CHECKNORP
+// RUN: %dxc -T lib_6_8 %s  | FileCheck %s -check-prefixes=CHECKTYPE,CHECKRP
+// RUN: %dxc -T lib_6_x %s  | FileCheck %s -check-prefixes=CHECKTYPE,CHECKRP
 
 // CHECK: res.Tex1
+
+// Make sure ResourceProperties are emitted.
+// CHECKTYPE: !dx.typeAnnotations = !{![[TyAnn:[0-9]+]]
+// CHECKTYPE: ![[TyAnn]] =
+// CHECKTYPE-SAME: %struct.Resources undef, ![[Resources_Ann:[0-9]+]],
+// CHECKTYPE: ![[Resources_Ann]] = !{i32 16, ![[Tex1_Ann:[0-9]+]],
+// CHECKRP: ![[Tex1_Ann]] = !{i32 6, !"Tex1",
+// CHECKRP-SAME: i32 10, %dx.types.ResourceProperties { i32 2, i32 1033 }
+// CHECKNORP: ![[Tex1_Ann]] = !{i32 6, !"Tex1",
+// CHECKNORP-NOT: %dx.types.ResourceProperties
 
 SamplerState Samp;
 struct Resources
