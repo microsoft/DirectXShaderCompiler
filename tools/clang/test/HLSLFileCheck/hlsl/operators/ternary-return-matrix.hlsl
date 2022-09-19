bca51458d28d92ccb35592da18d5adde6b2e8cb4
Chris Bieneman <chris.bieneman@me.com>
2022-09-19 10:20:55 -0500

    Fix broken test case
    Using CreateTempAlloca drops the debug location on the alloca, which is
    unfortunate, but should be fine.

--- tools/clang/test/HLSLFileCheck/hlsl/operators/ternary-return-matrix.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/operators/ternary-return-matrix.hlsl
@@ -12,7 +12,7 @@ float2x2 crashingFunction(bool b) {
 }
 
 // CHECK: define internal %class.matrix.float.2.2 @"\01?crashingFunction@@YA?AV?$matrix@M$01$01@@_N@Z"
-// CHECK: [[ALLOCA:%[0-9]+]] = alloca %class.matrix.float.2.2, !dbg !{{[0-9]+}} ; line:6 col:10
+// CHECK: [[ALLOCA:%[0-9]+]] = alloca %class.matrix.float.2.2
 // CHECK: preds = {{%[0-9a-z]+}}
 // CHECK: call %class.matrix.float.2.2 @"dx.hl.matldst.colStore.%class.matrix.float.2.2 (i32, %class.matrix.float.2.2*, %class.matrix.float.2.2)"(i32 1, %class.matrix.float.2.2* [[ALLOCA]], %class.matrix.float.2.2 %{{[0-9]+}})
 // CHECK: preds = {{%[0-9a-z]+}}
