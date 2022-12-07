ce6e0d64d40a2bab1cf38290a2675bd0a31b67a7
Chris Bieneman <cbieneman@microsoft.com>
2022-12-07 13:25:42 -0600

    Fix copy-in/copy-out parameter passing
    This change fixes HLSL's copy-in/copy-out parameter passing. Before
    this change we skip emitting copies for all local variable parameters,
    which results in code correctness issues.
    
    With this change, we do a poor-man's alias analysis walking each local
    variable back to either an argument or an alloca. If it tracks back to
    an argument marked noalias we can skip the copy. If it tracks to an
    alloca, we skip the copy for the first parameter that tracks to that
    alloca, and emit the copies for any subsequent parameters.
    
    This change does have one side-effect that I don't understand. It
    results in changing the ordering of some of the disassembly output. I
    suspect that is caused by something in the disassembly being
    order-dependent but haven't investigated fully.

--- tools/clang/test/HLSLFileCheck/hlsl/resource_binding/res_in_cb1.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/resource_binding/res_in_cb1.hlsl
@@ -4,17 +4,17 @@
 //CHECK: tx0.s                             sampler      NA          NA      S0             s0     1
 //CHECK: tx1.s                             sampler      NA          NA      S1             s1     1
 //CHECK: s                                 sampler      NA          NA      S2             s3     1
-//CHECK: tx0.t                             texture     f32          2d      T0             t0     1
-//CHECK: tx0.t2                            texture     f32          2d      T1             t1     1
-//CHECK: tx1.t                             texture     f32          2d      T2             t5     1
-//CHECK: tx1.t2                            texture     f32          2d      T3             t6     1
+//CHECK: tx0.t2                            texture     f32          2d      T0             t1     1
+//CHECK: tx0.t                             texture     f32          2d      T1             t0     1
+//CHECK: tx1.t2                            texture     f32          2d      T2             t6     1
+//CHECK: tx1.t                             texture     f32          2d      T3             t5     1
 //CHECK: x                                 texture     f32          2d      T4             t3     1
 
 struct LegacyTex
 {
-	Texture2D t;
-        Texture2D t2;
-	SamplerState  s;
+  Texture2D t;
+  Texture2D t2;
+  SamplerState  s;
 };
 
 LegacyTex tx0 : register(t0) : register(s0);
@@ -22,15 +22,15 @@ LegacyTex tx1 : register(t5) : register(s1);
 
 float4 tex2D(LegacyTex tx, float2 uv)
 {
-	return tx.t.Sample(tx.s,uv) + tx.t2.Sample(tx.s, uv);
+  return tx.t.Sample(tx.s,uv) + tx.t2.Sample(tx.s, uv);
 }
 
 cbuffer n {
-   Texture2D x: register(t3);
-   SamplerState s : register(s3);
+  Texture2D x: register(t3);
+  SamplerState s : register(s3);
 }
 
 float4 main(float2 uv:UV) : SV_Target
 {
-	return tex2D(tx0,uv) + tex2D(tx1,uv) + x.Sample(s,uv);
+  return tex2D(tx0,uv) + tex2D(tx1,uv) + x.Sample(s,uv);
 }
