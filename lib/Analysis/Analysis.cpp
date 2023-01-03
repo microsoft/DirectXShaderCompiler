2168dcb4fb0ac7841624228f8e186cc28f4cca91
Chris B <cbieneman@microsoft.com>
2023-01-03 10:04:22 -0600

    Clean up and rework DXIL library depencencies (#4877)
    * Clean up and rework DXIL library depencencies
    
    This change reworks the dependency specifications for the DXIL*
    libraries. Many of these libraries had over and under specified
    dependencies.
    
    This also collapses the DxilRDATBuidlder into the DxilContainer library
    to remove the cyclic dependency between the two libraries.
    
    * Break assert dependency between DXIL and Analysis
    
    In assert builds the DXILModule constructor was creating forced bindings
    to debugging methods that are often used from the debugger. This forced
    binding is useful, but doesn't need to live in the DXIL library.
    
    To break the dependency I've moved the code into Analysis. I've also
    made that code only included when building with MSVC. When using other
    compilers `__attribute__((used))` can be applied to the function via the
    `LLVM_DUMP_METHOD` annotation to have the same effect.

--- lib/Analysis/Analysis.cpp
+++ lib/Analysis/Analysis.cpp
@@ -18,6 +18,26 @@
 
 using namespace llvm;
 
+// HLSL Change Begin - Windows doesn't support __attribute__((used)) so these methods
+// need to be forcibly bound or they could be stripped at build time.
+#if defined(_MSC_VER) && (!defined(NDEBUG) || defined(LLVM_ENABLE_DUMP))
+#pragma optimize("", off)
+void BindDumpMethods() {
+  // Pin LLVM dump methods.
+  void (__thiscall Module::*pfnModuleDump)() const = &Module::dump;
+  (void)pfnModuleDump;
+  void (__thiscall Type::*pfnTypeDump)() const = &Type::dump;
+  (void)pfnTypeDump;
+  void (__thiscall Function::*pfnViewCFGOnly)() const = &Function::viewCFGOnly;
+  (void)pfnViewCFGOnly;
+}
+#pragma optimize("", on)
+#define HLSL_BIND_DUMP_METHODS BindDumpMethods();
+#else
+#define HLSL_BIND_DUMP_METHODS
+#endif
+// HLSL Change End
+
 /// initializeAnalysis - Initialize all passes linked into the Analysis library.
 void llvm::initializeAnalysis(PassRegistry &Registry) {
   initializeAliasAnalysisAnalysisGroup(Registry);
@@ -69,6 +89,8 @@ void llvm::initializeAnalysis(PassRegistry &Registry) {
   initializeTargetTransformInfoWrapperPassPass(Registry);
   initializeTypeBasedAliasAnalysisPass(Registry);
   initializeScopedNoAliasAAPass(Registry);
+
+  HLSL_BIND_DUMP_METHODS // HLSL Change - Force binding dump methods.
 }
 
 void LLVMInitializeAnalysis(LLVMPassRegistryRef R) {
