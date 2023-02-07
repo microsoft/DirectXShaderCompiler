a5887483deaad8d5fcce8b11e206fe6624f5389c
Xiang Li <python3kgae@outlook.com>
2023-02-07 17:08:55 -0500

    Fix assert when a function been called more than once by entry. (#4992)
    For better support of noinline.
    
    If a function is processed, just skip in DxilViewIdStateBuilder::ComputeReachableFunctionsRec.
    Add print to test the change.

--- tools/opt/opt.cpp
+++ tools/opt/opt.cpp
@@ -53,6 +53,7 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 // HLSL Change Starts
+#include "dxc/HLSL/ComputeViewIdState.h"
 #include "dxc/Support/Global.h"
 #include "llvm/Analysis/ReducibilityAnalysis.h"
 #include "dxc/Support/WinIncludes.h"
@@ -360,6 +361,7 @@ int __cdecl main(int argc, char **argv) {
   //initializeSjLjEHPreparePass(Registry);  // HLSL Change: remove EH passes
   // HLSL Change Starts
   initializeReducibilityAnalysisPass(Registry);
+  initializeComputeViewIdStatePass(Registry);
 #ifdef HAS_DXILCONV
   initializeDxilConvPasses(Registry);
 #endif
