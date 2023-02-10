919d2320a9df22fb8ecbba726cb274ef6aa04a5c
Chris Bieneman <cbieneman@microsoft.com>
2023-02-10 13:06:58 -0600

    Don't add template record decl to declcontext
    CXXRecordDecls that are part of ClassTemplateDecls should not be added
    to the DeclContext that the ClassTemplateDecl gets added to.
    
    I'm a little surprised this didn't cause problems somewhere, but it
    does result in the decl being dumped twice in the ast dumping. I think
    it should have also resulted in conflicting decls in name lookup, but
    there may be some other change somewhere that worked around that, or it
    might get ignored since the decl is invalid by itself.
    
    Either way, we can skip adding CXXRecordDecls for template types to the
    DeclContext and everything works fine.

--- tools/clang/test/HLSLFileCheck/infra/ast-dumping.hlsl
+++ tools/clang/test/HLSLFileCheck/infra/ast-dumping.hlsl
@@ -22,11 +22,8 @@ void main() {
 // incomplete by looking at the `Buffer` type which is unused in this code and
 // verifying it doesn't have any methods.
 
-// IMPLICIT: CXXRecordDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit class Buffer
-// IMPLICIT-NEXT: FinalAttr {{0x[0-9a-fA-F]+}} <<invalid sloc>> Implicit final
-// IMPLICIT-NEXT: FieldDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit h 'element'
-// IMPLICIT-NOT: CXXMethodDecl
-// IMPLICIT-NEXT: ClassTemplateDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit Buffer
+// IMPLICIT-NOT: CXXRecordDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit class Buffer
+// IMPLICIT: ClassTemplateDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit Buffer
 // IMPLICIT-NEXT: TemplateTypeParmDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> class element
 // IMPLICIT-NEXT: TemplateArgument type 'vector<float, 4>':'vector<float, 4>'
 // IMPLICIT-NEXT: CXXRecordDecl {{0x[0-9a-fA-F]+}} <<invalid sloc>> <invalid sloc> implicit class Buffer
