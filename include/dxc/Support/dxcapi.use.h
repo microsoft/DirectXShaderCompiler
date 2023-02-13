ece840d76c77e83fd9e26a3cce1d64726f1b21cb
Chris Bieneman <chris.bieneman@me.com>
2023-02-13 17:43:11 -0600

    Fxing dlsym calls and a bit more cleanup

--- include/dxc/Support/dxcapi.use.h
+++ include/dxc/Support/dxcapi.use.h
@@ -33,22 +33,24 @@ protected:
     m_dll = LoadLibraryA(dllName);
     if (m_dll == nullptr) return HRESULT_FROM_WIN32(GetLastError());
     m_createFn = (DxcCreateInstanceProc)GetProcAddress(m_dll, fnName);
+    
+    if (m_createFn == nullptr) {
+      HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
+      FreeLibrary(m_dll);
+      m_dll = nullptr;
+      return hr;
+    }
 #else
     m_dll = ::dlopen(dllName, RTLD_LAZY);
     if (m_dll == nullptr) return E_FAIL;
     m_createFn = (DxcCreateInstanceProc)::dlsym(m_dll, fnName);
-#endif
-
+    
     if (m_createFn == nullptr) {
-      HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
-#ifdef _WIN32
-      FreeLibrary(m_dll);
-#else
       ::dlclose(m_dll);
-#endif
       m_dll = nullptr;
-      return hr;
+      return E_FAIL;
     }
+#endif
 
     // Only basic functions used to avoid requiring additional headers.
     m_createFn2 = nullptr;
