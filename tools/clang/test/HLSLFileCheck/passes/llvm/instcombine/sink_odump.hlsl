f1c52a8fab41c172cd95287dee44d5f67b84f67b
Adam Yang <hanbyang@microsoft.com>
2023-02-27 14:40:30 -0800

    Deleted the command line odump test, which doesn't actually work

--- tools/clang/test/HLSLFileCheck/passes/llvm/instcombine/sink_odump.hlsl
+++
@@ -1,9 +0,0 @@
-// RUN: %dxc %s /T ps_6_0 -opt-disable sink -Odump | FileCheck %s -check-prefix=NO_SINK
-// RUN: %dxc %s /T ps_6_0                   -Odump | FileCheck %s -check-prefix=SINK
-
-// NO_SINK: -instcombine,NoSink=1
-// SINK:    -instcombine,NoSink=1
-
-float main() : SV_Target {
-    return 0;
-}
