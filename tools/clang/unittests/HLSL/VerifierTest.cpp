058d3d7ba991370d03b3679c9afe325b46c53e6c
Chris B <cbieneman@microsoft.com>
2023-03-02 14:52:36 -0600

    Add #pragma region support (#5074)
    Add the pragma region handler to the HLSL pragma handlers list.
    
    Fixes #676
    
    Sorry for the slow turn around time.

--- tools/clang/unittests/HLSL/VerifierTest.cpp
+++ tools/clang/unittests/HLSL/VerifierTest.cpp
@@ -62,6 +62,7 @@ public:
   TEST_METHOD(RunObjectOperators)
   TEST_METHOD(RunOutParamDiags)
   TEST_METHOD(RunPackReg)
+  TEST_METHOD(RunPragmaRegion)
   TEST_METHOD(RunRayTracings)
   TEST_METHOD(RunScalarAssignments)
   TEST_METHOD(RunScalarAssignmentsExactPrecision)
@@ -285,6 +286,10 @@ TEST_F(VerifierTest, RunOutParamDiags) {
 
 TEST_F(VerifierTest, RunPackReg) {
   CheckVerifiesHLSL(L"packreg.hlsl");
+}
+
+TEST_F(VerifierTest, RunPragmaRegion) {
+  CheckVerifiesHLSL(L"pragma-region.hlsl");
 }
 
 TEST_F(VerifierTest, RunRayTracings) {
