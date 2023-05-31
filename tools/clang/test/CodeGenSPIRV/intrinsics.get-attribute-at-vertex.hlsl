cd3b40bd2eec043410193a9d76466688886cd774
Chow <Shaochi.Zhou@amd.com>
2023-05-31 21:48:52 +0800

    [SPIR-V] add support for extension KHR_fragment_shading_barycentric (SV_Barycentrics) (#4638)
      Implement latest `SV_Barycentrics` semantics in DXC, according to DXC wiki spec.
    
      For variant qualifier decorated baryCoord inputs, as KHR extension has no matched built-in, still mapped to Bary*AMD.
    
      Add support for using input as first parameter of GetAttributeAtVertex. This would expand input's dimension to meet extension spec.
    
      Merge some features from AMD_shader_explicit_parameter and current extension together for MSAA usage. Keep generated SPIR-V codes similar to GLSL as possible.
    
      With concern to HLSL/DXIL part, keep most type modification/variable decoration change in SPIRV trans unit.
    
      Remove AMD_shader_explicit_parameter related features.
    
      Support struct member as parameter of GetAttributeAtVertex.
    
      Support using Decorator for MSAA features.

--- tools/clang/test/CodeGenSPIRV/intrinsics.get-attribute-at-vertex.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.get-attribute-at-vertex.hlsl
@@ -1,25 +1,42 @@
 // RUN: %dxc -T ps_6_1 -E main
 
-struct PSInput {
-  float4 position : SV_POSITION;
-  nointerpolation float3 color : COLOR;
+enum VertexID {
+    FIRST = 0,
+    SECOND = 1,
+    THIRD = 2
 };
 
-cbuffer constants : register(b0) {
-  float4 g_constants;
+
+// CHECK: OpCapability FragmentBarycentricKHR
+// CHECK: OpExtension "SPV_KHR_fragment_shader_barycentric"
+// CHECK: OpDecorate [[ivc:%\w+]] Flat
+// CHECK: OpDecorate [[ivc]] PerVertexKHR
+// CHECK: OpDecorate [[ivc2:%\w+]] Flat
+// CHECK: OpDecorate [[ivc2]] PerVertexKHR
+// CHECK: [[ivc]] = OpVariable %_ptr_Input__arr_v3float_uint_3 Input
+// CHECK: [[ivc2]] = OpVariable %_ptr_Input__arr_v3uint_uint_3 Input
+// CHECK: [[pvc:%\w+]] = OpVariable %_ptr_Function__arr_v3float_uint_3 Function
+// CHECK: [[pvc2:%\w+]] = OpVariable %_ptr_Function__arr_v3uint_uint_3 Function
+// CHECK: [[ivcl:%\d+]] = OpLoad %_arr_v3float_uint_3 [[ivc]]
+// CHECK: OpStore [[pvc]] [[ivcl]]
+// CHECK: [[ivcl2:%\d+]] = OpLoad %_arr_v3uint_uint_3 [[ivc2]]
+// CHECK: OpStore [[pvc2]] [[ivcl2]]
+float3 main( float3 vBaryWeights : SV_Barycentrics,
+    nointerpolation float3 Color : COLOR,
+    nointerpolation bool3 Color2 : COLOR2    ) : SV_Target
+{
+    float3 vColor;
+    float3 vColor0 = GetAttributeAtVertex( Color, VertexID::FIRST );
+    float3 vColor1 = GetAttributeAtVertex( Color, VertexID::SECOND );
+    float3 vColor2 = GetAttributeAtVertex( Color2, VertexID::THIRD );
+    vColor = vBaryWeights.x*vColor0 + vBaryWeights.y*vColor1 + vColor2;
+    return vColor;
 }
 
-float4 main(PSInput input) : SV_TARGET {
-  uint cmp = (uint)(g_constants[0]);
-
-  // CHECK: 16:21: error: GetAttributeAtVertex intrinsic function unimplemented
-  float colorAtV0 = GetAttributeAtVertex(input.color, 0)[cmp];
-
-  // CHECK: 19:21: error: GetAttributeAtVertex intrinsic function unimplemented
-  float colorAtV1 = GetAttributeAtVertex(input.color, 1)[cmp];
-
-  // CHECK: 22:21: error: GetAttributeAtVertex intrinsic function unimplemented
-  float colorAtV2 = GetAttributeAtVertex(input.color, 2)[cmp];
-
-  return float4(colorAtV0, colorAtV1, colorAtV2, 0);
-}
+// CHECK: [[color:%\w+]] = OpFunctionParameter %_ptr_Function__arr_v3float_uint_3
+// CHECK: [[color2:%\w+]] = OpFunctionParameter %_ptr_Function__arr_v3uint_uint_3
+// CHECK: [[c0:%\w+]] = OpAccessChain %_ptr_Function_v3float [[color]] %uint_0
+// CHECK: [[c0l:%\w+]] = OpLoad %v3float [[c0]]
+// CHECK: OpStore [[vC0:%\w+]] [[c0l]]
+// CHECK: [[c1Ne:%\w+]] = OpINotEqual %v3bool [[c1load:%\w+]] [[constZeroUint:%\w+]]
+// CHECK: [[c1Sel:%\w+]] = OpSelect %v3float [[c1Ne]] [[constOneTarget:%\w+]] [[constZeroTarget:%\w+]]
