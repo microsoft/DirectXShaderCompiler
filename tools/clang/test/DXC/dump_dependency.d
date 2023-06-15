bf7e0160e234570ab95c2a4da6f4381d01baf06b
Adam Yang <hanbyang@microsoft.com>
2023-06-15 20:08:22 +0800

    Deleted the accidentally added test residue

--- tools/clang/test/DXC/dump_dependency.d
+++
@@ -1,7 +0,0 @@
-C:\dxc\tools\clang\test\DXC\dump_dependency.hlsl: C:\dxc\tools\clang\test\DXC\dump_dependency.hlsl \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency0.h \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency1.h \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency2.h \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency3.h \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency4.h \
- C:\dxc\tools\clang\test\DXC/Inputs\include/dependency5.h
