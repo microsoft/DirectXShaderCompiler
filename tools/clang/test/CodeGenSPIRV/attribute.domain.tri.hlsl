3dc858f82f61f0402fe4064e232ba5fa1013b2a8
Xiang Li <python3kgae@outlook.com>
2023-06-22 07:14:41 -0400

    [NFC] [lit] [SPIRV] change header ext from hlsl to hlsli (#5304)
    This is to avoid lit treat the headers with hlsl ext as Unresolved
    Tests.

--- tools/clang/test/CodeGenSPIRV/attribute.domain.tri.hlsl
+++ tools/clang/test/CodeGenSPIRV/attribute.domain.tri.hlsl
@@ -1,6 +1,6 @@
 // RUN: %dxc -T hs_6_0 -E SubDToBezierHS
 
-#include "bezier_common_hull.hlsl"
+#include "bezier_common_hull.hlsli"
 
 // CHECK: OpExecutionMode %SubDToBezierHS Triangle
 
--- tools/clang/test/CodeGenSPIRV/hs.pcf.primitive-id.1.hlsl
+++ tools/clang/test/CodeGenSPIRV/hs.pcf.primitive-id.1.hlsl
@@ -1,6 +1,6 @@
 // RUN: %dxc -T hs_6_0 -E main
 
-#include "bezier_common_hull.hlsl"
+#include "bezier_common_hull.hlsli"
 
 // Test: PCF takes the PrimitiveID
 // Note that in this test, the main entry point has also taken the PrimitiveID as input.
