897da825305da48f0f9dc144989bd1e2c5cc7b3c
Steven Perron <stevenperron@google.com>
2023-06-26 12:57:09 -0400

    Add comments to the new test.

--- tools/clang/test/CodeGenSPIRV/select.long.lit.hlsl
+++ tools/clang/test/CodeGenSPIRV/select.long.lit.hlsl
@@ -1,6 +1,7 @@
 // RUN: %dxc -T ps_6_0 -HV 2018 -E main
 
-// the literal type.
+// Check that the literals get a 64-bit type, and the result of the select is
+// then cast to an unsigned 64-bit value.
 void foo(uint x) {
   // CHECK: %foo = OpFunction
   // CHECK: [[cond:%\d+]] = OpULessThan %bool {{%\d+}} %uint_64
@@ -9,6 +10,10 @@ void foo(uint x) {
   uint64_t value = x < 64 ? 1 : 0;
 }
 
+// Check that the literals get a 64-bit type, and the result of the select is
+// then cast to an signed 64-bit value. Note that the bitcast is redundant in
+// this case, but we add the bitcast before the type of the literal has been
+// determined, so we add it anyway.
 void bar(uint x) {
   // CHECK: %bar = OpFunction
   // CHECK: [[cond:%\d+]] = OpULessThan %bool {{%\d+}} %uint_64
