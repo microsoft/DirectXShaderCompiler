4925864c310c36ec61cfe9b7855fc2a46905315f
Xiang Li <python3kgae@outlook.com>
2023-07-12 17:31:21 -0400

    Use taef when end with \HLSLFileCheck.

--- utils/hct/hcttest.cmd
+++ utils/hct/hcttest.cmd
@@ -267,14 +267,19 @@ if "%TEST_CLEAN%"=="1" (
 if "%TEST_MANUAL_FILE_CHECK%"=="1" (
   echo %MANUAL_FILE_CHECK_PATH%|find /i "\HLSLFileCheck\" >nul
   if errorlevel 1 (
-        echo "run lit file-check"
-        set TEST_MANUAL_FILE_CHECK=0
-        set TEST_CLANG=0
-        set TEST_DXILCONV=0
-        set TEST_SPIRV=0
-        set TEST_EXEC=0
-        set TEST_CMD=0
-        py %BIN_DIR%\llvm-lit.py %MANUAL_FILE_CHECK_PATH% -v
+        if "%MANUAL_FILE_CHECK_PATH:~-14%" == "\HLSLFileCheck" (
+            set TEST_USE_LIT=0
+            echo "run taef file-check"
+        ) else (
+            echo "run lit file-check"
+            set TEST_MANUAL_FILE_CHECK=0
+            set TEST_CLANG=0
+            set TEST_DXILCONV=0
+            set TEST_SPIRV=0
+            set TEST_EXEC=0
+            set TEST_CMD=0
+            py %BIN_DIR%\llvm-lit.py %MANUAL_FILE_CHECK_PATH% -v
+		)
   ) else (
         set TEST_USE_LIT=0
         echo "run taef file-check"
