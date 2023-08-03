860de9f101679b9d53fa2ef7b8095e02fde60677
Joshua Batista <jbatista@microsoft.com>
2023-08-03 14:26:17 -0700

    clean up unused vars, rename instructions to be correct

--- tools/clang/test/HLSLFileCheck/hlsl/objects/RayQuery/readnone_intrinsicAttributeCollision.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/RayQuery/readnone_intrinsicAttributeCollision.hlsl
@@ -4,24 +4,21 @@
 // IncrementCounter called before GetRenderTargetSampleCount.
 // Don't be sensitive to HL Opcode because those can change.
 // make sure that we are detecting the .rn attribute in the call
-// CHECK: call float [[GetRenderTargetSampleCount:@"[^".]+\.[^.]+\.[^.]+\.rn[^"]+"]](i32
+// CHECK: call float [[EvaluateAttributeCentroid:@"[^".]+\.[^.]+\.[^.]+\.rn[^"]+"]](i32
 // ^ matches call i32 @"dx.hl.op.rn.i32 (i32)"(i32 19), !dbg !45 ; line:33 col:14
-// CHECK: call float [[IncrementCounter:@"[^"]+"]](i32
+// CHECK: call float [[QuadReadAcrossX:@"[^"]+"]](i32
 
 // Ensure HL declarations are not collapsed when attributes differ
-// CHECK-DAG: declare float [[GetRenderTargetSampleCount]]({{.*}}) #[[AttrGetRenderTargetSampleCount:[0-9]+]]
-// CHECK-DAG: declare float [[IncrementCounter]]({{.*}}) #[[AttrIncrementCounter:[0-9]+]]
+// CHECK-DAG: declare float [[EvaluateAttributeCentroid]]({{.*}}) #[[AttrEvauluateAttributeCentroid:[0-9]+]]
+// CHECK-DAG: declare float [[QuadReadAcrossX]]({{.*}}) #[[AttrQuadReadAcrossX:[0-9]+]]
 
 // Ensure correct attributes for each HL intrinsic
-// CHECK-DAG: attributes #[[AttrGetRenderTargetSampleCount]] = { nounwind readnone }
-// CHECK-DAG: attributes #[[AttrIncrementCounter]] = { nounwind }
+// CHECK-DAG: attributes #[[AttrEvauluateAttributeCentroid]] = { nounwind readnone }
+// CHECK-DAG: attributes #[[AttrQuadReadAcrossX]] = { nounwind }
 
-// Ensure GetRenderTargetSampleCount not eliminated in final DXIL:
+// Ensure EvaluateAttributeCentroid not eliminated in final DXIL:
 // CHECKDXIL: call float @dx.op.evalCentroid.f32(
 // CHECKDXIL: call float @dx.op.quadOp.f32(
-
-RaytracingAccelerationStructure AccelerationStructure : register(t0);
-RWByteAddressBuffer log : register(u0);
 
 StructuredBuffer<int> buf[]: register(t3);
 RWStructuredBuffer<int> uav;
