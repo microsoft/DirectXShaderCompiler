823e9ca3c24e07502b10ae8c39ecb83d6471d3d0
Adam Yang <hanbyang@microsoft.com>
2023-08-08 15:01:10 -0700

    Refactored opt toggles

--- tools/clang/lib/CodeGen/BackendUtil.cpp
+++ tools/clang/lib/CodeGen/BackendUtil.cpp
@@ -337,26 +337,19 @@ void EmitAssemblyHelper::CreatePasses() {
   PMBuilder.HLSLResMayAlias = CodeGenOpts.HLSLResMayAlias;
   PMBuilder.ScanLimit = CodeGenOpts.ScanLimit;
 
-  PMBuilder.EnableGVN = !CodeGenOpts.HLSLOptimizationToggles.count("gvn") ||
-                        CodeGenOpts.HLSLOptimizationToggles.find("gvn")->second;
-
-  PMBuilder.HLSLNoSink = CodeGenOpts.HLSLOptimizationToggles.count("sink") &&
-                         !CodeGenOpts.HLSLOptimizationToggles.find("sink")->second;
-
-  PMBuilder.StructurizeLoopExitsForUnroll =
-                        !CodeGenOpts.HLSLOptimizationToggles.count("structurize-loop-exits-for-unroll") ||
-                        CodeGenOpts.HLSLOptimizationToggles.find("structurize-loop-exits-for-unroll")->second;
-
-  PMBuilder.HLSLEnableDebugNops =
-                        !CodeGenOpts.HLSLOptimizationToggles.count("debug-nops") ||
-                        CodeGenOpts.HLSLOptimizationToggles.find("debug-nops")->second;
-
+  // Opt toggles
+  const hlsl::options::OptimizationToggles &OptToggles =
+      CodeGenOpts.HLSLOptimizationToggles;
+  PMBuilder.EnableGVN = OptToggles.IsEnabled(hlsl::options::TOGGLE_GVN);
+  PMBuilder.HLSLNoSink = !OptToggles.IsEnabled(hlsl::options::TOGGLE_SINK);
+  PMBuilder.StructurizeLoopExitsForUnroll = OptToggles.IsEnabled(
+      hlsl::options::TOGGLE_STRUCTURIZE_LOOP_EXITS_FOR_UNROLL);
+  PMBuilder.HLSLEnableDebugNops = OptToggles.IsEnabled(hlsl::options::TOGGLE_DEBUG_NOPS);
   PMBuilder.HLSLEnableLifetimeMarkers =
       CodeGenOpts.HLSLEnableLifetimeMarkers &&
-      (!CodeGenOpts.HLSLOptimizationToggles.count("lifetime-markers") ||
-       CodeGenOpts.HLSLOptimizationToggles.find("lifetime-markers")->second);
-
-  PMBuilder.HLSLEnablePartialLifetimeMarkers = CodeGenOpts.HLSLEnablePartialLifetimeMarkers;
+      OptToggles.IsEnabled(hlsl::options::TOGGLE_LIFETIME_MARKERS);
+  PMBuilder.HLSLEnablePartialLifetimeMarkers =
+      OptToggles.IsEnabled(hlsl::options::TOGGLE_PARTIAL_LIFETIME_MARKERS);
   // HLSL Change - end
 
   PMBuilder.DisableUnitAtATime = !CodeGenOpts.UnitAtATime;
