94d9a8a5ef0ac2bf264367266c0e15badd20110e
Cassandra Beckley <cbeckley@google.com>
2023-09-07 06:37:00 -0700

    [SPIR-V] Add fragment interlock extension by default (#5646)
    Adds the extension `SPV_EXT_fragment_shader_interlock` and the related
    capabilities
    
    - `spv::Capability::FragmentShaderSampleInterlockEXT`
    - `spv::Capability::FragmentShaderPixelInterlockEXT`
    - `spv::Capability::FragmentShaderShadingRateInterlockEXT`
    
    to every shader by default, and let the `spirv-opt` capability trimming
    pass remove them if unnecessary.

--- tools/clang/test/CodeGenSPIRV_Lit/capability.trimmed.o3.hlsl
+++ tools/clang/test/CodeGenSPIRV_Lit/capability.trimmed.o3.hlsl
@@ -5,6 +5,13 @@ SamplerState gSampler : register(s2);
 
 // CHECK-NOT: OpCapability MinLod
 
+// CHECK-NOT: OpExtension "SPV_EXT_fragment_shader_interlock"
+// CHECK-NOT: OpCapability FragmentShaderSampleInterlockEXT
+// CHECK-NOT: OpCapability FragmentShaderPixelInterlockEXT
+// CHECK-NOT: OpCapability FragmentShaderShadingRateInterlockEXT
+
+// CHECK-NOT: OpExtension "SPV_EXT_fragment_shader_interlock"
+
 float4 main(uint clamp : A) : SV_Target {
     if (clamp < 0) {
       return t.Sample(gSampler, 0.5f, 2, float(clamp));
