e08d8173f57ebbdaa568db5791b7d4e7e77b0405
Xiang Li <python3kgae@outlook.com>
2023-09-19 10:25:27 -0400

    Put echo message inside "".

--- utils/hct/hcttest.cmd
+++ utils/hct/hcttest.cmd
@@ -313,7 +313,7 @@ if "%TEST_USE_LIT%"=="1" (
     set RES_DXILCONV=!RES_CLANG!
     rem check exec.
     if defined EXEC_ADAPTER (
-        echo The -adapter parameter is supported only when running just execution tests (hcttest.cmd exec)
+        echo "The -adapter parameter is supported only when running just execution tests (hcttest.cmd exec)"
       )
     set RES_EXEC=!ERRORLEVEL!
   ) else (
