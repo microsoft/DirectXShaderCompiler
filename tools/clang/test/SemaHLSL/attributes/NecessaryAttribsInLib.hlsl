7b3f6d934a99fe7f91d1d284f36cd99f870f4116
Joshua Batista <jbatista@microsoft.com>
2023-10-03 14:27:02 -0700

    improve expected error readability

--- tools/clang/test/SemaHLSL/attributes/NecessaryAttribsInLib.hlsl
+++ tools/clang/test/SemaHLSL/attributes/NecessaryAttribsInLib.hlsl
@@ -6,7 +6,8 @@ struct Payload {
 
 //[numthreads(8, 1, 1)]
 [shader("amplification")]
-void ASmain() /* expected-error{{amplification entry point must have the numthreads attribute}} */
+/* expected-error@+1{{amplification entry point must have the numthreads attribute}} */
+void ASmain()
 {
     Payload pld;
     pld.dummy = float2(1.0,2.0);
@@ -19,7 +20,8 @@ void ASmain() /* expected-error{{amplification entry point must have the numthre
 [outputtopology("triangle_cw")]
 [outputcontrolpoints(3)]
 //[patchconstantfunc("HSPatch")]
-float4 HSmain(uint ix : SV_OutputControlPointID) /* expected-error{{hull entry point must have the patchconstantfunc attribute}} */
+/* expected-error@+1{{hull entry point must have the patchconstantfunc attribute}} */
+float4 HSmain(uint ix : SV_OutputControlPointID)
 {
   return 0;
 }
@@ -30,7 +32,8 @@ struct VSOut {
 
 //[maxvertexcount(3)]
 [shader("geometry")]
-void GSmain(inout TriangleStream<VSOut> stream) { /* expected-error{{geometry entry point must have the maxvertexcount attribute}} */
+/* expected-error@+1{{geometry entry point must have the maxvertexcount attribute}} */
+void GSmain(inout TriangleStream<VSOut> stream) {
   VSOut v = {0.0, 0.0, 0.0, 0.0};
   stream.Append(v);
   stream.RestartStrip();
@@ -44,7 +47,9 @@ struct myvert {
 [shader("mesh")]
 //[NumThreads(8, 8, 2)]
 //[OutputTopology("triangle")]
-void MSmain(out vertices myvert verts[32], /* expected-error{{mesh entry point must have the numthreads attribute}} */ /* expected-error{{mesh entry point must have the outputtopology attribute}} */
+// expected-error@+2{{mesh entry point must have the numthreads attribute}}
+// expected-error@+1{{mesh entry point must have the outputtopology attribute}}
+void MSmain(out vertices myvert verts[32],
           uint ix : SV_GroupIndex) {
   SetMeshOutputCounts(32, 16);
   myvert v = {0.0, 0.0, 0.0, 0.0};
