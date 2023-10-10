38702581575b5d0a3798dd509a48f618c67abcf8
Tim Corringham <timothy.corringham@amd.com>
2023-10-10 21:29:06 +0100

    Amend compute node compatibility DXIL validation (#5589)
    Update the DXIL validation for compute node compatibility, and for node
    input vs launch type compatibility, to make the diagnostics more
    informative, and update the validation tests accordingly.
    
    Fixes #5348
    
    ---------
    
    Co-authored-by: Tim Corringham <tcorring@amd.com>
    Co-authored-by: Joshua Batista <jbatista@microsoft.com>

--- tools/clang/test/DXILValidation/compute_node_compatibility.hlsl
+++ tools/clang/test/DXILValidation/compute_node_compatibility.hlsl
@@ -1,83 +1,39 @@
 // ==================================================================
-// Errors are expected for shaders with both "node" and "compute"
-// specified when:
-// - the launch type is not broadcasting
-// - a broadcasting node has an input record and/or output records
-// This test operates by changing the [Shader(node)] metadata entry
-// to [Shader(compute)] as if both had been specified in the HLSL,
-// for each shader in turn.
+// This HLSL source is used by ValidationTest::ComputeNodeCompatibility,
+// which modifies metadata in this compiled library to verify 
+// validation rules related to node shader entry metadata.
+// The shader is compiled with "-T lib_6_8 -HV 2021"
 // ==================================================================
 
 struct RECORD {
   uint a;
 };
 
-//[Shader("compute")]
 [Shader("node")]
-[NumThreads(128,1,1)]
-[NodeLaunch("coalescing")]
-void node01() { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(128,1,1)]
-[NodeLaunch("coalescing")]
-void node02(GroupNodeInputRecords<RECORD> input) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(128,1,1)]
-[NodeLaunch("coalescing")]
-void node03(RWGroupNodeInputRecords<RECORD> input) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(128,1,1)]
-[NodeLaunch("coalescing")]
-void node04(EmptyNodeInput input) { }
-
-//[Shader("compute")]
-[Shader("node")]
+[NodeLaunch("broadcasting")]
+[NodeDispatchGrid(1, 1, 1)]
 [NumThreads(1,1,1)]
-[NodeLaunch("thread")]
+void node01(DispatchNodeInputRecord<RECORD> input) { }
+
+[Shader("node")]
+[NodeLaunch("broadcasting")]
+[NodeDispatchGrid(2, 1, 1)]
+[NumThreads(1,1,1)]
+void node02(RWDispatchNodeInputRecord<RECORD> input) { }
+
+[Shader("node")]
+[NodeLaunch("broadcasting")]
+[NodeDispatchGrid(3, 1, 1)]
+[NumThreads(1,1,1)]
+void node03(NodeOutput<RECORD> output) { }
+
+[Shader("node")]
+[NodeLaunch("coalescing")]
+[NumThreads(1,1,1)]
+void node04() { }
+
+[Shader("node")]
+[NodeLaunch("broadcasting")]
+[NodeDispatchGrid(1, 1, 1)]
+[NumThreads(1,1,1)]
 void node05() { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(1,1,1)]
-[NodeLaunch("thread")]
-void node06(ThreadNodeInputRecord<RECORD> input) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(1,1,1)]
-[NodeLaunch("thread")]
-void node07(RWThreadNodeInputRecord<RECORD> input) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(1024,1,1)]
-[NodeLaunch("broadcasting")]
-[NodeDispatchGrid(128,1,1)]
-void node08(DispatchNodeInputRecord<RECORD> input) { }
-
-//[Shader("compute")]
-[NumThreads(1024,1,1)]
-[Shader("node")]
-[NodeLaunch("broadcasting")]
-[NodeDispatchGrid(128,1,1)]
-void node09(RWDispatchNodeInputRecord<RECORD> input) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NodeLaunch("broadcasting")]
-[NumThreads(1024,1,1)]
-[NodeDispatchGrid(128,1,1)]
-void node10(NodeOutput<RECORD> output) { }
-
-//[Shader("compute")]
-[Shader("node")]
-[NumThreads(1024,1,1)]
-[NodeLaunch("broadcasting")]
-[NodeDispatchGrid(128,1,1)]
-void node11(EmptyNodeOutput output) { }
