ca9ed1d6bca5054201adfac3ddf95d443e270674
Joshua Batista <jbatista@microsoft.com>
2023-10-18 16:15:14 -0700

    cf

--- lib/HLSL/HLOperationLower.cpp
+++ lib/HLSL/HLOperationLower.cpp
@@ -6441,7 +6441,7 @@ IntrinsicLower gLowerTable[] = {
     {IntrinsicOp::IOP_EvaluateAttributeCentroid, TranslateEvalCentroid,
      DXIL::OpCode::EvalCentroid},
     {IntrinsicOp::IOP_EvaluateAttributeSnapped, TranslateEvalSnapped,
-     DXIL::OpCode::NumOpCodes},    
+     DXIL::OpCode::NumOpCodes},
     {IntrinsicOp::IOP_GeometryIndex, TrivialNoArgWithRetOperation,
      DXIL::OpCode::GeometryIndex},
     {IntrinsicOp::IOP_GetAttributeAtVertex, TranslateGetAttributeAtVertex,
@@ -9096,7 +9096,7 @@ void TranslateHLSubscript(CallInst *CI, HLSubscriptOpcode opcode,
     Type *HandleTy = hlslOP->GetHandleType();
     if (ptr->getType() == hlslOP->GetNodeRecordHandleType()) {
       DXASSERT(false, "Shouldn't get here, NodeRecord subscripts should have "
-                          "generated DefaultSubscript intrinsic");
+                      "generated DefaultSubscript intrinsic");
       return;
     }
     if (ptr->getType() == HandleTy) {
