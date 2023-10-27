9bb8dfc06fdd20e03cb81d0e31e8633193f60218
Xiang Li <python3kgae@outlook.com>
2023-10-27 08:32:23 -0400

    Change name to main.

--- tools/clang/test/SemaHLSL/template-udt-load.hlsl
+++ tools/clang/test/SemaHLSL/template-udt-load.hlsl
@@ -6,7 +6,7 @@ RWBuffer<float> Out;
 
 [shader("compute")]
 [numthreads(1,1,1)]
-void CSMain()
+void main()
 { 
   RWBuffer<float> FB = In.Load<RWBuffer<float> >(0); // expected-error {{Explicit template arguments on intrinsic Load must be a single numeric type}}
   Out[0] = FB[0];
