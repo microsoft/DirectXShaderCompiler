7120cbf43561cfae6101ea7ef937bfee3e1f441f
Xiang Li <python3kgae@outlook.com>
2023-10-27 20:59:14 -0400

    Use int for start vertex location.

--- tools/clang/test/HLSL/system-values.hlsl
+++ tools/clang/test/HLSL/system-values.hlsl
@@ -40,7 +40,7 @@
 #define Def_Barycentrics DECLARE(float3 BaryWeights : SV_Barycentrics) USE(float, BaryWeights.x) USE(float, BaryWeights.y) USE(float, BaryWeights.z)
 #define Def_ShadingRate DECLARE(uint rate : SV_ShadingRate) USE(uint, rate)
 #define Def_CullPrimitive DECLARE(bool cullprim : SV_CullPrimitive) USE(bool, cullprim)
-#define Def_StartVertexLocation DECLARE(uint svloc : SV_StartVertexLocation) USE(int, svloc)
+#define Def_StartVertexLocation DECLARE(int svloc : SV_StartVertexLocation) USE(int, svloc)
 #define Def_StartInstanceLocation DECLARE(uint siloc : SV_StartInstanceLocation) USE(uint, siloc)
 
 #define Domain_Quad 0
