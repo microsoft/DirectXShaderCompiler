0b248730eccf174484eeae677daacaab1070faad
Joshua Batista <jbatista@microsoft.com>
2023-11-01 16:37:03 -0700

    consistent %

--- tools/clang/test/SemaHLSL/recursion/library_recursion.hlsl
+++ tools/clang/test/SemaHLSL/recursion/library_recursion.hlsl
@@ -1,4 +1,4 @@
-// RUN: dxc -Tlib_6_5 -verify %s 
+// RUN: %dxc -Tlib_6_5 -verify %s 
 
 // expected-error@+1{{recursive functions are not allowed: entry function calls recursive function 'recurse'}}
 void recurse(inout float4 f, float a) 
