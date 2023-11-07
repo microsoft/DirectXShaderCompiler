a608b69d903cbd8009eb10e3850d40b8724ba0af
Joshua Batista <jbatista@microsoft.com>
2023-11-06 16:34:20 -0800

    cf

--- tools/clang/lib/Sema/SemaHLSL.cpp
+++ tools/clang/lib/Sema/SemaHLSL.cpp
@@ -11401,17 +11401,17 @@ bool hlsl::DiagnoseNodeStructArgument(Sema *self, TemplateArgumentLoc ArgLoc,
   }
 }
 
-bool IsExported(Sema *self, clang::FunctionDecl *FD) {        
+bool IsExported(Sema *self, clang::FunctionDecl *FD) {
   if (FD->hasAttr<HLSLShaderAttr>()) {
-		return true;
-	}
-  
+    return true;
+  }
+
   bool isMarkedStatic = false;
   bool isMarkedExport = false;
 
   if (auto cDecl = dyn_cast<CXXMethodDecl>(FD)) {
     if (cDecl->isStatic()) {
-        isMarkedStatic = true;
+      isMarkedStatic = true;
     }
   }
 
@@ -11420,30 +11420,26 @@ bool IsExported(Sema *self, clang::FunctionDecl *FD) {
   }
 
   if (isMarkedStatic && isMarkedExport) {
-    self->Diag(FD->getLocation(), diag::err_hlsl_varmodifiersna)
-        << "static"
-        << "export"
-        << "function";
-		return false;
-	}
-
+    self->Diag(FD->getLocation(), diag::err_hlsl_varmodifiersna) << "static"
+                                                                 << "export"
+                                                                 << "function";
+    return false;
+  }
 
   Linkage L = FD->getLinkageAndVisibility().getLinkage();
 
-  
   const auto *shaderModel =
       hlsl::ShaderModel::GetByName(self->getLangOpts().HLSLProfile.c_str());
   // case 1 requires special assignments to linkage
   if (L != ExternalLinkage && L != InternalLinkage) {
     if (shaderModel->IsSM60Plus()) {
       L = ExternalLinkage;
-    }
-		else {
+    } else {
       L = InternalLinkage;
     }
-  }   
-  
-  // now case 2 and 3 can apply and we can determine 
+  }
+
+  // now case 2 and 3 can apply and we can determine
   // whether the function is exported.
   if (L == InternalLinkage && isMarkedExport) {
     return true;
@@ -11468,7 +11464,7 @@ bool IsExported(Sema *self, clang::FunctionDecl *FD) {
 // references a patch constant function, there exists a function declaration
 // that could serve as a candidate to that patch constant function.
 void ValidatePatchConstantFunctionsExist(clang::Sema *self) {
-  
+
   for (auto decl : self->getASTContext().getTranslationUnitDecl()->decls()) {
     // TODO: improve condition so that only exported functions are checked,
     // instead of all functions. Issue: #5857
