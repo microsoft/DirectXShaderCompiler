34b2f061c8aeb6a00861abd9fcd5e8cdb459c8f0
Joshua Batista <jbatista@microsoft.com>
2023-11-13 13:37:07 -0800

    perform refactor, still could use cleanup

--- tools/clang/test/HLSLFileCheck/hlsl/functions/recursion/library_hull_recursion.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/functions/recursion/library_hull_recursion.hlsl
@@ -1,23 +1,5 @@
 // RUN: %dxc -T lib_6_5 %s | FileCheck %s
 
-struct HSPerPatchData
-{
-  float edges[3] : SV_TessFactor;
-  float inside   : SV_InsideTessFactor;
-};
-
-HSPerPatchData HSPerPatchFunc1()
-{
-  HSPerPatchData d;
-
-  d.edges[0] = -5;
-  d.edges[1] = -6;
-  d.edges[2] = -7;
-  d.inside = -8;
-  // CHECK: error: recursive functions are not allowed: patch constant function calls recursive function 'HSPerPatchFunc1'
-  HSPerPatchFunc1();
-  return d;
-}
 
 // no error, because this function isn't exported. 
 // it's reachable from main, but recurse is detected first before recurse2
@@ -36,6 +18,25 @@ void recurse(inout float4 f, float a)
       recurse2(f, a-1);
     }
     f += abs(f+a);
+}
+
+struct HSPerPatchData
+{
+  float edges[3] : SV_TessFactor;
+  float inside   : SV_InsideTessFactor;
+};
+
+HSPerPatchData HSPerPatchFunc1()
+{
+  HSPerPatchData d;
+
+  d.edges[0] = -5;
+  d.edges[1] = -6;
+  d.edges[2] = -7;
+  d.inside = -8;
+  // CHECK: error: recursive functions are not allowed: patch constant function calls recursive function 'HSPerPatchFunc1'
+  HSPerPatchFunc1();
+  return d;
 }
 
 [shader("hull")]
