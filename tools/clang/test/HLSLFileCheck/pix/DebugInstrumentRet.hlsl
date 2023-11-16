caa782abf037cdae0cb9a11cb2837ee421aa1c87
Jeff Noyle <jeffno@microsoft.com>
2023-11-16 12:56:43 -0800

    Comment update

--- tools/clang/test/HLSLFileCheck/pix/DebugInstrumentRet.hlsl
+++ tools/clang/test/HLSLFileCheck/pix/DebugInstrumentRet.hlsl
@@ -3,9 +3,9 @@
 
 
 
-// The ret's instrument number should be 4 (the last integer on this line):
+// The ret's instruction number should be 4 (the last integer on this line):
 // CHECK: call void @dx.op.bufferStore.i32(i32 69, %dx.types.Handle %PIX_DebugUAV_Handle, i32 {{.*}}, i32 undef, i32 4
-// But we'll check that instrument number:
+// But we'll check that instruction number:
 // CHECK: ret void, !pix-dxil-inst-num [[RetInstNum:![0-9]+]]
 // CHECK-DAG: [[RetInstNum]] = !{i32 3, i32 4}
 
