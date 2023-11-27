5d5a940fdb25e1c70f0385674a4748b7326de765
Xiang Li <python3kgae@outlook.com>
2023-11-27 01:20:45 -0800

    [lit][SPIRV] convert runFileTest shaders to lit FileCheck test (61 left to go)  (#6062)
    Convert from bottom of CodeGenSpirvTest.cpp
    Skipped FileTest::CompatibilityWithVk1p1 as barrier for runCodeTest.

---
+++ tools/clang/test/CodeGenSPIRV_Lit/operator.overloading.resource.type.check.hlsl
@@ -0,0 +1,27 @@
+// RUN: %dxc -T hs_6_0 -E main -fcgl  %s -spirv | FileCheck %s
+
+struct INPUT
+{
+    float4 Pos : SV_Position;
+};
+
+struct OUTPUT
+{
+    float x[3] : SV_TessFactor;
+};
+
+[outputcontrolpoints(3)]
+[patchconstantfunc("foo")]
+
+INPUT main(InputPatch<INPUT, 3> Input,
+           uint PointID : SV_OutputControlPointID)
+{
+// CHECK: [[PointID:%[0-9]+]] = OpLoad %uint %PointID
+// CHECK: OpAccessChain %_ptr_Function_INPUT %Input [[PointID]]
+    return Input[PointID];
+}
+
+OUTPUT foo()
+{
+    return (OUTPUT)0;
+}
