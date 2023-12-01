a5ab095b93b6fe85da4843457b1e65e24c8cd1b5
Joshua Batista <jbatista@microsoft.com>
2023-11-30 16:21:43 -0800

    add diags for incompatible sv values

--- tools/clang/lib/CodeGen/CGHLSLMS.cpp
+++ tools/clang/lib/CodeGen/CGHLSLMS.cpp
@@ -1864,6 +1864,50 @@ void CGMSHLSLRuntime::AddHLSLFunctionInfo(Function *F, const FunctionDecl *FD) {
       funcProps->numThreads[1] = 1;
       funcProps->numThreads[2] = 1;
     }
+
+    // Validate Compute Shader system value inputs per launch mode
+    for (ParmVarDecl *param : FD->parameters()) {
+      for (const hlsl::UnusualAnnotation *it : param->getUnusualAnnotations()) {
+        if (it->getKind() == hlsl::UnusualAnnotation::UA_SemanticDecl) {
+          const hlsl::SemanticDecl *sd = cast<hlsl::SemanticDecl>(it);
+          // if the node launch type is Thread, then there are no system values
+          // allowed
+          if (funcProps->Node.LaunchType == DXIL::NodeLaunchType::Thread) {
+            if (sd->SemanticName.equals("SV_GroupThreadID") ||
+                sd->SemanticName.equals("SV_GroupIndex") ||
+                sd->SemanticName.equals("SV_GroupID") ||
+                sd->SemanticName.equals("SV_DispatchThreadID")) {
+              // emit diagnostic
+              unsigned DiagID = Diags.getCustomDiagID(
+                  DiagnosticsEngine::Error,
+                  "Invalid system value semantic '%0' for launchtype '%1'");
+              Diags.Report(param->getLocation(), DiagID)
+                  << sd->SemanticName << "Thread";
+            }
+          }
+
+          // if the node launch type is Coalescing, then
+          // SV_GroupIndex and SV_GroupThreadID are allowed
+          else if (funcProps->Node.LaunchType ==
+                   DXIL::NodeLaunchType::Coalescing) {
+            if (sd->SemanticName.equals("SV_GroupID") ||
+                sd->SemanticName.equals("SV_DispatchThreadID")) {
+              // emit diagnostic
+              unsigned DiagID = Diags.getCustomDiagID(
+                  DiagnosticsEngine::Error,
+                  "Invalid system value semantic '%0' for launchtype '%1'");
+              Diags.Report(param->getLocation(), DiagID)
+                  << sd->SemanticName << "Coalescing";
+            }
+          }
+          // Broadcasting nodes allow all node shader system value semantics
+          else if (funcProps->Node.LaunchType ==
+                   DXIL::NodeLaunchType::Broadcasting) {
+            continue;
+          }
+        }
+      }
+    }
   }
 
   const unsigned profileAttributes =
