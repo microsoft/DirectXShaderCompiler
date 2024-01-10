7cf55c19c69d14f89c770f64ba524ddd21d1e8c4
Natalie Chouinard <chouinard@google.com>
2024-01-10 16:51:30 +0000

    More -verify tests

--- tools/clang/test/CodeGenSPIRV/attribute.outputtopology.error.hlsl
+++ tools/clang/test/CodeGenSPIRV/attribute.outputtopology.error.hlsl
@@ -1,4 +1,4 @@
-// RUN: not %dxc -T hs_6_0 -E Hull -spirv %s 2>&1 | FileCheck %s
+// RUN: %dxc -T hs_6_0 -E Hull -spirv -verify %s
 
 struct ControlPoint { float4 position : POSITION; }; 
 struct HullPatchOut {
@@ -8,10 +8,10 @@ struct HullPatchOut {
 
 HullPatchOut HullConst (InputPatch<ControlPoint,3> v) { return (HullPatchOut)0; }
 
-//CHECK: error: hull entry point must have a valid outputtopology attribute
-
 [domain("tri")]
 [partitioning("fractional_odd")]
 [patchconstantfunc("HullConst")]
 [outputcontrolpoints(3)]
-ControlPoint Hull (InputPatch<ControlPoint,3> v, uint id : SV_OutputControlPointID) { return v[id]; }
+ControlPoint Hull (InputPatch<ControlPoint,3> v, uint id : SV_OutputControlPointID) { /* expected-error{{hull entry point must have a valid outputtopology attribute}} */
+    return v[id];
+}
