7f7e84e8bce01d2bfacc13417888d9cf2742784b
Tim Corringham <tcorring@amd.com>
2024-01-12 11:36:09 +0000

    Update test
    Modified the class_input_record.hlsl test to use -verify rather
    than FileCheck to check that no diagnostics are generated, and
    relocated the test to SemaHLSL

--- tools/clang/test/SemaHLSL/hlsl/workgraph/class_input_record.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/workgraph/class_input_record.hlsl
@@ -1,16 +1,14 @@
-// RUN: %dxc -T lib_6_8 `%s | FileCheck %s
+// RUN: %dxc -T lib_6_8 -verify %s
 // ==================================================================
 // Broadcasting launch node with class input record
 // ==================================================================
+
+// expected-no-diagnostics
 
 class ClassInputRecord
 {
   uint a;
 };
-
-// CHECK-NOT: error
-// CHECK: define void @node01()
-// CHECK-NOT: error
 
 [Shader("node")]
 [NodeLaunch("broadcasting")]
