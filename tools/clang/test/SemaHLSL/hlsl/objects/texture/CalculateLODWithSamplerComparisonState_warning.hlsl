daffd6bfaaa263ad85178912cfc2b2ae22cc5377
Xiang Li <python3kgae@outlook.com>
2024-01-16 13:27:39 -0500

    Test -Wno-error=

--- tools/clang/test/SemaHLSL/hlsl/objects/texture/CalculateLODWithSamplerComparisonState_warning.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/objects/texture/CalculateLODWithSamplerComparisonState_warning.hlsl
@@ -1,27 +1,26 @@
-// RUN: dxc -Tlib_6_7 -Wno-hlsl-availability  %s -verify
-// TODO: Enable -Wno-error=hlsl-availability after support -Wno-error=
-// XFAIL: dxc -Tlib_6_7 -Wno-error=hlsl-availability  %s -verify
+// RUN: dxc -Tlib_6_7 %s -Wno-error=hlsl-availability -verify
 
 SamplerComparisonState s;
 Texture1D t;
 
-// Make sure -Wno-hlsl-availability suppresses the error.
-// expected-no-diagnostics
+// Make sure direct call to CalculateLevelOfDetail and CalculateLevelOfDetailUnclamped get warning with -Wno-error.
 
 export
 float foo(float a) {
-  return t.CalculateLevelOfDetail(s, a) +
-    t.CalculateLevelOfDetailUnclamped(s, a);
-    }
+  return t.CalculateLevelOfDetail(s, a) + // expected-warning {{overload of intrinsic CalculateLevelOfDetail requires shader model 6.8 or greater}}
+    t.CalculateLevelOfDetailUnclamped(s, a); // expected-warning {{overload of intrinsic CalculateLevelOfDetailUnclamped requires shader model 6.8 or greater}}
+}
 
+// Make sure unused function call to CalculateLevelOfDetail and CalculateLevelOfDetailUnclamped don't get warning with -Wno-error.
 float bar(float a) {
   return t.CalculateLevelOfDetail(s, a) +
     t.CalculateLevelOfDetailUnclamped(s, a);
 }
 
+// Make sure nested call to CalculateLevelOfDetail and CalculateLevelOfDetailUnclamped get warning with -Wno-error.
 float foo2(float a) {
-  return t.CalculateLevelOfDetail(s, a) +
-    t.CalculateLevelOfDetailUnclamped(s, a);
+  return t.CalculateLevelOfDetail(s, a) + // expected-warning {{overload of intrinsic CalculateLevelOfDetail requires shader model 6.8 or greater}}
+    t.CalculateLevelOfDetailUnclamped(s, a); // expected-warning {{overload of intrinsic CalculateLevelOfDetailUnclamped requires shader model 6.8 or greater}}
 }
 
 export
@@ -29,14 +28,18 @@ float bar2(float a) {
   return foo2(a);
 }
 
+// Make sure report warning with -Wno-error when derivatives not supported.
+
 [shader("pixel")]
 float ps(float a:A) : SV_Target {
-  return t.CalculateLevelOfDetail(s, a) +
-    t.CalculateLevelOfDetailUnclamped(s, a);
+  return t.CalculateLevelOfDetail(s, a) + // expected-warning {{overload of intrinsic CalculateLevelOfDetail requires shader model 6.8 or greater}}
+    t.CalculateLevelOfDetailUnclamped(s, a); // expected-warning {{overload of intrinsic CalculateLevelOfDetailUnclamped requires shader model 6.8 or greater}}
 }
 
 [shader("vertex")]
 float4 vs(float a:A) : SV_Position {
-  return t.CalculateLevelOfDetail(s, a) +
-    t.CalculateLevelOfDetailUnclamped(s, a);
+  // expected-warning@+1 {{Derivatives intrinsic CalculateLevelOfDetail only works in ps and cs_6.6+/as_6.6+/ms_6.6+/node}}
+  return t.CalculateLevelOfDetail(s, a) + // expected-warning {{overload of intrinsic CalculateLevelOfDetail requires shader model 6.8 or greater}}
+  // expected-warning@+1 {{Derivatives intrinsic CalculateLevelOfDetailUnclamped only works in ps and cs_6.6+/as_6.6+/ms_6.6+/node}}
+    t.CalculateLevelOfDetailUnclamped(s, a); // expected-warning {{overload of intrinsic CalculateLevelOfDetailUnclamped requires shader model 6.8 or greater}}
 }
