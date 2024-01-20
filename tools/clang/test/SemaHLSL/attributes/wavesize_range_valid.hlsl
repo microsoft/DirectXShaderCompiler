8892782ad142fd959059e9385c692a80af7ef936
Joshua Batista <jbatista@microsoft.com>
2024-01-19 19:18:02 -0800

    add positive test that allows for 2 parameters to wavesize range

--- tools/clang/test/SemaHLSL/attributes/wavesize_range_valid.hlsl
+++ tools/clang/test/SemaHLSL/attributes/wavesize_range_valid.hlsl
@@ -19,3 +19,13 @@ struct INPUT_RECORD
 [NodeMaxDispatchGrid(32,1,1)]
 [WaveSize(4, 16, 8)]
 void node01(DispatchNodeInputRecord<INPUT_RECORD> input) { }
+
+// CHECK: @node02, !"node02", null, null, [[PROPS:![0-9]+]]}
+// CHECK: [[PROPS]] = !{i32 8, i32 15, i32 13, i32 1, i32 23, [[WS:![0-9]+]]
+// CHECK: [[WS]] = !{i32 4, i32 16, i32 0}
+[Shader("node")]
+[NodeLaunch("broadcasting")]
+[NumThreads(1,1,1)]
+[NodeMaxDispatchGrid(32,1,1)]
+[WaveSize(4, 16)]
+void node02(DispatchNodeInputRecord<INPUT_RECORD> input) { }
