74e3256484f9f29a717eb70be7c7448beebfc350
Steve Urquhart <53908460+SteveUrquhart@users.noreply.github.com>
2024-02-02 10:19:49 -0500

    [SPIRV] Always provide original source to debug (#6085)
    This PR improves the experience for debugging tools that consume SPIR-V
    produced by passing in-memory strings to dxcompiler.dll. When dxc.exe is
    used, OpSource and OpDebugSource are populated by reading original
    source from the filesystem. dxcompiler.dll users instead find their
    input string is preprocessed, which is confusing behavior.
    Contrary to the leading comment in the code this change modifies,
    debugging tools do not want preprocessed source. Such tools show and
    allow editing of include files. If everything is pasted into a single
    source string, finding what to edit is difficult. Editing the
    preprocessed source has the additional complication that a user would
    have to edit also the #line markers, which is hard to get right
    manually. In the preprocessed source, all comments are lost, which makes
    it hard to read the code. Finally in preprocessed source, all macros and
    code under conditional compilation are lost. That will make it
    impossible to modify those parts in an edit.
    
    Co-authored-by: Steve Urquhart <stevur01@ntd.nintendo.com>

--- tools/clang/unittests/SPIRV/CodeGenSpirvTest.cpp
+++ tools/clang/unittests/SPIRV/CodeGenSpirvTest.cpp
@@ -19,12 +19,26 @@ using ::testing::ContainsRegex;
 // This test is purely for demonstrating how to use `LibTest`, and does not
 // test anything specific in DXC itself.
 TEST_F(LibTest, SourceCodeWithoutFilePath) {
-  const std::string command(R"(// RUN: %dxc -T ps_6_0 -E PSMain -Zi)");
+  const std::string command(R"(// RUN: %dxc -T ps_6_0 -E PSMain)");
   const std::string code = command + R"(
 float4 PSMain(float4 color : COLOR) : SV_TARGET { return color; }
 )";
   std::string spirv = compileCodeAndGetSpirvAsm(code);
   EXPECT_THAT(spirv, ContainsRegex("%PSMain = OpFunction"));
+}
+
+// This test demonstrates that in-memory source is transmitted
+// to OpSource faithfully
+TEST_F(LibTest, InlinedCodeWithDebugTest) {
+  const std::string command(R"(// RUN: %dxc -T ps_6_0 -E PSMain -Zi)");
+  const std::string code = command + R"(
+float4 PSMain(float4 color : COLOR) : SV_TARGET { return color; }
+)";
+  std::string spirv = compileCodeAndGetSpirvAsm(code);
+  EXPECT_THAT(
+      spirv,
+      ContainsRegex(
+          "OpSource HLSL 600 %4 \"// RUN: %dxc -T ps_6_0 -E PSMain -Zi"));
 }
 
 } // namespace
