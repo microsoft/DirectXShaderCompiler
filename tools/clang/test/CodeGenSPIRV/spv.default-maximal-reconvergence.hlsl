b888e20dfba39d76e832903012b44bc04ca508c4
Nathan Gauër <brioche@google.com>
2024-02-07 17:21:27 +0100

    merge tests

--- tools/clang/test/CodeGenSPIRV/spv.default-maximal-reconvergence.hlsl
+++
@@ -1,9 +0,0 @@
-// RUN: %dxc -T cs_6_0 -E main %s -spirv | FileCheck %s
-
-// CHECK-NOT:       OpExtension "SPV_KHR_maximal_reconvergence"
-// CHECK:           OpEntryPoint GLCompute %main "main"
-// CHECK-NOT:       OpExecutionMode %main MaximallyReconvergesKHR
-[numthreads(1, 1, 1)]
-void main() {}
-
-
