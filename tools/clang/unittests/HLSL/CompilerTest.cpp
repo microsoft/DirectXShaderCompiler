7c1d73579f073298d09c94df3fc99de582cc665e
Adam Yang <31109344+adam-yang@users.noreply.github.com>
2024-02-07 16:29:23 -0800

    Added a test for UNC path

--- tools/clang/unittests/HLSL/CompilerTest.cpp
+++ tools/clang/unittests/HLSL/CompilerTest.cpp
@@ -2978,6 +2978,13 @@ TEST_F(CompilerTest, CompileWhenAllIncludeCombinations) {
            float main() : SV_Target { return foo(); } )"},
        {L".\\my_include_dir\\second_dir\\include.h", commonIncludeFile},
        {L"-I", L"my_include_dir"}},
+
+      {L"../main.hlsl",
+       {L".\\..\\main.hlsl",
+        R"(#include "\\my_include_dir\\second_dir/include.h" // <-- Network path
+           float main() : SV_Target { return foo(); } )"},
+       {L"\\\\my_include_dir\\second_dir\\include.h", commonIncludeFile},
+       {}},
   };
 
   for (TestCase &t : tests) {
