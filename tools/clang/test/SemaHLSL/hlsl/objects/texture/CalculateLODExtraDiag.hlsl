52bfcec7d2f3081ccd48ca21e58556383e359725
Xiang Li <python3kgae@outlook.com>
2024-02-09 17:21:53 -0500

    Add '' for function name in diag.

--- tools/clang/test/SemaHLSL/hlsl/objects/texture/CalculateLODExtraDiag.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/objects/texture/CalculateLODExtraDiag.hlsl
@@ -12,7 +12,7 @@ Texture1D        <float>  t1;
 [shader("compute")]
 void foo(uint3 id : SV_GroupThreadID)
 {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by foo requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'foo' requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
 }
 
@@ -29,7 +29,7 @@ void foo2(uint3 id : SV_GroupThreadID)
 [shader("compute")]
 void bar(uint3 id : SV_GroupThreadID)
 {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by bar requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'bar' requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
 }
 
@@ -38,7 +38,7 @@ void bar(uint3 id : SV_GroupThreadID)
 [numthreads(3,1,1)]
 [outputtopology("triangle")]
 void mesh(uint ix : SV_GroupIndex, uint3 id : SV_GroupThreadID) {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by mesh requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'mesh' requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
 }
 
@@ -52,7 +52,7 @@ struct Payload {
 [shader("amplification")]
 void ASmain()
 {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by ASmain requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'ASmain' requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
     Payload pld;
     pld.dummy = float2(1.0,2.0);
@@ -69,7 +69,7 @@ struct RECORD {
 [NodeDispatchGrid(1, 1, 1)]
 [NumThreads(1,1,1)]
 void node01(DispatchNodeInputRecord<RECORD> input) {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by node01 requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'node01' requires derivatives - when used in compute/amplification/mesh shaders or broadcast nodes, numthreads must be either 1D with X as a multiple of 4 or both X and Y must be multiples of 2}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
  }
 
@@ -80,7 +80,7 @@ void node01(DispatchNodeInputRecord<RECORD> input) {
 [NodeIsProgramEntry]
 void node02()
 {
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by node02 requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'node02' requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
     o[0] = t1.CalculateLevelOfDetail(ss, 0.5);
 }
 
@@ -89,7 +89,7 @@ void node02()
 float4 vs(float2 a :A) :SV_POSTION {
   float r = 0;
   if (1>3)
-    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by vs requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
+    // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'vs' requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
     r = t1.CalculateLevelOfDetail(ss, 0.5).x;
   return r;
 }
@@ -100,8 +100,8 @@ Texture1D t;
 // expected-note@+2{{declared here}}
 [shader("vertex")]
 float4 vs2(float a:A) : SV_Position {
-  // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by vs2 requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
+  // expected-error@+1 {{Intrinsic CalculateLevelOfDetail potentially used by 'vs2' requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
   return t.CalculateLevelOfDetail(s, a) +
-  // expected-error@+1 {{Intrinsic CalculateLevelOfDetailUnclamped potentially used by vs2 requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
+  // expected-error@+1 {{Intrinsic CalculateLevelOfDetailUnclamped potentially used by 'vs2' requires derivatives - only available in pixel, compute, amplification, mesh, or broadcast node shaders}}
     t.CalculateLevelOfDetailUnclamped(ss, a);
 }
