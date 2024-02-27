5a3178590581b4a7deea17dd45b6c4f1b51edb53
Jeff Noyle <jeffno@microsoft.com>
2024-02-27 11:00:23 -0800

    PIX: Preclude debug output of ray query handle from phi or select (since the value isn't really an int, it's a handle) (#6309)
    AllocateRayQuery returns an int, but it's not really an int: it's a
    handle to the query. Thus, it's not sensible for the PIX debugging
    instrumentation to attempt to write it out to the debug-data-UAV. The
    previous code did an explicit check against the type of the value to be
    written, but if that value is actually a phi itself, then that check
    would fail. So now we recursively run through the phi values looking to
    see if any of its antecedents are a phi, and if so, refuse to send its
    value to the UAV.

--- tools/clang/test/HLSLFileCheck/pix/TraceRayInline.hlsl
+++ tools/clang/test/HLSLFileCheck/pix/TraceRayInline.hlsl
@@ -4,14 +4,21 @@
 // CHECK: call void @dx.op.bufferStore.i32(i32 69, %dx.types.Handle
 
 RaytracingAccelerationStructure RTAS;
+RWStructuredBuffer<float> UAV : register(u0);
 
 void DoTrace(RayQuery<RAY_FLAG_FORCE_OPAQUE|RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES> rayQuery, RayDesc rayDesc) {
   rayQuery.TraceRayInline(RTAS, 0, 1, rayDesc);
 }
 
 float main(RayDesc rayDesc : RAYDESC) : OUT {
-  RayQuery<RAY_FLAG_FORCE_OPAQUE|RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES> rayQuery;
+  RayQuery<RAY_FLAG_FORCE_OPAQUE | RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES> rayQuery;
   DoTrace(rayQuery, rayDesc);
   rayQuery.TraceRayInline(RTAS, 1, 2, rayDesc);
+  while (rayQuery.Proceed()) {
+    UAV[0] = rayQuery.CandidateGeometryIndex();
+    UAV[1] = rayQuery.CandidatePrimitiveIndex();
+    UAV[2] = rayQuery.CandidateTriangleBarycentrics().x;
+    rayQuery.CommitNonOpaqueTriangleHit();
+  }
   return 0;
 }
