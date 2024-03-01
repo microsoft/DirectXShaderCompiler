7818222f87e6803933d0391e17976ba2a7d7e36b
Cooper Partin <coopp@ntdev.microsoft.com>
2024-03-01 13:03:24 -0800

    Updated test to check for present and missing expected timings when granularity is very large

--- tools/clang/test/DXC/ftime-trace-granularity.hlsl
+++ tools/clang/test/DXC/ftime-trace-granularity.hlsl
@@ -1,7 +1,12 @@
-// RUN: %dxc -E main -T vs_6_0 %s -ftime-trace | FileCheck %s
-// RUN: %dxc -E main -T vs_6_0 %s -ftime-trace=%t.json -ftime-trace-granularity=0
+// RUN: %dxc -E main -T vs_6_0 %s -ftime-trace -ftime-trace-granularity=99999 | FileCheck %s
+// RUN: %dxc -E main -T vs_6_0 %s -ftime-trace=%t.json -ftime-trace-granularity=99999
 // RUN: cat %t.json | FileCheck %s
 
+// Validate that we do not output named stats
+// but still continue to output the Totals
+// versions of them which is expected.
 // CHECK: { "traceEvents": [
+// CHECK-NOT: "name":"Frontend"
+// CHECK: "name":"Total Frontend"
 
 void main() {}
