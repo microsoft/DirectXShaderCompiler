30e6963a7d59cf6b97f852bb878164cfc075b2fa
Cooper Partin <coopp@ntdev.microsoft.com>
2024-03-04 13:41:38 -0800

    Add comment to test

--- tools/clang/test/DXC/ftime-trace-granularity.hlsl
+++ tools/clang/test/DXC/ftime-trace-granularity.hlsl
@@ -2,6 +2,7 @@
 // RUN: %dxc -E main -T vs_6_0 %s -ftime-trace=%t.json -ftime-trace-granularity=99999
 // RUN: cat %t.json | FileCheck %s
 
+// This test runs both stdout and file output paths.
 // Validate that we do not output named stats
 // but still continue to output the Totals
 // versions of them which is expected.
