d93ffade25e7339c11e9595a628510d6455f988d
Tex Riddell <texr@microsoft.com>
2024-03-19 19:53:41 -0700

    Remove assert on illegal DXIL overload
    Certain HL ops with no double overload will generate double overloads for literal types.
    These are lowered to double DXIL overloads, some of which are not legal for these ops.
    We currently rely on illegal intermediate DXIL op overloads for constant evaluation.
    
    Disabling this assert allows test cases to be written to verify the current behavior for these operations.

---
+++ tools/clang/test/HLSLFileCheck/hlsl/intrinsics/basic/normalize_const_eval.hlsl
@@ -0,0 +1,38 @@
+// RUN: %dxc -T vs_6_0 %s -E main | %FileCheck %s
+// RUN: %dxc -T vs_6_0 %s -E main -DNO_FOLD | %FileCheck %s -check-prefixes=NO_FOLD
+
+// Ensure normalize is constant evaluated during codegen, or dxil const eval
+// TODO: handle fp specials properly!
+
+RWBuffer<float4> results : register(u0);
+
+[shader("vertex")]
+void main(bool b : B) {
+    uint i = 0;
+
+    // Literal float
+    // CHECK: call void @dx.op.bufferStore.f32(i32 69, %dx.types.Handle %{{.+}}, i32 0, i32 undef, float 0x3FE6A09E60000000, float 0xBFE279A740000000, float 0x7FF8000000000000, float 0x7FF8000000000000, i8 15)
+    results[i++] = float4(normalize(0.5.xx).x,
+                          normalize(-1.5.xxx).x,
+                          normalize(0.0.xxxx).x,
+                          normalize(-0.0.xxxx).x);
+
+    // Explicit float
+    // CHECK: call void @dx.op.bufferStore.f32(i32 69, %dx.types.Handle %{{.+}}, i32 1, i32 undef, float 0x3FE6A09E60000000, float 0xBFE279A740000000, float 0x7FF8000000000000, float 0x7FF8000000000000, i8 15)
+    results[i++] = float4(normalize(0.5F.xx).x,
+                          normalize(-1.5F.xxx).x,
+                          normalize(0.0F.xxxx).x,
+                          normalize(-0.0F.xxxx).x);
+
+#ifdef NO_FOLD
+    // Currently, we rely on constant folding of DXIL ops to get rid of illegal
+    // double overloads. If this doesn't happen, we expect a validation error.
+    // Ternary operator can return literal type, while not being foldable due
+    // non-constant condition.
+    // NO_FOLD: error: validation errors
+    // NO_FOLD: error: DXIL intrinsic overload must be valid.
+    // NO_FOLD: note: at '%{{.+}} = call double @dx.op.dot3.f64(i32 55, double %{{.+}}, double %{{.+}}, double %{{.+}}, double %{{.+}}, double %{{.+}}, double %{{.+}})' in block '#0' of function 'main'.
+    float result = normalize((b ? 1.5 : 0.5).xxx).x;
+    results[i++] = float4(result, 0, 0, 0);
+#endif // NO_FOLD
+}
