a66b858050b6df299980ab3efd159ae0ec54df06
Antonio Maiorano <amaiorano@google.com>
2024-03-20 14:35:48 -0400

    Fix HLMatrixLowerPass leaving call to dangling FunctionVal
    When lowering an hl.cast, when the operand was an undef matrix, the pass
    would insert a call to a mat2vec stub, but since the undef value is not
    an alloca, it never gets handled, and the call to the temporary stub
    remains. Since the stub FunctionVal gets deleted, when the instruction
    is accessed in a future pass, it reads a dangling pointer.
    
    The fix is to handle undef similarly to how constant 0 is handled, and
    to return an undef vector from lowerHLCast.
    
    Signed-off-by: Chris Bieneman <cbieneman@microsoft.com>

--- lib/HLSL/HLMatrixLowerPass.cpp
+++ lib/HLSL/HLMatrixLowerPass.cpp
@@ -421,6 +421,11 @@ Value *HLMatrixLowerPass::getLoweredByValOperand(Value *Val,
   if (isa<ConstantAggregateZero>(Val))
     return ConstantAggregateZero::get(LoweredTy);
 
+  // Lower undef mat as undef vec
+  if (isa<UndefValue>(Val)) {
+      return UndefValue::get(LoweredTy);
+  }
+
   // Return a mat-to-vec translation stub
   FunctionType *TranslationStubTy =
       FunctionType::get(LoweredTy, {Ty}, /* isVarArg */ false);
