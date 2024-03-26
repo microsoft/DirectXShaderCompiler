8aad9a4ac37dc4935a6df624f81cc6f55f0c0a32
Helena Kotas <hekotas@microsoft.com>
2024-03-26 13:56:33 -0700

    Remove -spirv from new HLSL Sema test

--- tools/clang/test/SemaHLSL/hlsl/semantics/semantic.vertex.position.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/semantics/semantic.vertex.position.hlsl
@@ -1,4 +1,4 @@
-// RUN: %dxc -T vs_6_0 -E main -spirv -verify %s
+// RUN: %dxc -T vs_6_0 -E main -verify %s
 
 float4 main(float4 input : SV_Position) : POSITION { /* expected-warning{{'POSITION' is a user-defined semantic; did you mean 'SV_Position'?}} */
   return input;
--- tools/clang/test/SemaHLSL/hlsl/semantics/semantic.vertex.position.nowarn.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/semantics/semantic.vertex.position.nowarn.hlsl
@@ -1,4 +1,4 @@
-// RUN: %dxc -T vs_6_0 -E main -spirv -Wno-dx9-deprecation -verify %s
+// RUN: %dxc -T vs_6_0 -E main -Wno-dx9-deprecation -verify %s
 
 float4 main(float4 input : SV_Position) : POSITION { /* expected-no-diagnostics */
   return input;
