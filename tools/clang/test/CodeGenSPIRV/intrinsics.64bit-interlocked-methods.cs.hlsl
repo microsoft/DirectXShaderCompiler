c4c9b1fb5ed763940c00f2f1a6616b91f6fab267
Natalie Chouinard <chouinard@google.com>
2024-04-12 19:13:32 +0000

    [SPIR-V] Scope Atomic ops on Workgroup memory
    Set the scope of atomic operation on Workgroup-shared variables to the
    Workgroup rather than the Device.
    
    Fixes #6508

--- tools/clang/test/CodeGenSPIRV/intrinsics.64bit-interlocked-methods.cs.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.64bit-interlocked-methods.cs.hlsl
@@ -27,7 +27,7 @@ void main()
 // CHECK: OpCapability Int64Atomics
 
 // CHECK:        [[val1_i64:%[0-9]+]] = OpLoad %long %val1_i64
-// CHECK-NEXT: [[atomic_add:%[0-9]+]] = OpAtomicIAdd %long %dest_i %uint_1 %uint_0 [[val1_i64]]
+// CHECK-NEXT: [[atomic_add:%[0-9]+]] = OpAtomicIAdd %long %dest_i %uint_2 %uint_0 [[val1_i64]]
 // CHECK-NEXT:                       OpStore %original_i_val [[atomic_add]]
   InterlockedAdd(dest_i, val1_i64, original_i_val);
 
@@ -39,41 +39,41 @@ void main()
   InterlockedAdd(getDest()[0], val3_u64, original_u_val);
 
 // CHECK:        [[val3_u64_0:%[0-9]+]] = OpLoad %ulong %val3_u64
-// CHECK-NEXT: [[atomic_and:%[0-9]+]] = OpAtomicAnd %ulong %dest_u %uint_1 %uint_0 [[val3_u64_0]]
+// CHECK-NEXT: [[atomic_and:%[0-9]+]] = OpAtomicAnd %ulong %dest_u %uint_2 %uint_0 [[val3_u64_0]]
 // CHECK-NEXT:                       OpStore %original_u_val [[atomic_and]]
   InterlockedAnd(dest_u, val3_u64,  original_u_val);
 
 // CHECK:        [[val1_i64_0:%[0-9]+]] = OpLoad %long %val1_i64
-// CHECK-NEXT: [[atomic_max:%[0-9]+]] = OpAtomicSMax %long %dest_i %uint_1 %uint_0 [[val1_i64_0]]
+// CHECK-NEXT: [[atomic_max:%[0-9]+]] = OpAtomicSMax %long %dest_i %uint_2 %uint_0 [[val1_i64_0]]
 // CHECK-NEXT:                       OpStore %original_i_val [[atomic_max]]
   InterlockedMax(dest_i, val1_i64,  original_i_val);
 
 // CHECK:        [[val3_u64_1:%[0-9]+]] = OpLoad %ulong %val3_u64
-// CHECK-NEXT: [[atomic_min:%[0-9]+]] = OpAtomicUMin %ulong %dest_u %uint_1 %uint_0 [[val3_u64_1]]
+// CHECK-NEXT: [[atomic_min:%[0-9]+]] = OpAtomicUMin %ulong %dest_u %uint_2 %uint_0 [[val3_u64_1]]
 // CHECK-NEXT:                       OpStore %original_u_val [[atomic_min]]
   InterlockedMin(dest_u, val3_u64,  original_u_val);
 
 // CHECK:       [[val2_i64:%[0-9]+]] = OpLoad %long %val2_i64
-// CHECK-NEXT: [[atomic_or:%[0-9]+]] = OpAtomicOr %long %dest_i %uint_1 %uint_0 [[val2_i64_0:%[0-9]+]]
+// CHECK-NEXT: [[atomic_or:%[0-9]+]] = OpAtomicOr %long %dest_i %uint_2 %uint_0 [[val2_i64_0:%[0-9]+]]
 // CHECK-NEXT:                      OpStore %original_i_val [[atomic_or]]
   InterlockedOr (dest_i, val2_i64, original_i_val);
 
 // CHECK:        [[val3_u64_2:%[0-9]+]] = OpLoad %ulong %val3_u64
-// CHECK-NEXT: [[atomic_xor:%[0-9]+]] = OpAtomicXor %ulong %dest_u %uint_1 %uint_0 [[val3_u64_2]]
+// CHECK-NEXT: [[atomic_xor:%[0-9]+]] = OpAtomicXor %ulong %dest_u %uint_2 %uint_0 [[val3_u64_2]]
 // CHECK-NEXT:                       OpStore %original_u_val [[atomic_xor]]
   InterlockedXor(dest_u, val3_u64,  original_u_val);
 
 // CHECK:      [[val1_i64_1:%[0-9]+]] = OpLoad %long %val1_i64
 // CHECK-NEXT: [[val2_i64_1:%[0-9]+]] = OpLoad %long %val2_i64
-// CHECK-NEXT:          {{%[0-9]+}} = OpAtomicCompareExchange %long %dest_i %uint_1 %uint_0 %uint_0 [[val2_i64_1]] [[val1_i64_1]]
+// CHECK-NEXT:          {{%[0-9]+}} = OpAtomicCompareExchange %long %dest_i %uint_2 %uint_0 %uint_0 [[val2_i64_1]] [[val1_i64_1]]
   InterlockedCompareStore(dest_i, val1_i64, val2_i64);
 
-// CHECK:      [[ace:%[0-9]+]] = OpAtomicCompareExchange %ulong %dest_u %uint_1 %uint_0 %uint_0 %ulong_20 %ulong_15
+// CHECK:      [[ace:%[0-9]+]] = OpAtomicCompareExchange %ulong %dest_u %uint_2 %uint_0 %uint_0 %ulong_20 %ulong_15
 // CHECK-NEXT:                OpStore %original_u_val [[ace]]
   InterlockedCompareExchange(dest_u, 15u, 20u, original_u_val);
 
 // CHECK:      [[val2_i64_2:%[0-9]+]] = OpLoad %long %val2_i64
-// CHECK-NEXT:       [[ae:%[0-9]+]] = OpAtomicExchange %long %dest_i %uint_1 %uint_0 [[val2_i64_2]]
+// CHECK-NEXT:       [[ae:%[0-9]+]] = OpAtomicExchange %long %dest_i %uint_2 %uint_0 [[val2_i64_2]]
 // CHECK-NEXT:                     OpStore %original_i_val [[ae]]
   InterlockedExchange(dest_i, val2_i64, original_i_val);
 }
