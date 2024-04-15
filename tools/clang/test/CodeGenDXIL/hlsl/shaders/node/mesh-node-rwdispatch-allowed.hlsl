30ad624c62f1e93d5058079b070a2649d7db1825
Joshua Batista <jbatista@microsoft.com>
2024-04-15 11:25:17 -0700

    add globally coherent case

--- tools/clang/test/CodeGenDXIL/hlsl/shaders/node/mesh-node-rwdispatch-allowed.hlsl
+++ tools/clang/test/CodeGenDXIL/hlsl/shaders/node/mesh-node-rwdispatch-allowed.hlsl
@@ -20,3 +20,14 @@ void myFancyNode1(
     )
 {
 }
+
+[Shader("node")]
+[OutputTopology("line")]
+[NodeLaunch("mesh")]
+[NumThreads(4,5,6)]
+[NodeDispatchGrid(2,2,2)]
+void myFancyNode2(
+    globallycoherent RWDispatchNodeInputRecord<MY_MATERIAL_RECORD> myProgressCounter1        
+    )
+{
+}
