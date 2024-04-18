c954abbd1fbecfe845a564e25628594413fd15de
Jannik Silvanus <jannik.silvanus@amd.com>
2024-04-18 10:27:01 +0200

    Number individual repacking points

--- tools/clang/test/HLSLFileCheck/hlsl/intrinsics/wave/repacking.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/intrinsics/wave/repacking.hlsl
@@ -21,226 +21,226 @@ void RepackingPoint(int identifier) {
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 0
   // CHECK: call i1 @dx.op.waveIsFirstLane(i32 110
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 0
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 1
   // CHECK: call i1 @dx.op.waveIsFirstLane(i32 110
   RepackingPoint(0);
   p.value += WaveIsFirstLane();
-  RepackingPoint(0);
+  RepackingPoint(1);
   p.value += WaveIsFirstLane();
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 1
-  // CHECK: call i32 @dx.op.waveGetLaneIndex(i32 111
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 1
-  // CHECK: call i32 @dx.op.waveGetLaneIndex(i32 111
-  RepackingPoint(1);
-  p.value += WaveGetLaneIndex();
-  RepackingPoint(1);
-  p.value += WaveGetLaneIndex();
-
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 2
-  // CHECK: call i1 @dx.op.waveAnyTrue(i32 113, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 2
-  // CHECK: call i1 @dx.op.waveAnyTrue(i32 113, i1
-  RepackingPoint(2);
-  p.value += WaveActiveAnyTrue(oldValue == 17);
-  RepackingPoint(2);
-  p.value += WaveActiveAnyTrue(oldValue == 17);
-
+  // CHECK: call i32 @dx.op.waveGetLaneIndex(i32 111
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 3
-  // CHECK: call i1 @dx.op.waveAllTrue(i32 114, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 3
-  // CHECK: call i1 @dx.op.waveAllTrue(i32 114, i1
+  // CHECK: call i32 @dx.op.waveGetLaneIndex(i32 111
+  RepackingPoint(2);
+  p.value += WaveGetLaneIndex();
   RepackingPoint(3);
-  p.value += WaveActiveAllTrue(oldValue == 17);
-  RepackingPoint(3);
-  p.value += WaveActiveAllTrue(oldValue == 17);
+  p.value += WaveGetLaneIndex();
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 4
-  // CHECK: call i1 @dx.op.waveActiveAllEqual.i32(i32 115, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 4
-  // CHECK: call i1 @dx.op.waveActiveAllEqual.i32(i32 115, i32
-  RepackingPoint(4);
-  p.value += WaveActiveAllEqual(oldValue);
-  RepackingPoint(4);
-  p.value += WaveActiveAllEqual(oldValue);
-
+  // CHECK: call i1 @dx.op.waveAnyTrue(i32 113, i1
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 5
-  // CHECK: call %dx.types.fouri32 @dx.op.waveActiveBallot(i32 116, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 5
-  // CHECK: call %dx.types.fouri32 @dx.op.waveActiveBallot(i32 116, i1
+  // CHECK: call i1 @dx.op.waveAnyTrue(i32 113, i1
+  RepackingPoint(4);
+  p.value += WaveActiveAnyTrue(oldValue == 17);
   RepackingPoint(5);
-  p.value += WaveActiveBallot(oldValue).x;
-  RepackingPoint(5);
-  p.value += WaveActiveBallot(oldValue).x;
+  p.value += WaveActiveAnyTrue(oldValue == 17);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 6
-  // CHECK: call i32 @dx.op.waveReadLaneAt.i32(i32 117, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 6
-  // CHECK: call i32 @dx.op.waveReadLaneAt.i32(i32 117, i32
-  RepackingPoint(6);
-  p.value += WaveReadLaneAt(oldValue, 1);
-  RepackingPoint(6);
-  p.value += WaveReadLaneAt(oldValue, 1);
-
+  // CHECK: call i1 @dx.op.waveAllTrue(i32 114, i1
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 7
-  // CHECK: call i32 @dx.op.waveReadLaneFirst.i32(i32 118, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 7
-  // CHECK: call i32 @dx.op.waveReadLaneFirst.i32(i32 118, i32
+  // CHECK: call i1 @dx.op.waveAllTrue(i32 114, i1
+  RepackingPoint(6);
+  p.value += WaveActiveAllTrue(oldValue == 17);
   RepackingPoint(7);
-  p.value += WaveReadLaneFirst(oldValue);
-  RepackingPoint(7);
-  p.value += WaveReadLaneFirst(oldValue);
+  p.value += WaveActiveAllTrue(oldValue == 17);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 8
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 8
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
-  RepackingPoint(8);
-  p.value += WaveActiveSum(oldValue);
-  RepackingPoint(8);
-  p.value += WaveActiveSum(oldValue);
-
+  // CHECK: call i1 @dx.op.waveActiveAllEqual.i32(i32 115, i32
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 9
-  // CHECK: call i64 @dx.op.waveActiveOp.i64(i32 119, i64
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 9
-  // CHECK: call i64 @dx.op.waveActiveOp.i64(i32 119, i64
+  // CHECK: call i1 @dx.op.waveActiveAllEqual.i32(i32 115, i32
+  RepackingPoint(8);
+  p.value += WaveActiveAllEqual(oldValue);
   RepackingPoint(9);
-  p.value += WaveActiveProduct(oldValue == 17 ? 1 : 0);
-  RepackingPoint(9);
-  p.value += WaveActiveProduct(oldValue == 17 ? 1 : 0);
+  p.value += WaveActiveAllEqual(oldValue);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 10
-  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 10
-  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
-  RepackingPoint(10);
-  p.value += WaveActiveBitAnd(oldValue);
-  RepackingPoint(10);
-  p.value += WaveActiveBitAnd(oldValue);
-
+  // CHECK: call %dx.types.fouri32 @dx.op.waveActiveBallot(i32 116, i1
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 11
-  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 11
-  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
+  // CHECK: call %dx.types.fouri32 @dx.op.waveActiveBallot(i32 116, i1
+  RepackingPoint(10);
+  p.value += WaveActiveBallot(oldValue).x;
   RepackingPoint(11);
-  p.value += WaveActiveBitXor(oldValue);
-  RepackingPoint(11);
-  p.value += WaveActiveBitXor(oldValue);
+  p.value += WaveActiveBallot(oldValue).x;
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 12
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 12
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
-  RepackingPoint(12);
-  p.value += WaveActiveMin(oldValue);
-  RepackingPoint(12);
-  p.value += WaveActiveMin(oldValue);
-
+  // CHECK: call i32 @dx.op.waveReadLaneAt.i32(i32 117, i32
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 13
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 13
-  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  // CHECK: call i32 @dx.op.waveReadLaneAt.i32(i32 117, i32
+  RepackingPoint(12);
+  p.value += WaveReadLaneAt(oldValue, 1);
   RepackingPoint(13);
-  p.value += WaveActiveMax(oldValue);
-  RepackingPoint(13);
-  p.value += WaveActiveMax(oldValue);
+  p.value += WaveReadLaneAt(oldValue, 1);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 14
-  // CHECK: call i32 @dx.op.wavePrefixOp.i32(i32 121, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 14
-  // CHECK: call i32 @dx.op.wavePrefixOp.i32(i32 121, i32
-  RepackingPoint(14);
-  p.value += WavePrefixSum(oldValue);
-  RepackingPoint(14);
-  p.value += WavePrefixSum(oldValue);
-
+  // CHECK: call i32 @dx.op.waveReadLaneFirst.i32(i32 118, i32
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 15
-  // CHECK: call i64 @dx.op.wavePrefixOp.i64(i32 121, i64
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 15
-  // CHECK: call i64 @dx.op.wavePrefixOp.i64(i32 121, i64
+  // CHECK: call i32 @dx.op.waveReadLaneFirst.i32(i32 118, i32
+  RepackingPoint(14);
+  p.value += WaveReadLaneFirst(oldValue);
   RepackingPoint(15);
-  p.value += WavePrefixProduct(oldValue == 17 ? 1 : 0);
-  RepackingPoint(15);
-  p.value += WavePrefixProduct(oldValue == 17 ? 1 : 0);
+  p.value += WaveReadLaneFirst(oldValue);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 16
-  // CHECK: call i32 @dx.op.waveAllOp(i32 135, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 16
-  // CHECK: call i32 @dx.op.waveAllOp(i32 135, i1
-  RepackingPoint(16);
-  p.value += WaveActiveCountBits(oldValue == 17);
-  RepackingPoint(16);
-  p.value += WaveActiveCountBits(oldValue == 17);
-
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 17
-  // CHECK: call i32 @dx.op.wavePrefixOp(i32 136, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 17
-  // CHECK: call i32 @dx.op.wavePrefixOp(i32 136, i1
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  RepackingPoint(16);
+  p.value += WaveActiveSum(oldValue);
   RepackingPoint(17);
-  p.value += WavePrefixCountBits(oldValue == 17);
-  RepackingPoint(17);
-  p.value += WavePrefixCountBits(oldValue == 17);
+  p.value += WaveActiveSum(oldValue);
 
   // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 18
-  // CHECK: call %dx.types.fouri32 @dx.op.waveMatch.i32(i32 165, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 18
-  // CHECK: call %dx.types.fouri32 @dx.op.waveMatch.i32(i32 165, i32
+  // CHECK: call i64 @dx.op.waveActiveOp.i64(i32 119, i64
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 19
+  // CHECK: call i64 @dx.op.waveActiveOp.i64(i32 119, i64
   RepackingPoint(18);
+  p.value += WaveActiveProduct(oldValue == 17 ? 1 : 0);
+  RepackingPoint(19);
+  p.value += WaveActiveProduct(oldValue == 17 ? 1 : 0);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 20
+  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 21
+  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
+  RepackingPoint(20);
+  p.value += WaveActiveBitAnd(oldValue);
+  RepackingPoint(21);
+  p.value += WaveActiveBitAnd(oldValue);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 22
+  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 23
+  // CHECK: call i32 @dx.op.waveActiveBit.i32(i32 120, i32
+  RepackingPoint(22);
+  p.value += WaveActiveBitXor(oldValue);
+  RepackingPoint(23);
+  p.value += WaveActiveBitXor(oldValue);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 24
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 25
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  RepackingPoint(24);
+  p.value += WaveActiveMin(oldValue);
+  RepackingPoint(25);
+  p.value += WaveActiveMin(oldValue);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 26
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 27
+  // CHECK: call i32 @dx.op.waveActiveOp.i32(i32 119, i32
+  RepackingPoint(26);
+  p.value += WaveActiveMax(oldValue);
+  RepackingPoint(27);
+  p.value += WaveActiveMax(oldValue);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 28
+  // CHECK: call i32 @dx.op.wavePrefixOp.i32(i32 121, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 29
+  // CHECK: call i32 @dx.op.wavePrefixOp.i32(i32 121, i32
+  RepackingPoint(28);
+  p.value += WavePrefixSum(oldValue);
+  RepackingPoint(29);
+  p.value += WavePrefixSum(oldValue);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 30
+  // CHECK: call i64 @dx.op.wavePrefixOp.i64(i32 121, i64
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 31
+  // CHECK: call i64 @dx.op.wavePrefixOp.i64(i32 121, i64
+  RepackingPoint(30);
+  p.value += WavePrefixProduct(oldValue == 17 ? 1 : 0);
+  RepackingPoint(31);
+  p.value += WavePrefixProduct(oldValue == 17 ? 1 : 0);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 32
+  // CHECK: call i32 @dx.op.waveAllOp(i32 135, i1
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 33
+  // CHECK: call i32 @dx.op.waveAllOp(i32 135, i1
+  RepackingPoint(32);
+  p.value += WaveActiveCountBits(oldValue == 17);
+  RepackingPoint(33);
+  p.value += WaveActiveCountBits(oldValue == 17);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 34
+  // CHECK: call i32 @dx.op.wavePrefixOp(i32 136, i1
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 35
+  // CHECK: call i32 @dx.op.wavePrefixOp(i32 136, i1
+  RepackingPoint(34);
+  p.value += WavePrefixCountBits(oldValue == 17);
+  RepackingPoint(35);
+  p.value += WavePrefixCountBits(oldValue == 17);
+
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 36
+  // CHECK: call %dx.types.fouri32 @dx.op.waveMatch.i32(i32 165, i32
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 37
+  // CHECK: call %dx.types.fouri32 @dx.op.waveMatch.i32(i32 165, i32
+  RepackingPoint(36);
   uint4 mask = WaveMatch(oldValue);
-  RepackingPoint(18);
+  RepackingPoint(37);
   p.value += WaveMatch(oldValue).x;
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 19
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 38
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 19
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 39
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  RepackingPoint(19);
+  RepackingPoint(38);
   p.value += WaveMultiPrefixBitAnd(oldValue, mask);
-  RepackingPoint(19);
+  RepackingPoint(39);
   p.value += WaveMultiPrefixBitAnd(oldValue, mask);
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 20
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 40
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 20
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 41
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  RepackingPoint(20);
+  RepackingPoint(40);
   p.value += WaveMultiPrefixBitOr(oldValue, mask);
-  RepackingPoint(20);
+  RepackingPoint(41);
   p.value += WaveMultiPrefixBitOr(oldValue, mask);
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 21
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 42
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 21
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 43
   // CHECK: call i32 @dx.op.waveMultiPrefixOp.i32(i32 166, i32
-  RepackingPoint(21);
+  RepackingPoint(42);
   p.value += WaveMultiPrefixBitXor(oldValue, mask);
-  RepackingPoint(21);
+  RepackingPoint(43);
   p.value += WaveMultiPrefixBitXor(oldValue, mask);
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 22
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 44
   // CHECK: call i32 @dx.op.waveMultiPrefixBitCount(i32 167, i1
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 22
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 45
   // CHECK: call i32 @dx.op.waveMultiPrefixBitCount(i32 167, i1
-  RepackingPoint(22);
+  RepackingPoint(44);
   p.value += WaveMultiPrefixCountBits(oldValue == 17, mask);
-  RepackingPoint(22);
+  RepackingPoint(45);
   p.value += WaveMultiPrefixCountBits(oldValue == 17, mask);
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 23
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 46
   // CHECK: call i64 @dx.op.waveMultiPrefixOp.i64(i32 166, i64
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 23
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 47
   // CHECK: call i64 @dx.op.waveMultiPrefixOp.i64(i32 166, i64
-  RepackingPoint(23);
+  RepackingPoint(46);
   p.value += WaveMultiPrefixProduct(oldValue == 17 ? 1 : 0, mask);
-  RepackingPoint(23);
+  RepackingPoint(47);
   p.value += WaveMultiPrefixProduct(oldValue == 17 ? 1 : 0, mask);
 
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 24
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 48
   // CHECK: call i64 @dx.op.waveMultiPrefixOp.i64(i32 166, i64
-  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 24
+  // CHECK: call void @dx.op.callShader.struct.Payload(i32 159, i32 49
   // CHECK: call i64 @dx.op.waveMultiPrefixOp.i64(i32 166, i64
-  RepackingPoint(24);
+  RepackingPoint(48);
   p.value += WaveMultiPrefixSum(oldValue == 17 ? 1 : 0, mask);
-  RepackingPoint(24);
+  RepackingPoint(49);
   p.value += WaveMultiPrefixSum(oldValue == 17 ? 1 : 0, mask);
 }
