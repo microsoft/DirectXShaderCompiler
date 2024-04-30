54f04c9b41703a868874c3c9ca2e37b26a178799
Chris Bieneman <cbieneman@microsoft.com>
2024-04-30 11:39:06 -0500

    Make the format bot happy
    I moved this code block up to make the format bot happy without needing
    to add some odd indentation to a code comment I didn't actually change.
    'beanz/cbieneman/0017-conforming-literals'.

--- tools/clang/lib/Frontend/InitPreprocessor.cpp
+++ tools/clang/lib/Frontend/InitPreprocessor.cpp
@@ -376,6 +376,10 @@ static void InitializeStandardPredefinedMacros(const TargetInfo &TI,
     // HLSL Version
     Builder.defineMacro("__HLSL_VERSION",
                         Twine((unsigned int)LangOpts.HLSLVersion));
+    // This define is enabled in Clang and allows conditionally compiling code
+    // based on whether or not native 16-bit types are supported.
+    if (!LangOpts.UseMinPrecision)
+      Builder.defineMacro("__HLSL_ENABLE_16_BIT", "1");
     // Shader target information
     // "enums" for shader stages
     Builder.defineMacro("__SHADER_STAGE_VERTEX",  Twine((unsigned)hlsl::DXIL::ShaderKind::Vertex));
@@ -393,11 +397,6 @@ static void InitializeStandardPredefinedMacros(const TargetInfo &TI,
     // Add target versions
     Builder.defineMacro("__SHADER_TARGET_MAJOR", Twine(SM->GetMajor()));
     Builder.defineMacro("__SHADER_TARGET_MINOR", Twine(SM->GetMinor()));
-
-    // This define is enabled in Clang and allows conditionally compiling code
-    // based on whether or not native 16-bit types are supported.
-    if (!LangOpts.UseMinPrecision)
-      Builder.defineMacro("__HLSL_ENABLE_16_BIT", "1");
     // SPIRV Change Starts
 #ifdef ENABLE_SPIRV_CODEGEN
     if (LangOpts.SPIRV) {
