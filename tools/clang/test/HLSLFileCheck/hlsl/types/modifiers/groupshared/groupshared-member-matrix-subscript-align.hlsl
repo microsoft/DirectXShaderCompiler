d2b15bd05f3943e251cbfc2d5f2e67b0a807b9c1
Greg Roth <grroth@microsoft.com>
2024-05-06 10:47:32 -0700

    constrain tests just a bit more
    Added verification that dimensions and variables are consistent

--- tools/clang/test/HLSLFileCheck/hlsl/types/modifiers/groupshared/groupshared-member-matrix-subscript-align.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/types/modifiers/groupshared/groupshared-member-matrix-subscript-align.hlsl
@@ -9,7 +9,7 @@ struct Data {
 };
 
 // Ensure that the global has an alignment
-//CHECK: @"\01?GData@@{{.*}}" = addrspace(3) global [{{[0-9]*}} x float] undef, align 4
+//CHECK: @"[[GV:\\01\?GData@@.*]]" = addrspace(3) global [[DIM:\[[0-9]+]] x float] undef, align 4
 
 groupshared TYPE GData;
 StructuredBuffer<TYPE> input : register(t0);
@@ -19,41 +19,41 @@ RWStructuredBuffer<TYPE> output : register(u0);
 void main(uint Id : SV_DispatchThreadId, uint g : SV_GroupID)
 {
   // Ensure that the stores have the proper alignments
-  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 0), align 4
-  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 1), align 4
-  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 2), align 4
-  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 3), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 4), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 5), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 6), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 7), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 8), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 9), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 10), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 11), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 12), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 13), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 14), align 4
-  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 15), align 4
+  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 0), align 4
+  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 1), align 4
+  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 2), align 4
+  // CHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 3), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 4), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 5), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 6), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 7), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 8), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 9), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 10), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 11), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 12), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 13), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 14), align 4
+  // LCHECK: store float %{{[0-9]*}}, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 15), align 4
   GData = input[0];
   GroupMemoryBarrierWithGroupSync();
   // Ensure that the loads have the proper alignments
-  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 0), align 4
-  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 1), align 4
-  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 2), align 4
-  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 3), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 4), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 5), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 6), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 7), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 8), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 9), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 10), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 11), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 12), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 13), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 14), align 4
-  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([{{[0-9]*}} x float], [{{[0-9]*}} x float] addrspace(3)* @"\01?GData@@{{.*}}", i32 0, i32 15), align 4
+  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 0), align 4
+  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 1), align 4
+  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 2), align 4
+  // CHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 3), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 4), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 5), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 6), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 7), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 8), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 9), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 10), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 11), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 12), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 13), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 14), align 4
+  // LCHECK: load float, float addrspace(3)* getelementptr inbounds ([[DIM]] x float], [[DIM]] x float] addrspace(3)* @"[[GV]]", i32 0, i32 15), align 4
   output[Id] = GData;
 
 }
