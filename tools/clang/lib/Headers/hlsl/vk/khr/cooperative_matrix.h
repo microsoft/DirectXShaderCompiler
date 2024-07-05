ff7f770150b9c19ec5562e7771495f2e7d30f3c7
Steven Perron <stevenperron@google.com>
2024-07-05 10:22:00 -0400

    Add comments to header file.

--- tools/clang/lib/Headers/hlsl/vk/khr/cooperative_matrix.h
+++ tools/clang/lib/Headers/hlsl/vk/khr/cooperative_matrix.h
@@ -23,39 +23,82 @@
 namespace vk {
 namespace khr {
 
+// The base cooperative matrix class. The template arguments correspond to the
+// operands in the OpTypeCooperativeMatrixKHR instruction.
 template <typename ComponentType, Scope scope, uint rows, uint columns,
           CooperativeMatrixUse use>
 class CooperativeMatrix {
+
+  // Apply OpSNegate or OFNegate, depending on ComponentType, in a element by
+  // element manner.
   CooperativeMatrix negate();
+
+  // Apply OpIAdd or OFAdd, depending on ComponentType, in a element by element
+  // manner.
   CooperativeMatrix operator+(CooperativeMatrix other);
+
+  // Apply OpISub or OFSub, depending on ComponentType, in a element by element
+  // manner.
   CooperativeMatrix operator-(CooperativeMatrix other);
+
+  // Apply OpIMul or OFMul, depending on ComponentType, in a element by element
+  // manner.
   CooperativeMatrix operator*(CooperativeMatrix other);
+
+  // Apply OpSDiv, OpUDiv or OFDiv, depending on ComponentType, in a element by
+  // element manner.
   CooperativeMatrix operator/(CooperativeMatrix other);
+
+  // Apply OpMatrixTimesScalar in a element by element manner.
   CooperativeMatrix operator*(ComponentType scalar);
 
+  // Store the cooperative matrix using OpCooperativeMatrixStoreKHR to data[i]
+  // using memory layout RowMajorKHR.￼
   void StoreRowMajor(RWStructuredBuffer<ComponentType> data, uint32_t index);
+
+  // Store the cooperative matrix using OpCooperativeMatrixStoreKHR to data[i]
+  // using memory layout ColumnMajorKHR.￼
   void StoreColumnMajor(RWStructuredBuffer<ComponentType> data, uint32_t index);
+
+  // Store the cooperative matrix using OpCooperativeMatrixStoreKHR to data[i]
+  // using memory layout RowMajorKHR and the given stride and memory access
+  // mask.
   void StoreRowMajor(RWStructuredBuffer<ComponentType> data, uint32_t index,
                      uint32_t stride, MemoryAccessMask memoryAccessMask);
+
+  // Store the cooperative matrix using OpCooperativeMatrixStoreKHR to data[i]
+  // using memory layout ColumnMajorKHR and the given stride and memory access
+  // mask.
   void StoreColumnMajor(RWStructuredBuffer<ComponentType> data, uint32_t index,
                         uint32_t stride, MemoryAccessMask memoryAccessMask);
 
+  // Load the cooperative matrix using OpCooperativeMatrixLoadKHR from data[i]
+  // using memory layout RowMajorKHR.
   template <class BufferType>
   static CooperativeMatrix LoadRowMajor(BufferType buffer, uint32_t index);
 
+  // Loads a cooperative matrix using OpCooperativeMatrixLoadKHR from data[i]
+  // using memory layout ColumnMajorKHR.￼
   template <class BufferType>
   static CooperativeMatrix LoadColumnMajor(BufferType buffer, uint32_t index);
 
+  // Loads a cooperative matrix using OpCooperativeMatrixLoadKHR from data[i]
+  // using memory layout RowMajorKHR, and the given stride and memory access
+  // mask.
   template <class BufferType>
   static CooperativeMatrix
   LoadRowMajor(BufferType buffer, uint32_t index, uint32_t stride,
                MemoryAccessMask memoryAccessMask = MemoryAccessMaskNone);
 
+  // Loads a cooperative matrix using OpCooperativeMatrixLoadKHR from data[i]
+  // using memory layout ColumnMajorKHR, and the given stride and memory access
+  // mask.
   template <class BufferType>
   static CooperativeMatrix
   LoadColumnMajor(BufferType buffer, uint32_t index, uint32_t stride,
                   MemoryAccessMask memoryAccessMask = MemoryAccessMaskNone);
 
+  // Returns the result of OpCooperativeMatrixLengthKHR on the current type.￼
   static uint32_t GetLength();
 
   static const bool hasSignedIntegerComponentType =
@@ -66,27 +109,37 @@ class CooperativeMatrix {
       /* OpTypeCooperativeMatrixKHR */ 4456, ComponentType,
       vk::integral_constant<uint, scope>, vk::integral_constant<uint, rows>,
       vk::integral_constant<uint, columns>, vk::integral_constant<uint, use> >;
-  // pragma clang-format on
 
-  [[vk::ext_extension("SPV_KHR_cooperative_matrix")]] [[vk::ext_capability(
-      /* CooperativeMatrixKHRCapability */ 6022)]] SpirvMatrixType _matrix;
+  [[vk::ext_extension("SPV_KHR_cooperative_matrix")]]
+  [[vk::ext_capability(/* CooperativeMatrixKHRCapability */ 6022)]]
+  SpirvMatrixType _matrix;
+  // clang-format on
 };
 
+// Cooperative matrix that can be used in the "a" position of a multiple add
+// instruction (r = (a * b) + c).
 template <typename ComponentType, Scope scope, uint rows, uint columns>
 using CooperativeMatrixA =
     CooperativeMatrix<ComponentType, scope, rows, columns,
                       CooperativeMatrixUseMatrixAKHR>;
 
+// Cooperative matrix that can be used in the "b" position of a multiple add
+// instruction (r = (a * b) + c).
 template <typename ComponentType, Scope scope, uint rows, uint columns>
 using CooperativeMatrixB =
     CooperativeMatrix<ComponentType, scope, rows, columns,
                       CooperativeMatrixUseMatrixBKHR>;
 
+// Cooperative matrix that can be used in the "r" and "c" position of a multiple
+// add instruction (r = (a * b) + c).
 template <typename ComponentType, Scope scope, uint rows, uint columns>
 using CooperativeMatrixAccumulator =
     CooperativeMatrix<ComponentType, scope, rows, columns,
                       CooperativeMatrixUseMatrixAccumulatorKHR>;
 
+// Returns the result of OpCooperativeMatrixMulAddKHR when applied to a, b, and
+// c. The cooperative matrix operands are inferred, with the
+// SaturatingAccumulationKHR bit not set.
 template <typename ComponentType, Scope scope, uint rows, uint columns, uint K>
 CooperativeMatrixAccumulator<ComponentType, scope, rows, columns>
 cooperativeMatrixMultiplyAdd(
@@ -94,6 +147,9 @@ cooperativeMatrixMultiplyAdd(
     CooperativeMatrixB<ComponentType, scope, K, columns> b,
     CooperativeMatrixAccumulator<ComponentType, scope, rows, columns> c);
 
+// Returns the result of OpCooperativeMatrixMulAddKHR when applied to a, b, and
+// c. The cooperative matrix operands are inferred, with the
+// SaturatingAccumulationKHR bit set.
 template <typename ComponentType, Scope scope, uint rows, uint columns, uint K>
 CooperativeMatrixAccumulator<ComponentType, scope, rows, columns>
 cooperativeMatrixSaturatingMultiplyAdd(
