08f72353cede1ac2be14c437c82b8bfa0e76c46f
Chris Bieneman <cbieneman@microsoft.com>
2024-07-18 16:46:46 -0500

    [Metal] Add experimental Metal support
    This adds a new `-metal` flag to DXC which can be used to generate
    Metal's IR directly from DXC after compilation. There are some
    limitations in this flag which are worth noting:
    
    1) It does not support library shaders (yet)
    2) It does not support disassembly (yet)
    3) It is _wildly_ under tested because wtihout (2) we can't do anything
    to really verify correct output (yay?)

--- lib/DxcSupport/HLSLOptions.cpp
+++ lib/DxcSupport/HLSLOptions.cpp
@@ -1044,6 +1044,8 @@ int ReadDxcOpts(const OptTable *optionTable, unsigned flagsToInclude,
 
   addDiagnosticArgs(Args, OPT_W_Group, OPT_W_value_Group, opts.Warnings);
 
+  opts.GenMetal = Args.hasFlag(OPT_metal, OPT_INVALID, false);
+
   // SPIRV Change Starts
 #ifdef ENABLE_SPIRV_CODEGEN
   opts.GenSPIRV = Args.hasFlag(OPT_spirv, OPT_INVALID, false);
@@ -1252,6 +1254,21 @@ int ReadDxcOpts(const OptTable *optionTable, unsigned flagsToInclude,
   }
 #endif // ENABLE_SPIRV_CODEGEN
   // SPIRV Change Ends
+
+#ifndef ENABLE_METAL_CODEGEN
+  if (opts.GenMetal) {
+    errors << "Metal CodeGen not available. "
+              "Please rebuild with Metal IR Converter installed.";
+    return 1;
+  }
+#endif
+
+  if (opts.GenMetal) {
+    if (!opts.OutputObject.empty() && opts.AssemblyCode.empty()) {
+      errors << "Disassembly of Metal IR not supported (yet).";
+      return 1;
+    }
+  }
 
   // Validation for DebugInfo here because spirv uses same DebugInfo opt,
   // and legacy wrappers will add EmbedDebug in this case, leading to this
