b6f7ff81e9cea7b82a6412b6d2521dfdacc09517
Antonio Maiorano <amaiorano@google.com>
2024-07-29 16:42:09 -0400

    Use llvm_assert/assert.h on all platforms, not just Windows (#6815)
    This makes it possible to define how assert works on all platforms. The
    header was already being included by String.cpp, and was already
    designed to work for non-Windows platforms.
    
    Also modify the non-Windows llvm_assert to emit the assertion message to
    stderr and trap. We cannot call standard assert as we are overriding it
    via include dirs, so there's no way to include the standard one and call
    it.

--- lib/Support/assert.cpp
+++ lib/Support/assert.cpp
@@ -14,20 +14,23 @@
 #include "dxc/Support/Global.h"
 #include "windows.h"
 
-void llvm_assert(const char *_Message, const char *_File, unsigned _Line,
-                 const char *_Function) {
-  OutputDebugFormatA("Error: assert(%s)\nFile:\n%s(%d)\nFunc:\t%s\n", _Message,
-                     _File, _Line, _Function);
+void llvm_assert(const char *Message, const char *File, unsigned Line,
+                 const char *Function) {
+  OutputDebugFormatA("Error: assert(%s)\nFile:\n%s(%d)\nFunc:\t%s\n", Message,
+                     File, Line, Function);
   RaiseException(STATUS_LLVM_ASSERT, 0, 0, 0);
 }
 
 #else
 
-#include <assert.h>
+#include "llvm/Support/Compiler.h"
+#include "llvm/Support/raw_ostream.h"
 
-void llvm_assert(const char *message, const char *, unsigned,
-                 const char *_Function) {
-  assert(false && message);
+void llvm_assert(const char *Message, const char *File, unsigned Line,
+                 const char *Function) {
+  llvm::errs() << "Error: assert(" << Message << ")\nFile:\n"
+               << File << "(" << Line << ")\nFunc:\t" << Function << "\n";
+  LLVM_BUILTIN_TRAP;
 }
 
 #endif
