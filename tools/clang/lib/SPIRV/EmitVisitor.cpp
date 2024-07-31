3bf4340e7340464d384379ccc64293e822eae36f
Steven Perron <stevenperron@google.com>
2024-07-31 09:38:15 -0400

    Remove TODO comment that was done.

--- tools/clang/lib/SPIRV/EmitVisitor.cpp
+++ tools/clang/lib/SPIRV/EmitVisitor.cpp
@@ -431,7 +431,6 @@ void EmitVisitor::initInstruction(SpirvInstruction *inst) {
                                spv::Decoration::RelaxedPrecision, {});
   }
   // Emit NoContraction decoration (if any).
-  // STEVEN: Check if Gis is set here.
   if ((spvOptions.IEEEStrict || inst->isPrecise()) &&
       inst->isArithmeticInstruction()) {
     typeHandler.emitDecoration(getOrAssignResultId<SpirvInstruction>(inst),
