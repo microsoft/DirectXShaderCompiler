c88a1a68953cecb67ef54e0ef3b3db0165f54841
Nielsbishere <n@osomi.net>
2024-08-07 23:07:21 +0200

    DirectX-Headers is now always required and removed config file

--- include/dxc/config.h
+++
@@ -1,4 +0,0 @@
-/* Disable overriding memory allocators. */
-/* #undef DXC_DISABLE_ALLOCATOR_OVERRIDES */
-/* Generate a trap if an hlsl::Exception is thrown during code generation */
-/* #undef DXC_CODEGEN_EXCEPTIONS_TRAP */
