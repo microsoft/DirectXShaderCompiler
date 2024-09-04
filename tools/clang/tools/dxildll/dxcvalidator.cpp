f6e048349cddd4c30823e7221d3f5d339e90469e
Xiang Li <python3kgae@outlook.com>
2024-09-04 15:21:44 -0400

    Add IDxcVersionInfo2 for dxildll.

--- tools/clang/tools/dxildll/dxcvalidator.cpp
+++ tools/clang/tools/dxildll/dxcvalidator.cpp
@@ -17,10 +17,20 @@
 #include "dxc/Support/microcom.h"
 #include "dxc/dxcapi.h"
 
+#ifdef SUPPORT_QUERY_GIT_COMMIT_INFO
+#include "clang/Basic/Version.h"
+#endif // SUPPORT_QUERY_GIT_COMMIT_INFO
+
 using namespace llvm;
 using namespace hlsl;
 
-class DxcValidator : public IDxcValidator2, public IDxcVersionInfo {
+class DxcValidator : public IDxcValidator2,
+#ifdef SUPPORT_QUERY_GIT_COMMIT_INFO
+                     public IDxcVersionInfo2
+#else
+                     public IDxcVersionInfo
+#endif // SUPPORT_QUERY_GIT_COMMIT_INFO
+{
 private:
   DXC_MICROCOM_TM_REF_FIELDS()
 
@@ -54,6 +64,12 @@ public:
   // IDxcVersionInfo
   HRESULT STDMETHODCALLTYPE GetVersion(UINT32 *pMajor, UINT32 *pMinor) override;
   HRESULT STDMETHODCALLTYPE GetFlags(UINT32 *pFlags) override;
+
+#ifdef SUPPORT_QUERY_GIT_COMMIT_INFO
+  // IDxcVersionInfo2
+  HRESULT STDMETHODCALLTYPE GetCommitInfo(UINT32 *pCommitCount,
+                                          char **pCommitHash) override;
+#endif
 };
 
 // Compile a single entry point to the target shader model
@@ -81,6 +97,25 @@ HRESULT STDMETHODCALLTYPE DxcValidator::GetVersion(UINT32 *pMajor,
                                                    UINT32 *pMinor) {
   return hlsl::getValidationVersion(pMajor, pMinor);
 }
+
+#ifdef SUPPORT_QUERY_GIT_COMMIT_INFO
+HRESULT STDMETHODCALLTYPE DxcValidator::GetCommitInfo(UINT32 *pCommitCount,
+                                                      char **pCommitHash) {
+  if (pCommitCount == nullptr || pCommitHash == nullptr)
+    return E_INVALIDARG;
+
+  char *const hash = (char *)CoTaskMemAlloc(
+      8 + 1); // 8 is guaranteed by utils/GetCommitInfo.py
+  if (hash == nullptr)
+    return E_OUTOFMEMORY;
+  std::strcpy(hash, clang::getGitCommitHash());
+
+  *pCommitHash = hash;
+  *pCommitCount = clang::getGitCommitCount();
+
+  return S_OK;
+}
+#endif // SUPPORT_QUERY_GIT_COMMIT_INFO
 
 HRESULT STDMETHODCALLTYPE DxcValidator::GetFlags(UINT32 *pFlags) {
   DxcThreadMalloc TM(m_pMalloc);
