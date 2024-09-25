877a18ea6fdcc6047732589b2b849898cc892f04
Steven Perron <stevenperron@google.com>
2024-09-25 16:05:34 -0400

    [SPIRV] Change "dxc" to "%dxc" in tests
    The workgroupspirvpointer tests use `dxc` in the run commands, and it
    should be `%dxc`.

--- tools/clang/test/CodeGenSPIRV/workgroupspirvpointer.varpointer.hlsl
+++ tools/clang/test/CodeGenSPIRV/workgroupspirvpointer.varpointer.hlsl
@@ -1,4 +1,4 @@
-// RUN: dxc -fspv-target-env=vulkan1.3 -T cs_6_0 -E main -spirv -HV 2021 -I %hlsl_headers %s 2>&1 | FileCheck %s
+// RUN: %dxc -fspv-target-env=vulkan1.3 -T cs_6_0 -E main -spirv -HV 2021 -I %hlsl_headers %s 2>&1 | FileCheck %s
 
 #include "vk/spirv.h"
 
