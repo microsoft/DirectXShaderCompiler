b48341e4031fb5e0636c123bcea8e30223dcb340
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2024-10-04 16:05:20 -0400

    [NFC] Fix a spelling mistake (#6946)
    We have folks implementing the `firstbithigh` intrinsic and I noticed
    this SPIRV error has a misspelling.

--- tools/clang/test/CodeGenSPIRV/intrinsics.firstbithigh.64bit.hlsl
+++ tools/clang/test/CodeGenSPIRV/intrinsics.firstbithigh.64bit.hlsl
@@ -3,8 +3,8 @@
 void main() {
   uint64_t   uint_1;
   int64_t2   int_2;
-// CHECK: error: firstbithigh is currently limited to 32-bit width components when targetting SPIR-V
+// CHECK: error: firstbithigh is currently limited to 32-bit width components when targeting SPIR-V
   int fbh = firstbithigh(uint_1);
-// CHECK: error: firstbithigh is currently limited to 32-bit width components when targetting SPIR-V
+// CHECK: error: firstbithigh is currently limited to 32-bit width components when targeting SPIR-V
   int64_t2 fbh2 = firstbithigh(int_2);
 }
