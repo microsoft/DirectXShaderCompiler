3bd537e23b89877b3cd2333f4167a9c44cc4406c
Joshua Batista <jbatista@microsoft.com>
2025-02-06 19:48:54 -0800

    dont copy code

--- lib/Support/assert.cpp
+++ lib/Support/assert.cpp
@@ -11,6 +11,17 @@
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/raw_ostream.h"
 
+#if defined(LLVM_ASSERTIONS_TRAP) || !defined(WIN32)
+namespace {
+void llvm_assert_trap(const char *_Message, const char *_File, unsigned _Line,
+                      const char *_Function) {
+  llvm::errs() << "Error: assert(" << _Message << ")\nFile:\n"
+               << _File << "(" << _Line << ")\nFunc:\t" << _Function << "\n";
+  LLVM_BUILTIN_TRAP;
+}
+} // namespace
+#endif
+
 #ifdef _WIN32
 #include "dxc/Support/Global.h"
 #include "windows.h"
@@ -18,14 +29,6 @@
 void llvm_assert(const char *Message, const char *File, unsigned Line,
                  const char *Function) {
 #ifdef LLVM_ASSERTIONS_TRAP
-  namespace {
-  void llvm_assert_trap(const char *_Message, const char *_File, unsigned _Line,
-                        const char *_Function) {
-    llvm::errs() << "Error: assert(" << _Message << ")\nFile:\n"
-                 << _File << "(" << _Line << ")\nFunc:\t" << _Function << "\n";
-    LLVM_BUILTIN_TRAP;
-  }
-  } // namespace
   llvm_assert_trap(Message, File, Line, Function);
 #else
   OutputDebugFormatA("Error: assert(%s)\nFile:\n%s(%d)\nFunc:\t%s\n", Message,
@@ -35,14 +38,6 @@ void llvm_assert(const char *Message, const char *File, unsigned Line,
 }
 
 #else /* _WIN32 */
-namespace {
-void llvm_assert_trap(const char *_Message, const char *_File, unsigned _Line,
-                      const char *_Function) {
-  llvm::errs() << "Error: assert(" << _Message << ")\nFile:\n"
-               << _File << "(" << _Line << ")\nFunc:\t" << _Function << "\n";
-  LLVM_BUILTIN_TRAP;
-}
-} // namespace
 
 void llvm_assert(const char *Message, const char *File, unsigned Line,
                  const char *Function) {
