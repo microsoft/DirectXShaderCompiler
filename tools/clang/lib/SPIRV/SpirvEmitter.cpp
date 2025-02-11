6d18a4030f2a910e341f0121fc9ba0aecf425ce7
Steven Perron <stevenperron@google.com>
2025-02-11 14:14:48 -0500

    [SPIRV] Stop running ReplaceInvalidOpcPass
    That pass was intended to remove derivative instructions used in
    non-fragment shaders. Now they are available in more places because of
    new extensions. In general,
    it is better practice to issue an error if they are used in an place
    they are not allowed.
    
    Fixes https://github.com/microsoft/DirectXShaderCompiler/issues/7086

--- tools/clang/lib/SPIRV/SpirvEmitter.cpp
+++ tools/clang/lib/SPIRV/SpirvEmitter.cpp
@@ -15218,7 +15218,6 @@ bool SpirvEmitter::spirvToolsLegalize(std::vector<uint32_t> *mod,
     optimizer.RegisterPass(
         spvtools::CreateAggressiveDCEPass(spirvOptions.preserveInterface));
   }
-  optimizer.RegisterPass(spvtools::CreateReplaceInvalidOpcodePass());
   optimizer.RegisterPass(spvtools::CreateCompactIdsPass());
   optimizer.RegisterPass(spvtools::CreateSpreadVolatileSemanticsPass());
   if (spirvOptions.fixFuncCallArguments) {
