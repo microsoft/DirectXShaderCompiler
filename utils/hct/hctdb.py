2652be64c59511c18b245eae9ab7b60ddadbbb47
Greg Roth <grroth@microsoft.com>
2025-02-13 09:54:28 -0700

    NFC: Infrastructure changes for DXIL op vector and multi-dim-overloads
    This change adds vector and multi-dimensional overload support for DXIL operations.
    
    Incorporates change to add vector overloads from @pow2clk. This includes changes to hctdb*.py DxilOperations.* and DxilValidation.cpp.
    hctdb.py:
        Updates vector character from 't' to '<'
        Allows legal vector element overloads to be specified after vector overload character.
        Defaults legal vector element overloads to the scalar element overloads, if any.
        Adds 'x' for extended overload mechanism, which supports up to 2 overload dimensions at this point, but is easily expandable if necessary.
        Processes syntax using ',' to separate multiple overloads and defaulting vector element overloads into new breakdown of main overload string, vector overload list of strings, and list of extended overloads (used if main overload set to 'x').
    DxilOperations:
        Extend OpCodeProperty with ExtendedOverloads and AllowedVectorElements arrays using new OverloadMask.
        When extended bit set in main overloads, ExtendedOverloads array is used for each overload dimension
        When vector bit set in main overloads or each ExtendedOverloads dimension, AllowedVectorElements are set for corresponding dimension index for allowed element types.
        Updated generated DXIL op table
        Remove unused static methods in hlsl::OP
         I think these were leftover from an attempt to work around a name collision
         ultimately caused by dx.types.CBufRet.f16|i16 between min-precision (with 4 elements)
         and native low precision (with 8 elements), caused by failing to initialize the
         min precision mode correctly for linking.
         That issue was fixed, and the names made unique by adding ".8" to the end of
         the 8-element native low precision cbuffer return type.
         eliminate use of std::string, std::vector
          - Use Twine, raw_svector_ostream, and SmallVector storage to replace uses of std::string
          - Use SmallVector instead of std::vector for ArgTypes in GetOpFunc
    
    Rework DXIL op overload system
    
    Add comments explaining the new system.
    Eliminate bool array in favor of array of masks for up to N dimensions.
    Add NumOverloadDims instead of two-mode system.
    
    Rework TypeSlots:
    - use enum, categorize basic, limit masks to used bits
    - void doesn't need a type slot (NumOverloadDims == 0 instead)
    - m_OverloadTypeName only contains basic type names
    
    Handle multi-overload in FixOverloadNames; new MayHaveNonCanonicalOverload
    is used to determine whether the overload name could need fixing.
    
    Extended overload is still a distinction because of the way the overloads
    must be wrapped in an unnamed StructType.
    However, it does not need a bit in the overload mask.
    
    Renamed GetVectorType to GetStructVectorType, since it's just used to get a
    struct for a particular vector type, not a vector type itself.
    
    In hctdb.py, no longer separate extended and vector overloads, just verify correctness
    of the incoming string, and add default vector overloads if necessary.
    
    In hctdb_instrhelp.py, update according to changes in hctdb.py, and eliminate
    needless, problematic, outdated comment printing.

--- utils/hct/hctdb.py
+++ utils/hct/hctdb.py
@@ -37,6 +37,30 @@ extra_counters = [
     "array_local_ldst",
 ]
 
+# These are the valid overload type characters for DXIL instructions.
+# - "v" is for void, and can only be used alone.
+# - "u" is for user defined type (UDT), and is mutually exclusive with the other
+#   types.
+# - "o" is for an HLSL object type (e.g. Texture, Sampler, etc.), and is
+#   mutually exclusive with the other types.
+# - "<" is for vector overloads, and may be followed by a set of supported
+#   component types.
+#   - If "<" is not followed by any component types, any preceding scalar types
+#     are used.
+#   - Vector component types are captured into a separate list during
+#     processing.
+# - "," is used to separate multiple overload dimensions.
+#   - When used, only $x0, $x1, etc. are supported for overloaded parameter
+#     types.
+# dxil_all_user_oload_chars must be kept in sync with the indices in
+# hlsl::OP::TypeSlot in DxilOperations.h.
+dxil_all_user_oload_chars = "hfd18wiluo<"
+dxil_scalar_oload_chars = "hfd18wil"
+
+# Maximum number of overload dimensions supported through the extended overload
+# in DXIL instructions.
+dxil_max_overload_dims = 2
+
 
 class db_dxil_enum_value(object):
     "A representation for a value in an enumeration type"
@@ -81,6 +105,7 @@ class db_dxil_inst(object):
         self.ops = []  # the operands that this instruction takes
         self.is_allowed = True  # whether this instruction is allowed in a DXIL program
         self.oload_types = ""  # overload types if applicable
+        # Always call process_oload_types() after setting oload_types.
         self.fn_attr = ""  # attribute shorthands: rn=does not access memory,ro=only reads from memory,
         self.is_deriv = False  # whether this is some kind of derivative
         self.is_gradient = False  # whether this requires a gradient calculation
@@ -98,12 +123,136 @@ class db_dxil_inst(object):
         self.is_reserved = self.dxil_class == "Reserved"
         self.shader_model_translated = ()  # minimum shader model required with translation by linker
         self.props = {}  # extra properties
+        self.num_oloads = 0  # number of overloads for this instruction
+        if self.is_dxil_op:
+            self.process_oload_types()
 
     def __str__(self):
         return self.name
 
     def fully_qualified_name(self):
         return "{}::{}".format(self.fully_qualified_name_prefix, self.name)
+
+    def process_oload_types(self):
+        if type(self.oload_types) is not str:
+            raise ValueError(
+                f"overload for '{self.name}' should be a string - use empty if n/a"
+            )
+        # Early out for LLVM instructions
+        if not self.is_dxil_op:
+            return
+
+        self.num_oloads = 0
+
+        # Early out for void overloads.
+        if self.oload_types == "v":
+            return
+
+        if self.oload_types == "":
+            raise ValueError(
+                f"overload for '{self.name}' should not be empty - use void if n/a"
+            )
+        if "v" in self.oload_types:
+            raise ValueError(
+                f"void overload should be exclusive to other types for '({self.name})'"
+            )
+
+        # Process oload_types for extended and vector overloads.
+        # Contrived example: "hf<,<fd,i<1"
+        #   - "," splits multiple overload dimensions
+        #   - In the first overload dimension "hf<":
+        #     - "hf" means overloads for scalar half and float
+        #     - ending with "<" means vector overload supporting the same
+        #       components as defined for the scalar overload types.
+        #   - In the second overload dimension "<f":
+        #     - starting with "<" means only vector overloads are supported.
+        #     - "fd" means the vector supports float or double components.
+        #   - In the third overload dimension "i<1":
+        #     - "i" means it supports a scalar i32 overload
+        #     - "<1" means it also supports a vector overload with an i1
+        #       component type.
+        oload_types = self.oload_types.split(",")
+        self.num_oloads = len(oload_types)
+        if self.num_oloads > dxil_max_overload_dims:
+            raise ValueError(
+                "Too many overload dimensions for DXIL op "
+                f"{self.name}: '{self.oload_types}'"
+            )
+
+        def check_duplicate_overloads(oloads):
+            if len(oloads) != len(set(oloads)):
+                raise ValueError(
+                    "Duplicate overload types specified for DXIL op "
+                    f"{self.name}: '{oloads}' in '{self.oload_types}'"
+                )
+
+        def check_overload_chars(oloads, valid_chars):
+            invalid_chars = set(oloads).difference(set(valid_chars))
+            if invalid_chars:
+                raise ValueError(
+                    "Invalid overload type character(s) used for DXIL op "
+                    f"{self.name}: '{invalid_chars}' in '{oloads}' from "
+                    f"'{self.oload_types}'"
+                )
+
+        for n, oloads in enumerate(oload_types):
+            if len(oloads) == 0:
+                raise ValueError(
+                    f"Invalid empty overload type for DXIL op "
+                    f"{self.name}: '{self.oload_types}'"
+                )
+            check_overload_chars(oloads, dxil_all_user_oload_chars)
+
+            # split at vector for component overloads, if vector specified
+            # without following components, use the scalar overloads that
+            # precede the vector character.
+            split = oloads.split("<")
+            if len(split) == 1:
+                # No vector overload.
+                continue
+            elif len(split) != 2:
+                raise ValueError(
+                    f"Invalid vector overload for DXIL op {self.name}: "
+                    f"{oloads} in '{self.oload_types}'"
+                )
+
+            # Split into scalar and vector component overloads.
+            scalars, vector_oloads = split
+            check_duplicate_overloads(scalars)
+            if not vector_oloads:
+                vector_oloads = scalars
+            else:
+                check_duplicate_overloads(vector_oloads)
+            if not vector_oloads:
+                raise ValueError(
+                    "No scalar overload types provided with vector overload "
+                    f"for DXIL op {self.name}: '{self.oload_types}'"
+                )
+            check_overload_chars(vector_oloads, dxil_scalar_oload_chars)
+            oload_types[n] = scalars + "<" + vector_oloads
+        # Reconstruct overload string with default vector overloads.
+        self.oload_types = ",".join(oload_types)
+        self.check_extended_oload_ops()
+
+    def check_extended_oload_ops(self):
+        "Ensure ops has sequential extended overload references with $x0, $x1, etc."
+        if self.num_oloads < 2:
+            return
+        next_oload_idx = 0
+        for i in self.ops:
+            if i.llvm_type.startswith("$x"):
+                if i.llvm_type != "$x" + str(next_oload_idx):
+                    raise ValueError(
+                        "Extended overloads are not sequentially referenced in "
+                        f"DXIL op {self.name}: {i.llvm_type} != $x{next_oload_idx}"
+                    )
+                next_oload_idx += 1
+        if next_oload_idx != self.num_oloads:
+            raise ValueError(
+                "Extended overloads are not referenced for all overload "
+                f"dimensions in DXIL op {self.name}: {next_oload_idx} != "
+                f"{self.num_oloads}"
+            )
 
 
 class db_dxil_metadata(object):
@@ -477,9 +626,7 @@ class db_dxil(object):
                 "closesthit",
             )
         for i in "GeometryIndex".split(","):
-            self.name_idx[
-                i
-            ].category = (
+            self.name_idx[i].category = (
                 "Raytracing object space uint System Values, raytracing tier 1.1"
             )
             self.name_idx[i].shader_model = 6, 5
@@ -574,9 +721,7 @@ class db_dxil(object):
             self.name_idx[i].shader_model = 6, 3
             self.name_idx[i].shader_stages = ("library", "intersection")
         for i in "CreateHandleForLib".split(","):
-            self.name_idx[
-                i
-            ].category = (
+            self.name_idx[i].category = (
                 "Library create handle from resource struct (like HL intrinsic)"
             )
             self.name_idx[i].shader_model = 6, 3
@@ -5652,18 +5797,6 @@ class db_dxil(object):
         )
         for i in self.instr:
             self.verify_dense(i.ops, lambda x: x.pos, lambda x: i.name)
-        for i in self.instr:
-            if i.is_dxil_op:
-                assert i.oload_types != "", (
-                    "overload for DXIL operation %s should not be empty - use void if n/a"
-                    % (i.name)
-                )
-                assert i.oload_types == "v" or i.oload_types.find("v") < 0, (
-                    "void overload should be exclusive to other types (%s)" % i.name
-                )
-            assert (
-                type(i.oload_types) is str
-            ), "overload for %s should be a string - use empty if n/a" % (i.name)
 
         # Verify that all operations in each class have the same signature.
         import itertools
@@ -8390,6 +8523,7 @@ class db_hlsl_intrisic_param(object):
         self.idx = idx  # Argument index
         self.template_id_idx = template_id_idx  # Template ID numeric value
         self.component_id_idx = component_id_idx  # Component ID numeric value
+
 
 class db_hlsl(object):
     "A database of HLSL language data"
