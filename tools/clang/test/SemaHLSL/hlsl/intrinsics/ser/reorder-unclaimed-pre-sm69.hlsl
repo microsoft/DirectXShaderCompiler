a7878a4ac9c90ffe1f93cb1bce890acc3964d13d
Simon Moll <smoll@nvidia.com>
2025-02-20 21:07:20 +0100

    [ser] ReorderThread -> MaybeReorderTHread

--- tools/clang/test/SemaHLSL/hlsl/intrinsics/ser/reorder-unclaimed-pre-sm69.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/intrinsics/ser/reorder-unclaimed-pre-sm69.hlsl
@@ -4,10 +4,10 @@
 // Check that intrinsic names of Shader Execution Reordering are unclaimed pre SM 6.9.
 // expected-no-diagnostics
 
-void ReorderThread(uint CoherenceHint, uint NumCoherenceHintBitsFromLSB) {
+void MaybeReorderThread(uint CoherenceHint, uint NumCoherenceHintBitsFromLSB) {
 }
 
 [shader("raygeneration")]
 void main() {
-  ReorderThread(15u, 4u);
+  MaybeReorderThread(15u, 4u);
 }
