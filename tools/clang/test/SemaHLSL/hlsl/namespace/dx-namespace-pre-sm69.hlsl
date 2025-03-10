d0b8e201fb7a7489a86913d3cababcb14fc58968
Simon Moll <smoll@nvidia.com>
2025-03-10 10:52:01 +0100

    [ser] Add explicit test for absence of namespace 'dx' pre-SM6.9

--- tools/clang/test/SemaHLSL/hlsl/namespace/dx-namespace-pre-sm69.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/namespace/dx-namespace-pre-sm69.hlsl
@@ -1,9 +1,7 @@
 // RUN: %dxc -T lib_6_8 %s -verify
 
-namespace dx {}
-
 [shader("raygeneration")]
 void main() {
-// expected-error@+1{{no type named 'HitObject' in namespace 'dx'}}
+  // expected-error@+1{{use of undeclared identifier 'dx'}}
   dx::HitObject hit;
 }
