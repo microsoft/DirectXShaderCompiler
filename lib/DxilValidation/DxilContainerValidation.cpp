3b3823e476e0734a8b2c6626ebbbbd3db7f06d50
Joshua Batista <jbatista@microsoft.com>
2025-03-11 19:01:20 -0700

    shorten bool conversion, remove unneeded change

--- lib/DxilValidation/DxilContainerValidation.cpp
+++ lib/DxilValidation/DxilContainerValidation.cpp
@@ -337,12 +337,9 @@ void PSVContentVerifier::VerifySignatureElement(
 
   PSVSignatureElement PSVSE(StrTab, IndexTab, PSVSE0);
   if (SE.IsArbitrary())
-    Mismatch |=
-        strcmp(PSVSE.GetSemanticName(), SE.GetName()) == 0 ? false : true;
+    Mismatch |= strcmp(PSVSE.GetSemanticName(), SE.GetName()) != 0;
   else
-    Mismatch |= PSVSE0->SemanticKind != static_cast<uint8_t>(SE.GetKind()) == 0
-                    ? false
-                    : true;
+    Mismatch |= PSVSE0->SemanticKind != static_cast<uint8_t>(SE.GetKind());
 
   ModulePSVSE0.SemanticName = PSVSE0->SemanticName;
   // Compare all fields.
@@ -497,7 +494,7 @@ void PSVContentVerifier::Verify(unsigned ValMajor, unsigned ValMinor,
                         std::to_string(ShaderStage));
       return;
     }
-    bool ViewIDUsed = PSV1->UsesViewID == 0 ? false : true;
+    bool ViewIDUsed = PSV1->UsesViewID != 0;
     if (ViewIDUsed != DM.m_ShaderFlags.GetViewID())
       EmitMismatchError("UsesViewID", std::to_string(PSV1->UsesViewID),
                         std::to_string(DM.m_ShaderFlags.GetViewID()));
