769f9aee94a7815c2bf82c21f709b486f32f2142
Simon Moll <smoll@nvidia.com>
2025-03-13 14:54:06 +0100

    [ser] Add 'namespace dx' unconditionally

--- tools/clang/test/SemaHLSL/hlsl/namespace/dx-namespace-pre-sm69.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/namespace/dx-namespace-pre-sm69.hlsl
@@ -1,7 +1,8 @@
 // RUN: %dxc -T lib_6_8 %s -verify
 
+// expected-no-diagnostics
+using namespace dx;
+
 [shader("raygeneration")]
 void main() {
-  // expected-error@+1{{use of undeclared identifier 'dx'}}
-  dx::HitObject hit;
 }
