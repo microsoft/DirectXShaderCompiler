53fa9f6e249a3e38f080970bba33762820429c87
Joshua Batista <jbatista@microsoft.com>
2025-03-13 17:56:13 -0700

    merge verifier tests across shader models

--- tools/clang/test/HLSLFileCheck/hlsl/objects/RayQuery/rayquery-decls-errors.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/RayQuery/rayquery-decls-errors.hlsl
@@ -1,6 +1,14 @@
-// RUN: %dxc -T vs_6_5 -E main -verify %s 
+// RUN: %dxc -T vs_6_9 -E RayQueryTests -verify %s
+// RUN: %dxc -T vs_6_5 -E RayQueryTests2 -verify %s
 
-void main(uint i : IDX, RayDesc rayDesc : RAYDESC) {
+// validate OMM flags
+void RayQueryTests(uint i : IDX, RayDesc rayDesc : RAYDESC) {
+  // expected-error@+1{{When using 'RAY_FLAG_FORCE_OMM_2_STATE' in RayFlags, RayQueryFlags must have RAYQUERY_FLAG_ALLOW_OPACITY_MICROMAPS set.}}
+  RayQuery<RAY_FLAG_FORCE_OMM_2_STATE, 0> rayQuery0a;
+}
+
+
+void RayQueryTests2(uint i : IDX, RayDesc rayDesc : RAYDESC) {
   // expected-error@+1{{When using 'RAY_FLAG_FORCE_OMM_2_STATE' in RayFlags, RayQueryFlags must have RAYQUERY_FLAG_ALLOW_OPACITY_MICROMAPS set.}}
   RayQuery<RAY_FLAG_FORCE_OMM_2_STATE> rayQuery0c;  
 }
