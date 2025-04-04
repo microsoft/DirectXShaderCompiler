d343b2dd99ebdce1eb5b9dc63ba9b2de94061e7a
Simon Moll <smoll@nvidia.com>
2025-04-04 07:29:17 +0200

    [ser] Add 'reordercoherent' to dxcdisassembler and update tests

--- tools/clang/test/CodeGenDXIL/hlsl/attributes/reordercoherent_for_arg.hlsl
+++ tools/clang/test/CodeGenDXIL/hlsl/attributes/reordercoherent_for_arg.hlsl
@@ -3,7 +3,7 @@
 
 // CHECK: %[[uH:[^ ]+]] = load %dx.types.Handle, %dx.types.Handle* @"\01?u@@3V?$RWBuffer@M@@A", align 4
 // CHECK: %[[uLIBH:[^ ]+]] = call %dx.types.Handle @dx.op.createHandleForLib.dx.types.Handle(i32 160, %dx.types.Handle %[[uH]]) ; CreateHandleForLib(Resource)
-// CHECK: %[[uANNOT:[^ ]+]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle %[[uLIBH]], %dx.types.ResourceProperties { i32 69642, i32 265 }) ; AnnotateHandle(res,props) resource: RWTypedBuffer<F32>
+// CHECK: %[[uANNOT:[^ ]+]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle %[[uLIBH]], %dx.types.ResourceProperties { i32 69642, i32 265 }) ; AnnotateHandle(res,props) resource: reordercoherent RWTypedBuffer<F32>
 // CHECK: %{{[^ ]+}} = call %dx.types.ResRet.f32 @dx.op.bufferLoad.f32(i32 68, %dx.types.Handle %[[uANNOT]], i32 0, i32 undef) ; BufferLoad(srv,index,wot)
 
 RWBuffer<float> OutBuf : register(u1);
