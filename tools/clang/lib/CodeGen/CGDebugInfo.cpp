d2cbc3b8ccedc178842ec04b261f6148f3908644
Simon Moll <smoll@nvidia.com>
2025-04-10 10:44:27 +0200

    Move debug info fix into separate PR #7332

--- tools/clang/lib/CodeGen/CGDebugInfo.cpp
+++ tools/clang/lib/CodeGen/CGDebugInfo.cpp
@@ -1047,15 +1047,8 @@ bool CGDebugInfo::TryCollectHLSLRecordElements(const RecordType *Ty,
     unsigned VecSize = hlsl::GetHLSLVecSize(QualTy);
     unsigned ElemSizeInBits = CGM.getContext().getTypeSize(ElemQualTy);
     unsigned CurrentAlignedOffset = 0;
-    std::string FieldNameBuf;
     for (unsigned ElemIdx = 0; ElemIdx < VecSize; ++ElemIdx) {
-      StringRef FieldName;
-      if (VecSize <= 4) {
-        FieldName = StringRef(&"xyzw"[ElemIdx], 1);
-      } else {
-        FieldNameBuf = "c" + std::to_string(ElemIdx);
-        FieldName = FieldNameBuf;
-      }
+      StringRef FieldName = StringRef(&"xyzw"[ElemIdx], 1);
       CurrentAlignedOffset =
           llvm::RoundUpToAlignment(CurrentAlignedOffset, AlignBits);
       llvm::DIType *FieldType =
