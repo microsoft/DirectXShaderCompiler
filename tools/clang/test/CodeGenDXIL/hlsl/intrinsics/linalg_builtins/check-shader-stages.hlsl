9fcf8c758941704fbb13b6cd3cec0176ce12d3e6
Simon Moll <smoll@nvidia.com>
2025-04-10 11:29:53 +0200

    Factored common code into function in check-shader-stages.hlsl

--- tools/clang/test/CodeGenDXIL/hlsl/intrinsics/linalg_builtins/check-shader-stages.hlsl
+++ tools/clang/test/CodeGenDXIL/hlsl/intrinsics/linalg_builtins/check-shader-stages.hlsl
@@ -4,15 +4,7 @@ ByteAddressBuffer matrix_buffer;
 ByteAddressBuffer bias_buffer;
 RWByteAddressBuffer rw_matrix_buffer;
 
-// CHECK: define void @ps_main()
-// CHECK: call <4 x float> @dx.op.matVecMul
-// CHECK: call <4 x float> @dx.op.matVecMulAdd
-// CHECK: call void @dx.op.outerProductAccumulate
-// CHECK: call void @dx.op.vectorAccumulate
-
-[Shader("pixel")]
-void ps_main()
-{	
+void UseCoopVec() {
 	vector<float, 4> output_vector;
 	static const uint is_output_unsigned = 0;
 	
@@ -59,6 +51,18 @@ void ps_main()
 	   va_matrix_offset);
 }
 
+// CHECK: define void @ps_main()
+// CHECK: call <4 x float> @dx.op.matVecMul
+// CHECK: call <4 x float> @dx.op.matVecMulAdd
+// CHECK: call void @dx.op.outerProductAccumulate
+// CHECK: call void @dx.op.vectorAccumulate
+
+[Shader("pixel")]
+void ps_main()
+{	
+    UseCoopVec();
+}
+
 // CHECK: define void @cs_main()
 // CHECK: call <4 x float> @dx.op.matVecMul
 // CHECK: call <4 x float> @dx.op.matVecMulAdd
@@ -69,50 +73,7 @@ void ps_main()
 [NumThreads(1,1,1)]
 void cs_main()
 {	
-	vector<float, 4> output_vector;
-	static const uint is_output_unsigned = 0;
-	
-	vector<float, 4> input_vector;
-	const uint is_input_unsigned = 0;
-	const uint input_interpretation = 9; /*F32*/
-	
-	const uint matrix_offset = 0;
-	const uint matrix_interpretation = 9; /*F32*/
-	const uint matrix_dimM = 4;
-	const uint matrix_dimK = 4;
-	const uint matrix_layout = 0; /*RowMajor*/
-	const bool matrix_is_transposed = false; 
-	const uint matrix_stride = 64;
-
-	__builtin_MatVecMul(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride);
-
-	const uint bias_offset = 0;
-	const uint bias_interpretation = 9; /*F32*/
-
-	__builtin_MatVecMulAdd(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride, bias_buffer, bias_offset,
-	  bias_interpretation);
-
-	vector<uint, 8> input_vector1;
-	vector<uint, 8> input_vector2;
-	const uint opa_matrix_offset = 0;
-	const uint opa_matrix_interpretation = 5; /*U32*/
-	const uint opa_matrix_layout = 3; /*OuterProductOptimal*/
-	const uint opa_matrix_stride = 64;
-
-	__builtin_OuterProductAccumulate(input_vector1, input_vector2,
-	  rw_matrix_buffer, opa_matrix_offset, opa_matrix_interpretation,
-	  opa_matrix_layout, opa_matrix_stride);
-
-	const uint va_matrix_offset = 0;
-
-	 __builtin_VectorAccumulate(input_vector1, rw_matrix_buffer,
-	   va_matrix_offset); 
+	UseCoopVec();
 }
 
 // CHECK: define void @vs_main()
@@ -123,51 +84,9 @@ void cs_main()
 
 [Shader("vertex")]
 void vs_main()
-{	
-	vector<float, 4> output_vector;
-	static const uint is_output_unsigned = 0;
-	
-	vector<float, 4> input_vector;
-	const uint is_input_unsigned = 0;
-	const uint input_interpretation = 9; /*F32*/
-	
-	const uint matrix_offset = 0;
-	const uint matrix_interpretation = 9; /*F32*/
-	const uint matrix_dimM = 4;
-	const uint matrix_dimK = 4;
-	const uint matrix_layout = 0; /*RowMajor*/
-	const bool matrix_is_transposed = false; 
-	const uint matrix_stride = 64;
-
-	__builtin_MatVecMul(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride);
-
-	const uint bias_offset = 0;
-	const uint bias_interpretation = 9; /*F32*/
-
-	__builtin_MatVecMulAdd(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride, bias_buffer, bias_offset,
-	  bias_interpretation);
-
-	vector<uint, 8> input_vector1;
-	vector<uint, 8> input_vector2;
-	const uint opa_matrix_offset = 0;
-	const uint opa_matrix_interpretation = 5; /*U32*/
-	const uint opa_matrix_layout = 3; /*OuterProductOptimal*/
-	const uint opa_matrix_stride = 64;
-
-	__builtin_OuterProductAccumulate(input_vector1, input_vector2,
-	  rw_matrix_buffer, opa_matrix_offset, opa_matrix_interpretation,
-	  opa_matrix_layout, opa_matrix_stride);
-
-	const uint va_matrix_offset = 0;
-
-	 __builtin_VectorAccumulate(input_vector1, rw_matrix_buffer,
-	   va_matrix_offset); }
+{
+	UseCoopVec();
+}
 
 struct MyRecord{
 	uint a;
@@ -183,50 +102,7 @@ struct MyRecord{
 [NodeLaunch("thread")]
 void ns_main(ThreadNodeInputRecord<MyRecord> input)
 {	
-	vector<float, 4> output_vector;
-	static const uint is_output_unsigned = 0;
-	
-	vector<float, 4> input_vector;
-	const uint is_input_unsigned = 0;
-	const uint input_interpretation = 9; /*F32*/
-	
-	const uint matrix_offset = 0;
-	const uint matrix_interpretation = 9; /*F32*/
-	const uint matrix_dimM = 4;
-	const uint matrix_dimK = 4;
-	const uint matrix_layout = 0; /*RowMajor*/
-	const bool matrix_is_transposed = false; 
-	const uint matrix_stride = 64;
-
-	__builtin_MatVecMul(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride);
-
-	const uint bias_offset = 0;
-	const uint bias_interpretation = 9; /*F32*/
-
-	__builtin_MatVecMulAdd(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride, bias_buffer, bias_offset,
-	  bias_interpretation);
-
-	vector<uint, 8> input_vector1;
-	vector<uint, 8> input_vector2;
-	const uint opa_matrix_offset = 0;
-	const uint opa_matrix_interpretation = 5; /*U32*/
-	const uint opa_matrix_layout = 3; /*OuterProductOptimal*/
-	const uint opa_matrix_stride = 64;
-
-	__builtin_OuterProductAccumulate(input_vector1, input_vector2,
-	  rw_matrix_buffer, opa_matrix_offset, opa_matrix_interpretation,
-	  opa_matrix_layout, opa_matrix_stride);
-
-	const uint va_matrix_offset = 0;
-
-	 __builtin_VectorAccumulate(input_vector1, rw_matrix_buffer,
-	   va_matrix_offset);
+	UseCoopVec();
 }
 
 // Vertex shader output structure
@@ -250,49 +126,6 @@ struct GS_OUT {
 [maxvertexcount(3)]
 void gs_main(point VS_OUT input[1], 
 	inout TriangleStream<GS_OUT> OutputStream)
-{	
-	vector<float, 4> output_vector;
-	static const uint is_output_unsigned = 0;
-	
-	vector<float, 4> input_vector;
-	const uint is_input_unsigned = 0;
-	const uint input_interpretation = 9; /*F32*/
-	
-	const uint matrix_offset = 0;
-	const uint matrix_interpretation = 9; /*F32*/
-	const uint matrix_dimM = 4;
-	const uint matrix_dimK = 4;
-	const uint matrix_layout = 0; /*RowMajor*/
-	const bool matrix_is_transposed = false; 
-	const uint matrix_stride = 64;
-
-	__builtin_MatVecMul(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride);
-
-	const uint bias_offset = 0;
-	const uint bias_interpretation = 9; /*F32*/
-
-	__builtin_MatVecMulAdd(output_vector, is_output_unsigned, input_vector,
-	  is_input_unsigned, input_interpretation, matrix_buffer, matrix_offset,
-	  matrix_interpretation, matrix_dimM, matrix_dimK, matrix_layout,
-	  matrix_is_transposed, matrix_stride, bias_buffer, bias_offset,
-	  bias_interpretation);
-
-	vector<uint, 8> input_vector1;
-	vector<uint, 8> input_vector2;
-	const uint opa_matrix_offset = 0;
-	const uint opa_matrix_interpretation = 5; /*U32*/
-	const uint opa_matrix_layout = 3; /*OuterProductOptimal*/
-	const uint opa_matrix_stride = 64;
-
-	__builtin_OuterProductAccumulate(input_vector1, input_vector2,
-	  rw_matrix_buffer, opa_matrix_offset, opa_matrix_interpretation,
-	  opa_matrix_layout, opa_matrix_stride);
-
-	const uint va_matrix_offset = 0;
-
-	 __builtin_VectorAccumulate(input_vector1, rw_matrix_buffer,
-	   va_matrix_offset); 
+{
+    UseCoopVec();
 }
