5130f0a8f969b29bdd562c871669de73ffbce3a2
Joshua Batista <jbatista@microsoft.com>
2025-04-14 11:37:37 -0700

    add missing tests

---
+++ tools/clang/test/CodeGenDXIL/hlsl/linalg/mat-vec-mul-transpose-errors.hlsl
@@ -0,0 +1,31 @@
+// RUN: %dxc -T lib_6_9 -enable-16bit-types %s | FileCheck %s
+
+#include "linalg.h"
+
+ByteAddressBuffer Buf;
+
+export float4 Test1(vector<float, 4> Input) {
+  using namespace dx::linalg;
+
+  MatrixRef<DATA_TYPE_FLOAT16, 4, 4, MATRIX_LAYOUT_ROW_MAJOR, true> Matrix = {
+      Buf, 0, 0};
+
+  // clang-format off
+  // CHECK: error: something about transposing not supported for rowmajor / colmajor layouts
+  return Mul<float>(    
+      Matrix, MakeInterpretedVector<DATA_TYPE_FLOAT16>(Input));
+  // clang-format on
+}
+
+export vector<float, 8> Test2(vector<uint8_t4_packed, 6> Input) {
+  using namespace dx::linalg;
+
+  MatrixRef<DATA_TYPE_UINT8, 8, 6 * 4, MATRIX_LAYOUT_COLUMN_MAJOR> Matrix = {
+      Buf, 0, 0};
+
+  // clang-format off
+  // CHECK: error: something about transposing not supported for rowmajor / colmajor layouts
+  // clang-format on
+  return Mul<float>(Matrix,
+                    MakeInterpretedVector<DATA_TYPE_UINT8_T4_PACKED>(Input));
+}
--- tools/clang/test/CodeGenDXIL/hlsl/linalg/outerproductaccumulate-errors.hlsl
+++ tools/clang/test/CodeGenDXIL/hlsl/linalg/outerproductaccumulate-errors.hlsl
@@ -18,3 +18,35 @@ export void Test4(vector<half, 128> Input1, vector<half, 64> Input2) {
 
   OuterProductAccumulate(Input1, Input2, matrix);  
 }
+
+// now test for an error when element types differ
+export void Test4(vector<int, 128> Input1, vector<uint, 128> Input2) {
+  using namespace dx::linalg;
+
+  RWMatrixRef<DATA_TYPE_FLOAT16, 128, 128, MATRIX_LAYOUT_OUTER_PRODUCT_OPTIMAL, true>
+      matrix = {RWBuf, 0, 0};
+
+  // clang-format off
+  // CHECK: error: no matching function for call to 'OuterProductAccumulate'
+  // CHECK: note: candidate template ignored: could not match 0 against 1
+  // __builtin_OuterProductAccumulate(Input1, Input2, RWBuf, 0, DATA_TYPE_FLOAT16, MATRIX_LAYOUT_OUTER_PRODUCT_OPTIMAL, 0);
+  // clang-format on
+
+  OuterProductAccumulate(Input1, Input2, matrix);  
+}
+
+// now test for an error when matrix transpose parameter is false
+export void Test4(vector<int, 128> Input1, vector<uint, 128> Input2) {
+  using namespace dx::linalg;
+
+  RWMatrixRef<DATA_TYPE_FLOAT16, 128, 128, MATRIX_LAYOUT_OUTER_PRODUCT_OPTIMAL, false>
+      matrix = {RWBuf, 0, 0};
+
+  // clang-format off
+  // CHECK: error: no matching function for call to 'OuterProductAccumulate'
+  // CHECK: note: candidate template ignored: could not match 0 against 1
+  // __builtin_OuterProductAccumulate(Input1, Input2, RWBuf, 0, DATA_TYPE_FLOAT16, MATRIX_LAYOUT_OUTER_PRODUCT_OPTIMAL, 0);
+  // clang-format on
+
+  OuterProductAccumulate(Input1, Input2, matrix);  
+}
