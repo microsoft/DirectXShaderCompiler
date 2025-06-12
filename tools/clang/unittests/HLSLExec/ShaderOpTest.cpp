23d1ac77b4de9b62bc7d5f3465db368d9a6de9c1
Alex Sepkowski <5620315+alsepkow@users.noreply.github.com>
2025-06-11 17:31:08 -0700

    A little more cleanup

--- tools/clang/unittests/HLSLExec/ShaderOpTest.cpp
+++ tools/clang/unittests/HLSLExec/ShaderOpTest.cpp
@@ -86,9 +86,6 @@ static void ShaderOpLogFmt(const wchar_t *fmt, ...) {
 
 // Check the specified HRESULT and return the success value.
 static HRESULT CHECK_HR_RET(HRESULT hr) {
-  if (FAILED(hr)) {
-    DebugBreak();
-  }
   CHECK_HR(hr);
   return hr;
 }
