bf65be5cea5c4499c62f5e1cb896c0f5d2592030
Peter McNeeley <petermcneeley@google.com>
2025-07-03 11:15:26 -0400

    Minor fix for signedness of asuint bitcast

--- tools/clang/lib/AST/ExprConstant.cpp
+++ tools/clang/lib/AST/ExprConstant.cpp
@@ -3784,6 +3784,9 @@ static bool HandleIntrinsicCall(SourceLocation CallLoc, unsigned opcode,
     assert(Args.size() == 1 && "else call should be invalid");
     if (ArgValues[0].isInt()) {
       Result = ArgValues[0];
+      // The result of bitwise cast to uint will be unsigned.
+      const bool isUnsignedTrue = true;
+      Result.getInt().setIsSigned(!isUnsignedTrue);
     }
     else if (ArgValues[0].isFloat()) {
       const bool isUnsignedTrue = true;
