140cf246ca48ff7ed06fc62a55e48a72d5e4698a
Steven Perron <stevenperron@google.com>
2025-07-08 09:08:20 -0400

    Add test that fails validation

--- tools/clang/test/CodeGenSPIRV/vertex_shader_derivative_in_branch.hlsl
+++ tools/clang/test/CodeGenSPIRV/vertex_shader_derivative_in_branch.hlsl
@@ -1,7 +1,10 @@
-// RUN: %dxc -T vs_6_0 -E main -fspv-target-env=vulkan1.3 %s -spirv | FileCheck %s
-
+// RUN: %dxc -T vs_6_0 -E main -DCOND=false -fspv-target-env=vulkan1.3 %s -spirv | FileCheck %s
 // CHECK-NOT: OpCapability DerivativeControl
 // CHECK-NOT: OpExtension "SPV_KHR_compute_shader_derivatives"
+
+// RUN: not %dxc -T vs_6_0 -E main -DCOND=true -fspv-target-env=vulkan1.3 %s -spirv 2>&1 | FileCheck %s -check-prefix=ERROR
+// ERROR: generated SPIR-V is invalid:
+// ERROR-NEXT: Derivative instructions require Fragment, GLCompute, MeshEXT or TaskEXT execution model: DPdx
 
 struct VSOut
 {
@@ -12,7 +15,7 @@ VSOut main(float4 pos : POSITION)
 {
     VSOut output;
     output.pos = pos;
-    if (false)
+    if (COND)
     {
         output.pos += ddx(pos);
     }
