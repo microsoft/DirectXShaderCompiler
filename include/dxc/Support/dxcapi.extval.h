c8d867eed868b9aaec251013c95634c098a6927c
Joshua Batista <jbatista@microsoft.com>
2025-08-06 21:04:51 -0700

    add more overrides

--- include/dxc/Support/dxcapi.extval.h
+++ include/dxc/Support/dxcapi.extval.h
@@ -19,9 +19,10 @@ public:
     return !DxilDllPath.empty() && !DxilExtValSupport.IsEnabled();
   }
 
-  HRESULT CreateInstanceImpl(REFCLSID clsid, REFIID riid, IUnknown **pResult);
+  HRESULT CreateInstanceImpl(REFCLSID clsid, REFIID riid,
+                             IUnknown **pResult) override;
   HRESULT CreateInstance2Impl(IMalloc *pMalloc, REFCLSID clsid, REFIID riid,
-                              IUnknown **pResult);
+                              IUnknown **pResult) override;
 
   HRESULT Initialize() { return InitializeInternal("DxcCreateInstance"); }
 
@@ -29,24 +30,24 @@ public:
   to satisfy the IDllLoader interface. The parameter is ignored
   because the relevant dlls are specific and known: dxcompiler.dll
   and dxil.dll. This class is not designed to handle any other dlls */
-  HRESULT OverrideDll(LPCSTR dll, LPCSTR entryPoint) {
+  HRESULT OverrideDll(LPCSTR dll, LPCSTR entryPoint) override {
     return InitializeInternal(entryPoint);
   }
 
-  bool HasCreateWithMalloc() const {
+  bool HasCreateWithMalloc() const override {
     assert(DxCompilerSupport.HasCreateWithMalloc() &&
            DxilExtValSupport.HasCreateWithMalloc());
     return true;
   }
 
-  bool IsEnabled() const { return DxCompilerSupport.IsEnabled(); }
+  bool IsEnabled() const override { return DxCompilerSupport.IsEnabled(); }
 
   void Cleanup() override {
     DxilExtValSupport.Cleanup();
     DxCompilerSupport.Cleanup();
   }
 
-  HMODULE Detach() {
+  HMODULE Detach() override {
     // Can't Detach and return a handle for DxilSupport. Cleanup() instead.
     DxilExtValSupport.Cleanup();
     return DxCompilerSupport.Detach();
