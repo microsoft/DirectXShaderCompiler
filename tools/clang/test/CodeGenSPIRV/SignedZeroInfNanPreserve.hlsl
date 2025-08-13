f33ccd3be16938f4f4e5e0daf54f29e97a66c523
Chris B <cbieneman@microsoft.com>
2025-08-13 15:00:20 -0500

    Apply suggestions from code review
    Co-authored-by: Nathan Gauër <github@keenuts.net>

--- tools/clang/test/CodeGenSPIRV/SignedZeroInfNanPreserve.hlsl
+++ tools/clang/test/CodeGenSPIRV/SignedZeroInfNanPreserve.hlsl
@@ -1,8 +1,8 @@
-// RUN: %dxc -T cs_6_0 -spirv -Gis %s| FileCheck %s --check-prefixes=CHECK,PRE_1_3
+// RUN: %dxc -T cs_6_0 -spirv -Gis %s| FileCheck %s --check-prefixes=CHECK,PRE_1_2
 // RUN: %dxc -T cs_6_0 -spirv -Gis -fspv-target-env=vulkan1.2 %s| FileCheck %s 
 
 // CHECK: OpCapability SignedZeroInfNanPreserve
-// PRE_1_3: OpExtension "SPV_KHR_float_controls"
+// PRE_1_2: OpExtension "SPV_KHR_float_controls"
 // CHECK: OpEntryPoint
 // CHECK: OpExecutionMode %main SignedZeroInfNanPreserve 32
 
