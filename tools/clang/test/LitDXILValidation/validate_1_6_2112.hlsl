242679735d0a3cb8bf61e9073775245a75bcf868
Joshua Batista <jbatista@microsoft.com>
2025-09-12 17:31:10 -0700

    rename test file

--- tools/clang/test/LitDXILValidation/validate_1_6_2112.hlsl
+++ tools/clang/test/LitDXILValidation/validate_1_6_2112.hlsl
@@ -5,6 +5,9 @@
 // be accepted in newer validators
 
 // RUN: %dxc -T lib_6_9 %s | FileCheck %s 
+// CHECK: error: validation errors
+// CHECK: error: RWStructuredBuffers may increment or decrement their counters, but not both.
+// CHECK: note: at '%3 = call i32 @dx.op.bufferUpdateCounter(i32 70, %dx.types.Handle %1, i8 1)' in block '#0' of function 'main'.
 
 RWStructuredBuffer<uint> UAV         : register(u0);
 
