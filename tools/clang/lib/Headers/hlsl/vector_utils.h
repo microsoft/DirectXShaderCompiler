2b3a364a5a6b26aeaed36e3b89f8cacfdde7aace
Ashley Coleman <ascoleman@microsoft.com>
2025-09-30 15:57:03 -0600

    comments

--- tools/clang/lib/Headers/hlsl/vector_utils.h
+++ tools/clang/lib/Headers/hlsl/vector_utils.h
@@ -4,22 +4,12 @@
      (__SHADER_TARGET_MAJOR == 6 && __SHADER_TARGET_MINOR >= 9)) &&            \
     (__HLSL_VERSION >= 2021)
 
-namespace hlsl {
-
-// TODO: Make a dedicated file for this
-template <bool B, typename T> struct enable_if {};
-
-template <typename T> struct enable_if<true, T> {
-  using type = T;
-};
-
-} // namespace hlsl
+#include <enable_if.h>
 
 namespace hlsl {
-namespace vec {
 
 template <int Off, int OSz, typename T, int ISz>
-typename hlsl::enable_if<Off + OSz <= ISz, vector<T, OSz>>::type
+typename hlsl::enable_if<Off + OSz <= ISz, vector<T, OSz> >::type
 slice(vector<T, ISz> In) {
   vector<T, OSz> Result = (vector<T, OSz>)0;
   [unroll] for (int I = 0; I < OSz; ++I) Result[I] = In[I + Off];
@@ -31,7 +21,6 @@ vector<T, OSz> slice(vector<T, ISz> In) {
   return slice<0, OSz, T, ISz>(In);
 }
 
-} // namespace vec
 } // namespace hlsl
 
-#endif // SM 6.9 check and HV version check
+#endif
