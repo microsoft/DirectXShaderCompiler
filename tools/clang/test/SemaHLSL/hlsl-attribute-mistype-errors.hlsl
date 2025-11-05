f835f7b6938d6a221c1e86548ac2a25772611687
Nielsbishere <n@osomi.net>
2025-11-05 21:20:19 +0100

    Changed packofset to packoffsets and added one where it also uses invalid packoffset syntax (same test as with uav2)

--- tools/clang/test/SemaHLSL/hlsl-attribute-mistype-errors.hlsl
+++ tools/clang/test/SemaHLSL/hlsl-attribute-mistype-errors.hlsl
@@ -14,14 +14,17 @@ RWStructuredBuffer<float4> uav4 : UNDEFINED_MACRO(something, more, complex);
 
 cbuffer buf {
 
-  // expected-error@+1 {{Syntax indicated an hlsl attribute (: packoffset() or : register()), but unexpected attribute 'packofset' was used instead.}}
-  float4 v0 : packofset(c0);
+  // expected-error@+1 {{Syntax indicated an hlsl attribute (: packoffset() or : register()), but unexpected attribute 'packoffsets' was used instead.}}
+  float4 v0 : packoffsets(c0);
+
+  // expected-error@+1 {{Syntax indicated an hlsl attribute (: packoffset() or : register()), but unexpected attribute 'packoffsets' was used instead.}}
+  float4 v1 : packoffsets(invalid_syntax);
 
   // expected-error@+1 {{Syntax indicated an hlsl attribute (: packoffset() or : register()), but unexpected attribute 'UNDEFINED_MACRO2' was used instead.}}
-  float v1 : UNDEFINED_MACRO2(c0.w);
+  float v2 : UNDEFINED_MACRO2(c0.w);
 
   // expected-error@+1 {{Syntax indicated an hlsl attribute (: packoffset() or : register()), but unexpected attribute 'UNDEFINED_MACRO' was used instead.}}
-  float v2 : UNDEFINED_MACRO(something, more, complex);
+  float v3 : UNDEFINED_MACRO(something, more, complex);
 };
 
 [shader("pixel")]
@@ -29,7 +32,7 @@ float4 main(): SV_Target
 {
   uav1[0] = v0;
   uav2[0] = v1;
-  uav3[0] = 2;
-  uav4[0] = 2;
+  uav3[0] = v2;
+  uav4[0] = v3;
   return 0.xxxx;
 }
