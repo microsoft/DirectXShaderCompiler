f51add71e5f50ccfee923ee73f45b25bdb885012
Joao Saffran <jderezende@microsoft.com>
2025-11-25 15:39:22 -0800

    addressing comments from damyan

--- tools/clang/unittests/HLSLExec/LongVectors.cpp
+++ tools/clang/unittests/HLSLExec/LongVectors.cpp
@@ -1559,10 +1559,10 @@ template <typename T> struct ExpectedBuilder<OpType::WaveMatch, T> {
     const UINT HighWaves = WaveSize - LowWaves;
 
     const uint64_t LowWaveMask =
-        (LowWaves < 64) ? (1ULL << LowWaves) - 1U : ~0ULL;
+        (LowWaves < 64) ? (1ULL << LowWaves) - 1 : ~0ULL;
 
     const uint64_t HighWaveMask =
-        (HighWaves < 64) ? (1ULL << HighWaves) - 1U : ~0ULL;
+        (HighWaves < 64) ? (1ULL << HighWaves) - 1 : ~0ULL;
 
     const uint64_t LowExpected = ~1ULL & LowWaveMask;
     const uint64_t HighExpected = ~0ULL & HighWaveMask;
@@ -1575,7 +1575,7 @@ template <typename T> struct ExpectedBuilder<OpType::WaveMatch, T> {
     // all lanes other than the first one have the same result
     for (UINT I = 1; I < WaveSize; I++) {
       const UINT Index = I * 4;
-      Expected[Index] = static_cast<UINT>(LowExpected);
+      Expected[Index]     = static_cast<UINT>(LowExpected);
       Expected[Index + 1] = static_cast<UINT>(LowExpected >> 32);
       Expected[Index + 2] = static_cast<UINT>(HighExpected);
       Expected[Index + 3] = static_cast<UINT>(HighExpected >> 32);
