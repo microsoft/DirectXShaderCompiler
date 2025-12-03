1e4089072b2a853158dfe079c6ba64ab723d44c5
João Saffran <joaosaffranllvm@gmail.com>
2025-12-02 18:54:35 -0800

    adding NATIVE_16BIT_ENABLED flag

--- tools/clang/unittests/HLSLExec/LongVectors.cpp
+++ tools/clang/unittests/HLSLExec/LongVectors.cpp
@@ -321,8 +321,10 @@ std::string getCompilerOptionsString(
     std::optional<std::string> AdditionalOptions = std::nullopt) {
   std::stringstream CompilerOptions;
 
-  if (OpDataType.Is16Bit || OutDataType.Is16Bit)
+  if (OpDataType.Is16Bit || OutDataType.Is16Bit){
     CompilerOptions << " -enable-16bit-types";
+    CompilerOptions << " -DNATIVE_16BIT_ENABLED=1";
+  }
 
   CompilerOptions << " -DTYPE=" << OpDataType.HLSLTypeString;
   CompilerOptions << " -DNUM=" << VectorSize;
