631c859c1015b995c4459080d661283da90ebd68
Nathan Gauër <brioche@google.com>
2025-12-03 15:14:36 +0100

    [SPIR-V] Normalize inline SPIR-V for builtins
    Inline SPIR-V provides mutliple attributes with overlapping
    capabilities:
     - vk::builtin
     - vk::ext_input_builtin
     - vk::ext_output_builtin
     - vk::storage_class
     - vk::decorate
    
    The issue is that what should be syntactic sugar was not implemented as
    such: ext_input_builtin should be sugar for decorate + storage_class.
    This meant codegen has different paths depending on the attributes used,
    even if they should in the end have the same effect.
    As expected, those paths have different behaviors, and some are buggy.
    
    This commits adds a new step in sema: attribute normalization.
    The target idea is to `desugar` some attributes into a more basic
    format, but it requires large changes in CG. As of now, this commits
    normalizes a bit the attributes in sema, but might lift
    decoration+storage into ext_input_builtin.
    
    Properly doing de-sugar will require additional changes, especially
    around parameters & builtin through decorations which works only because
    codegen checks are not complete.

--- tools/clang/lib/SPIRV/DeclResultIdMapper.cpp
+++ tools/clang/lib/SPIRV/DeclResultIdMapper.cpp
@@ -1298,6 +1298,10 @@ SpirvVariable *DeclResultIdMapper::createExternVar(const VarDecl *var,
     return varInstr;
 
   const auto *bindingAttr = var->getAttr<VKBindingAttr>();
+
+  if (var->hasAttr<VKStorageClassExtAttr>() && !bindingAttr)
+    return varInstr;
+
   resourceVars.emplace_back(varInstr, var, loc, getResourceBinding(var),
                             bindingAttr, var->getAttr<VKCounterBindingAttr>());
 
