838b7fa418a14647bde6e90e8c62257cfb686baf
Nathan Gauër <brioche@google.com>
2025-12-05 15:10:41 +0100

    remove vk::binding from test

--- tools/clang/test/CodeGenSPIRV/inline-spirv/spv.raytracing.hlsl
+++ tools/clang/test/CodeGenSPIRV/inline-spirv/spv.raytracing.hlsl
@@ -3,9 +3,6 @@
 // CHECK-DAG: OpCapability RuntimeDescriptorArray
 // CHECK-DAG: OpCapability RayQueryKHR
 
-// CHECK-DAG: OpDecorate %MyScene DescriptorSet 1
-// CHECK-DAG: OpDecorate %MyScene Binding 3
-
 using A = vk::SpirvOpaqueType</* OpTypeAccelerationStructureKHR */ 5341>;
 // CHECK: %[[name:[^ ]+]] = OpTypeAccelerationStructureKHR
 
@@ -14,7 +11,6 @@ using RA [[vk::ext_capability(/* RuntimeDescriptorArray */ 5302)]] = vk::SpirvOp
 
 // CHECK: %[[ptr:[^ ]+]] = OpTypePointer UniformConstant %[[rarr]]
 // CHECK: %MyScene = OpVariable %[[ptr]] UniformConstant
-[[vk::binding(3, 1)]]
 [[vk::ext_storage_class(0)]]
 RA MyScene;
 
