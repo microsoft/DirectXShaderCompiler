8f5f6d3e86a1c0ac67abd3c2d9427f37bf92a9f1
Mike Apodaca <michaela@nvidia.com>
2025-12-08 16:28:58 -0800

    Update aligned_load_types.hlsl

--- tools/clang/test/HLSLFileCheck/hlsl/objects/ByteAddressBuffer/aligned_load_types.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/objects/ByteAddressBuffer/aligned_load_types.hlsl
@@ -1,48 +1,159 @@
-// RUN: %dxc -T cs_6_2 -enable-16bit-types -E main %s | FileCheck %s
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float -DALIGN=4 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT32_A4_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float -DALIGN=16 %s | FileCheck %s -check-prefix=CHK_FLOAT32_A16_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float -DALIGN=8 -DSRCRW %s | FileCheck %s -check-prefix=CHK_FLOAT32_A8_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float -DALIGN=32 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT32_A32_RW
 
-// Test AlignedLoad with different types including 16-bit and 64-bit
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float4 -DALIGN=4 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT32x4_A4_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float4 -DALIGN=16 %s | FileCheck %s -check-prefix=CHK_FLOAT32x4_A16_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float4 -DALIGN=8 -DSRCRW %s | FileCheck %s -check-prefix=CHK_FLOAT32x4_A8_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float4 -DALIGN=64 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT32x4_A64_RW
 
-ByteAddressBuffer buf : register(t0);
-RWByteAddressBuffer rwbuf : register(u0);
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float16_t -DALIGN=2 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT16_A2_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float16_t -DALIGN=8 %s | FileCheck %s -check-prefix=CHK_FLOAT16_A8_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float16_t -DALIGN=4 -DSRCRW %s | FileCheck %s -check-prefix=CHK_FLOAT16_A4_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=float16_t -DALIGN=16 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT16_A16_RW
+
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=double -DALIGN=8 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT64_A8_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=double -DALIGN=32 %s | FileCheck %s -check-prefix=CHK_FLOAT64_A32_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=double -DALIGN=16 -DSRCRW %s | FileCheck %s -check-prefix=CHK_FLOAT64_A16_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=double -DALIGN=64 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_FLOAT64_A64_RW
+
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint -DALIGN=4 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT32_A4_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint -DALIGN=16 %s | FileCheck %s -check-prefix=CHK_UINT32_A16_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint -DALIGN=8 -DSRCRW %s | FileCheck %s -check-prefix=CHK_UINT32_A8_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint -DALIGN=32 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT32_A32_RW
+
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint3 -DALIGN=4 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT32x3_A4_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint3 -DALIGN=16 %s | FileCheck %s -check-prefix=CHK_UINT32x3_A16_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint3 -DALIGN=8 -DSRCRW %s | FileCheck %s -check-prefix=CHK_UINT32x3_A8_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint3 -DALIGN=32 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT32x3_A32_RW
+
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint16_t -DALIGN=2 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT16_A2_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint16_t -DALIGN=8 %s | FileCheck %s -check-prefix=CHK_UINT16_A8_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint16_t -DALIGN=4 -DSRCRW %s | FileCheck %s -check-prefix=CHK_UINT16_A4_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=uint16_t -DALIGN=16 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_UINT16_A16_RW
+
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=int64_t -DALIGN=8 -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_INT64_A8_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=int64_t -DALIGN=32 %s | FileCheck %s -check-prefix=CHK_INT64_A32_RO
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=int64_t -DALIGN=16 -DSRCRW %s | FileCheck %s -check-prefix=CHK_INT64_A16_RW
+// RUN: %dxc -E main -T cs_6_2 -enable-16bit-types -DTY=int64_t -DALIGN=64 -DSRCRW -DCHKSTATUS %s | FileCheck %s -check-prefix=CHK_INT64_A64_RW
+
+
+// CHK_FLOAT32_A4_RO: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 4)
+// CHK_FLOAT32_A4_RO: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float undef, float undef, float undef, i8 1, i32 4)
+
+// CHK_FLOAT32_A16_RO: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 16)
+// CHK_FLOAT32_A16_RO: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float undef, float undef, float undef, i8 1, i32 16)
+
+// CHK_FLOAT32_A8_RW: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 8)
+// CHK_FLOAT32_A8_RW: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float undef, float undef, float undef, i8 1, i32 8)
+
+// CHK_FLOAT32_A32_RW: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 32)
+// CHK_FLOAT32_A32_RW: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float undef, float undef, float undef, i8 1, i32 32)
+
+// CHK_FLOAT32x4_A4_RO: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 15, i32 4)
+// CHK_FLOAT32x4_A4_RO: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float %{{.*}}, float %{{.*}}, float %{{.*}}, i8 15, i32 4)
+
+// CHK_FLOAT32x4_A16_RO: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 15, i32 16)
+// CHK_FLOAT32x4_A16_RO: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float %{{.*}}, float %{{.*}}, float %{{.*}}, i8 15, i32 16)
+
+// CHK_FLOAT32x4_A8_RW: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 15, i32 8)
+// CHK_FLOAT32x4_A8_RW: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float %{{.*}}, float %{{.*}}, float %{{.*}}, i8 15, i32 8)
+
+// CHK_FLOAT32x4_A64_RW: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 15, i32 64)
+// CHK_FLOAT32x4_A64_RW: call void @dx.op.rawBufferStore.f32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, float %{{.*}}, float %{{.*}}, float %{{.*}}, float %{{.*}}, i8 15, i32 64)
+
+// CHK_FLOAT16_A2_RO: call %dx.types.ResRet.f16 @dx.op.rawBufferLoad.f16(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 2)
+// CHK_FLOAT16_A2_RO: call void @dx.op.rawBufferStore.f16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, half %{{.*}}, half undef, half undef, half undef, i8 1, i32 2)
+
+// CHK_FLOAT16_A8_RO: call %dx.types.ResRet.f16 @dx.op.rawBufferLoad.f16(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 8)
+// CHK_FLOAT16_A8_RO: call void @dx.op.rawBufferStore.f16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, half %{{.*}}, half undef, half undef, half undef, i8 1, i32 8)
+
+// CHK_FLOAT16_A4_RW: call %dx.types.ResRet.f16 @dx.op.rawBufferLoad.f16(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 4)
+// CHK_FLOAT16_A4_RW: call void @dx.op.rawBufferStore.f16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, half %{{.*}}, half undef, half undef, half undef, i8 1, i32 4)
+
+// CHK_FLOAT16_A16_RW: call %dx.types.ResRet.f16 @dx.op.rawBufferLoad.f16(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 16)
+// CHK_FLOAT16_A16_RW: call void @dx.op.rawBufferStore.f16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, half %{{.*}}, half undef, half undef, half undef, i8 1, i32 16)
+
+// CHK_FLOAT64_A8_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 3, i32 8)
+// CHK_FLOAT64_A8_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 8)
+
+// CHK_FLOAT64_A32_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 3, i32 32)
+// CHK_FLOAT64_A32_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 32)
+
+// CHK_FLOAT64_A16_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 3, i32 16)
+// CHK_FLOAT64_A16_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 16)
+
+// CHK_FLOAT64_A64_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 3, i32 64)
+// CHK_FLOAT64_A64_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 64)
+
+// CHK_UINT32_A4_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 4)
+// CHK_UINT32_A4_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 undef, i32 undef, i32 undef, i8 1, i32 4)
+
+// CHK_UINT32_A16_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 16)
+// CHK_UINT32_A16_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 undef, i32 undef, i32 undef, i8 1, i32 16)
+
+// CHK_UINT32_A8_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 8)
+// CHK_UINT32_A8_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 undef, i32 undef, i32 undef, i8 1, i32 8)
+
+// CHK_UINT32_A32_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 32)
+// CHK_UINT32_A32_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 undef, i32 undef, i32 undef, i8 1, i32 32)
+
+// CHK_UINT32x3_A4_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 7, i32 4)
+// CHK_UINT32x3_A4_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i8 7, i32 4)
+
+// CHK_UINT32x3_A16_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 7, i32 16)
+// CHK_UINT32x3_A16_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i8 7, i32 16)
+
+// CHK_UINT32x3_A8_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 7, i32 8)
+// CHK_UINT32x3_A8_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i8 7, i32 8)
+
+// CHK_UINT32x3_A32_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 7, i32 32)
+// CHK_UINT32x3_A32_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i8 7, i32 32)
+
+// CHK_UINT16_A2_RO: call %dx.types.ResRet.i16 @dx.op.rawBufferLoad.i16(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 2)
+// CHK_UINT16_A2_RO: call void @dx.op.rawBufferStore.i16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i16 %{{.*}}, i16 undef, i16 undef, i16 undef, i8 1, i32 2)
+
+// CHK_UINT16_A8_RO: call %dx.types.ResRet.i16 @dx.op.rawBufferLoad.i16(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 1, i32 8)
+// CHK_UINT16_A8_RO: call void @dx.op.rawBufferStore.i16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i16 %{{.*}}, i16 undef, i16 undef, i16 undef, i8 1, i32 8)
+
+// CHK_UINT16_A4_RW: call %dx.types.ResRet.i16 @dx.op.rawBufferLoad.i16(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 4)
+// CHK_UINT16_A4_RW: call void @dx.op.rawBufferStore.i16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i16 %{{.*}}, i16 undef, i16 undef, i16 undef, i8 1, i32 4)
+
+// CHK_UINT16_A16_RW: call %dx.types.ResRet.i16 @dx.op.rawBufferLoad.i16(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 1, i32 16)
+// CHK_UINT16_A16_RW: call void @dx.op.rawBufferStore.i16(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i16 %{{.*}}, i16 undef, i16 undef, i16 undef, i8 1, i32 16)
+
+// CHK_INT64_A8_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 3, i32 8)
+// CHK_INT64_A8_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 8)
+
+// CHK_INT64_A32_RO: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_texture_rawbuf, i32 %mul, i32 undef, i8 3, i32 32)
+// CHK_INT64_A32_RO: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 32)
+
+// CHK_INT64_A16_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 3, i32 16)
+// CHK_INT64_A16_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 16)
+
+// CHK_INT64_A64_RW: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %srcbuf_UAV_rawbuf, i32 %mul, i32 undef, i8 3, i32 64)
+// CHK_INT64_A64_RW: call void @dx.op.rawBufferStore.i32(i32 140, %dx.types.Handle %dstbuf_UAV_rawbuf, i32 %mul, i32 undef, i32 %{{.*}}, i32 %{{.*}}, i32 undef, i32 undef, i8 3, i32 64)
+
+
+#ifdef SRCRW
+RWByteAddressBuffer srcbuf : register(u0);
+RWByteAddressBuffer dstbuf : register(u1);
+#else
+ByteAddressBuffer   srcbuf : register(t0);
+RWByteAddressBuffer dstbuf : register(u0);
+#endif
 
 [numthreads(1, 1, 1)]
 void main(uint3 tid : SV_DispatchThreadID) {
-    uint offset = tid.x * 64;
-    
-    // 16-bit types
-    // CHECK: call %dx.types.ResRet.i16 @dx.op.rawBufferLoad.i16(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 1, i32 2)
-    uint16_t data1 = buf.AlignedLoad<uint16_t>(offset, 2);
-    rwbuf.AlignedStore<uint16_t>(offset, 2, data1);
-    
-    // CHECK: call %dx.types.ResRet.f16 @dx.op.rawBufferLoad.f16(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 15, i32 8)
-    half4 data2 = buf.AlignedLoad<half4>(offset, 8);
-    rwbuf.AlignedStore<half4> (offset, 8, data2);
-    
-    // 32-bit types
-    // CHECK: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 1, i32 4)
-    int data3 = buf.AlignedLoad<int>(offset, 4);
-    rwbuf.AlignedStore<int>(offset, 4, data3);
-    
-    // CHECK: call %dx.types.ResRet.f32 @dx.op.rawBufferLoad.f32(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 15, i32 16)
-    float4 data4 = buf.AlignedLoad<float4>(offset, 16);
-    rwbuf.AlignedStore<float4>(offset, 16, data4);
-    
-    // 64-bit types
-    // CHECK: call %dx.types.ResRet.i64 @dx.op.rawBufferLoad.i64(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 1, i32 8)
-    uint64_t data5 = buf.AlignedLoad<uint64_t>(offset, 8);
-    rwbuf.AlignedStore<uint64_t>(offset, 8, data5);
-    
-    // CHECK: call %dx.types.ResRet.f64 @dx.op.rawBufferLoad.f64(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 1, i32 8)
-    double data6 = buf.AlignedLoad<double>(offset, 8);
-    rwbuf.AlignedStore<double>(offset, 8, data6);
-    
-    // Mixed vector sizes
-    // CHECK: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 3, i32 8)
-    uint2 data7 = buf.AlignedLoad<uint2>(offset, 8);
-    rwbuf.AlignedStore<uint2> (offset, 8, data7);
-    
-    // CHECK: call %dx.types.ResRet.i32 @dx.op.rawBufferLoad.i32(i32 139, %dx.types.Handle %{{[0-9]+}}, i32 %{{[0-9]+}}, i32 undef, i8 7, i32 16)
-    uint3 data8 = buf.AlignedLoad<uint3>(offset, 16);
-    rwbuf.AlignedStore<uint3>(offset, 16, data8);
+    const uint offset = tid.x * ALIGN;
+#ifdef CHKSTATUS
+    uint status = 0;
+    TY data = srcbuf.AlignedLoad<TY>(offset, ALIGN, status);
+    if (!CheckAccessFullyMapped(status)) return;
+#else
+    TY data = srcbuf.AlignedLoad<TY>(offset, ALIGN);
+#endif
+    dstbuf.AlignedStore<TY>(offset, ALIGN, data);
 }
 
