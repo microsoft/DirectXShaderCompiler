173024c7c854e9b01f2150e2088adfb666cc9542
Jack Elliott <jackell@microsoft.com>
2026-02-03 09:48:11 +1300

    Add AST and FCGL checks

--- tools/clang/test/HLSLFileCheckLit/hlsl/intrinsics/basic/isdebuggerpresent.hlsl
+++ tools/clang/test/HLSLFileCheckLit/hlsl/intrinsics/basic/isdebuggerpresent.hlsl
@@ -1,8 +1,16 @@
 // REQUIRES: dxil-1-10
 
 // RUN: %dxc -T cs_6_10 %s | FileCheck %s
+// RUN: %dxc -T cs_6_10 -fcgl %s | FileCheck %s --check-prefix=FCGL
+// RUN: %dxc -T cs_6_10 -ast-dump %s | FileCheck %s --check-prefix=AST
 
 // CHECK: call i1 @dx.op.isDebuggerPresent(i32 -2147483614)  ; IsDebuggerPresent()
+
+// FCGL: call i1 @"dx.hl.op.ro.i1 (i32)"(i32 426)
+
+// AST: CallExpr {{.*}} 'bool'
+// AST-NEXT: `-ImplicitCastExpr {{.*}} 'bool (*)()' <FunctionToPointerDecay>
+// AST-NEXT:   `-DeclRefExpr {{.*}} 'IsDebuggerPresent' 'bool ()'
 
 RWStructuredBuffer<uint> Output : register(u0);
 
