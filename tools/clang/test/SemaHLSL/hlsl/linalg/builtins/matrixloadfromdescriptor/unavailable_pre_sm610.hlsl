ec3277d1e5137b830102d93614b4ee79e282dce6
Ashley Coleman <ascoleman@microsoft.com>
2026-02-05 13:57:57 -0700

    [SM6.10] Implement MatrixLoadFromDescriptor Builtin

---
+++ tools/clang/test/SemaHLSL/hlsl/linalg/builtins/matrixloadfromdescriptor/unavailable_pre_sm610.hlsl
@@ -0,0 +1,11 @@
+// RUN: %dxc -I %hlsl_headers -T cs_6_9 -E main %s -verify
+
+RWByteAddressBuffer inbuf;
+
+[numthreads(4,1,1)]
+void main() {
+  __builtin_LinAlgMatrix [[__LinAlgMatrix_Attributes(1, 5, 4, 0, 0)]] mat;
+
+  // expected-error@+1{{intrinsic __builtin_LinAlg_MatrixLoadFromDescriptor potentially used by ''main'' requires shader model 6.10 or greater}}
+  __builtin_LinAlg_MatrixLoadFromDescriptor(mat, inbuf, 1, 1, 1);
+}
