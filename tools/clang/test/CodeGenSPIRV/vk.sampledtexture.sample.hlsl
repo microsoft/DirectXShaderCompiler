a5a2386c0b41047dec7cd07c00fbb37d454b9971
luciechoi <ychoi0407@gmail.com>
2026-02-09 18:13:34 +0000

    Bring back uint test and increase shader module version

--- tools/clang/test/CodeGenSPIRV/vk.sampledtexture.sample.hlsl
+++ tools/clang/test/CodeGenSPIRV/vk.sampledtexture.sample.hlsl
@@ -1,4 +1,4 @@
-// RUN: %dxc -T ps_6_0 -E main -fcgl %s -spirv | FileCheck %s
+// RUN: %dxc -T ps_6_7 -E main -fcgl %s -spirv | FileCheck %s
 
 // CHECK: OpCapability MinLod
 // CHECK: OpCapability SparseResidency
@@ -10,13 +10,17 @@
 // CHECK: [[type_sampled_image_1:%[a-zA-Z0-9_]+]] = OpTypeSampledImage [[type_2d_image_1]]
 // CHECK: [[ptr_type_1:%[a-zA-Z0-9_]+]] = OpTypePointer UniformConstant [[type_sampled_image_1]]
 
+// CHECK: [[type_2d_image_2:%[a-zA-Z0-9_]+]] = OpTypeImage %uint 2D 0 0 0 1 Unknown
+// CHECK: [[type_sampled_image_2:%[a-zA-Z0-9_]+]] = OpTypeSampledImage [[type_2d_image_2]]
+// CHECK: [[ptr_type_2:%[a-zA-Z0-9_]+]] = OpTypePointer UniformConstant [[type_sampled_image_2]]
+
 // CHECK: [[tex1:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_1]] UniformConstant
 // CHECK: [[tex2:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_1]] UniformConstant
-// CHECK: [[tex3:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_1]] UniformConstant
+// CHECK: [[tex3:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_2]] UniformConstant
 
 vk::SampledTexture2D tex1 : register(t0);
 vk::SampledTexture2D<float4> tex2 : register(t1);
-vk::SampledTexture2D<float2> tex3 : register(t2);
+vk::SampledTexture2D<uint> tex3 : register(t2);
 
 float4 main() : SV_Target {
 // CHECK: [[tex1_load:%[a-zA-Z0-9_]+]] = OpLoad [[type_sampled_image_1]] [[tex1]]
@@ -38,9 +42,9 @@ float4 main() : SV_Target {
     uint status;
     float4 val4 = tex2.Sample(float2(0.5, 0.25), int2(2, 3), 1.0f, status);
 
-// CHECK: [[tex5_load:%[a-zA-Z0-9_]+]] = OpLoad [[type_sampled_image_1]] [[tex3]]
-// CHECK: [[sampled_result5:%[a-zA-Z0-9_]+]] = OpImageSampleImplicitLod %v4float [[tex5_load]] [[v2fc]] None
-// CHECK: [[val5:%[a-zA-Z0-9_]+]] = OpVectorShuffle %v2float [[sampled_result5]] [[sampled_result5]] 0 1
-    float2 val5 = tex3.Sample(float2(0.5, 0.25));
+// CHECK: [[tex5_load:%[a-zA-Z0-9_]+]] = OpLoad [[type_sampled_image_2]] [[tex3]]
+// CHECK: [[sampled_result5:%[a-zA-Z0-9_]+]] = OpImageSampleImplicitLod %v4uint [[tex5_load]] [[v2fc]] None
+// CHECK: [[val5:%[a-zA-Z0-9_]+]] = OpCompositeExtract %uint [[sampled_result5]] 0
+    uint val5 = tex3.Sample(float2(0.5, 0.25));
     return 1.0;
 }
