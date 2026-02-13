87fd1b834e8d19349263fc051d4ccb53a04b64eb
Ashley Coleman <ascoleman@microsoft.com>
2026-02-13 14:44:58 -0700

    [SM6.10] Implement CopyConvertMatrix Builtin

---
+++ tools/clang/test/SemaHLSL/hlsl/linalg/builtins/copyconvertmatrix/nominal.hlsl
@@ -0,0 +1,11 @@
+// REQUIRES: dxil-1-10
+// RUN: %dxc -T cs_6_10 -E main %s -verify
+
+// expected-no-diagnostics
+
+[numthreads(1,1,1)]
+void main() {
+  __builtin_LinAlgMatrix [[__LinAlgMatrix_Attributes(4, 5, 4, 1, 2)]] mat1;
+  __builtin_LinAlgMatrix [[__LinAlgMatrix_Attributes(1, 5, 4, 2, 2)]] mat2;
+  __builtin_LinAlg_CopyConvertMatrix(mat2, mat1, true);
+}
