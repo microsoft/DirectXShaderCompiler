microsoft/DirectXShaderCompiler

commit 19ba28bf558f38d5302fa011dacdd8e9edf3e440

[SPIR-V][vk::SampledTexture] #7. Add subscriptor indexing methods for `vk::SampledTexture2D` type.  (#8158)
Part of https://github.com/microsoft/DirectXShaderCompiler/issues/7979

Support subscriptor indexing `[]` and `.mips[]` for `SampledTexture2D`.


`.sample[]` will not be allowed, as in `Texture2D`.

tools/clang/lib/Sema/SemaHLSL.cpp
@@ -1661,7 +1661,7 @@ static const SubscriptOperatorRecord g_ArBasicKindsSubscripts[] = {
     {0, MipsFalse, SampleFalse}, // AR_OBJECT_VK_SPV_INTRINSIC_TYPE
     {0, MipsFalse, SampleFalse}, // AR_OBJECT_VK_SPV_INTRINSIC_RESULT_ID
     {0, MipsFalse, SampleFalse}, // AR_OBJECT_VK_BUFFER_POINTER
-    {0, MipsFalse, SampleFalse}, // AR_OBJECT_VK_SAMPLED_TEXTURE2D
+    {2, MipsTrue, SampleFalse},  // AR_OBJECT_VK_SAMPLED_TEXTURE2D
 #endif                           // ENABLE_SPIRV_CODEGEN
     // SPIRV change ends
 
@@ -4101,6 +4101,8 @@ private:
             LookupVectorType(HLSLScalarType::HLSLScalarType_float, 4);
         recordDecl = DeclareVkSampledTextureType(
             *m_context, m_vkNSDecl, "SampledTexture2D", float4Type);
+        if (Attr)
+          recordDecl->addAttr(Attr);
         m_vkSampledTextureTemplateDecl =
             recordDecl->getDescribedClassTemplate();
       }
@@ -5046,6 +5048,7 @@ public:
       ResClass = DXIL::ResourceClass::UAV;
       return true;
     case AR_OBJECT_TEXTURE2D:
+    case AR_OBJECT_VK_SAMPLED_TEXTURE2D:
       ResKind = DXIL::ResourceKind::Texture2D;
       ResClass = DXIL::ResourceClass::SRV;
       return true;

tools/clang/lib/SPIRV/SpirvEmitter.cpp
@@ -6595,7 +6595,8 @@ SpirvEmitter::doCXXOperatorCallExpr(const CXXOperatorCallExpr *expr,
 
     // For Textures, regular indexing (operator[]) uses slice 0.
     if (isBufferTextureIndexing(expr, &baseExpr, &indexExpr)) {
-      auto *lod = isTexture(baseExpr->getType())
+      auto *lod = (isTexture(baseExpr->getType()) ||
+                   isSampledTexture(baseExpr->getType()))
                       ? spvBuilder.getConstantInt(astContext.UnsignedIntTy,
                                                   llvm::APInt(32, 0))
                       : nullptr;
@@ -7894,7 +7895,7 @@ bool SpirvEmitter::isTextureMipsSampleIndexing(const CXXOperatorCallExpr *expr,
 
   const Expr *object = memberExpr->getBase();
   const auto objectType = object->getType();
-  if (!isTexture(objectType))
+  if (!isTexture(objectType) && !isSampledTexture(objectType))
     return false;
 
   if (base)
@@ -7918,7 +7919,7 @@ bool SpirvEmitter::isBufferTextureIndexing(const CXXOperatorCallExpr *indexExpr,
   const Expr *object = indexExpr->getArg(0);
   const auto objectType = object->getType();
   if (isBuffer(objectType) || isRWBuffer(objectType) || isTexture(objectType) ||
-      isRWTexture(objectType)) {
+      isRWTexture(objectType) || isSampledTexture(objectType)) {
     if (base)
       *base = object;
     if (index)

tools/clang/test/CodeGenSPIRV/vk.sampledtexture.access.hlsl
@@ -1,0 +1,22 @@
+// RUN: %dxc -T ps_6_7 -E main -fcgl  %s -spirv | FileCheck %s
+
+// CHECK:  [[cu12:%[0-9]+]] = OpConstantComposite %v2uint %uint_1 %uint_2
+
+// CHECK: [[type_2d_image_1:%[a-zA-Z0-9_]+]] = OpTypeImage %float 2D 0 0 0 1 Unknown
+// CHECK: [[type_sampled_image_1:%[a-zA-Z0-9_]+]] = OpTypeSampledImage [[type_2d_image_1]]
+// CHECK: [[ptr_type_1:%[a-zA-Z0-9_]+]] = OpTypePointer UniformConstant [[type_sampled_image_1]]
+
+// CHECK: [[tex1:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_1]] UniformConstant
+
+vk::SampledTexture2D<float4> tex1 : register(t1);
+
+void main() {
+// CHECK:                    OpStore %pos1 [[cu12]]
+// CHECK-NEXT:    [[pos1:%[0-9]+]] = OpLoad %v2uint %pos1
+// CHECK-NEXT: [[tex1_load:%[a-zA-Z0-9_]+]] = OpLoad [[type_sampled_image_1]] [[tex1]]
+// CHECK-NEXT:    [[tex_img:%[0-9]+]] = OpImage [[type_2d_image_1]] [[tex1_load]]
+// CHECK-NEXT:      [[result1:%[0-9]+]] = OpImageFetch %v4float [[tex_img]] [[pos1]] Lod %uint_0
+// CHECK-NEXT:                    OpStore %a1 [[result1]]
+  uint2 pos1 = uint2(1,2);
+  float4 a1 = tex1[pos1];
+}

tools/clang/test/CodeGenSPIRV/vk.sampledtexture.mips-access.hlsl
@@ -1,0 +1,22 @@
+// RUN: %dxc -T ps_6_7 -E main -fcgl  %s -spirv | FileCheck %s
+
+// CHECK:  [[cu12:%[0-9]+]] = OpConstantComposite %v2uint %uint_1 %uint_2
+
+// CHECK: [[type_2d_image_1:%[a-zA-Z0-9_]+]] = OpTypeImage %float 2D 0 0 0 1 Unknown
+// CHECK: [[type_sampled_image_1:%[a-zA-Z0-9_]+]] = OpTypeSampledImage [[type_2d_image_1]]
+// CHECK: [[ptr_type_1:%[a-zA-Z0-9_]+]] = OpTypePointer UniformConstant [[type_sampled_image_1]]
+
+// CHECK: [[tex1:%[a-zA-Z0-9_]+]] = OpVariable [[ptr_type_1]] UniformConstant
+
+vk::SampledTexture2D<float4> tex1 : register(t1);
+
+void main() {
+// CHECK:                    OpStore %pos1 [[cu12]]
+// CHECK-NEXT:    [[pos1:%[0-9]+]] = OpLoad %v2uint %pos1
+// CHECK-NEXT: [[tex1_load:%[a-zA-Z0-9_]+]] = OpLoad [[type_sampled_image_1]] [[tex1]]
+// CHECK-NEXT:    [[tex_img:%[0-9]+]] = OpImage [[type_2d_image_1]] [[tex1_load]]
+// CHECK-NEXT:      [[result1:%[0-9]+]] = OpImageFetch %v4float [[tex_img]] [[pos1]] Lod %uint_2
+// CHECK-NEXT:                    OpStore %a1 [[result1]]
+  uint2 pos1 = uint2(1,2);
+  float4 a1 = tex1.mips[2][pos1];
+}

tools/clang/test/SemaHLSL/vk.sampledtexture.struct.error.hlsl
@@ -3,9 +3,9 @@
 
 struct Struct { float f; };
 
-vk::SampledTexture2D<Struct> t;
+vk::SampledTexture2D<Struct> t; // expected-error {{elements of typed buffers and textures must be scalars or vectors}}
 
 float4 main(float2 f2 : F2) : SV_TARGET
 {
-    return t.Sample(f2); // expected-error {{cannot Sample from resource containing}} expected-error {{cannot initialize return object}}
+    return t.Sample(f2);
 }

commit 8dd982b5e19e03994e3aab5a159c252dee24757e

[SPIR-V] Wrap `vk::SampledTexture2D` type inside spirv ifdef. (#8219)
Bug introduced by
https://github.com/microsoft/DirectXShaderCompiler/pull/8158

tools/clang/lib/Sema/SemaHLSL.cpp
@@ -5048,7 +5048,9 @@ public:
       ResClass = DXIL::ResourceClass::UAV;
       return true;
     case AR_OBJECT_TEXTURE2D:
+#ifdef ENABLE_SPIRV_CODEGEN
     case AR_OBJECT_VK_SAMPLED_TEXTURE2D:
+#endif
       ResKind = DXIL::ResourceKind::Texture2D;
       ResClass = DXIL::ResourceClass::SRV;
       return true;