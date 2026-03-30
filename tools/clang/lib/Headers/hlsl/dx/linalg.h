c6893856a7391c9e7a242ea6a028bbee6ed16205
Helena Kotas <hekotas@microsoft.com>
2026-03-30 10:27:02 -0700

    [SM6.10] Update LinAlg Matrix ComponentType enum (#8298)
    Component types valid for LinAlg Matrix are a subset of existing DXIL component type constants.
    
    Spec: [LinAlg hlsl-header](https://github.com/microsoft/hlsl-specs/blob/main/proposals/0035-linalg-matrix.md#appendix-1-hlsl-header)
    
    Part of #7839

--- tools/clang/lib/Headers/hlsl/dx/linalg.h
+++ tools/clang/lib/Headers/hlsl/dx/linalg.h
@@ -10,37 +10,75 @@ namespace hlsl {
 
 } // namespace hlsl
 
+namespace dxil {
+
+// This enum must _exactly_ match the DXIL constants.
+enum class ComponentType : uint32_t {
+  Invalid = 0,
+  I1 = 1,
+  I16 = 2,
+  U16 = 3,
+  I32 = 4,
+  U32 = 5,
+  I64 = 6,
+  U64 = 7,
+  F16 = 8,
+  F32 = 9,
+  F64 = 10,
+  SNormF16 = 11,
+  UNormF16 = 12,
+  SNormF32 = 13,
+  UNormF32 = 14,
+  SNormF64 = 15,
+  UNormF64 = 16,
+  PackedS8x32 = 17,
+  PackedU8x32 = 18,
+
+  // BEGIN NEW FOR SM 6.10
+  I8 = 19,
+  U8 = 20,
+  F8_E4M3 = 21,
+  F8_E5M2 = 22,
+  // END
+
+  LastEntry
+};
+
+} // namespace dxil
+
 namespace dx {
 
 namespace linalg {
 
+#define __COMPONENT_TYPE(type) type = (uint)dxil::ComponentType::type
+
+// This enum only defines values that are valid for Matrix component types.
+// Each enumeration's value matches the cooresponding DXIL constant.
 struct ComponentType {
   enum ComponentEnum {
-    Invalid = 0,
-    I1 = 1,
-    I16 = 2,
-    U16 = 3,
-    I32 = 4,
-    U32 = 5,
-    I64 = 6,
-    U64 = 7,
-    F16 = 8,
-    F32 = 9,
-    F64 = 10,
-    SNormF16 = 11,
-    UNormF16 = 12,
-    SNormF32 = 13,
-    UNormF32 = 14,
-    SNormF64 = 15,
-    UNormF64 = 16,
-    PackedS8x32 = 17,
-    PackedU8x32 = 18,
-    U8 = 19,
-    I8 = 20,
-    F8_E4M3 = 21,
-    F8_E5M2 = 22,
+    // Signed integers.
+    __COMPONENT_TYPE(I8),
+    __COMPONENT_TYPE(I16),
+    __COMPONENT_TYPE(I32),
+    __COMPONENT_TYPE(I64),
+
+    // Unsigned integers.
+    __COMPONENT_TYPE(U8),
+    __COMPONENT_TYPE(U16),
+    __COMPONENT_TYPE(U32),
+    __COMPONENT_TYPE(U64),
+
+    // Floating point types.
+    __COMPONENT_TYPE(F8_E4M3),
+    __COMPONENT_TYPE(F8_E5M2),
+    __COMPONENT_TYPE(F16),
+    __COMPONENT_TYPE(F32),
+    __COMPONENT_TYPE(F64),
   };
 };
+
+#undef __COMPONENT_TYPE
+
 using ComponentEnum = ComponentType::ComponentEnum;
 
 struct MatrixUse {
