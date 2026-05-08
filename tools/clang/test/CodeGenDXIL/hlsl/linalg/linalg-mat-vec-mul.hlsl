3fdc27d1c261cf29885baf49505bb4b411007f57
Gregory Roth <groth@nvidia.com>
2026-05-08 12:30:55 -0700

    Add Mat/Vec Multiply method with InterpretedVector

--- tools/clang/test/CodeGenDXIL/hlsl/linalg/linalg-mat-vec-mul.hlsl
+++ tools/clang/test/CodeGenDXIL/hlsl/linalg/linalg-mat-vec-mul.hlsl
@@ -23,8 +23,11 @@ RWByteAddressBuffer OutBuf;
 
 using nType = __detail::ComponentTypeTraits<ComponentType::NTY>::Type;
 using iType = __detail::ComponentTypeTraits<ComponentType::ITY>::Type;
+using cType = __detail::ComponentTypeTraits<ComponentType::CTY>::Type;
 
 // CHECK: %dx.types.LinAlgMatrixC[[ien]]M8N4U0S0 = type { i8* }
+// CHECK: %dx.types.LinAlgMatrixC[[ien]]M24N32U0S0 = type { i8* }
+// CHECK: %dx.types.LinAlgMatrixC[[ien]]M124N32U0S0 = type { i8* }
 
 // Basic test using unpacked types and native vectors
 // CHECK-LABEL: define void @"\01?NativeTest
@@ -46,6 +49,56 @@ export void NativeTest(vector<nType, 4> Input) {
   InterlockedAccumulate(OutVec, OutBuf, 47);
 }
 
+// Check matrix with interpreted input vector
+// CHECK-LABEL: define void @"\01?InterpretedTest
+export void InterpretedTest(vector<iType, 32> Input) {
+
+  typedef Matrix<ComponentType::ITY, 24, 32, MatrixUse::A, MatrixScope::Thread> MatrixTy;
+
+  // Create interpreted vector for uints containing 8-bit integers
+  InterpretedVector<iType, 32, ComponentType::ITY> IVec =  MakeInterpretedVector<ComponentType::ITY>(Input);
+
+  // CHECK: [[hdl:%.*]] = call %dx.types.Handle @dx.op.createHandleForLib.dx.types.Handle(i32 160, %dx.types.Handle %{{.*}})
+  // CHECK: [[buf:%.*]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle [[hdl]], %dx.types.ResourceProperties { i32 11, i32 0 })
+  // CHECK: [[lmtx:%.*]] = call %dx.types.LinAlgMatrixC[[ien]]M24N32U0S0 @dx.op.linAlgMatrixLoadFromDescriptor.mC[[ien]]M24N32U0S0(i32 -2147483634, %dx.types.Handle [[buf]], i32 184, i32 {{[0-9]*}}, i32 0{{.*}}
+  MatrixTy Mat = MatrixTy::Load<MatrixLayout::RowMajor>(Buf, 184, 24 * sizeof(iType));
+
+  // CHECK: [[ret:%.*]] = call <24 x [[ntype]]> @dx.op.linAlgMatVecMul.v24[[nty]].mC[[ien]]M24N32U0S0.v32[[ity]](i32 -2147483623, %dx.types.LinAlgMatrixC[[ien]]M24N32U0S0 [[lmtx]], i1 [[nsg]], <32 x [[itype]]> %Input, i32 [[ien]])
+  vector<nType, 24> OutVec = Multiply<nType>(Mat, IVec);
+
+  // CHECK: [[hdl:%.*]] = call %dx.types.Handle @dx.op.createHandleForLib.dx.types.Handle(i32 160, %dx.types.Handle %{{.*}})
+  // CHECK: [[rwbuf:%.*]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle [[hdl]], %dx.types.ResourceProperties { i32 4107, i32 0 })
+  // CHECK: call void @dx.op.linAlgVectorAccumulateToDescriptor.v24[[nty]](i32 -2147483617, <24 x [[ntype]]> [[ret]], %dx.types.Handle [[rwbuf]], i32 62)
+  InterlockedAccumulate(OutVec, OutBuf, 62);
+
+}
+
+// Check matrix with packed type interpreted input vector
+// CHECK-LABEL: define void @"\01?PackedInterpretedTest
+export void PackedInterpretedTest(vector<cType, 32> Input) {
+
+  typedef Matrix<ComponentType::ITY, 124, 32, MatrixUse::A, MatrixScope::Thread> MatrixTy;
+
+  // Create interpreted vector for uints containing 8-bit integers
+  // CHECK: [[ivec:%.*]] = call <8 x i32> @dx.op.linAlgConvert.v8i32.v32[[cty]](i32 -2147483618, <32 x [[ctype]]> %Input, i32 [[cen]], i32 [[pen]])
+  InterpretedVector<uint, 8, ComponentType::PTY> IVec =  Convert<ComponentEnum::PTY, ComponentEnum::CTY>(Input);
+
+  // CHECK: [[hdl:%.*]] = call %dx.types.Handle @dx.op.createHandleForLib.dx.types.Handle(i32 160, %dx.types.Handle %{{.*}})
+  // CHECK: [[buf:%.*]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle [[hdl]], %dx.types.ResourceProperties { i32 11, i32 0 })
+  // CHECK: [[lmtx:%.*]] = call %dx.types.LinAlgMatrixC[[ien]]M124N32U0S0 @dx.op.linAlgMatrixLoadFromDescriptor.mC[[ien]]M124N32U0S0(i32 -2147483634, %dx.types.Handle [[buf]], i32 184, i32 {{[0-9]*}}, i32 0{{.*}}
+  MatrixTy Mat = MatrixTy::Load<MatrixLayout::RowMajor>(Buf, 184, 124 * sizeof(iType));
+
+  // CHECK: [[ret:%.*]] = call <124 x [[ntype]]> @dx.op.linAlgMatVecMul.v124[[nty]].mC[[ien]]M124N32U0S0.v8i32(i32 -2147483623, %dx.types.LinAlgMatrixC[[ien]]M124N32U0S0 [[lmtx]], i1 [[nsg]], <8 x i32> [[ivec]], i32 [[pen]])
+  vector<nType, 124> OutVec = Multiply<nType>(Mat, IVec);
+
+  // CHECK: [[hdl:%.*]] = call %dx.types.Handle @dx.op.createHandleForLib.dx.types.Handle(i32 160, %dx.types.Handle %{{.*}})
+  // CHECK: [[rwbuf:%.*]] = call %dx.types.Handle @dx.op.annotateHandle(i32 216, %dx.types.Handle [[hdl]], %dx.types.ResourceProperties { i32 4107, i32 0 })
+  // CHECK: call void @dx.op.linAlgVectorAccumulateToDescriptor.v124[[nty]](i32 -2147483617, <124 x [[ntype]]> [[ret]], %dx.types.Handle [[rwbuf]], i32 162)
+  InterlockedAccumulate(OutVec, OutBuf, 162);
+}
+
 // CHECK-LABEL: !dx.targetTypes
-// CHECK-SAME:  = !{[[md0:[!][0-9]*]]
+// CHECK-SAME:  = !{[[md0:[!][0-9]*]], [[md1:[!][0-9]*]], [[md2:[!][0-9]*]]
 // CHECK: [[md0]] = !{%dx.types.LinAlgMatrixC[[ien]]M8N4U0S0 undef, i32 [[ien]], i32 8, i32 4, i32 0, i32 0}
+// CHECK: [[md1]] = !{%dx.types.LinAlgMatrixC[[ien]]M24N32U0S0 undef, i32 [[ien]], i32 24, i32 32, i32 0, i32 0}
+// CHECK: [[md2]] = !{%dx.types.LinAlgMatrixC[[ien]]M124N32U0S0 undef, i32 [[ien]], i32 124, i32 32, i32 0, i32 0}
