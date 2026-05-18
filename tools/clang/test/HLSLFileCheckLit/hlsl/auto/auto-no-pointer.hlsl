c5c4908faec7c4091024388f8f5bbaa88ebb1852
Chris Bieneman <cbieneman@microsoft.com>
2026-05-18 12:41:02 -0500

    Fix tests ../tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-pointer.hlsl ../tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-reference.hlsl

--- tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-pointer.hlsl
+++ tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-pointer.hlsl
@@ -1,12 +1,10 @@
-// RUN: not %dxc -T cs_6_0 %s 2>&1 | FileCheck %s
+// RUN: %dxc -T cs_6_0 -HV 202x -verify %s
 
 // Test that 'auto' cannot be used to declare pointer types in HLSL.
 // Pointers are unsupported in HLSL.
 
-// CHECK: error: pointers are unsupported in HLSL
-
 [numthreads(1,1,1)]
 void main() {
     int x = 1;
-    auto* ptr = &x;
+    auto* ptr = &x; // expected-error {{pointers are unsupported in HLSL}}
 }
--- tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-reference.hlsl
+++ tools/clang/test/HLSLFileCheckLit/hlsl/auto/auto-no-reference.hlsl
@@ -1,13 +1,11 @@
-// RUN: not %dxc -T cs_6_0 -HV 202x %s 2>&1 | FileCheck %s
+// RUN: %dxc -T cs_6_0 -HV 202x -verify %s
 
 // Test that 'auto' cannot be used to declare reference types in HLSL.
 // References are unsupported in HLSL.
-
-// CHECK: error: references are unsupported in HLSL
 
 int gVal = 42;
 
 [numthreads(1,1,1)]
 void main() {
-    auto& ref = gVal;
+    auto& ref = gVal; // expected-error {{references are unsupported in HLSL}}
 }
