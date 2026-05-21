39ff6b7e7eae2952a8da5ed91462e995e11c2b19
Chris B <cbieneman@microsoft.com>
2026-05-21 16:35:27 -0500

    Update nodiscard test
    This adds coverage of two uses missed in the original test. The first
    covers structs with nodiscard on the type declaration, the second covers
    selecting the method spelling of the attribute over the type attribute
    if both are present.

--- tools/clang/test/SemaHLSL/attributes/nodiscard.hlsl
+++ tools/clang/test/SemaHLSL/attributes/nodiscard.hlsl
@@ -8,6 +8,15 @@ using MatrixATy = Matrix<ComponentType::F32, 4, 4, MatrixUse::A, MatrixScope::Wa
 [nodiscard] int fn() { return 42; }
 [[nodiscard]] int fn2() { return 42; }
 
+struct [[nodiscard]] S {
+  int x;
+
+  [[clang::warn_unused_result]] static S fn4() { return (S)42; }
+  [[clang::warn_unused_result]] S fn5() { return (S)42; }
+};
+
+S fn3() { return (S)42; }
+
 [numthreads(4, 4, 4)]
 void main(uint ID : SV_GroupID)
 {
@@ -15,4 +24,8 @@ void main(uint ID : SV_GroupID)
   MatA1.Splat(1.0f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
   fn(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
   fn2(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  fn3(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  S::fn4(); // expected-warning {{ignoring return value of function declared with 'warn_unused_result' attribute}}
+  S s;
+  s.fn5(); // expected-warning {{ignoring return value of function declared with 'warn_unused_result' attribute}}
 }
