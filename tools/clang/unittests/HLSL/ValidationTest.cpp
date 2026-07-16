microsoft/DirectXShaderCompiler

commit 5ac91384d565576a6f003639db6e3b2aeffcd298

Implementation of GroupSharedLimit to allow increased GroupSharedMemory (#7871)
Add a new HLSL attribute for Compute, Amp and Mesh shaders:
GroupSharedLimit.

This is used to limit the amount of group shared memory a shader is
allowed to statically declare, and validation will fail if the limit is
exceeded.

There is no upper limit on this attribute, and it is expected that
shader writers set the limit as the lowest common denominator for their
target hardware and software use case (typically 48k or 64k for modern
GPUs).

If no attribute is declared the existing 32k limit is used to be
compatible with existing shaders.

Extends the PSV structures to include the selected limit so that runtime
validation can reject the shader if it exceeds the device support.

---------

Co-authored-by: Jack Elliott <jackell@ntdev.microsoft.com>
Co-authored-by: Tex Riddell <texr@microsoft.com>

tools/clang/unittests/HLSL/ValidationTest.cpp
@@ -4916,9 +4916,9 @@ TEST_F(ValidationTest, PSVStringTableReorder) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo3), PSVRuntimeInfo_size);
-  PSVRuntimeInfo3 *PSVInfo =
-      const_cast<PSVRuntimeInfo3 *>((const PSVRuntimeInfo3 *)PSVPtr);
+  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  PSVRuntimeInfo4 *PSVInfo =
+      const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(2u, PSVInfo->SigInputElements);
   PSVPtr += PSVRuntimeInfo_size / 4;
   uint32_t ResourceCount = *(PSVPtr++);
@@ -5108,9 +5108,9 @@ TEST_F(ValidationTest, PSVSemanticIndexTableReorder) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo3), PSVRuntimeInfo_size);
-  PSVRuntimeInfo3 *PSVInfo =
-      const_cast<PSVRuntimeInfo3 *>((const PSVRuntimeInfo3 *)PSVPtr);
+  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  PSVRuntimeInfo4 *PSVInfo =
+      const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(PSVInfo->SigInputElements, 3u);
   VERIFY_ARE_EQUAL(PSVInfo->SigOutputElements, 3u);
   VERIFY_ARE_EQUAL(PSVInfo->SigPatchConstOrPrimElements, 2u);
@@ -5453,10 +5453,11 @@ SimplePSV::SimplePSV(const DxilPartHeader *pPSVPart) {
   const uint32_t *PSVPtrEnd = PSVPtr + PartSize / 4;
 
   uint32_t PSVRuntimeInfoSize = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo3), PSVRuntimeInfoSize);
-  PSVRuntimeInfo3 *PSVInfo3 =
-      const_cast<PSVRuntimeInfo3 *>((const PSVRuntimeInfo3 *)PSVPtr);
-  PSVInfo = PSVInfo3;
+  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfoSize);
+  PSVRuntimeInfo4 *PSVInfo4 =
+      const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
+  PSVInfo = PSVInfo4;
+  PSVRuntimeInfo3 *PSVInfo3 = reinterpret_cast<PSVRuntimeInfo3 *>(PSVInfo4);
 
   PSVPtr += PSVRuntimeInfoSize / 4;
   uint32_t ResourceCount = *(PSVPtr++);
@@ -6559,9 +6560,9 @@ TEST_F(ValidationTest, WrongPSVSizeOnZeros) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo3), PSVRuntimeInfo_size);
-  PSVRuntimeInfo3 *PSVInfo =
-      const_cast<PSVRuntimeInfo3 *>((const PSVRuntimeInfo3 *)PSVPtr);
+  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  PSVRuntimeInfo4 *PSVInfo =
+      const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(2u, PSVInfo->SigInputElements);
   PSVPtr += PSVRuntimeInfo_size / 4;
   uint32_t *ResourceCountPtr = const_cast<uint32_t *>(PSVPtr++);
@@ -6793,7 +6794,7 @@ TEST_F(ValidationTest, WrongPSVVersion) {
   CheckOperationResultMsgs(
       p60WithPSV68Result,
       {"DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
-       "part:('52') and DXIL module:('24')"},
+       "part:('56') and DXIL module:('24')"},
       /*maySucceedAnyway*/ false, /*bRegex*/ false);
 
   // Create a new Blob.
@@ -6811,6 +6812,6 @@ TEST_F(ValidationTest, WrongPSVVersion) {
   CheckOperationResultMsgs(
       p68WithPSV60Result,
       {"DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
-       "part:('24') and DXIL module:('52')"},
+       "part:('24') and DXIL module:('56')"},
       /*maySucceedAnyway*/ false, /*bRegex*/ false);
 }

include/dxc/DXIL/DxilFunctionProps.h
@@ -117,6 +117,7 @@ struct DxilFunctionProps {
     memset(&Node, 0, sizeof(Node));
     Node.LaunchType = DXIL::NodeLaunchType::Invalid;
     Node.LocalRootArgumentsTableIndex = -1;
+    groupSharedLimitBytes = 0;
   }
   union {
     // Geometry shader.
@@ -173,6 +174,8 @@ struct DxilFunctionProps {
 
   // numThreads shared between multiple shader types and node shaders.
   unsigned numThreads[3];
+
+  unsigned groupSharedLimitBytes;
 
   struct NodeProps {
     DXIL::NodeLaunchType LaunchType = DXIL::NodeLaunchType::Invalid;

include/dxc/DXIL/DxilMetadataHelper.h
@@ -320,6 +320,7 @@ public:
   static const unsigned kDxilNodeOutputsTag = 21;
   static const unsigned kDxilNodeMaxDispatchGridTag = 22;
   static const unsigned kDxilRangedWaveSizeTag = 23;
+  static const unsigned kDxilGroupSharedLimitTag = 24;
 
   // Node Input/Output State.
   static const unsigned kDxilNodeOutputIDTag = 0;

include/dxc/DXIL/DxilModule.h
@@ -254,6 +254,10 @@ public:
   void SetNumThreads(unsigned x, unsigned y, unsigned z);
   unsigned GetNumThreads(unsigned idx) const;
 
+  unsigned GetGroupSharedLimit() const;
+  // The total amount of group shared memory (in bytes) used by the shader.
+  unsigned GetTGSMSizeInBytes() const;
+
   // Compute shader
   DxilWaveSize &GetWaveSize();
   const DxilWaveSize &GetWaveSize() const;

include/dxc/DxilContainer/DxilPipelineStateValidation.h
@@ -175,6 +175,10 @@ struct PSVRuntimeInfo3 : public PSVRuntimeInfo2 {
   uint32_t EntryFunctionName;
 };
 
+struct PSVRuntimeInfo4 : public PSVRuntimeInfo3 {
+  uint32_t NumBytesGroupSharedMemory;
+};
+
 enum class PSVResourceType {
   Invalid = 0,
 
@@ -474,7 +478,7 @@ public:
              const uint32_t *SemanticIndexes) const;
 };
 
-#define MAX_PSV_VERSION 3
+#define MAX_PSV_VERSION 4
 
 struct PSVInitInfo {
   PSVInitInfo(uint32_t psvVersion) : PSVVersion(psvVersion) {}
@@ -491,7 +495,7 @@ struct PSVInitInfo {
   uint8_t SigPatchConstOrPrimVectors = 0;
   uint8_t SigOutputVectors[PSV_GS_MAX_STREAMS] = {0, 0, 0, 0};
 
-  static_assert(MAX_PSV_VERSION == 3, "otherwise this needs updating.");
+  static_assert(MAX_PSV_VERSION == 4, "otherwise this needs updating.");
   uint32_t RuntimeInfoSize() const {
     switch (PSVVersion) {
     case 0:
@@ -500,10 +504,12 @@ struct PSVInitInfo {
       return sizeof(PSVRuntimeInfo1);
     case 2:
       return sizeof(PSVRuntimeInfo2);
+    case 3:
+      return sizeof(PSVRuntimeInfo3);
     default:
       break;
     }
-    return sizeof(PSVRuntimeInfo3);
+    return sizeof(PSVRuntimeInfo4);
   }
   uint32_t ResourceBindInfoSize() const {
     if (PSVVersion < 2)
@@ -519,6 +525,7 @@ class DxilPipelineStateValidation {
   PSVRuntimeInfo1 *m_pPSVRuntimeInfo1 = nullptr;
   PSVRuntimeInfo2 *m_pPSVRuntimeInfo2 = nullptr;
   PSVRuntimeInfo3 *m_pPSVRuntimeInfo3 = nullptr;
+  PSVRuntimeInfo4 *m_pPSVRuntimeInfo4 = nullptr;
   uint32_t m_uResourceCount = 0;
   uint32_t m_uPSVResourceBindInfoSize = 0;
   void *m_pPSVResourceBindInfo = nullptr;
@@ -633,6 +640,8 @@ public:
   PSVRuntimeInfo2 *GetPSVRuntimeInfo2() const { return m_pPSVRuntimeInfo2; }
 
   PSVRuntimeInfo3 *GetPSVRuntimeInfo3() const { return m_pPSVRuntimeInfo3; }
+
+  PSVRuntimeInfo4 *GetPSVRuntimeInfo4() const { return m_pPSVRuntimeInfo4; }
 
   uint32_t GetBindCount() const { return m_uResourceCount; }
 
@@ -949,6 +958,8 @@ DxilPipelineStateValidation::ReadOrWrite(const void *pBits, uint32_t *pSize,
                 m_uPSVRuntimeInfoSize); // failure ok
   AssignDerived(&m_pPSVRuntimeInfo3, m_pPSVRuntimeInfo0,
                 m_uPSVRuntimeInfoSize); // failure ok
+  AssignDerived(&m_pPSVRuntimeInfo4, m_pPSVRuntimeInfo0,
+                m_uPSVRuntimeInfoSize); // failure ok
 
   // In RWMode::CalcSize, use temp runtime info to hold needed values from
   // initInfo
@@ -1137,11 +1148,13 @@ void SetupPSVInitInfo(PSVInitInfo &InitInfo, const DxilModule &DM);
 void SetShaderProps(PSVRuntimeInfo0 *pInfo, const DxilModule &DM);
 void SetShaderProps(PSVRuntimeInfo1 *pInfo1, const DxilModule &DM);
 void SetShaderProps(PSVRuntimeInfo2 *pInfo2, const DxilModule &DM);
+void SetShaderProps(PSVRuntimeInfo4 *pInfo4, const DxilModule &DM);
 
 void PrintPSVRuntimeInfo(llvm::raw_ostream &OS, PSVRuntimeInfo0 *pInfo0,
                          PSVRuntimeInfo1 *pInfo1, PSVRuntimeInfo2 *pInfo2,
-                         PSVRuntimeInfo3 *pInfo3, uint8_t ShaderKind,
-                         const char *EntryName, const char *Comment);
+                         PSVRuntimeInfo3 *pInfo3, PSVRuntimeInfo4 *pInfo4,
+                         uint8_t ShaderKind, const char *EntryName,
+                         const char *Comment);
 
 } // namespace hlsl
 

lib/DXIL/DxilMetadataHelper.cpp
@@ -1624,6 +1624,13 @@ MDTuple *DxilMDHelper::EmitDxilEntryProperties(uint64_t rawShaderFlag,
       }
       MDVals.emplace_back(MDNode::get(m_Ctx, WaveSizeVal));
     }
+
+    const hlsl::ShaderModel *SM = GetShaderModel();
+    if (SM->IsSMAtLeast(6, 10)) {
+      MDVals.emplace_back(
+          Uint32ToConstMD(DxilMDHelper::kDxilGroupSharedLimitTag));
+      MDVals.emplace_back(Uint32ToConstMD(props.groupSharedLimitBytes));
+    }
   } break;
     // Geometry shader.
   case DXIL::ShaderKind::Geometry: {
@@ -1771,6 +1778,13 @@ void DxilMDHelper::LoadDxilEntryProperties(const MDOperand &MDO,
       props.numThreads[0] = ConstMDToUint32(pNode->getOperand(0));
       props.numThreads[1] = ConstMDToUint32(pNode->getOperand(1));
       props.numThreads[2] = ConstMDToUint32(pNode->getOperand(2));
+    } break;
+
+    case DxilMDHelper::kDxilGroupSharedLimitTag: {
+      DXASSERT(props.IsCS(), "else invalid shader kind");
+      props.groupSharedLimitBytes = ConstMDToUint32(MDO);
+      if (!m_pSM->IsSMAtLeast(6, 10))
+        m_bExtraMetadata = true;
     } break;
 
     case DxilMDHelper::kDxilGSStateTag: {

lib/DXIL/DxilModule.cpp
@@ -412,6 +412,28 @@ unsigned DxilModule::GetNumThreads(unsigned idx) const {
   return props.numThreads[idx];
 }
 
+unsigned DxilModule::GetGroupSharedLimit() const {
+  DXASSERT(m_DxilEntryPropsMap.size() == 1 &&
+               (m_pSM->IsCS() || m_pSM->IsMS() || m_pSM->IsAS()),
+           "only works for CS/MS/AS profiles");
+  const DxilFunctionProps &props = m_DxilEntryPropsMap.begin()->second->props;
+  DXASSERT_NOMSG(m_pSM->GetKind() == props.shaderKind);
+  return props.groupSharedLimitBytes;
+}
+
+unsigned DxilModule::GetTGSMSizeInBytes() const {
+  const DataLayout &DL = m_pModule->getDataLayout();
+  unsigned TGSMSize = 0;
+
+  for (GlobalVariable &GV : m_pModule->globals()) {
+    if (GV.getType()->getAddressSpace() == DXIL::kTGSMAddrSpace) {
+      TGSMSize += DL.getTypeAllocSize(GV.getType()->getElementType());
+    }
+  }
+
+  return TGSMSize;
+}
+
 DxilWaveSize &DxilModule::GetWaveSize() {
   return const_cast<DxilWaveSize &>(
       static_cast<const DxilModule *>(this)->GetWaveSize());

lib/DxilContainer/DxilContainerAssembler.cpp
@@ -798,6 +798,8 @@ public:
     PSVRuntimeInfo1 *pInfo1 = m_PSV.GetPSVRuntimeInfo1();
     PSVRuntimeInfo2 *pInfo2 = m_PSV.GetPSVRuntimeInfo2();
     PSVRuntimeInfo3 *pInfo3 = m_PSV.GetPSVRuntimeInfo3();
+    PSVRuntimeInfo4 *pInfo4 = m_PSV.GetPSVRuntimeInfo4();
+
     if (pInfo)
       hlsl::SetShaderProps(pInfo, m_Module);
     if (pInfo1)
@@ -806,6 +808,8 @@ public:
       hlsl::SetShaderProps(pInfo2, m_Module);
     if (pInfo3)
       pInfo3->EntryFunctionName = EntryFunctionName;
+    if (pInfo4)
+      hlsl::SetShaderProps(pInfo4, m_Module);
 
     // Set resource binding information
     UINT uResIndex = 0;

commit 4e5d61efd54e352cda0a6924c12e60493d7e5dea

Make PSV/DebugBreak unit tests robust to validator version (#8625)
The PSV and DebugBreak unit tests hard-code the newest PSV runtime-info
version, which is a latent fragility unrelated to any single release.

- `ValidationTest` PSV tests hard-coded `sizeof(PSVRuntimeInfo4)` (and
the literal string `56`). That silently assumes the validator always
emits the newest PSV struct. It is wrong for any validator `< 1.10`
(which emits `PSVRuntimeInfo3`), including downlevel/external-validator
runs and any future shader-model cap. The expected size is now derived
from the validator version via a `GetExpectedPSVRuntimeInfoSize()`
helper.
- `PixTest::DebugBreakInstrumentation_{Basic,Multiple}` hard-code
`cs_6_10`; they now skip when Dxil/Validator `< 1.10`.

This is behavior-preserving on `main` (validator is 1.10 there, so PSV
v4 is still expected and the tests still run), while removing the
fragility. The latent bug was introduced in #7871, which added
`PSVRuntimeInfo4` and the `< 1.10 -> v3` gating together; main has
always run validator 1.10 since, so the `v3` path was never exercised.

Intended to be cherry-picked to `release-1.9.2607` (where #8618 caps the
validator to 1.9), unblocking the SM 6.9 cap.

Assisted by co-pilot

---------

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

tools/clang/unittests/HLSL/PixTest.cpp
@@ -3389,6 +3389,8 @@ void RaygenInternalName()
 }
 
 TEST_F(PixTest, DebugBreakInstrumentation_Basic) {
+  if (m_ver.SkipDxilVersion(1, 10))
+    return;
 
   const char *source = R"x(
 [numthreads(1, 1, 1)]
@@ -3426,6 +3428,8 @@ void main() {
 }
 
 TEST_F(PixTest, DebugBreakInstrumentation_Multiple) {
+  if (m_ver.SkipDxilVersion(1, 10))
+    return;
 
   const char *source = R"x(
 RWByteAddressBuffer buf : register(u0);

tools/clang/unittests/HLSL/ValidationTest.cpp
@@ -4882,6 +4882,15 @@ TEST_F(ValidationTest, CacheInitWithLowPrec) {
   TestCheck(L"..\\DXILValidation\\val-dx-type-lowprec.ll");
 }
 
+// PSVRuntimeInfo4 adds NumBytesGroupSharedMemory and is only emitted for
+// validator version >= 1.10; earlier validators emit PSVRuntimeInfo3.
+static uint32_t GetExpectedPSVRuntimeInfoSize(const VersionSupportInfo &ver) {
+  bool HasV4 =
+      ver.m_ValMajor > 1 || (ver.m_ValMajor == 1 && ver.m_ValMinor >= 10);
+  return HasV4 ? static_cast<uint32_t>(sizeof(PSVRuntimeInfo4))
+               : static_cast<uint32_t>(sizeof(PSVRuntimeInfo3));
+}
+
 TEST_F(ValidationTest, PSVStringTableReorder) {
   if (!m_ver.m_InternalValidator)
     if (m_ver.SkipDxilVersion(1, 8))
@@ -4916,7 +4925,7 @@ TEST_F(ValidationTest, PSVStringTableReorder) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  VERIFY_ARE_EQUAL(GetExpectedPSVRuntimeInfoSize(m_ver), PSVRuntimeInfo_size);
   PSVRuntimeInfo4 *PSVInfo =
       const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(2u, PSVInfo->SigInputElements);
@@ -5108,7 +5117,7 @@ TEST_F(ValidationTest, PSVSemanticIndexTableReorder) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  VERIFY_ARE_EQUAL(GetExpectedPSVRuntimeInfoSize(m_ver), PSVRuntimeInfo_size);
   PSVRuntimeInfo4 *PSVInfo =
       const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(PSVInfo->SigInputElements, 3u);
@@ -5443,17 +5452,18 @@ struct SimplePSV {
   llvm::MutableArrayRef<uint32_t> PCInputToOutputTable;
   llvm::MutableArrayRef<uint32_t> ViewIDOutputMask[DXIL::kNumOutputStreams];
   llvm::MutableArrayRef<uint32_t> ViewIDPCOutputMask;
-  SimplePSV(const DxilPartHeader *pPSVPart);
+  SimplePSV(const DxilPartHeader *pPSVPart, uint32_t ExpectedRuntimeInfoSize);
 };
 
-SimplePSV::SimplePSV(const DxilPartHeader *pPSVPart) {
+SimplePSV::SimplePSV(const DxilPartHeader *pPSVPart,
+                     uint32_t ExpectedRuntimeInfoSize) {
   uint32_t PartSize = pPSVPart->PartSize;
   uint32_t *PSVPtr =
       const_cast<uint32_t *>((const uint32_t *)GetDxilPartData(pPSVPart));
   const uint32_t *PSVPtrEnd = PSVPtr + PartSize / 4;
 
   uint32_t PSVRuntimeInfoSize = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfoSize);
+  VERIFY_ARE_EQUAL(ExpectedRuntimeInfoSize, PSVRuntimeInfoSize);
   PSVRuntimeInfo4 *PSVInfo4 =
       const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   PSVInfo = PSVInfo4;
@@ -5581,7 +5591,7 @@ TEST_F(ValidationTest, PSVContentValidationVS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
 
   // Update PSV.
   PSV.SigInput[0].InterpolationMode = 20;
@@ -5737,7 +5747,7 @@ TEST_F(ValidationTest, PSVContentValidationHS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
 
   // Update PSV.
   PSV.SigPatchConstOrPrim[0].InterpolationMode = 20;
@@ -5887,7 +5897,7 @@ TEST_F(ValidationTest, PSVContentValidationDS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
 
   // Update PSV.
   PSV.SigPatchConstOrPrim[0].InterpolationMode = 20;
@@ -6044,7 +6054,7 @@ TEST_F(ValidationTest, PSVContentValidationGS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
   // Update PSV.
   PSV.PSVInfo->MaxVertexCount = 2;
 
@@ -6132,7 +6142,7 @@ TEST_F(ValidationTest, PSVContentValidationPS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
 
   // Update PSV.
   PSV.PSVInfo->PS.DepthOutput = 1;
@@ -6217,7 +6227,7 @@ TEST_F(ValidationTest, PSVContentValidationCS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
   // Update PSV.
   PSV.PSVInfo->NumThreadsX = 1;
 
@@ -6299,7 +6309,7 @@ TEST_F(ValidationTest, PSVContentValidationMS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
   // Update PSV.
   memset(PSV.ViewIDOutputMask[0].data(), 0,
          PSV.ViewIDOutputMask[0].size() * sizeof(uint32_t));
@@ -6366,7 +6376,7 @@ TEST_F(ValidationTest, PSVContentValidationAS) {
   VERIFY_ARE_NOT_EQUAL(hlsl::end(pHeader), pPartIter);
 
   const DxilPartHeader *pPSVPart = (const DxilPartHeader *)(*pPartIter);
-  SimplePSV PSV(pPSVPart);
+  SimplePSV PSV(pPSVPart, GetExpectedPSVRuntimeInfoSize(m_ver));
 
   // Update PSV.
   PSV.PSVInfo->AS.PayloadSizeInBytes = 0;
@@ -6560,7 +6570,7 @@ TEST_F(ValidationTest, WrongPSVSizeOnZeros) {
   const uint32_t *PSVPtr = (const uint32_t *)GetDxilPartData(pPSVPart);
 
   uint32_t PSVRuntimeInfo_size = *(PSVPtr++);
-  VERIFY_ARE_EQUAL(sizeof(PSVRuntimeInfo4), PSVRuntimeInfo_size);
+  VERIFY_ARE_EQUAL(GetExpectedPSVRuntimeInfoSize(m_ver), PSVRuntimeInfo_size);
   PSVRuntimeInfo4 *PSVInfo =
       const_cast<PSVRuntimeInfo4 *>((const PSVRuntimeInfo4 *)PSVPtr);
   VERIFY_ARE_EQUAL(2u, PSVInfo->SigInputElements);
@@ -6791,11 +6801,14 @@ TEST_F(ValidationTest, WrongPSVVersion) {
   VERIFY_IS_NOT_NULL(p60WithPSV68Result);
   VERIFY_SUCCEEDED(p60WithPSV68Result->GetStatus(&status));
   VERIFY_FAILED(status);
-  CheckOperationResultMsgs(
-      p60WithPSV68Result,
-      {"DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
-       "part:('56') and DXIL module:('24')"},
-      /*maySucceedAnyway*/ false, /*bRegex*/ false);
+  std::string ExpectedPSVSizeStr =
+      std::to_string(GetExpectedPSVRuntimeInfoSize(m_ver));
+  std::string Msg60WithPSV68 =
+      "DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
+      "part:('" +
+      ExpectedPSVSizeStr + "') and DXIL module:('24')";
+  CheckOperationResultMsgs(p60WithPSV68Result, {Msg60WithPSV68.c_str()},
+                           /*maySucceedAnyway*/ false, /*bRegex*/ false);
 
   // Create a new Blob.
   CComPtr<IDxcBlobEncoding> pProgram68WithPSV60;
@@ -6809,9 +6822,10 @@ TEST_F(ValidationTest, WrongPSVVersion) {
   VERIFY_IS_NOT_NULL(p68WithPSV60Result);
   VERIFY_SUCCEEDED(p68WithPSV60Result->GetStatus(&status));
   VERIFY_FAILED(status);
-  CheckOperationResultMsgs(
-      p68WithPSV60Result,
-      {"DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
-       "part:('24') and DXIL module:('56')"},
-      /*maySucceedAnyway*/ false, /*bRegex*/ false);
+  std::string Msg68WithPSV60 =
+      "DXIL container mismatch for 'PSVRuntimeInfoSize' between 'PSV0' "
+      "part:('24') and DXIL module:('" +
+      ExpectedPSVSizeStr + "')";
+  CheckOperationResultMsgs(p68WithPSV60Result, {Msg68WithPSV60.c_str()},
+                           /*maySucceedAnyway*/ false, /*bRegex*/ false);
 }