e87a9e5758a00818081ded395b10a9fdac5ff35f
Chris Bieneman <cbieneman@microsoft.com>
2026-09-11 13:46:44 -0500

    Fix broken SPIRV test
    Not sure how I missed this before pushing initially...

--- tools/clang/test/CodeGenSPIRV/variadic.templates.builtin.hlsl
+++ tools/clang/test/CodeGenSPIRV/variadic.templates.builtin.hlsl
@@ -1,11 +1,6 @@
 // RUN: %dxc -E main -T ps_6_0 -HV 202x -fcgl %s -spirv | FileCheck %s
 
-// Verify pack expansion into HLSL vector, matrix, and resource templates.
-
-template <typename... Ts>
-struct Holder {
-  StructuredBuffer<Ts...> Buf;
-};
+// Verify pack expansion into HLSL vector and matrix templates.
 
 template <typename T, typename... Rest>
 vector<T, 1 + sizeof...(Rest)> MakeVector(T First, Rest... Others) {
@@ -17,12 +12,9 @@ struct MatrixWrapper {
   matrix<T, Dims...> M;
 };
 
-// CHECK: OpName %type_StructuredBuffer_float "type.StructuredBuffer.float"
-// CHECK: OpName %g_Holder "g_Holder"
 // CHECK: OpName %MatrixWrapper "MatrixWrapper"
 // CHECK: OpMemberName %MatrixWrapper 0 "M"
 // CHECK: OpName %MakeVector "MakeVector"
-Holder<float> g_Holder : register(t0);
 
 // CHECK-LABEL: %src_main = OpFunction %v4float
 float4 main(float4 a : A) : SV_Target {
@@ -33,10 +25,7 @@ float4 main(float4 a : A) : SV_Target {
   // CHECK: OpCompositeConstruct %mat2v2float
   mw.M = matrix<float, 2, 2>(v.x, v.y, v.z, v.w);
 
-  // CHECK: OpAccessChain %_ptr_Uniform_float {{%[0-9]+}} %int_0 %int_0
-  float bufVal = g_Holder.Buf.Load(0);
-
-  return float4(mw.M._11, mw.M._22, bufVal, v.w);
+  return float4(mw.M._11, mw.M._22, v.z, v.w);
 }
 
 // CHECK-LABEL: %MakeVector = OpFunction %v4float
